92e7b015be10417a7c9a6218a9a587cfca87e9ce
zeo <234521890@qq.com>
2017-10-20 14:52:58 +0800

    by zeo

--- resources/views/dashboard/login/display.blade.php
+++ resources/views/dashboard/login/display.blade.php
@@ -33,6 +33,9 @@
                 <div class="form-group">
                     <input type="password" class="form-control" placeholder="密码" required>
                 </div>
+                <div class="form-group">
+                    <input type="password" class="form-control col-md-2" placeholder="验证码" required>
+                </div>
                 <button type="submit" class="btn btn-primary block full-width m-b">登陆</button>
             </form>
             <p class="m-t"> <small>零壹新科技（深圳）有限公司 &copy; 2017-10-20</small> </p>
