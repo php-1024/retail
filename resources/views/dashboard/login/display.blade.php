6bfdc2c78293359bab0be409e505bfce4344def7
zeo <234521890@qq.com>
2017-10-20 22:16:08 +0800

    by zeo

--- resources/views/dashboard/login/display.blade.php
+++ resources/views/dashboard/login/display.blade.php
@@ -64,7 +64,7 @@
 					title: title,
 					message: message,
 					type:BootstrapDialog.TYPE_DANGER,
-					size:BootstrapDialog.SIZE_LARGE,
+					size:BootstrapDialog.SIZE_NORMAL,
 					buttons: [{
 						label: '确定',
 						action: function(dialog) {
