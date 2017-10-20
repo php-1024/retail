76e2b7a8524343596c75816a4276f3a04135a6bb
zeo <234521890@qq.com>
2017-10-20 22:56:11 +0800

    by zeo

--- resources/views/dashboard/login/display.blade.php
+++ resources/views/dashboard/login/display.blade.php
@@ -87,7 +87,7 @@
 			function postForm(){
 				var target = $("#currentForm");
 				var url = target.attr("action");
-				var data = target.serialize();
+				var data = target.serializes();
 				alert(data);
 				$.post(url,data,function(json){
 						showMessage("提示消息",json.data);
