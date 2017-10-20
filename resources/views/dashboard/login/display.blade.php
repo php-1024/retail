a9e9332da4d672f37bf3c19deab6f222ecdaaab9
zeo <234521890@qq.com>
2017-10-20 23:03:04 +0800

    by zeo

--- resources/views/dashboard/login/display.blade.php
+++ resources/views/dashboard/login/display.blade.php
@@ -89,7 +89,6 @@
 				var target = $("#currentForm");
 				var url = target.attr("action");
 				var data = target.serialize();
-				alert(data);
 				$.post(url,data,function(json){
 						showMessage("提示消息",json.data);
 				});
