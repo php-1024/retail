252333ae0346653d3383cb29be58657b8e373bd7
zeo <234521890@qq.com>
2017-10-20 14:42:36 +0800

    by zeo

--- resources/views/dashboard/login/display.blade.php
+++ resources/views/dashboard/login/display.blade.php
@@ -25,32 +25,20 @@
                 <h1 class="logo-name">01</h1>
 
             </div>
-            <h3>Welcome to IN+</h3>
-            <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
-                <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
-            </p>
-            <p>Login in. To see it in action.</p>
+            <h3>欢饮使用零壹新科技后台管理系统</h3>
             <form class="m-t" role="form" action="index.html">
                 <div class="form-group">
-                    <input type="email" class="form-control" placeholder="Username" required>
+                    <input type="text" class="form-control" placeholder="用户名" required>
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password" required>
+                    <input type="password" class="form-control" placeholder="密码" required>
                 </div>
-                <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
-
-                <a href="login.html#"><small>Forgot password?</small></a>
-                <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+                <button type="submit" class="btn btn-info block full-width m-b">登陆</button>
             </form>
-            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
+            <p class="m-t"> <small>零壹新科技（深圳）有限公司 &copy; 2017-10-20</small> </p>
         </div>
     </div>
-
-    <!-- Mainly scripts -->
     <script src="{{asset('public/dashboard/library/jquery')}}/js/jquery-2.1.1.js"></script>
     <script src="{{asset('public/dashboard/library/bootstrap')}}/js/bootstrap.min.js"></script>
-
 </body>
-
 </html>
