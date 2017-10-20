87678db9b4855af8decc4580586c2470eee95c4f
zeo <234521890@qq.com>
2017-10-20 16:33:29 +0800

    by zeo

--- app/Http/Controllers/dashboard/LoginController.php
+++ app/Http/Controllers/dashboard/LoginController.php
@@ -6,10 +6,37 @@
 namespace App\Http\Controllers\dashboard;
 use App\Http\Controllers\Controller;
 
+use Gregwar\Captcha\CaptchaBuilder;
+use Session;
+
 class LoginController extends Controller{
-    public function display(){
+    /*
+     * 登陆页面
+     */
+    public function display()
+    {
         //echo assets('public/dashboard/library/bootstrap/test.js');
         return view('dashboard/login/display');
+
+    }
+    /*
+     * 验证码
+     */
+    public function captcha()
+    {
+        //生成验证码图片的Builder对象，配置相应属性
+        $builder = new CaptchaBuilder;
+        //可以设置图片宽高及字体
+        $builder->build($width = 100, $height = 40, $font = null);
+        //获取验证码的内容
+        $phrase = $builder->getPhrase();
+
+        //把内容存入session
+        Session::flash('milkcaptcha', $phrase);
+        //生成图片
+        header("Cache-Control: no-cache, must-revalidate");
+        header('Content-Type: image/jpeg');
+        $builder->output();
     }
 }
 ?>
