a7560a3d8523df590999d21a8d19bec4f0403a04
zeo <234521890@qq.com>
2017-11-02 22:40:12 +0800

    by zeo

--- app/Http/Controllers/dashboard/LoginController.php
+++ app/Http/Controllers/dashboard/LoginController.php
@@ -55,10 +55,10 @@ class LoginController extends Controller{
         $key = config("app.encrypt_key");//获取加密盐
         $encrypted = Crypt::encryptString($password);//加密密码第一重
         $encryptPwd = md5("lingyikeji".$encrypted.$key);//加密密码第二重
-        $admininfo = Admin::where('username',$username)->get()->toArray();
+        $admininfo = Admin::where('username',$username)->find()->toArray();
         dump($admininfo);
 
-        return response()->json(['data' => '登录成功', 'status' => '1']);
+       // return response()->json(['data' => '登录成功', 'status' => '1']);
 
     }
 }
