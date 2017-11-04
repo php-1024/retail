faebc093bd5f36bf6c7537f176bd3aa8b0fb8690
zeo <234521890@qq.com>
2017-11-04 22:50:08 +0800

    by zeo

---
+++ app/Http/Controllers/dashboard/RedisController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zeo
+ * Date: 2017/11/4
+ * Time: 22:45
+ * 本文件用于学习Redis.
+ */
+namespace App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Controller;
+class RedisController extends Controller{
+    public function study(){
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,14 @@ Route::group(['prefix' => 'ajax'],function(){
     Route::post('checklogin','dashboard\LoginController@checkLogin')->middleware(['CheckNotLogin','DashBoardLoginPost']);
 });
 
+Route::group(['prefix'=>'redis'],function(){
+    Route::post('study','dashboard\RedisController@study');
+});
+
 Route::get('tt',function(){
     $data = ['这是Laravel框架优美的打印函数','忙活了一天终于搭建成功啦。已将公司所有码农拉进码云项目组','码云地址：https://gitee.com/dzckzeo/lingyikeji_mvc.git','码农们准备撸起袖子干吧'];
     dump($data);
     return "零壹新科技Larael框架测试环境搭建成功啦";
 });
+
+
