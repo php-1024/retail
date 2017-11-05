27f952694a6d67b087a9668addf96c6afc74bfd1
zeo <234521890@qq.com>
2017-11-05 22:10:42 +0800

    by zeo

--- app/Http/Controllers/dashboard/RedisController.php
+++ app/Http/Controllers/dashboard/RedisController.php
@@ -71,7 +71,7 @@ class RedisController extends Controller{
         dump(Redis::get('test1'));
 
         dump('setRange()部分替换操作,第二个参数为0时等同于set操作');
-        dump(Redis::setRange('test1',0,'hello'));
+        dump(Redis::setRange('test1',0,'hello!'));
         dump(Redis::get('test1'));
         Redis::setRange('test1',11,'hi');
         dump(Redis::get('test1'));
