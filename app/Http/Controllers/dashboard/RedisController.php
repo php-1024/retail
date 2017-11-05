97f83facc4d7a9477996c5857a7cb705226d4955
zeo <234521890@qq.com>
2017-11-05 21:46:31 +0800

    by zeo

--- app/Http/Controllers/dashboard/RedisController.php
+++ app/Http/Controllers/dashboard/RedisController.php
@@ -57,7 +57,10 @@ class RedisController extends Controller{
         dump('foo递减1，同时返回递减后的值',Redis::decr('foo'));
         dump('foo递减N，同时返回递减后的值,N为你定义的值，如2',Redis::decrby('foo',2));
 
-        dump('exists检测是否存在值');
+        dump('exists()检测是否存在值');
         dump(Redis::exists('foo'));
+
+        dump('type()检测类型');
+        dump(Redis::type('foo'));
     }
 }
