1894b950a426443e790ba65c730a2fbe6214b206
zeo <234521890@qq.com>
2017-11-05 21:30:12 +0800

    by zeo

--- app/Http/Controllers/dashboard/RedisController.php
+++ app/Http/Controllers/dashboard/RedisController.php
@@ -18,13 +18,13 @@ class RedisController extends Controller{
         //Redis::connection('zeo'); 连接到我定义的redis服务器
         //Redis::connection('lingyikeji');//连接到lingyikeji集群对象
 
-        dump("最简单的Redis的存取");
+        dump("set(),get()最简单的Redis的存取");
         Redis::connection('zeo');//连接到我的redis服务器
         Redis::set('test1', '这是第一个Redis示例');
         $test1 = Redis::get('test1');
         dump($test1);
 
-        dump("同时存储多个key-value");
+        dump("mset(),mget()同时存取多个key-value");
         $list = array(
             'zeo:0001' => '第一个值',
             'zeo:0002' => '第二个值',
@@ -38,7 +38,7 @@ class RedisController extends Controller{
         //Redis::setex('name', 10, '薛志豪');//存10秒
         //dump(Redis::get('name'));
 
-        dump('添加值时不覆盖原有值');
+        dump('setnx添加值时不覆盖原有值');
         /*
         $aa=Redis::setnx('foo', 12) ;  // 返回 true ， 添加成功
         $bb=Redis::setnx('foo', 34) ;  // 返回 false， 添加失败，因为已经存在键名为 foo 的记录
@@ -47,8 +47,15 @@ class RedisController extends Controller{
         dump(Redis::get('foo'));
         */
 
-        dump('替换一个值，并换回替换前的值');
-        $foo = Redis::getset('foo',88);
-        dump($foo);
+        dump('getset()替换一个值，并换回替换前的值');
+        //$foo = Redis::getset('foo',88);
+        //dump($foo);
+
+        dump('incrby(),incr(),decrby(),decr() 对值的递增和递减');
+        dump('foo递增1，同时返回递增后的值',Redis::incr('foo'));
+        dump('foo递增N，同时返回递增后的值,N为你定义的值，如2',Redis::incrBy('foo',2));
+        dump('foo递减1，同时返回递减后的值',Redis::decr('foo'));
+        dump('foo递减N，同时返回递减后的值,N为你定义的值，如2',Redis::decrby('foo',2));
+
     }
 }
