348214df59327c89fcf62cc9bf776e7a913e78ee
zeo <234521890@qq.com>
2017-11-29 11:13:14 +0800

    by zeo

--- app/Http/Controllers/Program/LoginController.php
+++ app/Http/Controllers/Program/LoginController.php
@@ -17,7 +17,7 @@ class LoginController extends Controller{
     public function display()
     {
         $data['random']=time();//生成调用验证码的随机数
-        return view('Program/login/display',$data);
+        return view('Program/Login/display',$data);
     }
     /*
      * 生成验证码
