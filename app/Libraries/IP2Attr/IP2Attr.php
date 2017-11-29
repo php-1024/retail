25725d19dc3c52797722219827e65cd65fe8e675
zeo <234521890@qq.com>
2017-11-29 16:32:12 +0800

    by zeo

--- app/Libraries/IP2Attr/IP2Attr.php
+++ app/Libraries/IP2Attr/IP2Attr.php
@@ -1,8 +1,17 @@
 <?php
 namespace App\Libraries\IP2Attr;
+use App\Libraries\Curl\HttpCurl;
+/*
+ * 根据IP获取地址接口
+ */
 class IP2Attr{
     public static function getAttr($ip){
         echo $ip;
+    }
+    public static function sina_api($ip){
+        $url = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js&ip='.$ip;
+        $response = HttpCurl::doGet($url);
+        dump($response);
     }
 }
 ?>
