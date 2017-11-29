b14e0205712ca7721160539197c096d9cacc622e
zeo <234521890@qq.com>
2017-11-29 16:39:04 +0800

    by zeo

--- app/Libraries/IP2Attr/IP2Attr.php
+++ app/Libraries/IP2Attr/IP2Attr.php
@@ -6,12 +6,20 @@ use App\Libraries\Curl\HttpCurl;
  */
 class IP2Attr{
     public static function getAttr($ip){
-        return self::sina_api($ip);
+        if($ip=='127.0.0.1'){
+            return '本机';
+        }else if($ip=='0.0.0.0'){
+
+        }else{
+            return self::sina_api($ip);
+        }
     }
     public static function sina_api($ip){
         $url = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js&ip='.$ip;
         $response = HttpCurl::doGet($url);
-        dump($response);
+        if(preg_match('/{.*}/iUs',$response,$arr)){
+            dump($arr);
+        }
     }
 }
 ?>
