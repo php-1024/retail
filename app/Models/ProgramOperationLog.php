9c126b838218a27e9b025677e2d5c33e3d6d7ec7
zeo <234521890@qq.com>
2017-11-29 14:23:17 +0800

    by zeo

--- app/Models/ProgramOperationLog.php
+++ app/Models/ProgramOperationLog.php
@@ -5,7 +5,7 @@
  */
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class Module extends Model{
+class PragramOperationLog extends Model{
     protected $table = 'program_operation_log';
     protected $primaryKey = 'id';
     public $timestamps = false;
