3093b44f6acf1bee939ae92c333dcb618a9f414f
zeo <234521890@qq.com>
2017-11-29 11:33:33 +0800

    by zeo

--- app/Http/Controllers/Program/LoginController.php
+++ app/Http/Controllers/Program/LoginController.php
@@ -16,6 +16,10 @@ class LoginController extends Controller{
      */
     public function display()
     {
+        $key = config("app.program_encrypt_key");//获取加密盐
+        $encrypted = md5('admin123');//加密密码第一重
+        $encryptPwd = md5("lingyikeji".$encrypted.$key);//加密密码第二重
+       echo $encryptPwd;
         $data['random']=time();//生成调用验证码的随机数
         return view('Program/Login/display',$data);
     }
