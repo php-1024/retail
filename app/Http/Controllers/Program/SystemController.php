9b32183541ec2e70188b6d7674dddb252002ea88
zeo <234521890@qq.com>
2017-11-30 16:15:50 +0800

    by zeo

--- app/Http/Controllers/Program/SystemController.php
+++ app/Http/Controllers/Program/SystemController.php
@@ -9,9 +9,8 @@ use Session;
 class SystemController extends Controller{
     public function dashboard(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        $uri = $request->path();
-        dump($uri);
-        return view('Program/System/dashboard',['admin_data'=>$admin_data]);
+        $route_name = $request->path();
+        return view('Program/System/dashboard',['admin_data'=>$admin_data,'route_name'=>$route_name]);
     }
 
     public function quit(Request $request){
