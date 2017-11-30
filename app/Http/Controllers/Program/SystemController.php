3b313fbc6f5dfeffde0c37b25d389fe555423dd3
zeo <234521890@qq.com>
2017-11-30 16:19:02 +0800

    by zeo

--- app/Http/Controllers/Program/SystemController.php
+++ app/Http/Controllers/Program/SystemController.php
@@ -10,8 +10,6 @@ class SystemController extends Controller{
     public function dashboard(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $route_name = $request->path();
-        dump($admin_data);
-        exit;
         return view('Program/System/dashboard',['admin_data'=>$admin_data,'route_name'=>$route_name]);
     }
 
