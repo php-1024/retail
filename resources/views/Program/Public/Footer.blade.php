1e32be36af0503f2190b7176b2eb8089e8f81d8b
zeo <234521890@qq.com>
2017-11-30 13:56:06 +0800

    by zeo

--- resources/views/Program/Public/Footer.blade.php
+++ resources/views/Program/Public/Footer.blade.php
@@ -1,6 +1,6 @@
 <div class="footer" >
     <div class="pull-right">
-        您登陆的时间是：2017-10-24 16:26:30
+        您本次登陆信息 ：2017-10-24 16:26:30 广东深圳
     </div>
     <div>
         <strong>Copyright</strong> 零壹新科技（深圳有限公司）&copy; 2017-2027
