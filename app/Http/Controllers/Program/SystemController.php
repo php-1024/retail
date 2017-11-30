ffb659a9109c2bba8cb2a934279488fc0c8b635f
zeo <234521890@qq.com>
2017-11-30 14:22:51 +0800

    by zeo

--- app/Http/Controllers/Program/SystemController.php
+++ app/Http/Controllers/Program/SystemController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 class SystemController extends Controller{
     public function dashboard(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        Session::put('zerone_program_account_id','');//存储登录session_id为当前用户ID
         return view('Program/System/dashboard',['admin_data'=>$admin_data]);
     }
 }
