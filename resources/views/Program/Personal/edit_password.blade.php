d53f9cff8cb248f9e6c2f742311842c8a8beb51c
zeo <234521890@qq.com>
2017-11-30 22:34:45 +0800

    by zeo

--- resources/views/Program/Personal/edit_password.blade.php
+++ resources/views/Program/Personal/edit_password.blade.php
@@ -48,7 +48,7 @@
                             <h5>添加账号</h5>
                         </div>
                         <div class="ibox-content">
-                            <form method="post" class="form-horizontal"  role="form" id="currentForm" action="{{ url('program/ajax/check_account_add') }}">
+                            <form method="post" class="form-horizontal"  role="form" id="currentForm" action="{{ url('program/ajax/check_edit_password') }}">
                                 <input type="hidden" name="_token" value="{{csrf_token()}}">
                                 <div class="form-group"><label class="col-sm-2 control-label">登陆账号</label>
                                     <div class="col-sm-10" style="padding-top:7px;">admin</div>
@@ -56,15 +56,15 @@
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group">
                                     <label class="col-sm-2 control-label">原登陆密码</label>
-                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
+                                    <div class="col-sm-10"><input type="password" name="oldpassword" class="form-control"></div>
                                 </div>
                                 <div class="form-group">
                                     <label class="col-sm-2 control-label">新登陆密码</label>
-                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
+                                    <div class="col-sm-10"><input type="password" name="password" class="form-control"></div>
                                 </div>
                                 <div class="form-group">
                                     <label class="col-sm-2 control-label">重复新密码</label>
-                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
+                                    <div class="col-sm-10"><input type="password" name="repassword" class="form-control"></div>
                                 </div>
 
                                 <div class="hr-line-dashed"></div>
