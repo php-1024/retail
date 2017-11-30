86a6ac9c58db41407f2c33006de1145af08914f2
zeo <234521890@qq.com>
2017-11-30 15:28:01 +0800

    by zeo

--- app/Http/Controllers/Program/SystemController.php
+++ app/Http/Controllers/Program/SystemController.php
@@ -10,5 +10,10 @@ class SystemController extends Controller{
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         return view('Program/System/dashboard',['admin_data'=>$admin_data]);
     }
+
+    public function quit(){
+        Session::put('zerone_program_account_id','');
+        return redirect('Program/login');
+    }
 }
 ?>
