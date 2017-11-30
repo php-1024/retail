cc041309b928a944e13d8426b95ef566e0efb677
zeo <234521890@qq.com>
2017-11-30 13:51:59 +0800

    by zeo

--- resources/views/Program/System/dashboard.blade.php
+++ resources/views/Program/System/dashboard.blade.php
@@ -23,21 +23,7 @@
     @include('Public/Nav')
 
     <div id="page-wrapper" class="gray-bg">
-        <div class="row border-bottom">
-            <div class="navbar-header">
-                <a class="navbar-minimalize minimalize-styl-2 btn btn-info " href="javascript:;"><i class="fa fa-bars"></i> </a>
-            </div>
-            <ul class="nav navbar-top-links navbar-right">
-                <li>
-                    <span class="m-r-sm text-muted welcome-message">欢迎登录！<strong>超级管理员-薛志豪</strong></span>
-                </li>
-                <li>
-                    <a href="JavaScript:;">
-                        <i class="fa fa-sign-out"></i>退出系统
-                    </a>
-                </li>
-            </ul>
-        </div>
+
         <div class="row wrapper border-bottom white-bg page-heading">
             <div class="col-sm-4">
                 <h2>管理首页</h2>
