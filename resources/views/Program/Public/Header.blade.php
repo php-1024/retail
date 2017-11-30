be09b7216d9e8ba81ebf04699fc897dabc2db085
zeo <234521890@qq.com>
2017-11-30 16:25:51 +0800

    by zeo

--- resources/views/Program/Public/Header.blade.php
+++ resources/views/Program/Public/Header.blade.php
@@ -4,7 +4,16 @@
     </div>
     <ul class="nav navbar-top-links navbar-right">
         <li>
-            <span class="m-r-sm text-muted welcome-message">欢迎登录！<strong>超级管理员-薛志豪</strong></span>
+            <span class="m-r-sm text-muted welcome-message">
+                欢迎登录！
+                <strong>
+                    @if ($admin_data['admin_is_super']==1)
+                        超级管理员-{{ $admin_data['account'] }}
+                    @else
+                        管理员-{{ $admin_data['account'] }}
+                    @endif
+                </strong>
+            </span>
         </li>
         <li>
             <a href="{{ url('program/quit') }}">
