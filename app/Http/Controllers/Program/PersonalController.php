40ea700274c483eeedf59c85bf70a994be596e88
zeo <234521890@qq.com>
2017-11-30 23:02:21 +0800

    by zeo

--- app/Http/Controllers/Program/PersonalController.php
+++ app/Http/Controllers/Program/PersonalController.php
@@ -29,8 +29,7 @@ class PersonalController extends Controller{
         $admin = new ProgramAdmin();
         $sql_password = $admin->where('id',$admin_data['admin_id'])->pluck('password')->toArray();//查询当前用户的登录密码
         $sql_password = $sql_password[0];//数组转化为字符串
-        var_dump($sql_password);
-        exit();
+
         if($old_encryptPwd != $sql_password){//判断原登录密码是否输入正确
             return response()->json(['data' => '原登录密码输入错误', 'status' => '0']);
         }
