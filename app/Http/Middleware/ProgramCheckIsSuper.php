4853ae511762a2604726f0a141ceb991ef5af282
zeo <234521890@qq.com>
2017-11-30 17:18:21 +0800

    by zeo

--- app/Http/Middleware/ProgramCheckIsSuper.php
+++ app/Http/Middleware/ProgramCheckIsSuper.php
@@ -10,8 +10,11 @@ use Illuminate\Support\Facades\Redis;
 class ProgramCheckIsSuper{
     public function handle($request,Closure $next){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dump($admin_data);
-        return $next($request);
+        if($admin_data['admin_is_super']!=1){
+            return redirect('program');
+        }else{
+            return $next($request);
+        }
     }
 }
 ?>
