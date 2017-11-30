8031bb46e0d064798122f87aa05e8080b4fc9428
zeo <234521890@qq.com>
2017-11-30 14:42:41 +0800

    by zeo

--- app/Http/Controllers/Program/SystemController.php
+++ app/Http/Controllers/Program/SystemController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 class SystemController extends Controller{
     public function dashboard(Request $request){
-        echo date_default_timezone_get();
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dump(date('Y-m-d H:i:s'));
         return view('Program/System/dashboard',['admin_data'=>$admin_data]);
     }
 }
