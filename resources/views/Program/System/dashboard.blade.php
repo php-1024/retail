a714cd579bdd13e30a2191b0dd4eaf4459580d10
zeo <234521890@qq.com>
2017-11-30 13:44:48 +0800

    by zeo

--- resources/views/Program/System/dashboard.blade.php
+++ resources/views/Program/System/dashboard.blade.php
@@ -1,1 +1,386 @@
-这里是首页
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>零壹新科技程序管理平台</title>
+
+    <link href="{{asset('public/Program/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('public/Program/library/font')}}/css/font-awesome.css" rel="stylesheet">
+
+    <link href="{{asset('public/Program')}}/css/animate.css" rel="stylesheet">
+    <link href="{{asset('public/Program')}}/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="">
+
+<div id="wrapper">
+
+    <nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+            <ul class="nav metismenu" id="side-menu">
+                <li class="nav-header">
+                    <div class="dropdown profile-element">  
+                    	<span>
+                            <img alt="image"  src="img/zerone_logo.png" />
+                         </span>
+
+                    </div>
+                    <div class="logo-element">
+                        01
+                    </div>
+                </li>
+                <li class="active">
+                    <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">系统管理</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li class="active"><a href="index.html">管理首页</a></li>
+                        <li><a href="addaccount.html">添加账号</a></li>
+
+                        <li><a href="allctrllog.html">所有操作记录</a></li>
+                        <li><a href="allloginlog.html">所有登陆记录</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="index.html"><i class="fa fa-user"></i> <span class="nav-label">个人中心</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="editpassword.html">登陆密码修改</a></li>
+
+                        <li><a href="ctrllog.html">我的操作日志</a></li>
+                        <li><a href="loginlog.html">我的登录日志</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="index.html"><i class="fa fa-language"></i> <span class="nav-label">程序管理</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="addsystem.html">添加程序</a></li>
+                        <li><a href="system.html">程序列表</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="index.html"><i class="fa fa-slack"></i> <span class="nav-label">功能模块管理</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="addfuncmodule.html">添加模块</a></li>
+                        <li><a href="funcmodule.html">模块列表</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="index.html"><i class="fa fa-steam"></i> <span class="nav-label">功能节点管理</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="addnode.html">添加节点</a></li>
+                        <li><a href="node.html">节点列表</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="layouts.html"><i class="fa fa-sign-out"></i> <span class="nav-label">退出登录</span></a>
+                </li>
+
+
+            </ul>
+
+        </div>
+    </nav>
+
+    <div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom">
+            <div class="navbar-header">
+                <a class="navbar-minimalize minimalize-styl-2 btn btn-info " href="javascript:;"><i class="fa fa-bars"></i> </a>
+            </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <span class="m-r-sm text-muted welcome-message">欢迎登录！<strong>超级管理员-薛志豪</strong></span>
+                </li>
+                <li>
+                    <a href="JavaScript:;">
+                        <i class="fa fa-sign-out"></i>退出系统
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="row wrapper border-bottom white-bg page-heading">
+            <div class="col-sm-4">
+                <h2>管理首页</h2>
+                <ol class="breadcrumb">
+                    <li>
+                        <a href="JavaScript:;">系统管理</a>
+                    </li>
+                    <li class="active">
+                        <strong>管理首页</strong>
+                    </li>
+                </ol>
+            </div>
+
+        </div>
+
+        <div class="wrapper wrapper-content animated fadeInRight">
+            <div class="row ">
+                <div class="col-lg-12">
+                    <div class="ibox-content forum-container">
+
+                        <div class="forum-title">
+
+                            <h3>管理人员统计</h3>
+                        </div>
+
+                        <div class="forum-item active">
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <div class="forum-icon">
+                                        <i class="fa fa-area-chart"></i>
+                                    </div>
+                                    <a href="javascript:;" class="forum-item-title">程序数量</a>
+                                    <div class="forum-sub-title">零壹开发的所有程序数量</div>
+                                </div>
+
+
+                                <div class="col-md-3 forum-info">
+                                            <span class="views-number">
+                                                14套
+                                            </span>
+                                    <div>
+                                        <small>总计</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="forum-item active">
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <div class="forum-icon">
+                                        <i class="fa fa-area-chart"></i>
+                                    </div>
+                                    <a href="javascript:;" class="forum-item-title">功能模块数量</a>
+                                    <div class="forum-sub-title">零壹开发的所有功能模块的数量</div>
+                                </div>
+
+                                <div class="col-md-3 forum-info">
+                                            <span class="views-number">
+                                                150个
+                                            </span>
+                                    <div>
+                                        <small>总计</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="forum-item active">
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <div class="forum-icon">
+                                        <i class="fa fa-area-chart"></i>
+                                    </div>
+                                    <a href="javascript:;" class="forum-item-title">功能节点数量</a>
+                                    <div class="forum-sub-title">组成零壹所有系统的程序节点数量</div>
+                                </div>
+                                <div class="col-md-3 forum-info">
+                                            <span class="views-number">
+                                                1300
+                                            </span>
+                                    <div>
+                                        <small>总计</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="row wrapper wrapper-content animated fadeInRight">
+                <div class="col-lg-7">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>用户登陆日志</h5>
+                        </div>
+                        <div class="ibox-content">
+                            <table class="table table-hover no-margins">
+                                <thead>
+                                <tr>
+                                    <th>用户</th>
+                                    <th>登陆IP</th>
+                                    <th>登陆地址</th>
+                                    <th>登陆时间</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>192.168.0.0.1</td>
+                                    <td>广东深圳</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>用户操作日志</h5>
+                        </div>
+                        <div class="ibox-content">
+                            <table class="table table-hover no-margins">
+                                <thead>
+                                <tr>
+                                    <th>用户</th>
+
+                                    <th>操作</th>
+                                    <th>时间</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="footer" >
+            <div class="pull-right">
+                您登陆的时间是：2017-10-24 16:26:30
+            </div>
+            <div>
+                <strong>Copyright</strong> 零壹新科技（深圳有限公司）&copy; 2017-2027
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!-- Mainly scripts -->
+<script src="{{asset('public/Program/library/jquery')}}/js/jquery-2.1.1.js"></script>
+<script src="{{asset('public/Program/library/bootstrap')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Program/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
+<script src="{{asset('public/Program/library/slimscroll')}}/js/query.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="{{asset('public/Program')}}/js/inspinia.js"></script>
+<script src="{{asset('public/Program/library/pace')}}/js/pace.min.js"></script>
+
+</body>
+
+</html>
