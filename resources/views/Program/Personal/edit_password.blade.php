ea3709e81b902d46ea019f966b4de6b12b4329d5
zeo <234521890@qq.com>
2017-11-30 22:31:11 +0800

    by zeo

--- resources/views/Program/Personal/edit_password.blade.php
+++ resources/views/Program/Personal/edit_password.blade.php
@@ -50,24 +50,28 @@
                         <div class="ibox-content">
                             <form method="post" class="form-horizontal"  role="form" id="currentForm" action="{{ url('program/ajax/check_account_add') }}">
                                 <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <div class="form-group"><label class="col-sm-2 control-label">登陆账号</label>
+                                    <div class="col-sm-10" style="padding-top:7px;">admin</div>
+                                </div>
+                                <div class="hr-line-dashed"></div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">登陆账号</label>
-                                    <div class="col-sm-10"><input type="text" name="account" class="form-control" placeholder="登陆账号"></div>
+                                    <label class="col-sm-2 control-label">原登陆密码</label>
+                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">登陆密码</label>
-                                    <div class="col-sm-10"><input type="password" name="password" class="form-control" placeholder="登陆密码"></div>
+                                    <label class="col-sm-2 control-label">新登陆密码</label>
+                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">重复登陆密码</label>
-                                    <div class="col-sm-10"><input type="password" name="repassword" class="form-control" placeholder="重复登陆密码"></div>
+                                    <label class="col-sm-2 control-label">重复新密码</label>
+                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
                                 </div>
 
                                 <div class="hr-line-dashed"></div>
 
                                 <div class="form-group ">
                                     <div class="col-sm-4 col-sm-offset-5">
-                                        <button class="btn btn-primary" id="addbtn" type="button" onclick="return postForm();">确认添加</button>
+                                        <button class="btn btn-primary" id="addbtn" type="button">确认修改</button>
                                     </div>
                                 </div>
                             </form>
