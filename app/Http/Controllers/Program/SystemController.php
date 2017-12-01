4878e85475df5aa34a529a39a4bd1a172cfb9efe
zeo <234521890@qq.com>
2017-12-01 15:53:47 +0800

    by zeo

--- app/Http/Controllers/Program/SystemController.php
+++ app/Http/Controllers/Program/SystemController.php
@@ -63,7 +63,7 @@ class SystemController extends Controller{
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $admin = new ProgramAdmin();//实例化模型
-        $list = $admin->paginate(15);
+        $list = $admin->paginate(1);
         return view('Program/System/account_list',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'action_name'=>'system']);
     }
 
