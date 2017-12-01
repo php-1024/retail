3a4d8569ac2e1b9c75e3845aa2d15d9946cd9767
zeo <234521890@qq.com>
2017-12-01 16:10:16 +0800

    by zeo

--- app/Http/Controllers/Program/SystemController.php
+++ app/Http/Controllers/Program/SystemController.php
@@ -64,10 +64,14 @@ class SystemController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
 
         $account = $request->input('account');//通过登录页账号查询
-        dump($account);
+
         $search_data = ['account'=>$account];
         $admin = new ProgramAdmin();//实例化模型
+        if(!empty($account)){
+            $list = $admin->where('account','like','%'.$account.'%');
+        }
         $list = $admin->paginate(1);
+
         return view('Program/System/account_list',['list'=>$list,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'action_name'=>'system']);
     }
 
