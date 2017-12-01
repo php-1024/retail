9cfd078ac5c20c182887c1e169645c8483ba474c
zeo <234521890@qq.com>
2017-12-01 14:27:27 +0800

    by zeo

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::group(['prefix'=>'program'],function(){
 
     //功能模块组
     Route::group(['prefix'=>'module'],function(){
-        Route::get('add_module', 'Program\NodeController@add_module')->middleware('ProgramCheckIsLogin');//修改密码路由
+        Route::get('add_module', 'Program\ModuleController@add_module')->middleware('ProgramCheckIsLogin');//修改密码路由
     });
 
     //节点管理组
