46a1aafb4640239a8e70551051ca047dd7d71440
zeo <234521890@qq.com>
2017-12-01 14:38:53 +0800

    by zeo

--- resources/views/Program/Public/Nav.blade.php
+++ resources/views/Program/Public/Nav.blade.php
@@ -17,8 +17,8 @@
                 <ul class="nav nav-second-level collapse">
                     <li @if ($route_name=='program')class="active"@endif><a href="{{ url('program') }}">管理首页</a></li>
                     @if($admin_data['admin_is_super']==1)
-                    <li  @if ($route_name=='program/dashboard/account_add')class="active"@endif><a href="{{ url('program/dashboard/account_add') }}">添加账号</a></li>
-                    <li><a href="javascript:;">账号列表</a></li>
+                    <li @if ($route_name=='program/dashboard/account_add')class="active"@endif><a href="{{ url('program/dashboard/account_add') }}">添加账号</a></li>
+                    <li @if ($route_name=='program/dashboard/account_list')class="active"@endif><a href="{{ url('program/dashboard/account_list') }}">账号列表</a></li>
                     <li><a href="allctrllog.html">所有操作记录</a></li>
                     <li><a href="allloginlog.html">所有登陆记录</a></li>
                     @endif
