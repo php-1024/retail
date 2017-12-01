c7f4b80127179e310d75ea100b87fb46b4dbfd48
zeo <234521890@qq.com>
2017-12-01 17:18:15 +0800

    by zeo

--- resources/views/Program/System/account_edit.blade.php
+++ resources/views/Program/System/account_edit.blade.php
@@ -1,26 +1,23 @@
 <form method="post" role="form" id="currentForm" action="{{ url('program/ajax/check_edit_password') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <input type="hidden" name="id" value="{{ $id }}">
     <div class="modal-dialog modal-sm">
         <div class="modal-content animated fadeIn">
             <div class="modal-header">
                 修改用户密码
             </div>
             <div class="modal-body">
-                <div class="form-group"><label class="col-sm-2 control-label">登陆账号</label>
-                    <div class="col-sm-10" style="padding-top:7px;">admin</div>
-                </div>
-                <div class="hr-line-dashed"></div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">原登陆密码</label>
-                    <div class="col-sm-10"><input type="password" name="oldpassword" class="form-control" placeholder="原登陆密码"></div>
+                    <label>用户账号</label>
+                    admin
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">新登陆密码</label>
-                    <div class="col-sm-10"><input type="password" name="password" class="form-control" placeholder="新登陆密码"></div>
+                    <label>新登陆密码</label>
+                    <input type="password" placeholder="请输入新登陆密码" class="form-control">
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">重复新密码</label>
-                    <div class="col-sm-10"><input type="password" name="repassword" class="form-control" placeholder="重复新密码"></div>
+                    <label>重复新密码</label>
+                    <input type="password" placeholder="请再次输入新登陆密码" class="form-control">
                 </div>
             </div>
 
