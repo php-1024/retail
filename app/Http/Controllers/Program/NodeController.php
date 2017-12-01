2b3488168fa34c500ed9bf30e1640d072813f63d
zeo <234521890@qq.com>
2017-12-01 14:07:11 +0800

    by zeo

--- app/Http/Controllers/Program/NodeController.php
+++ app/Http/Controllers/Program/NodeController.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Session;
 use Illuminate\Support\Facades\DB;
-use App\Models\ProgramAdmin;
+use App\Models\Node;
 use App\Libraries\ZeroneLog\ProgramLog;
 
 class NodeController extends Controller{
@@ -17,8 +17,29 @@ class NodeController extends Controller{
     //提交修改个人密码数据
     public function check_add_node(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        $route_name = $request->path();//获取当前的页面路由
+        $current_route_name = $request->path();//获取当前的页面路由
 
+        $node_name = $request->input('node_name');//提交上来的节点名称
+        $route_name = $request->input('route_name');//提交上来的路由名称
+
+        $node = new Node();
+        $info = $node->where('node',$node_name)->orWhere('route_name',$route_name)->pluck('id')->toArray();//查询是否有相同的节点名称或路由名称存在
+        if(!empty($info)){
+            return response()->json(['data' => '节点名称或路由名称已经存在', 'status' => '0']);
+        }else{
+            DB::beginTransaction();
+            try{
+                $node = new Node();//重新实例化模型，避免重复
+                $node->node_name = $node_name;//节点名称
+                $node->route_name = $route_name;//路由名称
+                $node->save();//添加账号
+                ProgramLog::setOperationLog($admin_data['admin_id'],$current_route_name,'新增了节点'.$node_name);//保存操作记录
+                DB::commit();//提交事务
+            }catch (\Exception $e) {
+                DB::rollBack();//事件回滚
+                return response()->json(['data' => '添加账号失败，请检查', 'status' => '0']);
+            }
+        }
     }
 }
 ?>
