1b023e42e2cd7ea24146bbf8dd90918db3fab58e
zeo <234521890@qq.com>
2017-12-02 16:18:06 +0800

    by zeo

--- app/Http/Controllers/Program/NodeController.php
+++ app/Http/Controllers/Program/NodeController.php
@@ -27,7 +27,15 @@ class NodeController extends Controller{
         $list = $node->paginate(15);
         return view('Program/Node/node_list',['list'=>$list,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'action_name'=>'node']);
     }
-    //提交修改个人密码数据
+
+    //编辑节点
+    public function node_edit(Request $request){
+        $id = $request->input('id');
+        $info = ProgramAdmin::find($id);
+        return view('Program/System/account_edit',['info'=>$info]);
+    }
+
+    //提交添加节点数据
     public function check_add_node(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $current_route_name = $request->path();//获取当前的页面路由
