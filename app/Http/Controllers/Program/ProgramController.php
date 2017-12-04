b369a6e44ba7c0b45aff44da9992a4b8d789c5c0
zeo <234521890@qq.com>
2017-12-04 21:28:12 +0800

    by zeo

--- app/Http/Controllers/Program/ProgramController.php
+++ app/Http/Controllers/Program/ProgramController.php
@@ -3,6 +3,7 @@
  * 系统管理
  */
 namespace App\Http\Controllers\Program;
+use App\Models\Module;
 use App\Models\OperationLog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -12,12 +13,23 @@ use App\Models\ProgramAdmin;
 use App\Models\ProgramOperationLog;
 use App\Models\ProgramLoginLog;
 use App\Libraries\ZeroneLog\ProgramLog;
-
+use APP\Models\Program;
 class ProgramController extends Controller{
-    public function program_add(Request $request){
+    public function program_add(Request $request)
+    {
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        //$node_list = Node::where('is_delete','0')->get();
+
+        $module = new Module(); //实例化功能模块模型
+        $module_list = $module->where('is_delete', '0')->get()->toArray();
+        $node_list = [];
+        if (!empty($module_list)) {
+            foreach ($module_list as $key => $val) {
+                $node_list[$val->id] = ModuleNode::where('module_id', $val->id)->where('is_delete', 0)->get()->toArray();
+            }
+        }
+        dump($module_list);
+        dump($node_list);
         return view('Program/Program/program_add',['admin_data'=>$admin_data,'route_name'=>$route_name,'action_name'=>'program']);
     }
 }
