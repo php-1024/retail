31c5072111e30b96f00629a1102b2dcfd9408422
zeo <234521890@qq.com>
2017-12-04 17:31:29 +0800

    by zeo

--- resources/views/Program/Module/module_edit.blade.php
+++ resources/views/Program/Module/module_edit.blade.php
@@ -1,7 +1,7 @@
 <form method="post" role="form" id="currentForm" action="{{ url('program/ajax/node_edit_check') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" value="{{ $info->id }}">
-    <div class="modal-dialog modal">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content animated fadeIn">
             <div class="modal-header">
                 修改功能节点
