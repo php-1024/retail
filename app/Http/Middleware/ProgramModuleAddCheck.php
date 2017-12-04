1eebc208af42ce1d5ef0b1c54f94d1bf2d0726df
zeo <234521890@qq.com>
2017-12-04 14:36:57 +0800

    by zeo

--- app/Http/Middleware/ProgramModuleAddCheck.php
+++ app/Http/Middleware/ProgramModuleAddCheck.php
@@ -12,7 +12,7 @@ class ProgramModuleAddCheck{
         if(empty($request->input('module_name'))){
             return response()->json(['data' => '请输入模块名称', 'status' => '0']);
         }
-        if(empty($request->input('route_name'))){
+        if(empty($request->input('nodes'))){
             return response()->json(['data' => '请选择该模块的功能节点到右边选框', 'status' => '0']);
         }
         return $next($request);
