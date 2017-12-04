ccf9de0a18c81474d5cc9e61057a56295b523dde
zeo <234521890@qq.com>
2017-12-04 10:47:29 +0800

    by zeo

--- app/Http/Controllers/Program/ModuleController.php
+++ app/Http/Controllers/Program/ModuleController.php
@@ -8,13 +8,13 @@ use App\Models\Node;
 use App\Libraries\ZeroneLog\ProgramLog;
 
 class ModuleController extends Controller{
-    //修改个人密码
+    //添加功能模块
     public function module_add(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         return view('Program/Moudle/add_moudle',['admin_data'=>$admin_data,'route_name'=>$route_name,'action_name'=>'module']);
     }
-    //提交修改个人密码数据
+    //提交添加功能模块数据
     public function module_add_check(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $current_route_name = $request->path();//获取当前的页面路由
