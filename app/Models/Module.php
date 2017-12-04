084de0e181cd92f4e0003777daa10f1955a7e887
zeo <234521890@qq.com>
2017-12-04 15:07:52 +0800

    by zeo

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -6,7 +6,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class Module extends Model{
-    protected $table = 'node';
+    protected $table = 'module';
     protected $primaryKey = 'id';
     public $timestamps = true;
 
