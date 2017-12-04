93d3ddf40388c3cd2eac4e64f8dccae7b2ff53ae
zeo <234521890@qq.com>
2017-12-04 10:55:26 +0800

    by zeo

--- resources/views/Program/Moudle/module_add.blade.php
+++ resources/views/Program/Moudle/module_add.blade.php
@@ -49,7 +49,7 @@
 
                         </div>
                         <div class="ibox-content">
-                            <form method="post" class="form-horizontal" action=" {{ url('program/ajax/node_add_check') }} ">
+                            <form method="post" class="form-horizontal" action=" {{ url('program/ajax/module_add_check') }} ">
                                 <div class="form-group"><label class="col-sm-2 control-label">模块名称</label>
 
                                     <div class="col-sm-10"><input type="text" name="module_name" class="form-control"></div>
@@ -80,7 +80,7 @@
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group ">
                                     <div class="col-sm-4 col-sm-offset-5">
-                                        <button class="btn btn-primary" id="addbtn" type="button">确认添加</button>
+                                        <button class="btn btn-primary" id="addbtn" type="button" onclick="return postForm();">确认添加</button>
                                         <button class="btn btn-write" onClick="location.href='node.html'" type="button">回到列表</button>
                                     </div>
                                 </div>
