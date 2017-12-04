adf61e36a7e06b631c55af32801bf5f93b161362
zeo <234521890@qq.com>
2017-12-04 17:29:01 +0800

    by zeo

--- app/Http/Controllers/Program/ModuleController.php
+++ app/Http/Controllers/Program/ModuleController.php
@@ -86,7 +86,7 @@ class ModuleController extends Controller{
         $node = new Node();
         $node_list_unselected = $node->whereNotIn('id',$selected_id)->where('is_delete','0')->get();
 
-        return view('Program/System/module_edit',['info'=>$info,'node_list_selected'=>$node_list_selected,'node_list_unselected'=>$node_list_unselected]);
+        return view('Program/Module/module_edit',['info'=>$info,'node_list_selected'=>$node_list_selected,'node_list_unselected'=>$node_list_unselected]);
     }
 }
 ?>
