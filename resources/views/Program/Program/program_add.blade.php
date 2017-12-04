76fbde130e6642fe9066848623d94b72c7820146
zeo <234521890@qq.com>
2017-12-04 22:07:57 +0800

    by zeo

--- resources/views/Program/Program/program_add.blade.php
+++ resources/views/Program/Program/program_add.blade.php
@@ -82,13 +82,13 @@
                                         <group class="checked_box_group">
                                             <div>
                                                 <label class="i-checks">
-                                                    <input type="checkbox" class="checkbox_module_name" value="option1" id="inlineCheckbox1"> {{ $val['module_name'] }}
+                                                    <input type="checkbox" class="checkbox_module_name" value="option1"> {{ $val['module_name'] }}
                                                 </label>
                                             </div>
                                             <div>
                                                 @foreach($node_list[$val['id']] as $kk=>$vv)
                                                 <label class="checkbox-inline i-checks">
-                                                    <input type="checkbox" class="checkbox_node_name" value="option1" id="inlineCheckbox1"> {{$vv['node_name']}}
+                                                    <input type="checkbox" class="checkbox_node_name" value="option1"> {{$vv['node_name']}}
                                                 </label>
                                                 @endforeach;
                                             </div>
