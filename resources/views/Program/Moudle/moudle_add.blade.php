9a523c14e1f75944742a900960f8bc683932f600
zeo <234521890@qq.com>
2017-12-04 10:31:07 +0800

    by zeo

--- resources/views/Program/Moudle/moudle_add.blade.php
+++ resources/views/Program/Moudle/moudle_add.blade.php
@@ -52,7 +52,7 @@
                             <form method="post" class="form-horizontal" action="">
                                 <div class="form-group"><label class="col-sm-2 control-label">模块名称</label>
 
-                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
+                                    <div class="col-sm-10"><input type="text" name="module_name" class="form-control"></div>
                                 </div>
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group"><label class="col-sm-2 control-label">模块功能节点</label>
@@ -80,7 +80,7 @@
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group ">
                                     <div class="col-sm-4 col-sm-offset-5">
-                                        <button class="btn btn-primary" id="addbtn" type="submit">确认添加</button>
+                                        <button class="btn btn-primary" id="addbtn" type="button">确认添加</button>
                                         <button class="btn btn-write" onClick="location.href='node.html'" type="button">回到列表</button>
                                     </div>
                                 </div>
