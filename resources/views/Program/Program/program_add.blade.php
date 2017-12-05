988897edfbfe91f8db3ff87fd28fa3cc2530b012
zeo <234521890@qq.com>
2017-12-05 10:22:42 +0800

    by zeo

--- resources/views/Program/Program/program_add.blade.php
+++ resources/views/Program/Program/program_add.blade.php
@@ -147,7 +147,7 @@
         $('.checkbox_node_name').on('ifUnchecked',function(event){
             var group_id = $(this).attr('data-group_id');
             $('.check_node_name_'+group_id).each(function(i,v){
-                alert(v.attr('checked'));
+                alert(v.val());
             });
         });
 
