4e0059ae2e1d5e2de281d11f1ffe0a0d91443a65
zeo <234521890@qq.com>
2017-12-05 11:06:07 +0800

    by zeo

--- resources/views/Program/Program/program_add.blade.php
+++ resources/views/Program/Program/program_add.blade.php
@@ -52,7 +52,7 @@
                         </div>
                         <div class="ibox-content">
                             <form method="post" class="form-horizontal"  role="form" id="currentForm" action="{{ url('program/ajax/program_add_check') }}">
-                                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                 <div class="form-group"><label class="col-sm-2 control-label">程序名称</label>
                                     <div class="col-sm-10"><input type="text" name="program_name" class="form-control"></div>
                                 </div>
