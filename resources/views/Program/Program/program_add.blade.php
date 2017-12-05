b8cc56daaff11cf7ce442e9f205782a1cd7e0355
zeo <234521890@qq.com>
2017-12-05 10:35:44 +0800

    by zeo

--- resources/views/Program/Program/program_add.blade.php
+++ resources/views/Program/Program/program_add.blade.php
@@ -147,9 +147,7 @@
 
         $('.checkbox_node_name').on('ifUnchecked',function(event){
             var group_id = $(this).attr('data-group_id');
-            $('.check_node_name_'+group_id).each(function(i,v){
-               alert(v);
-            });
+            alert($('.check_node_name_'+group_id).val());
         });
 
         //设置CSRF令牌
