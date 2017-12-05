c8976d5a8d39e97fdb3def8bc08af3e117c95031
zeo <234521890@qq.com>
2017-12-05 11:35:04 +0800

    by zeo

--- resources/views/Program/Program/program_add.blade.php
+++ resources/views/Program/Program/program_add.blade.php
@@ -69,7 +69,7 @@
                                 <div class="form-group">
                                     <label class="col-sm-2 control-label" >是否通用</label>
                                     <div class="col-sm-10">
-                                        <input type="checkbox" name="is_universal" class="js-switch" checked  value="1"/>
+                                        <input type="checkbox" name="is_universal" class="js-switch"  value="1"/>
                                     </div>
                                 </div>
                                 <div class="hr-line-dashed"></div>
