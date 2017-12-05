1c0179d0c28e2c6e4edfd539b89edf7bfbd97594
zeo <234521890@qq.com>
2017-12-05 14:30:24 +0800

    by zeo

--- resources/views/Program/Program/program_add.blade.php
+++ resources/views/Program/Program/program_add.blade.php
@@ -63,7 +63,7 @@
                                     <div class="col-sm-10">
                                         <select class="form-control m-b" name="pid">
                                             <option value="0">独立主程序</option>
-                                            @foreaah($plist as $key=>$val)
+                                            @foreach($plist as $key=>$val)
                                             <option value="{{ $val->id }}">{{ $val->program_name }}</option>
                                             @endforeach
                                         </select>
