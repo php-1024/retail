cdfb1ca362cfd574b9a97ac3e330065f18fe122b
追梦小窝 <mail@54zm.com>
2017-12-06 11:12:36 +0800

    110000...

--- resources/views/Program/Login/display.blade.php
+++ resources/views/Program/Login/display.blade.php
@@ -74,7 +74,9 @@
     function postForm(){
         var target = $("#currentForm");
         var url = target.attr("action");
+        console.log(url);
         var data = target.serialize();
+        console.log(data);
         $.post(url,data,function(json){
             if(json.status==1){
                 window.location.reload();
