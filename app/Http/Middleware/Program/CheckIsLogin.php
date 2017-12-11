edc0941c528643d050eb437361d2ac76888ddec5
zeo <234521890@qq.com>
2017-12-11 18:07:24 +0800

    zeo

---
+++ app/Http/Middleware/Program/CheckIsLogin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 检测是否登陆的中间件
+ */
+namespace App\Http\Middleware\Program;
+use Closure;
+use Session;
+use Illuminate\Support\Facades\Redis;
+
+class CheckIsLogin{
+    public function handle($request,Closure $next){
+        //获取用户登陆存储的SessionId
+        $sess_key = Session::get('zerone_program_account_id');
+        //如果为空跳转到登陆页面
+        if(empty($sess_key)) {
+            return redirect('program/login');
+        }else{
+            $sess_key = Session::get('zerone_program_account_id');//获取管理员ID
+            $sess_key = decrypt($sess_key);//解密管理员ID
+            Redis::connect('zeo');//连接到我的缓存服务器
+            $admin_data = Redis::get('program_system_admin_data_'.$sess_key);//获取管理员信息
+            $admin_data = unserialize($admin_data);//解序列我的信息
+            $request->attributes->add(['admin_data'=>$admin_data]);//添加参数
+            //把参数传递到下一个中间件
+            return $next($request);
+        }
+    }
+}
+
+
+?>
