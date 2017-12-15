7ca0dfb2daf461afa49cb53e5821402e465676f8
zeo <234521890@qq.com>
2017-12-15 22:21:18 +0800

    by zeo

--- app/Http/Controllers/Program/TestController.php
+++ app/Http/Controllers/Program/TestController.php
@@ -3,9 +3,9 @@ namespace App\Http\Controllers\Program;
 use App\Http\Controllers\Controller;
 class TestController extends Controller{
     public function test(){
-        echo 123;
-       // $info = HttpCurl::doget('http://www.baidu.com');
-        //dump($info);
+
+        $info = HttpCurl::doget('http://www.baidu.com');
+        dump($info);
     }
 }
 ?>
