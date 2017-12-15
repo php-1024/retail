3ea018e1aaf0b49c09195e93e1212ba98a265772
kbzz <463661663@qq.com>
2017-12-15 15:08:33 +0800

    1

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
-var_dump(__DIR__.'/public'.$uri);exit;
+
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
