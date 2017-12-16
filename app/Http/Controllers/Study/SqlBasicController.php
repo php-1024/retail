3ae81b92551c0875415c1f2c3b5ebdc6b0c308c4
zeo <234521890@qq.com>
2017-12-16 16:50:21 +0800

    by zeo

--- app/Http/Controllers/Study/SqlBasicController.php
+++ app/Http/Controllers/Study/SqlBasicController.php
@@ -6,9 +6,9 @@ class SqlBasicController extends Controller{
     //Laravel数据库基本用法
     public function insertDb(){
         $db = new DB();
-        $db = $db::connection('study');
+
         for($i = 1 ; $i<=10; $i++){
-            $db->insert("insert into study_test (name) VALUES (?)",['test'.$i]);
+            $db::connection('study')->insert("insert into study_test (name) VALUES (?)",['test'.$i]);
         }
     }
 }
