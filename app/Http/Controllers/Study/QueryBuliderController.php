ff7f1e1801366a24b9c13ba3742c945db150e24a
zeo <234521890@qq.com>
2017-12-18 14:31:18 +0800

    by zeo

--- app/Http/Controllers/Study/QueryBuliderController.php
+++ app/Http/Controllers/Study/QueryBuliderController.php
@@ -14,7 +14,7 @@ class QueryBuliderController extends Controller{
     //从数据库中获取一行数据
     public function select_first(){
         $row = DB::connection('study')->table('test')->where('name','like','%test2%')->first();
-
+        dump($row);
     }
 
 }
