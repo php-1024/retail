7eca0c0a77e2f65ea39f105366b4783adb71dba0
zeo <234521890@qq.com>
2017-12-18 14:24:44 +0800

    by zeo

--- app/Http/Controllers/Study/QueryBuliderController.php
+++ app/Http/Controllers/Study/QueryBuliderController.php
@@ -7,9 +7,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 class QueryBuliderController extends Controller{
     //从数据表中查询所有数据
-    public function selectAll(){
+    public function select_all(){
         $list = DB::connection('study')->table('test')->get();
         dump($list);
     }
+    //从数据库中获取一行数据
+    public function select_first(){
+        $row = DB::connection('study')->table('test')->where('name','like','%test%')->first();
+        dump($row);
+    }
+
 }
 ?>
