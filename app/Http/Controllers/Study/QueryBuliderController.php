0eb17c5fe76d806e2bf5e1cf889a2d9070cb5936
zeo <234521890@qq.com>
2017-12-19 17:41:36 +0800

    by zeo

--- app/Http/Controllers/Study/QueryBuliderController.php
+++ app/Http/Controllers/Study/QueryBuliderController.php
@@ -175,7 +175,7 @@ class QueryBuliderController extends Controller{
     //groupBy分组
     public function select_groupby(){
         $db = DB::connection('study');
-        $list = $db->table('test')->groupby('age')->get();
+        $list = $db->table('test')->groupBy('age')->get();
         dump($list);
     }
 }
