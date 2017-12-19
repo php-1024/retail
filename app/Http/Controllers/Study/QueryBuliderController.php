d6019ada270500cee3354b61340f2344753212cb
zeo <234521890@qq.com>
2017-12-19 18:28:38 +0800

    by zeo

--- app/Http/Controllers/Study/QueryBuliderController.php
+++ app/Http/Controllers/Study/QueryBuliderController.php
@@ -204,6 +204,8 @@ class QueryBuliderController extends Controller{
         $str = '牛肉+水豆腐+鸡排+排骨汤水+牛排+鸡排+牛丸+鸡肉+玉米+白菜+青菜+豆腐+花生+鱼丸';
         $arr = explode('+',$str);
         dump($arr);
+        $str2 =implode(',',$arr);
+        dump($str2);
         $db = DB::connection('study');
         $list = $db->table('test')->having('id','>',3)->skip(1)->take(5)->get();
         dump($list);
