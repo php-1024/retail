16b850f3aa98c41bfea2e058a7c9d4bbd8d97123
zeo <234521890@qq.com>
2017-12-19 14:55:20 +0800

    by zeo

--- app/Http/Controllers/Study/QueryBuliderController.php
+++ app/Http/Controllers/Study/QueryBuliderController.php
@@ -78,9 +78,11 @@ class QueryBuliderController extends Controller{
     //关联查询
     public function select_join(){
         $list =  DB::connection('study')->table('test')->join('test_sex','test_sex.test_id','=','test.id')->select('test.*','test_sex.sex')->get();
-        dump($list);
+        dump("inner join:".$list);
         $list2 =  DB::connection('study')->table('test')->leftjoin('test_sex','test_sex.test_id','=','test.id')->select('test.*','test_sex.sex')->get();
-        dump($list2);
+        dump("left join".$list2);
+        $list3 =  DB::connection('study')->table('test')->rightjoin('test_sex','test_sex.test_id','=','test.id')->select('test.*','test_sex.sex')->get();
+        dump("right join".$list3);
     }
 }
 ?>
