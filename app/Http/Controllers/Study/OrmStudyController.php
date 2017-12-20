a1e75186c1f24e139e0f17d3606feab84e75f15c
zeo <234521890@qq.com>
2017-12-20 17:40:52 +0800

    by zeo

--- app/Http/Controllers/Study/OrmStudyController.php
+++ app/Http/Controllers/Study/OrmStudyController.php
@@ -3,8 +3,14 @@ namespace App\Http\Controllers\Study;
 use App\Http\Controllers\Controller;
 use App\Models\Study\Test;
 class OrmStudyController extends Controller{
+    //调用model里的all方法
     public function getAll(){
         $list = Test::get_all();
+        dump($list);
+    }
+    //调用model里的get方法
+    public function getList(){
+        $list = Test::get_list();
         dump($list);
     }
 }
