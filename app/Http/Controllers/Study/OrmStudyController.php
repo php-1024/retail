755b84d0eb609c6183dd3b0a18064ca9de7ab282
zeo <234521890@qq.com>
2017-12-20 17:37:44 +0800

    by zeo

--- app/Http/Controllers/Study/OrmStudyController.php
+++ app/Http/Controllers/Study/OrmStudyController.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Study\Test;
 class OrmStudyController extends Controller{
     public function getAll(){
-        $list = Test::all();
+        $list = Test::get_all();
         dump($list);
     }
 }
