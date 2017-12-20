68fb0a0445cfd9e82275213e0730842034b2f157
zeo <234521890@qq.com>
2017-12-20 17:30:40 +0800

    by zeo

--- app/Http/Controllers/Study/OrmStudyController.php
+++ app/Http/Controllers/Study/OrmStudyController.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Study\Test;
 class QueryBuliderController extends Controller{
     public function getAll(){
-
+        $list = Test::all();
+        dump($list);
     }
 }
 ?>
