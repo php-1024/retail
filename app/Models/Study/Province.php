1f27753e9c1ac9e700b3451f0dd82e5a11cb49d2
zeo <234521890@qq.com>
2017-12-21 18:33:20 +0800

    by zeo

--- app/Models/Study/Province.php
+++ app/Models/Study/Province.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     protected $connection = 'study';//设置数据库连接，默认连接到database.php mysql设置的数据库.
-    protected $table = 'provice';//数据表名
+    protected $table = 'province';//数据表名
     protected $primaryKey = 'id';//主键
     public $timestamps = true;//是否使用时间戳created_at和updated_at
     public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
