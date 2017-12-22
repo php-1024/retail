f0b92a3138c2a9656e74a4403ba6dede9a5f0d32
zeo <234521890@qq.com>
2017-12-22 14:57:05 +0800

    by zeo

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        App\Providers\HttpCurlServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -180,6 +180,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\HttpCurlServiceProvider::class,
+        App\Providers\IP2AttrServiceProvider::class,
     ],
 
     /*
@@ -228,7 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-       'HttpCurl' => App\Facades\HttpCurlFacade::class,
+        'HttpCurl' => App\Facades\HttpCurlFacade::class,
+        'IP2Attr' => App\Facades\IP2AttrFacade::class,
     ],
 
 ];
