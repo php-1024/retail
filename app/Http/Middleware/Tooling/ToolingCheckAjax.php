6b5a3a9c2d7b953eca76885cde614b9fd5fb2c26
zeo <234521890@qq.com>
2017-12-22 17:22:38 +0800

    by zeo

--- app/Http/Middleware/Tooling/ToolingCheckAjax.php
+++ app/Http/Middleware/Tooling/ToolingCheckAjax.php
@@ -10,7 +10,6 @@ class ToolingCheckAjax {
     public function handle($request,Closure $next){
         $route_name = $request->path();//获取当前的页面路由
         switch($route_name){
-
             case "tooling/ajax/checklogin"://后台首页
                 $re = $this->checkLoginPost($request);
                 if($re['status']=='0'){
@@ -19,10 +18,38 @@ class ToolingCheckAjax {
                     return $next($re['response']);
                 }
                 break;
+
+            case "tooling/ajax/program_edit"://修改程序
+                $re = $this->checkIsLogin($request);
+                if($re['status']=='0'){
+                    return $re['response'];
+                }else{
+                    return $next($re['response']);
+                }
+                break;
+
         }
     }
 
-    //j检测登陆提交数据
+    //检测是否登陆
+    public function checkIsLogin($request){
+        $sess_key = Session::get('zerone_tooling_account_id');
+        //如果为空返回登陆失效
+        if(!empty($sess_key)) {
+            return self::res(0,response()->json(['data' => '登陆状态失效', 'status' => '-1']));
+        }else{
+            $sess_key = Session::get('zerone_tooling_account_id');//获取管理员ID
+            $sess_key = decrypt($sess_key);//解密管理员ID
+            Redis::connect('zeo');//连接到我的缓存服务器
+            $admin_data = Redis::get('tooling_system_admin_data_'.$sess_key);//获取管理员信息
+            $admin_data = unserialize($admin_data);//解序列我的信息
+            $request->attributes->add(['admin_data'=>$admin_data]);//添加参数
+            //把参数传递到下一个中间件
+            return self::res(1,$request);
+        }
+    }
+
+    //检测登陆提交数据
     public function checkLoginPost($request){
         if(empty($request->input('username'))){
             return self::res(0,response()->json(['data' => '请输入用户名', 'status' => '0']));
