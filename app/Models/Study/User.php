bb821761c3ba695e905f1302ee86d283237d4e2f
zeo <234521890@qq.com>
2017-12-22 14:05:36 +0800

    by zeo

--- app/Models/Study/User.php
+++ app/Models/Study/User.php
@@ -20,7 +20,7 @@ class User extends Model
     }
 
     public function searchRoles(){
-        $list = $this->where('name','zh1')->get()->roles;
+        $list = $this->where('name','zh1')->first()->roles;
         return $list;
     }
 }
