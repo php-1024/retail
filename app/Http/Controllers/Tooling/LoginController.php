f0fff62523ae11e3580f40e7a5b299995bf8def4
zeo <234521890@qq.com>
2017-12-23 10:26:00 +0800

    byzeo

--- app/Http/Controllers/Tooling/LoginController.php
+++ app/Http/Controllers/Tooling/LoginController.php
@@ -19,6 +19,13 @@ class LoginController extends Controller{
      */
     public function display()
     {
+        $ip = Request::getClientIp();//获取访问者IP
+        $addr_arr = \IP2Attr::find($ip);//获取访问者地址
+        $addr = $addr_arr[0].$addr_arr[1].$addr_arr[2].$addr_arr[3];//获取访问者地址
+        $ip = ip2long($ip);//IP查询完地址后转化为整型。便于存储和查询
+        $error = new ToolingErrorlog();
+        $error_log = $error->where('ip',$ip)->first();//获取该IP的错误记录
+        dump($error_log);
         return view('Tooling/Login/display');
     }
     /*
--- app/Models/ToolingErrorLog.php
+++ app/Models/ToolingErrorLog.php
@@ -6,12 +6,18 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class ToolingErrorLog extends Model{
-    protected $table = 'tooling_error_log';
-    protected $primaryKey = 'id';
-    public $timestamps = true;
+    protected $table = 'tooling_error_log';//数据表
+    protected $primaryKey = 'id';//主键ID
+    public $timestamps = true;//是否使用时间戳
+    public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
 
     public function fromDateTime($value){
         return strtotime(parent::fromDateTime($value));
+    }
+
+    //简易型查询单条数据
+    public static function getOne($where){
+        return self::where($where)->first();
     }
 }
 ?>
