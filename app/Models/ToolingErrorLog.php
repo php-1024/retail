3029f3afb28d2213935fc7b88b088cfd469841c4
zeo <234521890@qq.com>
2017-12-23 11:40:28 +0800

    byzeo

--- app/Models/ToolingErrorLog.php
+++ app/Models/ToolingErrorLog.php
@@ -11,10 +11,6 @@ class ToolingErrorLog extends Model{
     public $timestamps = true;//是否使用时间戳
     public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
 
-    public function fromDateTime($value){
-        return strtotime(parent::fromDateTime($value));
-    }
-
     //简易型查询单条数据
     public static function getOne($where){
         return self::where($where)->first();
@@ -22,12 +18,11 @@ class ToolingErrorLog extends Model{
 
     //根据用户IP增加错误次数
     public static function addErrorTimes($ip){
-        $model = new ToolingErrorLog();
-        $error_log = $model->where('ip',$ip)->first();//获取该IP的错误记录
+        $error_log = self::where('ip',$ip)->first();//获取该IP的错误记录
         if(empty($error_log)){//没有错误记录，插入错误记录，有错误记录，更新错误记录
-            $model->ip = $ip;
-            $model->error_time = 1;
-            $model->save();
+            $error_log->ip = $ip;
+            $error_log->error_time = 1;
+            $error_log->save();
         }else{
             $error_log->error_time = $error_log->error_time+1;
             $error_log->save();
--- app/Models/ToolingLoginLog.php
+++ app/Models/ToolingLoginLog.php
@@ -7,11 +7,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class ToolingLoginLog extends Model{
     protected $table = 'tooling_login_log';
-    protected $primaryKey = 'id';
-    public $timestamps = true;
+    protected $primaryKey = 'id';//主键ID
+    public $timestamps = true;//是否使用时间戳
+    public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
 
     public function fromDateTime($value){
         return strtotime(parent::fromDateTime($value));
+    }
+
+    public function addLoginLog($account_id,$ip,$addr){
+        $loginlog = new ToolingLoginLog();
+        $loginlog->account_id = $account_id;
+        $loginlog->ip = $ip;
+        $loginlog->ip_position = $addr;
+        $loginlog->save();
+        $id = $loginlog->id;
+        if(empty($id)){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
 ?>
