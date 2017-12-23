870ee030869ef176406548f17316ab6247dad549
zeo <234521890@qq.com>
2017-12-23 14:51:43 +0800

    byzeo

--- app/Http/Controllers/Tooling/SystemController.php
+++ app/Http/Controllers/Tooling/SystemController.php
@@ -58,10 +58,7 @@ class SystemController extends Controller{
         $encrypted = md5($password);//加密密码第一重
         $encryptPwd = md5("lingyikeji".$encrypted.$encrypt_key);//加密密码第二重
 
-        $admin = new ToolingAccount();//实例化模型
-        $info = $admin->where('account',$account)->where('is_delete','0')->pluck('id')->toArray();//查询是否有相同的账号存在
-
-        if(ToolingAccount::checkRowExists([[ $account , 'account' ]])){//如果存在相同的账号则报错
+        if(ToolingAccount::checkRowExists([[ 'account' , $account  ]])){//如果存在相同的账号则报错
             return response()->json(['data' => '该账号已存在', 'status' => '0']);
         }else{
             DB::beginTransaction();
