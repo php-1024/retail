0a83a35e8d9b137ead37e1cc1f11168127870d26
zeo <234521890@qq.com>
2017-12-23 11:11:45 +0800

    byzeo

--- app/Models/ToolingErrorLog.php
+++ app/Models/ToolingErrorLog.php
@@ -24,6 +24,7 @@ class ToolingErrorLog extends Model{
     public function addErrorTimes($ip){
         $model = new ToolingErrorLog();
         $error_log = $model->where('ip',$ip)->first();//获取该IP的错误记录
+        dump($error_log);
         if(empty($error_log)){//没有错误记录，插入错误记录，有错误记录，更新错误记录
             $model->ip = $ip;
             $model->error_time = 1;
