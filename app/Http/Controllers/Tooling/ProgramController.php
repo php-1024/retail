3e811f0be1fb4be59880927329f617ed46b67b02
zeo <234521890@qq.com>
2017-12-26 19:05:29 +0800

    by zeo

--- app/Http/Controllers/Tooling/ProgramController.php
+++ app/Http/Controllers/Tooling/ProgramController.php
@@ -92,9 +92,12 @@ class ProgramController extends Controller{
         $node_list = [];//功能节点列表
         $pname = [];//上级程序名称列表
         foreach($list as $key=>$val){
-            $module_list[$val->id] = ProgramModuleNode::where('program_id',$val->id)->where('program_module_node.is_delete','0')->join('module',function($join){
-                $join->on('program_module_node.module_id','=','module.id');
-            })->distinct()->select('program_module_node.module_id as id','module.module_name')->get();
+
+            $program_id = $val->id;
+            $module_list[$val->id] =Module::whereIn('id',function($query) use ($program_id){
+                $query->from('program_module_node')->where('program_id',$program_id)->select('module_id')->groupBy('module_id');
+            })->where('is_delete',0)->get();
+
             $ppname = Program::where('id',$val->pid)->pluck('program_name')->toArray();//获取用户名称
             if(empty($ppname)){
                 $pname[$val->id] = '独立主程序';
