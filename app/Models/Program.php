a75598bd77db0b94c6b7e460f116821f2c84213b
zeo <234521890@qq.com>
2017-12-26 14:45:56 +0800

    by zeo

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -11,6 +11,16 @@ class Program extends Model{
     public $timestamps = true;
     public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
 
+
+    //获取列表
+    public static function getList($where,$limit=0,$orderby,$sort='DESC'){
+        $model = new Program();
+        if(!empty($limit)){
+            $model = $model->limit($limit);
+        }
+        return $model->where($where)->where('is_delete','0')->orderBy($orderby,$sort)->get();
+    }
+
     //获取总数
     public static function getCount($where=[]){
         return self::where($where)->where('is_delete','0')->count();
