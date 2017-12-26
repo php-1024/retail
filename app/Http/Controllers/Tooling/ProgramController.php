2ce06b0f38283ca22d4aa198a88f8a1245f70eeb
zeo <234521890@qq.com>
2017-12-26 22:03:59 +0800

    by zeo

--- app/Http/Controllers/Tooling/ProgramController.php
+++ app/Http/Controllers/Tooling/ProgramController.php
@@ -92,11 +92,11 @@ class ProgramController extends Controller{
         foreach($list as $key=>$val){
             $program_id = $val->id;
             $module_list[$val->id] =Module::getListProgram($program_id,[],0,'id');
-            $ppname = Program::getPluck([['id',$val->complete_id]],'program_name')->toArray();//获取用户名称
+            $ppname = Program::getPluck([['id',$val->complete_id]],'program_name');//获取用户名称
             if(empty($ppname)){
                 $pname[$val->id] = '独立主程序';
             }else{
-                $pname[$val->id] = $ppname[0];
+                $pname[$val->id] = $ppname->program_name;
             }
         }
 
