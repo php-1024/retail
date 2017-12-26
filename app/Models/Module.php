c3133073c127da67703953465845fad9362d167c
zeo <234521890@qq.com>
2017-12-26 21:27:31 +0800

    by zeo

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -96,7 +96,7 @@ class Module extends Model{
 
     //去重后获取程序的模型
     public static function getProgramModules($program_id){
-        $tt = self::from('program_module_node')->select('module_id')->where('program_id',$program_id)->get();
+        $tt = self::from('program_module_node')->select('module_id')->where('program_id',$program_id)->groupBy('module_id')->get();
         dump($tt);
         return self::whereIn('id',function($query) use ($program_id){
             $query->from('program_module_node')->select('module_id')->where('program_id',$program_id);
