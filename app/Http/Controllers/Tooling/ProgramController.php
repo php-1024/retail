f452056995a404531cc4dcbab3423746623090fa
zeo <234521890@qq.com>
2017-12-26 16:07:10 +0800

    by zeo

--- app/Http/Controllers/Tooling/ProgramController.php
+++ app/Http/Controllers/Tooling/ProgramController.php
@@ -32,6 +32,7 @@ class ProgramController extends Controller{
             $node_list = [];
         }else{//有主程序时
            $module_list = Module::getListProgram($pid,[],0,'id');
+            dump($module_list);
             $node_list = [];
             /*
             $module_list = ProgramModuleNode::where('program_id',$pid)->where('program_module_node.is_delete','0')->join('module',function($join){
