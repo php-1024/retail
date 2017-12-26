6388998d4f46e97f82297b6d1b066bd504c5bf43
zeo <234521890@qq.com>
2017-12-26 17:35:04 +0800

    by zeo

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -41,7 +41,9 @@ class Module extends Model{
     //获取新建独立主程序时的模块列表
     public static function getListProgram($program_id,$where,$limit=0,$orderby,$sort='DESC'){
         $model = self::with(['program_nodes'=>function($query) use ($program_id){
-                $query->where('program_id',$program_id);
+            $query->where('program_id',$program_id);
+        }])->with(['programs'=>function($query) use ($program_id){
+            $query->where('program_id',$program_id);
         }])->where($where)->where('is_delete','0')->orderBy($orderby,$sort);
 
         if(!empty($limit)){
