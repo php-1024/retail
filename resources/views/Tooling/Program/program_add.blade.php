4823db20b51fe58499196fb2b660f20c2a466f32
zeo <234521890@qq.com>
2017-12-26 17:28:28 +0800

    by zeo

--- resources/views/Tooling/Program/program_add.blade.php
+++ resources/views/Tooling/Program/program_add.blade.php
@@ -96,7 +96,7 @@
                                 <div class="form-group ">
                                     <div class="col-sm-4 col-sm-offset-5">
                                         <button class="btn btn-primary" id="addbtn" onclick="return postForm();" type="button">确认添加</button>&nbsp;&nbsp;
-                                        <button class="btn btn-write" onClick="location.href='node.html'" type="button">回到列表</button>
+                                        <button class="btn btn-write" onClick="location.href='{{  url('tooling/program/program_list') }}'" type="button">回到列表</button>
                                     </div>
                                 </div>
                             </form>
