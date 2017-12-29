d42d281c7f34cdb161ec41e084b1239e53e4fdf7
zeo <234521890@qq.com>
2017-12-29 17:03:14 +0800

    by zeo

--- resources/views/Tooling/Program/menu_add.blade.php
+++ resources/views/Tooling/Program/menu_add.blade.php
@@ -58,8 +58,38 @@
 
         <div class="modal-footer">
             <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
-            <button type="button" class="btn btn-primary">保存</button>
+            <button type="button" class="btn btn-primary" onclick="return postForm();">保存</button>
         </div>
     </div>
 </div>
 </form>
+<script>
+//提交表单
+function postForm() {
+    var target = $("#currentForm");
+    var url = target.attr("action");
+    var data = target.serialize();
+    $.post(url, data, function (json) {
+        if (json.status == -1) {
+            window.location.reload();
+        } else if(json.status == 1) {
+            swal({
+                title: "提示信息",
+                text: json.data,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+            },function(){
+                window.location.reload();
+            });
+        }else{
+            swal({
+                title: "提示信息",
+                text: json.data,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+                //type: "warning"
+            });
+        }
+    });
+}
+</script>
