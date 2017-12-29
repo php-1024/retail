fbd1fa01d230c08cb0bf8023f57ea8d39ba0586c
zeo <234521890@qq.com>
2017-12-29 16:38:32 +0800

    by zeo

---
+++ resources/views/Tooling/Program/menu_add.blade.php
@@ -0,0 +1,63 @@
+<div class="modal-dialog modal-lg">
+    <div class="modal-content animated fadeIn">
+        <div class="modal-header">
+            添加/修改菜单
+        </div>
+        <div class="modal-body">
+
+            <div class="form-group">
+                <label class="col-sm-2 control-label">菜单名称</label>
+                <div class="col-sm-10"><input type="text" class="form-control" value="" placeholder="菜单名称"></div>
+            </div>
+
+            <div style="clear:both"></div>
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">上级菜单</label>
+                <div class="col-sm-10">
+                    <select class="form-control m-b" name="account">
+                        <option>一级菜单</option>
+                        <option>系统管理</option>
+                        <option>人员管理</option>
+                    </select>
+                </div>
+            </div>
+            <div style="clear:both"></div>
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">根菜单（带链接）</label>
+                <div class="col-sm-10">
+                    <select class="form-control m-b" name="account">
+                        <option>是</option>
+                        <option>否</option>
+                    </select>
+                </div>
+            </div>
+            <div style="clear:both"></div>
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">ICON样式</label>
+                <div class="col-sm-10"><input type="text" class="form-control" value="" placeholder="菜单名称"></div>
+            </div>
+            <div style="clear:both"></div>
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">路由地址</label>
+                <div class="col-sm-10"><input type="text" class="form-control" value="" placeholder="路由地址"></div>
+            </div>
+            <div style="clear:both"></div>
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">关联路由列表</label>
+                <div class="col-sm-10"><input type="text" class="form-control" value="" placeholder="关联路由列表"></div>
+            </div>
+            <div style="clear:both"></div>
+            <div class="hr-line-dashed"></div>
+        </div>
+
+        <div class="modal-footer">
+            <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
+            <button type="button" class="btn btn-primary">保存</button>
+        </div>
+    </div>
+</div>
