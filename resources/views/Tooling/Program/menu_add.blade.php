be0e19454b363818e3d233ebd3a899f8af2e699f
zeo <234521890@qq.com>
2017-12-29 16:50:19 +0800

    by zeo

--- resources/views/Tooling/Program/menu_add.blade.php
+++ resources/views/Tooling/Program/menu_add.blade.php
@@ -10,7 +10,7 @@
 
             <div class="form-group">
                 <label class="col-sm-2 control-label">菜单名称</label>
-                <div class="col-sm-10"><input type="text" class="form-control" value="" placeholder="菜单名称"></div>
+                <div class="col-sm-10"><input type="text" class="form-control" name="menu_name" value="" placeholder="菜单名称"></div>
             </div>
 
             <div style="clear:both"></div>
@@ -18,10 +18,8 @@
             <div class="form-group">
                 <label class="col-sm-2 control-label">上级菜单</label>
                 <div class="col-sm-10">
-                    <select class="form-control m-b" name="account">
-                        <option>一级菜单</option>
-                        <option>系统管理</option>
-                        <option>人员管理</option>
+                    <select class="form-control m-b" name="parent_id">
+                        <option value="0">一级菜单</option>
                     </select>
                 </div>
             </div>
@@ -40,7 +38,7 @@
             <div class="hr-line-dashed"></div>
             <div class="form-group">
                 <label class="col-sm-2 control-label">ICON样式</label>
-                <div class="col-sm-10"><input type="text" class="form-control" value="" placeholder="菜单名称"></div>
+                <div class="col-sm-10"><input type="text" class="form-control" value="" placeholder="ICON样式"></div>
             </div>
             <div style="clear:both"></div>
             <div class="hr-line-dashed"></div>
