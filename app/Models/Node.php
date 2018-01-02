4b66993a15e220dced58107ee567d840e46c822a
zeo <234521890@qq.com>
2018-01-02 16:14:00 +0800

    by zeo

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -7,7 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 class Node extends Model{
-    use SoftDeletes;
+
     protected $table = 'node';
     protected $primaryKey = 'id';
     public $timestamps = true;
