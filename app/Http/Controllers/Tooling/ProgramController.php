6e76b3cdd0934096721a8a953d292e01a3e27e10
zeo <234521890@qq.com>
2018-01-02 18:50:47 +0800

    by zeo

--- app/Http/Controllers/Tooling/ProgramController.php
+++ app/Http/Controllers/Tooling/ProgramController.php
@@ -355,5 +355,21 @@ class ProgramController extends Controller{
         }
         return response()->json(['data' => '强制删除套餐成功', 'status' => '1']);
     }
+    //软删除模块
+    public function menu_delete(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+        $id = $request->input('id');//提交上来的ID
+        DB::beginTransaction();
+        try{
+            ProgramMenu::deleteMenu([['id',$id]]);
+            ToolingOperationLog::addOperationLog($admin_data['admin_id'],$route_name,'删除了套餐，ID为：'.$id);//保存操作记录
+            DB::commit();//提交事务
+        }catch (\Exception $e) {
+            DB::rollBack();//事件回滚
+            return response()->json(['data' => '删除套餐失败，请检查', 'status' => '0']);
+        }
+        return response()->json(['data' => '删除套餐成功', 'status' => '1']);
+    }
 }
 ?>
