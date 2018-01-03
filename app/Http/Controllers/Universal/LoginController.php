519c1d8b1d05c44dfd3472e792c9f8b670227ada
zeo <234521890@qq.com>
2018-01-03 16:40:13 +0800

    by zeo

--- app/Http/Controllers/Universal/LoginController.php
+++ app/Http/Controllers/Universal/LoginController.php
@@ -46,9 +46,7 @@ class LoginController extends Controller{
         $addr_arr = \IP2Attr::find($ip);//获取访问者地址
         $addr = $addr_arr[0].$addr_arr[1].$addr_arr[2].$addr_arr[3];//获取访问者地址
         $ip = ip2long($ip);//IP查询完地址后转化为整型。便于存储和查询
-
         $allowed_error_times = config("app.allowed_error_times");//允许登录错误次数
-
         $username = Request::input('username');//接收用户名
         $password = Request::input('password');//接收用户密码
         $key = config("app.zerone_encrypt_key");//获取加密盐
@@ -57,38 +55,9 @@ class LoginController extends Controller{
 
         //实例化错误记录表模型
         $error_log = ErrorLog::getOne([['ip',$ip]]);//查询该IP下的错误记录
-
-
         //如果没有错误记录 或 错误次数小于允许错误的最大次数 或 错误次数超出 但时间已经过了10分钟
         if(empty($error_log) || $error_log['error_time'] <  $allowed_error_times || (strtotime($error_log['error_time']) >= $allowed_error_times && time()-strtotime($error_log['updated_at']) >= 600)) {
-            $admininfo = Account::getOne([['account', $username]])->toArray();//根据账户查询用户信息
-            if (!empty($admininfo)) {//如果查询不到，则提示账号或密码错误
-                if ($encryptPwd != $admininfo['password']) {//查询密码是否对的上
-                    ErrorLog::addErrorTimes($ip);
-                    return response()->json(['data' => '登录账号或密码错误', 'status' => '0']);
-                } elseif($admininfo['status']=='0'){//查询账号状态
-                    ErrorLog::addErrorTimes($ip);
-                    return response()->json(['data' => '您的账号已被冻结', 'status' => '0']);
-                }else{
-                    ErrorLog::clearErrorTimes($ip);//清除掉错误记录
-                    //插入登录记录
-                    if(LoginLog::addLoginLog($admininfo['id'],$ip,$addr)) {
-                        Session::put('tooling_account_id',encrypt($admininfo['id']));//存储登录session_id为当前用户ID
-                        //构造用户缓存数据
-                        $admin_data = ['admin_id'=>$admininfo['id'],'admin_account'=>$admininfo['account'],'admin_is_super'=>$admininfo['is_super'],'admin_login_ip'=>$ip,'admin_login_position'=>$addr,'admin_login_time'=>time()];
-                        $admin_data = serialize($admin_data);
-                        Redis::connection('zeo');//连接到我的redis服务器
-                        $data_key = 'tooling_system_admin_data_'.$admininfo['id'];
-                        Redis::set($data_key,$admin_data);
-                        return response()->json(['data' => '登录成功', 'status' => '1']);
-                    }else{
-                        return response()->json(['data' => '登录失败', 'status' => '0']);
-                    }
-                }
-            } else {
-                ErrorLog::addErrorTimes($ip);
-                return response()->json(['data' => '登录账号或密码错误', 'status' => '0']);
-            }
+
         }else{
             return response()->json(['data' => '您短时间内错误的次数超过'.$allowed_error_times.'次，请稍候再尝试登陆 ','status' => '0']);
         }
