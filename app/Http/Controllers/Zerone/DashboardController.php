f7140adc8b910b787fe89e98ee9d5e23be019d7d
zeo <234521890@qq.com>
2018-01-03 14:46:58 +0800

    by zeo

---
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *新版本登陆界面
+ *
+ **/
+namespace App\Http\Controllers\Universal;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request;
+use Gregwar\Captcha\CaptchaBuilder;
+use App\Models\ToolingAccount;
+use App\Models\ToolingErrorLog;
+use App\Models\ToolingLoginLog;
+use Session;
+use Illuminate\Support\Facades\Redis;
+
+class DashboardController extends Controller{
+    /*
+     * 登陆页面
+     */
+    public function display()
+    {
+        echo "这里是零壹管理平台";
+        return view('Universal/Login/zerone_display');
+    }
+}
+?>
