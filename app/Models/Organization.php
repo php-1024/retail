9082ca2a95a8f46c1a890a948ea72661cc000d49
zeo <234521890@qq.com>
2018-01-03 11:00:01 +0800

    by zeo

---
+++ app/Models/Organization.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * module_node表的模型
+ *
+ */
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Organization extends Model{
+    use SoftDeletes;
+    protected $table = 'organization';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
+}
+?>
