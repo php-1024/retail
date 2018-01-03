eab15be4be4f71c964e7bc53b02f02e7f338c13a
kbzz <463661663@qq.com>
2018-01-03 17:25:41 +0800

    1

--- app/Http/Controllers/Tooling/ProgramController.php
+++ app/Http/Controllers/Tooling/ProgramController.php
@@ -74,7 +74,6 @@ class ProgramController extends Controller{
                 ToolingOperationLog::addOperationLog($admin_data['admin_id'],$route_name,'添加了程序'.$program_name);//保存操作记录
                 DB::commit();//提交事务
             }catch (\Exception $e) {
-                dump($e);
                 DB::rollBack();//事件回滚
                 return response()->json(['data' => '添加程序失败，请检查', 'status' => '0']);
             }
