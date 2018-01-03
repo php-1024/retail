10a3e3c3afa3c7505a28ff0618af6b5ab25a7143
追梦小窝 <mail@54zm.com>
2018-01-03 17:42:19 +0800

    111111111

--- app/Http/Controllers/Study/SqlBasicController.php
+++ app/Http/Controllers/Study/SqlBasicController.php
@@ -14,7 +14,7 @@ class SqlBasicController extends Controller{
     }
     //Laravel数据库基本用法查询
     public function selectDb(){
-        $list = DB::connection('study')->select("select * from study_test where id > :id",['id'=2]);
+        $list = DB::connection('study')->select("select * from study_test where id = :id",['id'=>2]);
         dump($list);
     }
     //Laravel数据库基本用法更新
