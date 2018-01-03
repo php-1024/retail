45c02a4e3c912e11e0e35542694da82f10befc35
zeo <234521890@qq.com>
2018-01-03 18:03:06 +0800

    by zeo

--- routes/web.php
+++ routes/web.php
@@ -163,6 +163,8 @@ Route::group(['prefix'=>'tooling'],function(){
 /**********************零壹管理系统*********************/
 Route::group(['prefix'=>'zerone'],function(){
     Route::get('/', 'Zerone\DashboardController@display')->middleware('ZeroneCheck');//系统首页
+    Route::get('quit','Zerone\DashboardController@quit');//退出系统
+
     //登陆页面组
     Route::group(['prefix'=>'login'],function(){
         Route::get('/', 'Zerone\LoginController@display')->middleware('ZeroneCheck');//登陆页面路由
