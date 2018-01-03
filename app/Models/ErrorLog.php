e85574e4a03b6b32a78eb618c75679f2a91f7d81
zeo <234521890@qq.com>
2018-01-03 15:51:17 +0800

    by zeo

--- app/Models/ErrorLog.php
+++ app/Models/ErrorLog.php
@@ -20,14 +20,13 @@ class ErrorLog extends Model{
 
     //根据用户IP增加错误次数
     public static function addErrorTimes($ip){
-        $error_log = self::where('ip',$ip)->first();//获取该IP的错误记录
-        if(empty($error_log)){//没有错误记录，插入错误记录，有错误记录，更新错误记录
-            $error_log = new ToolingErrorLog();//没有记录，新建一个模型，然后保存
-            $error_log->ip = $ip;
-            $error_log->error_time = 1;
+        if($error_log = self::where('ip',$ip)->first()){//没有错误记录，插入错误记录，有错误记录，更新错误记录
+            $error_log->error_time = $error_log->error_time+1;
             $error_log->save();
         }else{
-            $error_log->error_time = $error_log->error_time+1;
+            $error_log = new ErrorLog();//没有记录，新建一个模型，然后保存
+            $error_log->ip = $ip;
+            $error_log->error_time = 1;
             $error_log->save();
         }
     }
