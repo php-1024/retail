bbf13ceaa1202b6f3e87fd8fe478a228e33ad493
追梦小窝 <mail@54zm.com>
2018-01-04 11:13:42 +0800

    111111111

--- app/Http/Middleware/Tooling/ToolingCheck.php
+++ app/Http/Middleware/Tooling/ToolingCheck.php
@@ -25,7 +25,7 @@ class ToolingCheck{
             case "tooling/dashboard/account_list"://账号列表
             case "tooling/dashboard/account_add"://添加账号
                 $re = $this->checkLoginAndSuper($request);//判断是否登陆和是否超级管理员
-                dump($route_name);
+                dump($request);
                 return self::format_response($re,$next);
             break;
 
