070639c53e5bd4c63d75c733e0337377a822cf2f
zeo <234521890@qq.com>
2018-01-04 09:57:32 +0800

    by zeo

--- app/Http/Controllers/Tooling/ProgramController.php
+++ app/Http/Controllers/Tooling/ProgramController.php
@@ -205,8 +205,6 @@ class ProgramController extends Controller{
         $id = $request->input('id');
         $info = ProgramMenu::find($id);
         $list = ProgramMenu::getList([[ 'parent_id',0],['program_id',$info->program_id]],0,'id','asc');
-        dump($list);
-        exit();
         return view('Tooling/Program/menu_edit',['list'=>$list,'info'=>$info,'action_name'=>'program']);
     }
     //编辑菜单数据检测
