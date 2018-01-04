b2176713cae041beeea40bf6f2a4814277de61ae
追梦小窝 <mail@54zm.com>
2018-01-04 15:44:40 +0800

    111111111

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -15,6 +15,7 @@ class DashboardController extends Controller{
      */
     public function display(Request $request)
     {
+        dump($request);
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
