a7d94c6313fb7c890d26108484c0bc7aaac067ef
zeo <234521890@qq.com>
2018-01-04 11:30:31 +0800

    by zeo

--- resources/views/Zerone/Public/Header.blade.php
+++ resources/views/Zerone/Public/Header.blade.php
@@ -7,10 +7,10 @@
             <span class="m-r-sm text-muted welcome-message">
                 欢迎登录！
                 <strong>
-                    @if ($admin_data['admin_is_super']==1)
-                        超级管理员-{{ $admin_data['admin_account'] }}
+                    @if ($admin_data['is_super']==1)
+                        超级管理员-{{ $admin_data['account'] }}
                     @else
-                        管理员-{{ $admin_data['admin_account'] }}
+                        管理员-{{ $admin_data['account'] }}
                     @endif
                 </strong>
             </span>
