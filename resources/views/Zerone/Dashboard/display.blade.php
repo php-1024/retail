d496de62711bb81fb52ba130d2c9f8c6c9a8196f
zeo <234521890@qq.com>
2018-01-04 11:21:32 +0800

    by zeo

--- resources/views/Zerone/Dashboard/display.blade.php
+++ resources/views/Zerone/Dashboard/display.blade.php
@@ -8,11 +8,11 @@
 
     <title>零壹新科技程序管理平台</title>
 
-    <link href="{{asset('public/Tooling/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{asset('public/Tooling/library/font')}}/css/font-awesome.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/font')}}/css/font-awesome.css" rel="stylesheet">
 
-    <link href="{{asset('public/Tooling')}}/css/animate.css" rel="stylesheet">
-    <link href="{{asset('public/Tooling')}}/css/style.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone')}}/css/animate.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone')}}/css/style.css" rel="stylesheet">
 
 </head>
 
@@ -45,14 +45,14 @@
 </div>
 
 <!-- Mainly scripts -->
-<script src="{{asset('public/Tooling/library/jquery')}}/js/jquery-2.1.1.js"></script>
-<script src="{{asset('public/Tooling/library/bootstrap')}}/js/bootstrap.min.js"></script>
-<script src="{{asset('public/Tooling/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
-<script src="{{asset('public/Tooling/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
+<script src="{{asset('public/Zerone/library/bootstrap')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Zerone/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
+<script src="{{asset('public/Zerone/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
 
 <!-- Custom and plugin javascript -->
-<script src="{{asset('public/Tooling')}}/js/inspinia.js"></script>
-<script src="{{asset('public/Tooling/library/pace')}}/js/pace.min.js"></script>
+<script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
+<script src="{{asset('public/Zerone/library/pace')}}/js/pace.min.js"></script>
 
 </body>
 
