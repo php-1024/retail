2a72c3f3d7e09027c520a0fdcbffa24820ac7a0a
ismzxw <mail@54zm.com>
2018-01-05 22:39:44 +0800

    test

--- app/Http/Controllers/Study/OrmStudyController.php
+++ app/Http/Controllers/Study/OrmStudyController.php
@@ -11,13 +11,11 @@ use App\Models\Study\City;
 use App\Models\Study\Area;
 
 use Illuminate\Http\Request;
-use App\Models\Node;
-use App\Models\Module;
-use App\Models\Program;
 
 class OrmStudyController extends Controller{
     //调用model里的all方法获取所有数据
-    public function getAll(){
+    public function getAll(Request $request){
+        dump($request);
         $list = Test::get_all();
         dump($list);
     }
