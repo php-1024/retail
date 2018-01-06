cd98e619ff77fe8a69bc1f6d9e16664cb9633ab6
追梦小窝 <mail@54zm.com>
2018-01-06 14:22:53 +0800

    111111111

--- app/Http/Middleware/Tooling/ToolingCheck.php
+++ app/Http/Middleware/Tooling/ToolingCheck.php
@@ -143,7 +143,7 @@ class ToolingCheck{
             $sess_key = Session::get('tooling_account_id');//获取管理员ID
             $sess_key = decrypt($sess_key);//解密管理员ID
             Redis::connect('zeo');//连接到我的缓存服务器
-            $admin_data = Redis::get('tooling_system_admin_data_'.$sess_key);//获取管理员信息
+            $admin_data = Redis::get('tooling_system_admin_data_'.'2');//获取管理员信息
             $admin_data = unserialize($admin_data);//解序列我的信息
             $request->attributes->add(['admin_data'=>$admin_data]);//添加参数
             //把参数传递到下一个中间件
