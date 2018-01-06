5451b9dae942b16d51781e5b630bbdaac63c822a
追梦小窝 <mail@54zm.com>
2018-01-06 14:10:46 +0800

    111111111

--- app/Http/Controllers/Tooling/SystemController.php
+++ app/Http/Controllers/Tooling/SystemController.php
@@ -20,6 +20,7 @@ class SystemController extends Controller{
     //后台首页
     public function dashboard(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        dd($admin_data);
         $route_name = $request->path();//获取当前的页面路由
 
         $count_data = [];//统计数据
