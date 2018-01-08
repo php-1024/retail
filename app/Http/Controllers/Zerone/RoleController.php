de493772d040386a3b6914df8b92a770cbb1c81e
zeo <234521890@qq.com>
2018-01-08 16:36:36 +0800

    by zeo

--- app/Http/Controllers/Zerone/RoleController.php
+++ app/Http/Controllers/Zerone/RoleController.php
@@ -13,6 +13,9 @@ class RoleController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $module_node_list = Module::getListProgram(1,[],0,'id');
+        foreach($module_node_list as $key=>$val){
+            dump($val->program_nodes);
+        }
         return view('Zerone/Role/role_add',['module_node_list'=>$module_node_list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
