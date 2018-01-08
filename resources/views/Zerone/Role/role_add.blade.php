84fa87ac28a00ab30de096522f8bef4bb4025a20
zeo <234521890@qq.com>
2018-01-08 16:39:08 +0800

    by zeo

--- resources/views/Zerone/Role/role_add.blade.php
+++ resources/views/Zerone/Role/role_add.blade.php
@@ -60,7 +60,7 @@
                                 </div>
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label" >程序功能模块</label>
+                                    <label class="col-sm-2 control-label" >角色权限</label>
                                     <div class="col-sm-10">
                                         @foreach($module_node_list as $key=>$val)
                                             <group class="checked_box_group_{{ $val->id }}">
@@ -72,7 +72,7 @@
                                                 <div>
                                                     @foreach($val->program_nodes as $kk=>$vv)
                                                         <label class="checkbox-inline i-checks">
-                                                            <input type="checkbox"  data-group_id="{{ $val->id }}" class="checkbox_node_name checkbox_node_name_{{ $val->id }}" name="module_node_ids[]" value="{{ $val->id.'_'.$vv->id }}"> {{$vv->node_name}}
+                                                            <input type="checkbox"  data-group_id="{{ $val->id }}" class="checkbox_node_name checkbox_node_name_{{ $val->id }}" name="module_node_ids[]" value="{{ $vv->id }}"> {{$vv->node_name}}
                                                         </label>
                                                     @endforeach
                                                 </div>
