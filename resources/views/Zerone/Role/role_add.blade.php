a03dfd01b5be30f477ae4ec5e0de69be3bb6bb79
zeo <234521890@qq.com>
2018-01-08 16:29:24 +0800

    by zeo

--- resources/views/Zerone/Role/role_add.blade.php
+++ resources/views/Zerone/Role/role_add.blade.php
@@ -59,29 +59,29 @@
 
                                 </div>
                                 <div class="hr-line-dashed"></div>
-
-                                <label class="col-sm-2 control-label" >程序功能模块</label>
-                                <div class="col-sm-10">
-                                    @foreach($module_node_list as $key=>$val)
-                                        <group class="checked_box_group_{{ $val->id }}">
-                                            <div>
-                                                <label class="i-checks">
-                                                    <input type="checkbox" class="checkbox_module_name checkbox_module_name_{{ $val->id }}" name="module_id[]" value="{{ $val->id }}"> {{ $val->module_name }}
-                                                </label>
-                                            </div>
-                                            <div>
-                                                @if(!empty($pid)) <? $val->nodes = $val->program_nodes ?>@endif
-                                                @foreach($val->nodes as $kk=>$vv)
-                                                    <label class="checkbox-inline i-checks">
-                                                        <input type="checkbox"  data-group_id="{{ $val->id }}" class="checkbox_node_name checkbox_node_name_{{ $val->id }}" name="module_node_ids[]" value="{{ $val->id.'_'.$vv->id }}"> {{$vv->node_name}}
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label" >程序功能模块</label>
+                                    <div class="col-sm-10">
+                                        @foreach($module_node_list as $key=>$val)
+                                            <group class="checked_box_group_{{ $val->id }}">
+                                                <div>
+                                                    <label class="i-checks">
+                                                        <input type="checkbox" class="checkbox_module_name checkbox_module_name_{{ $val->id }}" name="module_id[]" value="{{ $val->id }}"> {{ $val->module_name }}
                                                     </label>
-                                                @endforeach
-                                            </div>
-                                        </group>
-                                        <div class="hr-line-dashed" style="clear: both;"></div>
-                                    @endforeach
+                                                </div>
+                                                <div>
+                                                    @if(!empty($pid)) <? $val->nodes = $val->program_nodes ?>@endif
+                                                    @foreach($val->nodes as $kk=>$vv)
+                                                        <label class="checkbox-inline i-checks">
+                                                            <input type="checkbox"  data-group_id="{{ $val->id }}" class="checkbox_node_name checkbox_node_name_{{ $val->id }}" name="module_node_ids[]" value="{{ $val->id.'_'.$vv->id }}"> {{$vv->node_name}}
+                                                        </label>
+                                                    @endforeach
+                                                </div>
+                                            </group>
+                                            <div class="hr-line-dashed" style="clear: both;"></div>
+                                        @endforeach
+                                    </div>
                                 </div>
-
 
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group">
