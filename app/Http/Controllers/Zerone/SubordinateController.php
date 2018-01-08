07a0091554125276b3f73b24ee14b24c82c2597d
zeo <234521890@qq.com>
2018-01-08 15:09:55 +0800

    by zeo

--- app/Http/Controllers/Zerone/SubordinateController.php
+++ app/Http/Controllers/Zerone/SubordinateController.php
@@ -49,7 +49,11 @@ class SubordinateController extends Controller{
 
     //下级人员结构
     public function subordinate_structure(Request $request){
-        echo "这里是下级人员结构";
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+        return view('Zerone/Subordinate/subordinate_structure',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 }
 ?>
