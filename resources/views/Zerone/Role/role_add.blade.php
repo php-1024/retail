3e789cca98cd10cf0b05de148e3de07c19014929
zeo <234521890@qq.com>
2018-01-08 15:57:18 +0800

    by zeo

--- resources/views/Zerone/Role/role_add.blade.php
+++ resources/views/Zerone/Role/role_add.blade.php
@@ -160,6 +160,12 @@
                                     </div>
 
                                 </div>
+                                <div class="hr-line-dashed"></div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">安全密码</label>
+                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
+
+                                </div>
 
 
                                 <div class="form-group">
