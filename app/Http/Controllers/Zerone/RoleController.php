d6d7f4d56355ebb70e66912e31a5382b456dc4d6
zeo <234521890@qq.com>
2018-01-08 10:23:08 +0800

    by zeo

--- app/Http/Controllers/Zerone/RoleController.php
+++ app/Http/Controllers/Zerone/RoleController.php
@@ -7,7 +7,11 @@ use Session;
 class RoleController extends Controller{
     //添加权限角色
     public function role_add(Request $request){
-        echo "这是添加权限角色";
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+        return view('Zerone/Role/role_add',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
     //提交添加权限假设数据
