f6cc294cc4794ad32a667f00e3e5f562de0ae62c
zeo <234521890@qq.com>
2018-01-08 15:44:47 +0800

    by zeo

--- resources/views/Zerone/Role/role_add.blade.php
+++ resources/views/Zerone/Role/role_add.blade.php
@@ -38,7 +38,141 @@
             </div>
 
         </div>
+        <div class="wrapper wrapper-content animated fadeInRight ecommerce">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ibox">
+                        <div class="ibox-title">
+                            <h5>添加权限角色</h5>
 
+                        </div>
+                        <div class="ibox-content">
+                            <p>
+                                权限角色主要用于区分用户职责以及为用户快速分配权限
+                            </p>
+                            <div class="hr-line-dashed"></div>
+                            <form method="get" class="form-horizontal">
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">角色名称</label>
+                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
+
+                                </div>
+                                <div class="hr-line-dashed"></div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">角色权限</label>
+                                    <div class="col-sm-10">
+                                        <div>
+                                            <label class="i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单模块
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单编辑
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option2" id="inlineCheckbox2"> 订单查询
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单添加
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单删除
+                                            </label>
+                                        </div>
+
+                                        <div class="hr-line-dashed"></div>
+                                        <div>
+                                            <label class="i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单模块
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单编辑
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option2" id="inlineCheckbox2"> 订单查询
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单添加
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单删除
+                                            </label>
+                                        </div>
+                                        <div class="hr-line-dashed"></div>
+
+                                        <div>
+                                            <label class="i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单模块
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单编辑
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option2" id="inlineCheckbox2"> 订单查询
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单添加
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单删除
+                                            </label>
+                                        </div>
+                                        <div class="hr-line-dashed"></div>
+
+                                        <div>
+                                            <label class="i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单模块
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option1" id="inlineCheckbox1"> 订单编辑
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option2" id="inlineCheckbox2"> 订单查询
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单添加
+                                            </label>
+
+                                            <label class="checkbox-inline i-checks">
+                                                <input type="checkbox" value="option3" id="inlineCheckbox3"> 订单删除
+                                            </label>
+                                        </div>
+                                        <div class="hr-line-dashed"></div>
+                                    </div>
+
+                                </div>
+
+
+                                <div class="form-group">
+                                    <div class="col-sm-4 col-sm-offset-5">
+                                        <button class="btn btn-primary" id="addbtn" type="button">确认添加</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
 
     </div>
 
