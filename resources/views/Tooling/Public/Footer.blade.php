15f0f2b5d04d411f9ae00ba23ada5b9d8f2bbfe9
zeo <234521890@qq.com>
2018-01-09 17:58:01 +0800

    by zeo

--- resources/views/Tooling/Public/Footer.blade.php
+++ resources/views/Tooling/Public/Footer.blade.php
@@ -3,6 +3,6 @@
         您本次登陆信息 ：{{ date('Y-m-d H:i:s',$admin_data['admin_login_time']) }}&nbsp;&nbsp;{{ $admin_data['admin_login_position'] }}
     </div>
     <div>
-        <strong>Copyright</strong> 零壹新科技（深圳有限公司）&copy; 2017-2027
+        <strong>Copyright</strong> 零壹新科技（深圳）有限公司&copy; 2017-2027
     </div>
 </div>
