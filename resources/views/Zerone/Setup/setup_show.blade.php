8f3ae7b77d8b56b5d15457c2fbc5316842f9eaad
追梦小窝 <mail@54zm.com>
2018-01-09 19:51:52 +0800

    by iszmxw

--- resources/views/Zerone/Setup/setup_show.blade.php
+++ resources/views/Zerone/Setup/setup_show.blade.php
@@ -18,18 +18,18 @@
 
 <body class="">
 
-<div id="wrapper">
+{{--<div id="wrapper">--}}
 
     {{--@include('Zerone/Public/Nav')--}}
 
-    <div id="page-wrapper" class="gray-bg">
-        @include('Zerone/Public/Header')
+    {{--<div id="page-wrapper" class="gray-bg">--}}
+        {{--@include('Zerone/Public/Header')--}}
 
 
 
-        @include('Zerone/Public/Footer')
-        </div>
-    </div>
+        {{--@include('Zerone/Public/Footer')--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
     <!-- Mainly scripts -->
     <script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
