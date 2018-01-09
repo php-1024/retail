fa74003d4e1f93872b826e09cf26cbb5f6614e77
追梦小窝 <mail@54zm.com>
2018-01-09 16:08:53 +0800

    by iszmxw

--- resources/views/Zerone/Dashboard/display.blade.php
+++ resources/views/Zerone/Dashboard/display.blade.php
@@ -186,7 +186,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($login_log_list as $key=>$val)
+                                @foreach($login_log_list as $val)
                                 <tr>
                                     <td>{{  $val->accounts->account }}</td>
                                     <td>{{  long2ip($val->ip) }}</td>
@@ -215,14 +215,20 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($operation_log_list as $key=>$val)
-                                    <tr>
-                                        <td>{{ $val->accounts->account }}</td>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>修改了登陆密码</td>
+                                    <td>2017-11-24 10:41:20</td>
+                                </tr>
 
-                                        <td>{{ $val->operation_info }}</td>
-                                        <td>{{ $val->created_at }}</td>
-                                    </tr>
-                                @endforeach
+                                {{--@foreach($operation_log_list as $key=>$val)--}}
+                                    {{--<tr>--}}
+                                        {{--<td>{{ $val->accounts->account }}</td>--}}
+
+                                        {{--<td>{{ $val->operation_info }}</td>--}}
+                                        {{--<td>{{ $val->created_at }}</td>--}}
+                                    {{--</tr>--}}
+                                {{--@endforeach--}}
                                 </tbody>
                             </table>
                         </div>
