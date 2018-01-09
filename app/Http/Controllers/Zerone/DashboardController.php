187b05ea8cdfbc49ed58501e9bbaeb9bd1987e44
ismzxw <mail@54zm.com>
2018-01-09 21:00:46 +0800

    1

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -18,6 +18,7 @@ class DashboardController extends Controller{
      */
     public function display(Request $request)
     {
+        dd($request);
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
