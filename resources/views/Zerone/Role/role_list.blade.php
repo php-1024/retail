d05c71421823f48a5e2c64b57da9deb0597ebe41
zeo <234521890@qq.com>
2018-01-09 17:21:21 +0800

    by zeo

--- resources/views/Zerone/Role/role_list.blade.php
+++ resources/views/Zerone/Role/role_list.blade.php
@@ -86,7 +86,7 @@
                                 @foreach($list as $key=>$val)
                                     <tr>
                                         <td>{{ $val->id }}</td>
-                                        <td>{{ $val->id }}</td>
+                                        <td>{{ $val->create_account->account }}</td>
                                         <td>{{ $val->role_name }}</td>
                                         <td>
                                             <label class="label label-primary" data-container="body" data-toggle="popover" data-placement="top" data-content="订单查询，订单编辑，订单添加，订单删除" style="display:inline-block">订单模块</label>&nbsp;&
