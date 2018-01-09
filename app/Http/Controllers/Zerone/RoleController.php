1633cd12f2288e80846a2c06a4a3632bcbf06c38
zeo <234521890@qq.com>
2018-01-09 17:47:53 +0800

    by zeo

--- app/Http/Controllers/Zerone/RoleController.php
+++ app/Http/Controllers/Zerone/RoleController.php
@@ -60,7 +60,7 @@ class RoleController extends Controller{
         $role_modules = [];
         $role_nodes = [] ;
         foreach($list as $key=>$val){
-            dump($val->nodes);
+            dump($val->nodes->program_modules);
         }
         return view('Zerone/Role/role_list',['list'=>$list,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
