f6fd3dadf8cb9046ede97a2a469406a867095370
追梦小窝 <mail@54zm.com>
2018-01-09 19:38:12 +0800

    by iszmxw

--- app/Http/Controllers/Zerone/SetupController.php
+++ app/Http/Controllers/Zerone/SetupController.php
@@ -13,7 +13,7 @@ use Session;
 class SetupController extends Controller{
     //参数设置展示
     public function setup_show(Request $request){
-        echo "这里参数设置展示页面！";
+        return view('Zerone/Setup/setup_show');
     }
     //参数设置编辑
     public function setup_edit(Request $request){
