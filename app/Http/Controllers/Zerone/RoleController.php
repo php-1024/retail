1eae947ea6d01a4c469c6f26d6fd7da60c4b4738
zeo <234521890@qq.com>
2018-01-09 18:32:03 +0800

    by zeo

--- app/Http/Controllers/Zerone/RoleController.php
+++ app/Http/Controllers/Zerone/RoleController.php
@@ -69,7 +69,7 @@ class RoleController extends Controller{
                     $program_module_nodes[$val->module_name][] = $v->node_name;
                 }
         }
-
+        dump($program_module_nodes);
 
         //获取零壹管理程序的所有模块及节点并组成数组。
         return view('Zerone/Role/role_list',['list'=>$list,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
