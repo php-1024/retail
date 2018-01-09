f93423a8e31d6258692bd6ed2bc9349549576132
zeo <234521890@qq.com>
2018-01-09 19:55:24 +0800

    by zeo

--- resources/views/Zerone/Role/role_list.blade.php
+++ resources/views/Zerone/Role/role_list.blade.php
@@ -89,9 +89,9 @@
                                         <td>{{ $val->create_account->account }}</td>
                                         <td>{{ $val->role_name }}</td>
                                         <td>
-                                            @foreach($role_module_nodes as $key=>$val)
-                                                {{ dump($val) }}
+                                            @foreach($role_module_nodes as $k=>$v)
 
+                                            <label class="label label-primary" data-container="body" data-toggle="popover" data-placement="top" data-content="@foreach($v as $kk=>$vv){{$vv}}@endforeach" style="display:inline-block">{{$key}}</label>&nbsp;&nbsp;
                                             @endforeach
                                         </td>
                                         <td>{{ $val->created_at }}</td>
