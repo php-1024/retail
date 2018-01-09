79c2bf9d7089333ef96a2656b3409b8b9450da97
追梦小窝 <mail@54zm.com>
2018-01-09 14:42:22 +0800

    by iszmxw

--- app/Models/Statistics.php
+++ app/Models/Statistics.php
@@ -10,7 +10,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-class Account extends Model
+class Statistics extends Model
 {
     use SoftDeletes;
     protected $table = 'statistics';
