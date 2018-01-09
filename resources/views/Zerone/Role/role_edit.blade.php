a861d1aa9641f1e3cd85cf61ac53e685f8c8016d
zeo <234521890@qq.com>
2018-01-09 23:32:47 +0800

    by zeo

--- resources/views/Zerone/Role/role_edit.blade.php
+++ resources/views/Zerone/Role/role_edit.blade.php
@@ -28,7 +28,7 @@
                                 <div>
                                     @foreach($val->program_nodes as $kk=>$vv)
                                         <label class="checkbox-inline i-checks">
-                                            <input type="checkbox"  data-group_id="{{ $val->id }}" class="checkbox_node_name checkbox_node_name_{{ $val->id }}" name="module_node_ids[]" value="{{ $vv->id }}"> {{$vv->node_name}}
+                                            <input type="checkbox" @if(in_array($val->id,$selected_nodes)) checked="checked" @endif  data-group_id="{{ $val->id }}" class="checkbox_node_name checkbox_node_name_{{ $val->id }}" name="module_node_ids[]" value="{{ $vv->id }}"> {{$vv->node_name}}
                                         </label>
                                     @endforeach
                                 </div>
