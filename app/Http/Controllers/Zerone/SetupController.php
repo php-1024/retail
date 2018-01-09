18f4cae685686c89f4ee39ca1499f844efc7b5f0
追梦小窝 <mail@54zm.com>
2018-01-09 19:40:54 +0800

    by iszmxw

--- app/Http/Controllers/Zerone/SetupController.php
+++ app/Http/Controllers/Zerone/SetupController.php
@@ -13,7 +13,15 @@ use Session;
 class SetupController extends Controller{
     //参数设置展示
     public function setup_show(Request $request){
-        return view('Zerone/Setup/setup_show');
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+
+        $role_name = $request->input('role_name');
+        $search_data = ['role_name'=>$role_name];
+        
+        return view('Zerone/Setup/setup_show',['list'=>$list,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //参数设置编辑
     public function setup_edit(Request $request){
