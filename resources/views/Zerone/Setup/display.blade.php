8e37df5fac238fb2282577bc01fc135116fbe4e8
追梦小窝 <mail@54zm.com>
2018-01-10 10:20:26 +0800

    2018年01月10日

---
+++ resources/views/Zerone/Setup/display.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>零壹新科技程序管理平台</title>
+
+    <link href="{{asset('public/Zerone/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/font')}}/css/font-awesome.css" rel="stylesheet">
+
+    <link href="{{asset('public/Zerone')}}/css/animate.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone')}}/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="">
+
+<div id="wrapper">
+
+    @include('Zerone/Public/Nav')
+
+    <div id="page-wrapper" class="gray-bg">
+        @include('Zerone/Public/Header')
+
+
+        <div class="wrapper wrapper-content animated fadeInRight ecommerce">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>系统参数设置</h5>
+
+                        </div>
+                        <div class="ibox-content">
+                            <form method="get" class="form-horizontal">
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label" >服务商通道链接</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control"  value="http://o2o.01nnt.com/123456.html"/>
+                                    </div>
+                                </div>
+                                <div class="hr-line-dashed"></div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label" >是否开启服务商注册通道</label>
+                                    <div class="col-sm-10">
+                                        <input type="checkbox" class="js-switch" checked  value="1"/>
+                                    </div>
+                                </div>
+
+                                <div class="hr-line-dashed"></div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label" >商户通道链接</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control"  value="http://o2o.01nnt.com/123456.html"/>
+                                    </div>
+                                </div>
+                                <div class="hr-line-dashed"></div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label" >是否开启商户注册通道</label>
+                                    <div class="col-sm-10">
+                                        <input type="checkbox" class="js-switch2" checked  value="1"/>
+                                    </div>
+                                </div>
+
+                                <div class="hr-line-dashed"></div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">零壹人员构深度设置</label>
+                                    <div class="col-sm-1"><input type="text" class="form-control"></div>
+                                </div>
+
+                                <div class="hr-line-dashed"></div>
+
+                                <div class="form-group ">
+                                    <div class="col-sm-4 col-sm-offset-5">
+                                        <button class="btn btn-primary" id="addbtn" type="button">确认修改</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+        @include('Zerone/Public/Footer')
+    </div>
+</div>
+
+    <!-- Mainly scripts -->
+    <script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
+    <script src="{{asset('public/Zerone/library/bootstrap')}}/js/bootstrap.min.js"></script>
+    <script src="{{asset('public/Zerone/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
+    <script src="{{asset('public/Zerone/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
+    <script src="{{asset('public/Zerone/library/pace')}}/js/pace.min.js"></script>
+
+</body>
+
+</html>
