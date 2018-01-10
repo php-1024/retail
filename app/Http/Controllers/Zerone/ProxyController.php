520a0da3277663c543a67364f39b6d89ba82b094
kbzz <463661663@qq.com>
2018-01-10 15:55:29 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers\Zerone;
 use App\Http\Controllers\Controller;
 use App\Models\Study\Test;
-use App\Models\Study\Warzone;
+use App\Models\Warzone;
 use Illuminate\Http\Request;
 use Session;
 class ProxyController extends Controller{
