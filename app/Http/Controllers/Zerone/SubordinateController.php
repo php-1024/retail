6510e825bcffcc9bec087200c88eede3c951caff
kbzz <463661663@qq.com>
2018-01-10 12:16:59 +0800

    1

--- app/Http/Controllers/Zerone/SubordinateController.php
+++ app/Http/Controllers/Zerone/SubordinateController.php
@@ -50,6 +50,7 @@ class SubordinateController extends Controller{
     //下级人员结构
     public function subordinate_structure(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        dd($admin_data);
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
