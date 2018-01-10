09a550ffebeb1bc53d196c888212839f838d6468
kbzz <463661663@qq.com>
2018-01-10 15:43:51 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\Zerone;
 use App\Http\Controllers\Controller;
-use App\Models\Warzone;
+use App\Models\Account;
 use Illuminate\Http\Request;
 use Session;
 class ProxyController extends Controller{
@@ -11,7 +11,7 @@ class ProxyController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $aa=Warzone::get_all();
+        $aa=Account::get_all();
         dd($aa);
         return view('Zerone/Proxy/proxy_add',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
