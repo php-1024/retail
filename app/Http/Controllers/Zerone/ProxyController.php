046852e17be23efcae641edbe49caebc59210614
kbzz <463661663@qq.com>
2018-01-10 19:59:19 +0800

    '123'

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -17,7 +17,8 @@ class ProxyController extends Controller{
     }
     //提交服务商数据
     public function proxy_add_check(Request $request){
-
+        echo 123;
+        exit();
         $where = [['proxy_name',$request->input('proxy_name')]];
 
         $name = ProxyApply::getPluck($where,'proxy_name');
