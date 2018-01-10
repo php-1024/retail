6730e98c028c12305e82b2c6c82f750c869aae79
zeo <234521890@qq.com>
2018-01-10 17:58:57 +0800

    by zeo

--- app/Http/Controllers/Zerone/SubordinateController.php
+++ app/Http/Controllers/Zerone/SubordinateController.php
@@ -14,8 +14,14 @@ class SubordinateController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
         //获取当前用户添加的权限角色
         $role_list = OrganizationRole::getList([['program_id',1],['created_by',$admin_data['id']]],0,'id');
-        $module_node_list = Module::getListProgram(1,[],0,'id');//获取当前系统的所有模块和节点
+        $module_node_list = $this->getProgramModuleNode();
         return view('Zerone/Subordinate/subordinate_add',['role_list'=>$role_list,'module_node_list'=>$module_node_list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+    }
+
+    //获取当前程序的功能模块和节点
+    public function getModuleNode($account_id,$tag=true){
+        $module_node_list = Module::getListProgram(1,[],0,'id');//获取当前系统的所有模块和节点
+        return $module_node_list;
     }
 
     //添加下级人员数据提交
