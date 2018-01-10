b8bcd2e30b83dcf296fbeaf7069c0d8923ab5da5
追梦小窝 <mail@54zm.com>
2018-01-10 12:17:28 +0800

    0110

--- app/Http/Controllers/Zerone/SetupController.php
+++ app/Http/Controllers/Zerone/SetupController.php
@@ -15,7 +15,8 @@ class SetupController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
 //        $setup_list = Module::getListProgram(1,[],0,'id');//获取当前系统的所有模块和节点
         $setup_list = Setup::all();
-        dump($setup_list);
+        $setup = Setup::getOne([]);
+        dump($setup);
         return view('Zerone/Setup/display',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //参数设置编辑
--- app/Models/Setup.php
+++ app/Models/Setup.php
@@ -27,7 +27,7 @@ class Setup extends Model{
     //简易型查询单条数据
     public static function getOne($where)
     {
-        return self::with('organization')->where($where)->first();
+        return self::with('cfg_name','cfg_value')->where($where)->first();
     }
     //登陆时通过输入的用户名或手机号查询用户
     public static function getOneForLogin($username){
