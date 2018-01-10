25354a01cd253512cb11decae7254c869a0e5e4a
kbzz <463661663@qq.com>
2018-01-10 14:49:49 +0800

    1

--- resources/views/Zerone/Proxy/proxy_list.blade.php
+++ resources/views/Zerone/Proxy/proxy_list.blade.php
@@ -45,69 +45,24 @@
 
     </div>
     <!-- Mainly scripts -->
-    <script src="{{asset('public/Zerone/library/pace')}}/js/jquery-2.1.1.js"></script>
-    <script src="{{asset('public/Zerone/library/pace')}}/js/bootstrap.min.js"></script>
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/jquery-2.1.1.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/bootstrap.min.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
     <!-- Custom and plugin javascript -->
-    <script src="{{asset('public/Zerone/library/pace')}}/js/inspinia.js"></script>
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/pace/pace.min.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/pace/pace.min.js"></script>
     <!-- Data picker -->
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/datapicker/bootstrap-datepicker.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/datapicker/bootstrap-datepicker.js"></script>
     <!-- Sweet alert -->
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/sweetalert/sweetalert.min.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/sweetalert/sweetalert.min.js"></script>
     <!-- FooTable -->
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/footable/footable.all.min.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/footable/footable.all.min.js"></script>
 
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/iCheck/icheck.min.js"></script>
-    <script src="{{asset('public/Zerone/library/pace')}}/js/plugins/switchery/switchery.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/iCheck/icheck.min.js"></script>
+    <script src="{{asset('public/Zerone')}}/js/plugins/switchery/switchery.js"></script>
     <!-- Page-Level Scripts -->
-    <script>
-        $(document).ready(function() {
-            var elem = document.querySelector('.js-switch');
-            var switchery = new Switchery(elem, { color: '#1AB394' });
-            $('.i-checks').iCheck({
-                checkboxClass: 'icheckbox_square-green',
-                radioClass: 'iradio_square-green',
-            });
-            $('.footable').footable();
-
-            $('#date_added').datepicker({
-                todayBtn: "linked",
-                keyboardNavigation: false,
-                forceParse: false,
-                calendarWeeks: true,
-                autoclose: true
-            });
-
-            $('#date_modified').datepicker({
-                todayBtn: "linked",
-                keyboardNavigation: false,
-                forceParse: false,
-                calendarWeeks: true,
-                autoclose: true
-            });
-            $("#editBtn").click(function(){
-                $('#myModal').modal();
-            });
-            $('#lockBtn').click(function(){
-                $('#myModal3').modal();
-            });
-            $('#removeBtn').click(function(){
-                $('#myModal3').modal();
-            });
-            $('.saveBtn').click(function(){
-                swal({
-                    title: "温馨提示",
-                    text: "操作成功",
-                    type: "success"
-                },function(){
-                    window.location.reload();
-                });
-            });
-        });
-    </script>
 </div>
 </body>
 
