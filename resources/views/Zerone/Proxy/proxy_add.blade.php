82f3b23bfaa886b9bfbbe979df51b4713752254a
zeo <234521890@qq.com>
2018-01-10 20:26:46 +0800

    by zeo

--- resources/views/Zerone/Proxy/proxy_add.blade.php
+++ resources/views/Zerone/Proxy/proxy_add.blade.php
@@ -138,37 +138,34 @@
         });
     });
 
-    //提交表单
-    function postForm() {
-        var target = $("#proxyForm");
-        var url = target.attr("action");
-        var data = target.serialize();
-
-        $.post(url, data, function (json) {
-            console.log(json);
-//            if (json.status == -1) {
-//                window.location.reload();
-//            } else
-//                if(json.status == 1) {
-//                swal({
-//                    title: "提示信息",
-//                    text: json.data,
-//                    confirmButtonColor: "#DD6B55",
-//                    confirmButtonText: "确定",
-//                },function(){
-//                    window.location.reload();
-//                });
-//            }else{
-//                swal({
-//                    title: "提示信息",
-//                    text: json.data,
-//                    confirmButtonColor: "#DD6B55",
-//                    confirmButtonText: "确定",
-//                    //type: "warning"
-//                });
-//            }
-        });
-    }
+//提交表单
+function postForm() {
+    var target = $("#currentForm");
+    var url = target.attr("action");
+    var data = target.serialize();
+    $.post(url, data, function (json) {
+        if (json.status == -1) {
+            window.location.reload();
+        } else if(json.status == 1) {
+            swal({
+                title: "提示信息",
+                text: json.data,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+            },function(){
+                window.location.reload();
+            });
+        }else{
+            swal({
+                title: "提示信息",
+                text: json.data,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+                //type: "warning"
+            });
+        }
+    });
+}
 </script>
 </body>
 
