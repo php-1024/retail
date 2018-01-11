d06573bec1400678d1c5ce4e80204ada89286195
zeo <234521890@qq.com>
2018-01-11 10:09:15 +0800

    by zeo

--- resources/views/Zerone/Subordinate/subordinate_add.blade.php
+++ resources/views/Zerone/Subordinate/subordinate_add.blade.php
@@ -137,7 +137,7 @@
     <script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
     <script src="{{asset('public/Zerone/library/pace')}}/js/pace.min.js"></script>
     <script src="{{asset('public/Zerone/library/wizard')}}/js/jquery.bootstrap.wizard.min.js"></script>
-
+    <script src="{{asset('public/Zerone/library/sweetalert')}}/js/sweetalert.min.js"></script>
     <script>
         $(document).ready(function() {
             $('#rootwizard').bootstrapWizard({'tabClass': 'bwizard-steps'});
