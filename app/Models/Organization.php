2de7acf77583b56ca039f6e992faa57916981013
kbzz <463661663@qq.com>
2018-01-11 18:18:28 +0800

    1

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -36,6 +36,7 @@ class Organization extends Model{
     //查询数据是否存在（仅仅查询ID增加数据查询速度）
     public static function checkRowExists($where){
         $row = self::getPluck($where,'id')->toArray();
+        echo $row;exit;
         if(empty($row)){
             return false;
         }else{
