1bf67859951b7f87e87c061f4af7fa088f969d5b
kbzz <463661663@qq.com>
2018-01-11 18:25:00 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -22,9 +22,9 @@ class ProxyController extends Controller{
 
         $organization_name = $request->input('organization_name');//服务商名称
 
-        $where = ['organization_name',$organization_name];
+        $where = [['organization_name',$organization_name]];
 
-        $name = Organization::checkRowExists($where,'id');
+        $name = Organization::checkRowExists($where);
 
         if(!empty($name)){
             return response()->json(['data' => '服务商名称已存在', 'status' => '0']);
