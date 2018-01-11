3f9826057104beae0448ebf26ccbd62566bd460b
zeo <234521890@qq.com>
2018-01-11 09:33:55 +0800

    by zeo

--- resources/views/Zerone/Subordinate/subordinate_add.blade.php
+++ resources/views/Zerone/Subordinate/subordinate_add.blade.php
@@ -77,7 +77,7 @@
                                             <div class="hr-line-dashed"></div>
                                             <div class="form-group">
                                                 <label class="col-sm-2 control-label">真实姓名</label>
-                                                <div class="col-sm-10"><input type="text" class="form-control"></div>
+                                                <div class="col-sm-10"><input type="text" name="realname" class="form-control"></div>
                                             </div>
                                             <div class="hr-line-dashed"></div>
                                             <div class="form-group">
