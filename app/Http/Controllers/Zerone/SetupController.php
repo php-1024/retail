d4d740d5d9cf1c3cd8707866a67cba2d7aef6bed
追梦小窝 <mail@54zm.com>
2018-01-12 18:11:43 +0800

    01

--- app/Http/Controllers/Zerone/SetupController.php
+++ app/Http/Controllers/Zerone/SetupController.php
@@ -14,15 +14,17 @@ class SetupController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $setup_list = Setup::get_all();
+
+        dump(Setup::all());
+
+        dd(Setup::getOne([['id','2']]));
+
+
         return view('Zerone/Setup/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name,'setup_list'=>$setup_list]);
     }
     //参数设置编辑
     public function setup_edit_check(Request $request){
         $cfg_value_arr = $request->input('cfg_value');//[0]服务商通道链接  [1]商户通道链接  [2]人员构深度设置
-        $cfg_value_arr[0];
-        dump(Setup::all());
-        
-        dd(Setup::getOne([['id','2']]));
         $cfg_value_arr[1];
         $cfg_value_arr[2];
         return response()->json(['data' => '系统参数修改成功！', 'status' => '1']);
