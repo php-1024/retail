c569b24ac99b1e373ea21bd64fd6546483f67877
追梦小窝 <mail@54zm.com>
2018-01-12 19:31:26 +0800

    01

--- app/Http/Controllers/Zerone/SetupController.php
+++ app/Http/Controllers/Zerone/SetupController.php
@@ -24,7 +24,7 @@ class SetupController extends Controller{
     //参数设置编辑
     public function setup_edit_check(Request $request){
         $cfg_value_arr = $request->input('cfg_value');//[0]服务商通道链接  [1]商户通道链接  [2]人员构深度设置
-        Setup::editSetup([['id',1]],['cfg_value',$cfg_value_arr[0]]);//[0]修改保存服务商通道链接
+        Setup::editSetup([['id',1]],[['cfg_value',$cfg_value_arr[0]]]);//[0]修改保存服务商通道链接
         return response()->json(['data' => '系统参数修改成功！', 'status' => '1']);
     }
 }
