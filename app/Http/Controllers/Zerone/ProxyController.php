13e6f0768416a21c60e67e49f49625b48a06a2ee
kbzz <463661663@qq.com>
2018-01-12 18:30:07 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -11,7 +11,7 @@ use Session;
 class ProxyController extends Controller{
     //添加服务商
     public function proxy_add(Request $request){
-        $admin_data = LoginLog::where('id','1')->first();
+        $admin_data = LoginLog::where('id','1')->get();
         dd($admin_data);
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
