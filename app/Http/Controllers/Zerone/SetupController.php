e29fde2f620fe169870f6cb40703a156208707f3
ismzxw <mail@54zm.com>
2018-01-12 23:33:26 +0800

    01

--- app/Http/Controllers/Zerone/SetupController.php
+++ app/Http/Controllers/Zerone/SetupController.php
@@ -26,9 +26,21 @@ class SetupController extends Controller{
         $serviceurl = $request->input('serviceurl');//[serviceurl]服务商通道链接
         $merchanturl = $request->input('merchanturl');//[merchant]商户通道链接
         $depth = $request->input('depth');//[depth]人员构深度设置
+        $serviceurl_deleted = $request->input('serviceurl_deleted');//是否开启服务商通道链接
+        $merchanturl_deleted = $request->input('merchanturl_deleted');//是否开启商户通道链接
         Setup::editSetup([['id',1]],['cfg_value'=>$serviceurl]);//修改保存服务商通道链接
         Setup::editSetup([['id',2]],['cfg_value'=>$merchanturl]);//修改保存商户通道链接
         Setup::editSetup([['id',3]],['cfg_value'=>$depth]);//修改保存人员构深度设置
+        if(empty($serviceurl_deleted)){
+            Setup::editSetup([['id',1]],['deleted_at'=>time()]);//修改保存服务商通道链接开启状态
+        }else{
+            Setup::editSetup([['id',1]],['deleted_at'=>null]);//修改保存服务商通道链接开启状态
+        }
+        if(empty($merchanturl_deleted)){
+            Setup::editSetup([['id',2]],['deleted_at'=>time()]);//修改保存商户通道链接开启状态
+        }else{
+            Setup::editSetup([['id',1]],['deleted_at'=>null]);//修改保存服务商通道链接开启状态
+        }
         return response()->json(['data' => '系统参数修改成功！', 'status' => '1']);
     }
 }
--- app/Http/Middleware/Zerone/ZeroneCheckAjax.php
+++ app/Http/Middleware/Zerone/ZeroneCheckAjax.php
@@ -203,9 +203,6 @@ class ZeroneCheckAjax
     }
     //检测编辑系统参数设置数据
     public function checkSetupEdit($request){
-        echo $request->input('serviceurl_deleted');
-        echo $request->input('merchanturl_deleted');
-        exit();
         if(empty($request->input('serviceurl'))){
             return self::res(0,response()->json(['data' => '请输入服务商通道链接', 'status' => '0']));
         }
