637dbab423179273d076e3d2c99add4fa5b87040
zeo <234521890@qq.com>
2018-01-12 18:11:31 +0800

    by zeo

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -13,6 +13,12 @@ class Account extends Model{
     public $timestamps = true;
     public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
 
+    //和账号多对多的关系
+    public function packages()
+    {
+        return $this->belongsToMany('App\Models\Package','package_program','program_id','package_id');
+    }
+
     //和个人信息表一对一的关系
     public function account_info(){
         return $this->hasOne('App\Models\AccountInfo', 'account_id');
@@ -48,7 +54,6 @@ class Account extends Model{
         $model->save();
         return $model->id;
     }
-
     //查询数据是否存在（仅仅查询ID增加数据查询速度）
     public static function checkRowExists($where){
         $row = self::getPluck($where,'id')->toArray();
@@ -58,10 +63,13 @@ class Account extends Model{
             return true;
         }
     }
-
     //获取单行数据的其中一列
     public static function getPluck($where,$pluck){
         return self::where($where)->pluck($pluck);
+    }
+    //获取分页数据
+    public static function getPaginage($where,$paginate,$orderby,$sort='DESC'){
+        return self::where($where)->orderBy($orderby,$sort)->paginate($paginate);
     }
 }
 ?>
