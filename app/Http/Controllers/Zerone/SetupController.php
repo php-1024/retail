62a76b8387e59bfaa644c2cead7a6fc4fd1e9377
追梦小窝 <mail@54zm.com>
2018-01-13 10:17:47 +0800

    01

--- app/Http/Controllers/Zerone/SetupController.php
+++ app/Http/Controllers/Zerone/SetupController.php
@@ -17,7 +17,7 @@ class SetupController extends Controller{
 
         dump(Setup::getOne([['id','2']])->cfg_value);
         dump($request);
-        $re = Setup::deleteSetup([['id','1']]);//修改保存服务商通道链接开启状态(软删除)
+        $re = Setup::deleteSetup(['id','1']);//修改保存服务商通道链接开启状态(软删除)
         if ($re){
             dump('删除成功');
         }else{
