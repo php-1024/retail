b0ac475da1dfa8e527b3e46f8c09f6a4dba445ef
kbzz <463661663@qq.com>
2018-01-13 20:07:23 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -53,8 +53,10 @@ class ProxyController extends Controller{
         try{
             $listdata = ['organization_name'=>$organization_name,'parent_id'=>0,'parent_tree'=>0,'program_id'=>0,'type'=>2,'status'=>1];
             $organization_id = Organization::addProgram($listdata); //返回值为商户的id
+
             $proxydata = ['organization_id'=>$organization_id,'zone_id'=>$zone_id];
             WarzoneProxy::addWarzoneProxy($proxydata);
+
             $account  = 'P'.$mobile.'_'.$organization_id;//用户账号
             $accdata = ['parent_id'=>$parent_id,'parent_tree'=>$parent_tree,'deepth'=>$deepth,'mobile'=>$mobile,'password'=>$encryptPwd,'organization_id'=>$organization_id,'account'=>$account];
             $account_id = Account::addAccount($accdata);//添加账号返回id
@@ -66,7 +68,6 @@ class ProxyController extends Controller{
             OperationLog::addOperationLog('1',$admin_this['organization_id'],$admin_this['id'],$route_name,'添加了服务商：'.$organization_name);//保存操作记录
             DB::commit();//提交事务
         }catch (\Exception $e) {
-            dd($e);
             DB::rollBack();//事件回滚
             return response()->json(['data' => '注册失败', 'status' => '0']);
         }
