f73359ac2631600c3fd0ecc8643ece607f36b5b3
追梦小窝 <mail@54zm.com>
2018-01-13 15:02:29 +0800

    0222

--- app/Models/WarzoneProvince.php
+++ app/Models/WarzoneProvince.php
@@ -14,13 +14,13 @@ class WarzoneProvince extends Model{
     public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
 
     //和战区表一对一的关系
-    public function warzone(){
+    public function warzoneprovince(){
         return $this->belongsTo('App\Models\Warzone', 'zone_id');
     }
 
     //获取程序分页列表
     public static function getPaginage($where,$paginate,$orderby,$sort='DESC'){
-        return self::with('warzone')->where($where)->orderBy($orderby,$sort)->paginate($paginate);
+        return self::with('warzoneprovince')->where($where)->orderBy($orderby,$sort)->paginate($paginate);
     }
 
 }
