7ae0ed4f8c6b63e429a7d11939894cb9e5c0a618
kbzz <463661663@qq.com>
2018-01-13 16:01:15 +0800

    1

--- resources/views/Zerone/Proxy/proxy_examine.blade.php
+++ resources/views/Zerone/Proxy/proxy_examine.blade.php
@@ -12,6 +12,7 @@
                     <label class="col-sm-2 control-label">服务商名称</label>
                     <div class="col-sm-10">kbzz</div>
                 </div>
+                <div style="clear:both"></div>
                 <div class="form-group">
                     <label class="col-sm-2 control-label">安全密码</label>
                     <div class="col-sm-10"><input type="text" class="form-control" value=""></div>
