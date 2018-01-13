b6b31ba986dc599634f69e9a85b643067fe10d35
kbzz <463661663@qq.com>
2018-01-13 16:22:42 +0800

    1

--- resources/views/Zerone/Proxy/proxy_examine.blade.php
+++ resources/views/Zerone/Proxy/proxy_examine.blade.php
@@ -1,5 +1,5 @@
 <link href="{{asset('public/Zerone/library/iCheck')}}/css/custom.css" rel="stylesheet">
-<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/role_edit_check') }}">
+<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/proxy_examine_check') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" id="id" value="{{$info->id}}">
     <div class="modal-dialog modal-lg">
@@ -17,14 +17,14 @@
             <div class="modal-body">
                 <div class="form-group">
                     <label class="col-sm-2 control-label">安全密码</label>
-                    <div class="col-sm-10"><input type="text" class="form-control" value=""></div>
+                    <div class="col-sm-10"><input type="text" class="form-control" value="" name="safe_password"></div>
                 </div>
                 <div style="clear:both"></div>
             </div>
 
             <div class="modal-footer">
                 <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
-                <button type="button" class="btn btn-primary saveBtn">保存</button>
+                <button type="button" class="btn btn-primary saveBtn" onclick="postForm()" >保存</button>
             </div>
         </div>
     </div>
@@ -36,27 +36,29 @@
         var target = $("#currentForm");
         var url = target.attr("action");
         var data = target.serialize();
-        $.post(url, data, function (json) {
-            if (json.status == -1) {
-                window.location.reload();
-            } else if(json.status == 1) {
-                swal({
-                    title: "提示信息",
-                    text: json.data,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "确定",
-                },function(){
-                    window.location.reload();
-                });
-            }else{
-                swal({
-                    title: "提示信息",
-                    text: json.data,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "确定",
-                    //type: "warning"
-                });
-            }
-        });
+        console.log(data);
+//        $.post(url, data, function (json) {
+//            if (json.status == -1) {
+//                window.location.reload();
+//            } else if(json.status == 1) {
+//                swal({
+//                    title: "提示信息",
+//                    text: json.data,
+//                    confirmButtonColor: "#DD6B55",
+//                    confirmButtonText: "确定",
+//                },function(){
+//                    window.location.reload();
+//                });
+//            }else{
+//                console.log(json);
+////                swal({
+////                    title: "提示信息",
+////                    text: json.data,
+////                    confirmButtonColor: "#DD6B55",
+////                    confirmButtonText: "确定",
+////                    //type: "warning"
+////                });
+//            }
+//        });
     }
 </script>
