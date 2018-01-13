79620523ae2928c9ea00c5fca4ecee6033a23b6a
zeo <234521890@qq.com>
2018-01-13 14:43:41 +0800

    byzeo

--- resources/views/Zerone/Subordinate/subordinate_edit.blade.php
+++ resources/views/Zerone/Subordinate/subordinate_edit.blade.php
@@ -20,7 +20,7 @@
 
                 <div class="form-group">
                     <label class="col-sm-2 control-label">登陆密码</label>
-                    <div class="col-sm-10"><input type="text" class="form-control" name="password" value=""></div>
+                    <div class="col-sm-10"><input type="password" class="form-control" name="password" value=""></div>
                 </div>
                 <div style="clear:both"></div>
                 <div class="hr-line-dashed"></div>
