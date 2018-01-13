960c1c25ea08af930448d53eb87a7119cb450743
追梦小窝 <mail@54zm.com>
2018-01-13 18:14:56 +0800

    0222

--- app/Http/Controllers/Zerone/WarzoneController.php
+++ app/Http/Controllers/Zerone/WarzoneController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Zerone;
 use App\Http\Controllers\Controller;
 use App\Models\Warzone;
+use App\Models\Module;
 use App\Models\WarzoneProvince;
 use Illuminate\Http\Request;
 use Session;
@@ -17,5 +18,16 @@ class WarzoneController extends Controller{
         $warzone = WarzoneProvince::all();
         dump($warzone);
         return view('Zerone/Warzone/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+    }
+
+    //功能模块列表
+    public function module_list(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+        $module_name = $request->input('module_name');
+        $search_data = ['module_name'=>$module_name];
+        $list = Module::getPaginage([[ 'module_name','like','%'.$module_name.'%' ]],15,'id');
+        dump($list);
+        return view('Tooling/Module/module_list',['list'=>$list,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'action_name'=>'module']);
     }
 }
