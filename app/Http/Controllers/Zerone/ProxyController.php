d8e4d57d0f476208c5fe69b0fa49b094e3524446
kbzz <463661663@qq.com>
2018-01-13 17:27:07 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -91,13 +91,13 @@ class ProxyController extends Controller{
         $id = $request->input('id');//服务商id
         $sta = $request->input('sta');//是否通过值 1为通过 -1为不通过
         $proxylist = ProxyApply::getOne([['id',$id]]);
-        echo $proxylist['proxy_name'];exit;
         if($sta==-1){
             DB::beginTransaction();
             try{
                 ProxyApply::editProxyApply(['id'=>$id],['status'=>$sta]);//拒绝申请
                 //添加操作日志
-                OperationLog::addOperationLog('1',$admin_data['organization_id'],$admin_data['id'],$route_name,'拒绝了服务商：'.$proxylist['proxy_name']);//保存操作记录
+                $aa= OperationLog::addOperationLog('1',$admin_data['organization_id'],$admin_data['id'],$route_name,'拒绝了服务商：'.$proxylist['proxy_name']);//保存操作记录
+                    echo $aa;exit;
                 DB::commit();//提交事务
             }catch (\Exception $e) {
                 DB::rollBack();//事件回滚
