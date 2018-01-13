e5f51400d22ef5f31ff675800c029e722c2881ae
追梦小窝 <mail@54zm.com>
2018-01-13 14:14:29 +0800

    0222

--- app/Http/Controllers/Zerone/WarzoneController.php
+++ app/Http/Controllers/Zerone/WarzoneController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Zerone;
 use App\Http\Controllers\Controller;
 use App\Models\Warzone;
+use App\Models\WarzoneProvince;
 use Illuminate\Http\Request;
 use Session;
 
@@ -14,7 +15,13 @@ class WarzoneController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $warzone = Warzone::all();
+//        foreach ($warzone as $key => $val){
+//            $val->id
+//
+//        }
+        $warzone_province = WarzoneProvince::warzone();
         dump($warzone);
+        dump($warzone_province);
         return view('Zerone/Warzone/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
     //参数设置编辑
---
+++ app/Models/WarzoneProvince.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * warzone_province(战区关系表模型)表的模型
+ *
+ */
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class WarzoneProvince extends Model{
+    use SoftDeletes;
+    protected $table = 'warzone_province';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
+
+    //和战区表一对一的关系
+    public function warzone(){
+        return $this->belongsTo('App\Models\Warzone', 'zone_id');
+    }
+}
+?>
