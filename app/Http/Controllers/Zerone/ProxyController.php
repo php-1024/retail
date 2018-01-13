87caa31e5af9214f650e66cc0c10dca7aad90221
kbzz <463661663@qq.com>
2018-01-13 10:57:49 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -75,7 +75,7 @@ class ProxyController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
 
-        $list = ProxyApply::getList('','5','id');
+        $list = ProxyApply::getList([],'5','id');
         return view('Zerone/Proxy/proxy_examinelist',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //服务商审核列表
