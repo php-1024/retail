801b2955cfd14aa423538e9c29db3cd7f74379ee
zeo <234521890@qq.com>
2018-01-14 22:14:28 +0800

    by zeo

--- app/Http/Controllers/Zerone/SubordinateController.php
+++ app/Http/Controllers/Zerone/SubordinateController.php
@@ -281,10 +281,20 @@ class SubordinateController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = 1;//当前组织ID，零壹管理平台组织只能为1
         $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.$admin_data['parent_tree'].','.$admin_data['id'].'%']],0,'tree_sort','asc');
+        $list = $this->create_structure($list,$admin_data['id']);
         dump($list);
         return view('Zerone/Subordinate/subordinate_structure',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
-
+    private function create_structure($list,$id){
+        $arr = [];
+        foreach($list as $key=>$val){
+            if($val->parent_id == $id){
+                $arr[$id] = $val;
+                $arr[$id]['son_list'] = $this->create_structure($list,$val->id);
+            }
+        }
+        return $arr;
+    }
 }
 ?>
