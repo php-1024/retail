ad7e3f9912047b1fdf9271b9419c241f85d13e02
zeo <234521890@qq.com>
2018-01-14 21:55:07 +0800

    by zeo

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -53,7 +53,7 @@ class Account extends Model{
         if(!empty($limit)){
             $model = $model->limit($limit);
         }
-        return $model->where($where)->orderBy($orderby,$sort)->select('compact(parent_id,"-",id) as tree_sort')->get();
+        return $model->where($where)->orderBy($orderby,$sort)->select(self::raws('compact(parent_id,"-",id) as tree_sort'))->get();
     }
 
     //登陆时通过输入的用户名或手机号查询用户
