f0bd5e0160ba2a2942be340e54253f7be2c81219
ismzxw <mail@54zm.com>
2018-01-14 22:18:00 +0800

    01

--- app/Http/Controllers/Zerone/WarzoneController.php
+++ app/Http/Controllers/Zerone/WarzoneController.php
@@ -15,7 +15,7 @@ class WarzoneController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $zone_id = $request->input('$zone_id');
+        $zone_id = '1';
         $warzone = WarzoneProvince::getPaginage([[ 'zone_id','like','%'.$zone_id.'%' ]],15,'id');
         dump($warzone);
         return view('Zerone/Warzone/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name,'warzone'=>$warzone]);
