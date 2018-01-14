183368ed6042c98bc49ad1e68764f8d6454ee851
zeo <234521890@qq.com>
2018-01-14 21:24:32 +0800

    by zeo

--- app/Http/Controllers/Zerone/SubordinateController.php
+++ app/Http/Controllers/Zerone/SubordinateController.php
@@ -280,7 +280,8 @@ class SubordinateController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
 
-        $list =
+        $list = Account::getList([['parent_tree','like','%'.$admin_data['parent_tree'].','.$admin_data['id'].'%']],0,'id');
+        dump($list);
         return view('Zerone/Subordinate/subordinate_structure',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 }
