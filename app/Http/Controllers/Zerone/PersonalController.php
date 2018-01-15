80384ab3cdd7e6ac47f02a36791c63e4f19fe268
追梦小窝 <mail@54zm.com>
2018-01-15 11:39:18 +0800

    0222

--- app/Http/Controllers/Zerone/PersonalController.php
+++ app/Http/Controllers/Zerone/PersonalController.php
@@ -24,7 +24,11 @@ class PersonalController extends Controller{
     }
     //个人中心——安全密码设置
     public function security_password(Request $request){
-        dd('个人中心——安全密码设置');
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+        return view('Zerone/Personal/security_password',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
     //个人中心——我的操作日志
     public function operation_log(Request $request){
