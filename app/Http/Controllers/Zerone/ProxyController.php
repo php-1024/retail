e5037f5e08a74855137423576cdc7947178522e7
kbzz <463661663@qq.com>
2018-01-15 17:54:11 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -196,6 +196,13 @@ class ProxyController extends Controller{
                  $orginfodata = ['proxy_owner'=>$realname];
                  OrganizationProxyinfo::editOrganizationProxyinfo(['organization_id'=>$id],$orginfodata);
              }
+             if(!empty($password)){
+                 $key = config("app.zerone_encrypt_key");//获取加密盐
+                 $encrypted = md5($password);//加密密码第一重
+                 $encryptPwd = md5("lingyikeji".$encrypted.$key);//加密密码第二重
+                 $accountdata = ['password'=>$encryptPwd];
+                 Account::editAccount(['organization_id'=>$id],$accountdata);
+             }
 
 
 //            //添加操作日志
