b966a7ce6ea0d1fc2171e12a3ddd97c841fac9a7
ismzxw <mail@54zm.com>
2018-01-15 23:43:29 +0800

    01

--- app/Http/Controllers/Zerone/PersonalController.php
+++ app/Http/Controllers/Zerone/PersonalController.php
@@ -51,12 +51,12 @@ class PersonalController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
+        dump($admin_data);
         return view('Zerone/Personal/safe_password',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
     //个人中心——安全密码修改(设置)
     public function safe_password_edit_check(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dump($admin_data);
         $safe_password = $request->input('safe_password');           //安全密码
         $new_safe_password = $request->input('new_safe_password');   //新安全密码
         $key = config("app.zerone_safe_encrypt_key");//获取加密盐
