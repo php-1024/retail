b17c24e76f38f6c3bc8ab51c1fe9deba0dafaa34
zeo <234521890@qq.com>
2018-01-15 10:44:13 +0800

    by zeo

--- app/Http/Controllers/Zerone/RoleController.php
+++ app/Http/Controllers/Zerone/RoleController.php
@@ -105,16 +105,40 @@ class RoleController extends Controller{
     }
     //编辑权限角色
     public function role_edit(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $id = $request->input('id');//权限角色ID
         $info = OrganizationRole::getOne([['id',$id]]);//获取该ID的信息
         $node_list = ProgramModuleNode::getRoleModuleNodes(1,$id);//获取当前角色拥有权限的模块和节点
         $selected_nodes = [];//选中的节点
         $selected_modules = [];//选中的模块
+
         foreach($node_list as $key=>$val){
             $selected_modules[] = $val->module_id;
             $selected_nodes[] = $val->node_id;
         }
-        $module_node_list = Module::getListProgram(1,[],0,'id');//获取当前系统的所有模块和节点
+
+        if($admin_data['id'] == 1) {
+            $module_node_list = Module::getListProgram(1, [], 0, 'id');//获取当前系统的所有模块和节点
+        }else{
+            $account_node_list = ProgramModuleNode::getAccountModuleNodes(1,$admin_data['id']);//获取当前用户具有权限的节点
+            $modules = [];
+            $nodes = [];
+            $module_node_list = [];
+            //过滤重复选出的节点和模块
+            foreach($account_node_list as $key=>$val){
+                $modules[$val->module_id] = $val->module_name;
+                $nodes[$val->module_id][$val->node_id] = $val->node_name;
+            }
+            //遍历，整理为合适的格式
+            foreach($modules as $key=>$val){
+                $module = ['id'=>$key,'module_name'=>$val];
+                foreach($nodes[$key] as $k=>$v){
+                    $module['program_nodes'][] = array('id'=>$k,'node_name'=>$v);
+                }
+                $module_node_list[] = $module;
+                unset($module);
+            }
+        }
         return view('Zerone/Role/role_edit',['info'=>$info,'selected_modules'=>$selected_modules,'selected_nodes'=>$selected_nodes,'module_node_list'=>$module_node_list]);
     }
 
