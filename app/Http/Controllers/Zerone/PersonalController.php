61090e04d8dfa70dbe871b7c411518318fcb37c2
追梦小窝 <mail@54zm.com>
2018-01-15 14:47:49 +0800

    0222

--- app/Http/Controllers/Zerone/PersonalController.php
+++ app/Http/Controllers/Zerone/PersonalController.php
@@ -13,8 +13,8 @@ class PersonalController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         dump($admin_data);
-        $account_data = Account::getList([['id',$admin_data->id],['organization_id',$admin_data->organization_id]],'id');
-        
+        $account_data = Account::getList([['id',$admin_data['id']],['organization_id',$admin_data['organization_id']]],'id');
+
         dump($account_data);
         return view('Zerone/Personal/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
