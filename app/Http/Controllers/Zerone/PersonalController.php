8883d091c30ccb68ffcb934ab9966d2300d96238
追梦小窝 <mail@54zm.com>
2018-01-15 17:09:52 +0800

    0222

--- app/Http/Controllers/Zerone/PersonalController.php
+++ app/Http/Controllers/Zerone/PersonalController.php
@@ -29,6 +29,7 @@ class PersonalController extends Controller{
 
     //个人中心——登录密码修改
     public function password_edit_check(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $account = Account::getOne([['id',$admin_data['id']]]);
         dump($account);
         $password = $request->input('password');
