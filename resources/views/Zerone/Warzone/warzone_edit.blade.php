b75d4d5d8e0403f8ba310055c04dfcf52b09d1ea
追梦小窝 <mail@54zm.com>
2018-01-16 15:11:04 +0800

    01

--- resources/views/Zerone/Warzone/warzone_edit.blade.php
+++ resources/views/Zerone/Warzone/warzone_edit.blade.php
@@ -1,5 +1,5 @@
 <div class="modal inmodal" id="myModal2" tabindex="-1" role="dialog" aria-hidden="true">
-<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/role_edit_check') }}">
+<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/dashboard_warzone_edit') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" id="id" value="id">
     <div class="modal-dialog">
@@ -35,10 +35,40 @@
 
                 <div class="modal-footer">
                     <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
-                    <button type="button" class="btn btn-primary">保存</button>
+                    <button type="button" class="btn btn-primary" onclick="return postForm();">保存</button>
                 </div>
             </div>
         </div>
     </div>
 </form>
 </div>
+<script>
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    //type: "warning"
+                });
+            }
+        });
+    }
+</script>
