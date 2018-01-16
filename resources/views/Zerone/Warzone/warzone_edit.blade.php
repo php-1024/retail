d42ff38ed9a02040f47441e44891d7a95cfb4051
追梦小窝 <mail@54zm.com>
2018-01-16 15:48:53 +0800

    01

--- resources/views/Zerone/Warzone/warzone_edit.blade.php
+++ resources/views/Zerone/Warzone/warzone_edit.blade.php
@@ -16,15 +16,15 @@
                 <div class="form-group">
                     <label>包含省份</label>
                     <div style="clear: both;"></div>
-                    <select data-placeholder="请选择省份" class="chosen-select2" multiple style="width:350px;" tabindex="2">
+                    <select data-placeholder="请选择省份" name="province_name[]" class="chosen-select2" multiple style="width:350px;" tabindex="2">
                         {{--所有战区当前选中的战区--}}
                         @foreach($val->province as $kk=>$vv)
-                            <option selected="selected" name="province_name[]" value="{{ $vv->id }}">{{ $vv->province_name }}</option>
+                            <option selected="selected" value="{{ $vv->id }}">{{ $vv->province_name }}</option>
                         @endforeach
                         {{--所有战区当前选中的战区--}}
                         {{--所有战区未选中的战区--}}
                         @foreach($new_province_name as $k=>$v)
-                            <option name="province_name[]" value="{{$k}}">{{$v}}</option>
+                            <option value="{{$k}}">{{$v}}</option>
                         @endforeach
                         {{--所有战区未选中的战区--}}
                     </select>
