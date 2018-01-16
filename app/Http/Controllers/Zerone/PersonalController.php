b7e3a52d37750ebdc093ddacbdd149cd4b0c6ec7
追梦小窝 <mail@54zm.com>
2018-01-16 09:47:08 +0800

    01

--- app/Http/Controllers/Zerone/PersonalController.php
+++ app/Http/Controllers/Zerone/PersonalController.php
@@ -88,7 +88,14 @@ class PersonalController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        return view('Zerone/Personal/operation_log',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+        $where = [];
+        if($admin_data['id']<>1){   //不是超级管理员的时候，只查询自己相关的数据
+            $where = [
+                ['account_id',$admin_data['id']]
+            ];
+        }
+        $operation_log_list = OperationLog::getPaginage($where,10,'id');//操作记录
+        return view('Zerone/Personal/operation_log',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name,'operation_log_list'=>$operation_log_list]);
     }
     //个人中心——我的登录日志
     public function login_log(Request $request){
@@ -96,7 +103,14 @@ class PersonalController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        return view('Zerone/Personal/login_log',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+        $where = [];
+        if($admin_data['id']<>1){   //不是超级管理员的时候，只查询自己相关的数据
+            $where = [
+                ['account_id',$admin_data['id']]
+            ];
+        }
+        $login_log_list = LoginLog::getPaginage($where,10,'id');//登录记录
+        return view('Zerone/Personal/login_log',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'login_log_list'=>$login_log_list]);
     }
 }
 ?>
