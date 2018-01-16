3644b2d832edca62cc94dac37c142e17c3668b73
kbzz <463661663@qq.com>
2018-01-16 18:05:33 +0800

    1

--- app/Http/Controllers/Zerone/CompanyController.php
+++ app/Http/Controllers/Zerone/CompanyController.php
@@ -181,6 +181,9 @@ class CompanyController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $listorg = Organization::getCompany(['type'=>'3'],'5','id');
+        foreach ($listorg as $k=>$v){
+            dump($v);
+        }
         return view('Zerone/Company/company_list',['listorg'=>$listorg,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //服务商编辑ajaxshow显示页面
