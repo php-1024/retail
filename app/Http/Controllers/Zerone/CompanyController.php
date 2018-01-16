3a252f21e53089854efd722924205552543ca134
kbzz <463661663@qq.com>
2018-01-16 15:30:51 +0800

    1

--- app/Http/Controllers/Zerone/CompanyController.php
+++ app/Http/Controllers/Zerone/CompanyController.php
@@ -20,7 +20,7 @@ class CompanyController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $list = Organization::where(['type'=>'1'])->where(['`status`'=>'1'])->orwhere(['type'=>'2'])->get();
+        $list = Organization::where(['type'=>'1','status'=>'1'])->where(['status'=>'1'])->orwhere(['type'=>'2'])->get();
 
         return view('Zerone/Company/company_add',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'list'=>$list]);
     }
