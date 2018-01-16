a4a747c8ca73950da3393c0baeb241dfbcbe12c9
kbzz <463661663@qq.com>
2018-01-16 11:18:15 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -275,13 +275,16 @@ class ProxyController extends Controller{
 //        $warzone = Warzone::all();
         return view('Zerone/Proxy/proxy_list_delete');
     }
-//服务商冻结ajaxshow显示页面
+//服务商下级人员架构
     public function proxy_structure(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-//        $listorg = Organization::getPaginage(['type'=>'2'],'5','id');
+        $id = $request->input('id');
+        $list = Account::getList([['organization_id',$id],['parent_tree','like','%'.$admin_data['parent_tree'].','.$admin_data['id'].'%']],0,'id','asc')->toArray();
+        $structure = $this->create_structure($list,$admin_data['id']);
+        dd($structure);
         return view('Zerone/Proxy/proxy_structure',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
