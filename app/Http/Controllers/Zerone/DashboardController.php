4260a24e29e354d9af0b1343263c370d4abc214a
追梦小窝 <mail@54zm.com>
2018-01-16 12:46:05 +0800

    01

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -80,7 +80,7 @@ class DashboardController extends Controller{
         dump($warzone);
         $province = Province::getpluck('id');
         dump($province);
-        return view('Zerone/Warzone/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name,'warzone'=>$warzone]);
+        return view('Zerone/Warzone/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name,'warzone'=>$warzone,'province'=>$province]);
     }
     //战区管理编辑弹出
     public function warzone_edit(){
