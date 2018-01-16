55063835ff8f0564f84c853795be672df3ff921a
追梦小窝 <mail@54zm.com>
2018-01-16 13:57:47 +0800

    01

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -89,8 +89,7 @@ class DashboardController extends Controller{
         foreach ($province as $key=>$val){
             $all_province_name[] = $val->province_name;
         }
-//        dump(array_diff($province_name,$province));
-        dump($all_province_name);
+        dump(array_diff($province_name,$all_province_name));
         return view('Zerone/Warzone/display',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name,'warzone'=>$warzone,'province'=>$province]);
     }
     //战区管理编辑弹出
