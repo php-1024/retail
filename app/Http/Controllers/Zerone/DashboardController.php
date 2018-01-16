eea7f5dc61fc2b08827dbf675e6c86b5547e257b
mail@54zm.com <mail@54zm.com>
2018-01-16 19:27:44 +0800

    1

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -94,8 +94,7 @@ class DashboardController extends Controller{
         }
         $new_province_name = array_diff($all_province_name,$province_name);
 
-//        $zone_id = $request->input('zone_id');
-        $zone_id = '1';
+        $zone_id = $request->input('zone_id');
         $zone_info = Warzone::getPaginage([[ 'id','like','%'.$zone_id.'%' ]],10,'id');
         return view('Zerone/Warzone/warzone_edit',['zone_info'=>$zone_info,'new_province_name'=>$new_province_name]);
     }
