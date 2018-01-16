a23c3b46440366f0d9ab696f1ba1de79e1ddcba7
zeo <234521890@qq.com>
2018-01-16 22:01:41 +0800

    by zeo

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -186,8 +186,8 @@ class DashboardController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = 1;//当前组织ID，零壹管理平台组织只能为1
-        $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.$admin_data['parent_tree'].$admin_data['id'].',%']],0,'id','asc')->toArray();
-        $structure = $this->create_structure($list,$admin_data['id']);
+        $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','0,%']],0,'id','asc')->toArray();
+        $structure = $this->create_structure($list,1);//获取重Admin开始的
         return view('Zerone/Subordinate/subordinate_structure',['structure'=>$structure ,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
