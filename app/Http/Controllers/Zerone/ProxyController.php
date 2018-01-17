2c0cef075b7dfa71d583ab10b9ab34a8a84292c2
kbzz <463661663@qq.com>
2018-01-17 14:36:47 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -289,8 +289,8 @@ class ProxyController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = $request->input('organization_id');//服务商id
         $listOrg = Organization::getOne([['id',$organization_id]]);
-        dd($listOrg);
-        $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.$admin_data['parent_tree'].','.$admin_data['id'].'%']],0,'id','asc')->toArray();
+        $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.$listOrg['parent_tree'].','.$listOrg['id'].'%']],0,'id','asc')->toArray();
+        dd($list);
         $structure = $this->proxy_str($list,$organization_id);
         return view('Zerone/Proxy/proxy_structure',['structure'=>$structure,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
