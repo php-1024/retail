6c95722d8a350b4be6e0b50330e678745aa0206b
kbzz <463661663@qq.com>
2018-01-17 18:52:10 +0800

    1

--- app/Http/Controllers/Zerone/CompanyController.php
+++ app/Http/Controllers/Zerone/CompanyController.php
@@ -345,5 +345,14 @@ class CompanyController extends Controller{
 
         return view('Zerone/Company/company_program',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
+    //商户程序管理
+    public function company_store(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+
+        return view('Zerone/Company/company_store',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+    }
 }
 ?>
