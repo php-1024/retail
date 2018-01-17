2740166c874fde962689d9fbb270e926498fa2f4
mail@54zm.com <mail@54zm.com>
2018-01-17 18:37:57 +0800

    01

--- app/Models/OperationLog.php
+++ app/Models/OperationLog.php
@@ -30,10 +30,9 @@ class OperationLog extends Model{
         }
         return $model->where($where)->orderBy($orderby,$sort)->get();
     }
-
     //根据时间戳操作用户分页查询获取列表
     public static function getPaginate($where,$time_st_format,$time_nd_format,$paginate,$orderby,$sort='DESC'){
-        $model = self::with('accounts')->where($where);
+        $model = self::where($where);
         if(!empty($time_st_format) && !empty($time_nd_format)){
             $model = $model->whereBetween('created_at',[$time_st_format,$time_nd_format]);
         }
