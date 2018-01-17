a50a2293be54e44a9109048c5a4d48bc91be557d
kbzz <463661663@qq.com>
2018-01-17 15:05:41 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -306,7 +306,7 @@ class ProxyController extends Controller{
             if($val['parent_id'] == $id) {
                 unset($list[$key]);
                 $val['sonlist'] = $this->create_structure($list, $val['id']);
-                dump($val['sonlist']);exit;
+                dump($val['sonlist']);
                 //$arr[] = $val;
                 $structure .= '<ol class="dd-list"><li class="dd-item" data-id="' . $val['id'] . '">' ;
                 $structure .= '<div class="dd-handle">';
