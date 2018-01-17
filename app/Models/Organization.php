417ca5b864b13709e0c659955e345d17587ad086
kbzz <463661663@qq.com>
2018-01-17 14:08:04 +0800

    1

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -82,7 +82,7 @@ class Organization extends Model{
     }
     //获取分页数据-服务商
     public static function getPaginage($where,$paginate,$orderby,$sort='DESC'){
-        return self::where($where)->join('warzone_proxy','warzone_proxy.organization_id','=','organization.id')->orderBy($orderby,$sort)->paginate($paginate);
+        return Organization::where($where)->join('warzone_proxy','warzone_proxy.organization_id','=','organization.id')->orderBy($orderby,$sort)->paginate($paginate);
     }
     //获取分页数据-商户
     public static function getCompany($where,$paginate,$orderby,$sort='DESC'){
