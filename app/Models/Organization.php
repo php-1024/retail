6db3f5af23183b33913dc333c6f127e5223da719
kbzz <463661663@qq.com>
2018-01-17 13:55:17 +0800

    1

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -82,7 +82,7 @@ class Organization extends Model{
     }
     //获取分页数据-服务商
     public static function getPaginage($where,$paginate,$orderby,$sort='DESC'){
-        return self::with('warzoneProxy','organizationproxyinfo')->join('warzone','warzone.id','=','warzone_proxy.zone_id')->where($where)->orderBy($orderby,$sort)->paginate($paginate);
+        return self::with('warzoneProxy','organizationproxyinfo')->join('warzone','warzone.id','=','warzoneProxy.zone_id')->where($where)->orderBy($orderby,$sort)->paginate($paginate);
     }
     //获取分页数据-商户
     public static function getCompany($where,$paginate,$orderby,$sort='DESC'){
