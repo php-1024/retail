66e3acd2b35148fab8048eb0ecd310ceb26cf3ad
mail@54zm.com <mail@54zm.com>
2018-01-17 10:39:00 +0800

    01

--- app/Models/Warzone.php
+++ app/Models/Warzone.php
@@ -43,7 +43,7 @@ class Warzone extends Model{
         $model = new Warzone();
         $model->zone_name = $param;
         $re = $model->save();
-        return $re->id;
+        return $re['id'];
     }
 
     //修改战区
