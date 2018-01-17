7f2717025e47b537d785646e0b12c08b244968b1
kbzz <463661663@qq.com>
2018-01-17 17:39:15 +0800

    1

--- resources/views/Zerone/Proxy/proxy_program.blade.php
+++ resources/views/Zerone/Proxy/proxy_program.blade.php
@@ -159,20 +159,8 @@
                     </div>
                 </div>
             </div>
-
-
         </div>
-
-
-        <div class="footer" >
-            <div class="pull-right">
-                您登陆的时间是：2017-10-24 16:26:30
-            </div>
-            <div>
-                <strong>Copyright</strong> 零壹新科技（深圳有限公司）&copy; 2017-2027
-            </div>
-        </div>
-
+        @include('Zerone/Public/Footer')
     </div>
     <div class="modal inmodal" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-lg">
