069d246900d864ef2d1d13d7d4394b3ddf61c030
mail@54zm.com <mail@54zm.com>
2018-01-17 16:39:10 +0800

    01

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -211,6 +211,20 @@ class DashboardController extends Controller{
             ];
         }
         $operation_log_list = OperationLog::getPaginage($where,10,'id');//操作记录
+
+
+
+        $time_st = $request->input('time_st');//查询时间开始
+        $time_nd = $request->input('time_nd');//查询时间结束
+        $time_st_format = $time_nd_format = 0;//实例化时间格式
+        if(!empty($time_st) && !empty($time_nd)) {
+            $time_st_format = strtotime($time_st . ' 00:00:00');//开始时间转时间戳
+            $time_nd_format = strtotime($time_nd . ' 23:59:59');//结束时间转时间戳
+        }
+        $search_data = ['time_st'=>$time_st,'time_nd'=>$time_nd];
+        $list = ToolingOperationLog::getPaginate([['account_id',$admin_data['admin_id']]],$time_st_format,$time_nd_format,15,'id');
+
+
         return view('Zerone/Dashboard/operation_log',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'operation_log_list'=>$operation_log_list]);
     }
     //所有登录记录
