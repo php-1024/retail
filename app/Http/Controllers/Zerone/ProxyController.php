bee370a260bd65c38fe72db5bbc05144d5ced6b9
kbzz <463661663@qq.com>
2018-01-17 10:53:10 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -177,7 +177,7 @@ class ProxyController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $listorg = Organization::getPaginage([['type','2']],'5','id');
+        $listorg = Organization::getPaginage(['type'=>'2'],'5','id');
 //        foreach ($listorg['data'] as $k=>$v){
 //            $warzone_id= $v['warzone_proxy']['id'];
 //            $listorg['data'][$k]['zhanquname'] = Warzone::getPluck(['id'=>$warzone_id],'zone_name')->toArray();
