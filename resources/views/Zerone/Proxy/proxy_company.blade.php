4b46bb5d586bd8e65f2ca16be29a3bddd8c955f7
kbzz <463661663@qq.com>
2018-01-17 17:33:57 +0800

    1

--- resources/views/Zerone/Proxy/proxy_company.blade.php
+++ resources/views/Zerone/Proxy/proxy_company.blade.php
@@ -2,23 +2,15 @@
 <html>
 
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>零壹新科技程序管理平台</title>
     <link href="{{asset('public/Zerone/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{asset('public/Zerone/library/font')}}/css/font-awesome.css" rel="stylesheet">
     <link href="{{asset('public/Zerone/library/sweetalert')}}/css/sweetalert.css" rel="stylesheet">
-
     <link href="{{asset('public/Zerone')}}/css/animate.css" rel="stylesheet">
     <link href="{{asset('public/Zerone')}}/css/style.css" rel="stylesheet">
-
     <link href="{{asset('public/Zerone/library/chosen')}}/css/chosen.css" rel="stylesheet">
-
-
-    {{--<link href="css/plugins/switchery/switchery.css" rel="stylesheet">--}}
-
 </head>
 
 <body class="">
@@ -244,19 +236,8 @@
 
 <script src="{{asset('public/Tooling/library/switchery')}}/js/switchery.js"></script>
 
-<script src="{{asset('public/Zerone/library/nestable')}}/js/jquery.nestable.js"></script>
+{{--<script src="{{asset('public/Zerone/library/nestable')}}/js/jquery.nestable.js"></script>--}}
 <script src="{{asset('public/Zerone/library/chosen')}}/js/chosen.jquery.js"></script>
-
-
-{{--<script src="js/plugins/chosen/chosen.jquery.js"></script>--}}
-
-
-
-
-
-
-
-
 
 <script>
     $(document).ready(function() {
