8a2903ca65793716a898f069d888b8da1f134564
kbzz <463661663@qq.com>
2018-01-17 09:59:17 +0800

    1

--- resources/views/Zerone/Proxy/proxy_list.blade.php
+++ resources/views/Zerone/Proxy/proxy_list.blade.php
@@ -103,16 +103,16 @@
                                     @foreach($listorg as $key=>$value)
                                     <tr>
                                         <td>{{$value['id']}}</td>
-                                        <td>{{$value->organization_name}}</td>
-                                        <td>{{$value->warzoneProxy->zone_id}}</td>
-                                        <td>{{$value->organizationproxyinfo->proxy_owner}}</td>
+                                        <td>{{$value['organization_name']}}</td>
+                                        <td>{{$value['warzoneProxy']['zone_id']}}</td>
+                                        <td>{{$value['organizationproxyinfo']['proxy_owner']}}</td>
 
 
-                                        <td>{{$value->organizationproxyinfo->proxy_owner_mobile}}</td>
+                                        <td>{{$value['organizationproxyinfo']['proxy_owner_mobile']}}</td>
                                         <td>
-                                            @if($value->status == 1)
+                                            @if($value['status'] == 1)
                                                 <label class="label label-primary">正常</label>
-                                            @elseif($value->status == 0)
+                                            @elseif($value['status'] == 0)
                                                 <label class="label label-danger">已冻结</label>
                                             @endif
                                         </td>
