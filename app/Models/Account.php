65965deeabe15aeff0d2a11b4ef41900fe7fd63f
mail@54zm.com <mail@54zm.com>
2018-01-18 18:28:57 +0800

    01

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -6,7 +6,6 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\DB;
 class Account extends Model{
     use SoftDeletes;
     protected $table = 'account';
