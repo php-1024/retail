f70683fedb7a5cd9e5a348e480d406bdb12068e2
kbzz <463661663@qq.com>
2018-01-18 16:22:50 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -183,7 +183,7 @@ class ProxyController extends Controller{
         }
         $listorg = Organization::getPaginage($where,'5','id');
         foreach ($listorg as $K=>$v){
-            dump($v);
+            dump($v['warzoneProxy']);
         }
         return view('Zerone/Proxy/proxy_list',['search_data'=>$search_data,'listorg'=>$listorg,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
