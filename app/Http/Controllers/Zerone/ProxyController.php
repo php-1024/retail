bcfdf07af9ef71b917319f6df2ea358a49641a90
kbzz <463661663@qq.com>
2018-01-18 15:21:40 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -20,7 +20,7 @@ class ProxyController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $warzone_list = Warzone::all();
-        $admin_id = Account::where('id',1)->pluck('id')->first();//查找超级管理员的数据
+        $admin_id = Account::where('id',1)->select('id')->first();//查找超级管理员的数据
         echo $admin_id;
         return view('Zerone/Proxy/proxy_add',['warzone_list'=>$warzone_list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
