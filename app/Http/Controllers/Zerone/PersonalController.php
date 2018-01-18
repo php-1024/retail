c93ec79b3c3199f2d656ce93847f85beac6d2fd1
mail@54zm.com <mail@54zm.com>
2018-01-18 13:49:01 +0800

    01

--- app/Http/Controllers/Zerone/PersonalController.php
+++ app/Http/Controllers/Zerone/PersonalController.php
@@ -70,6 +70,8 @@ class PersonalController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
+        $account = Account::getOne([['id',$admin_data['id']]]);
+        dump($account);
         return view('Zerone/Personal/password_edit',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
