07617536539d0e7956ba763c1b1835b966680f5e
kbzz <463661663@qq.com>
2018-01-19 17:36:32 +0800

    1

--- app/Http/Controllers/Zerone/CompanyController.php
+++ app/Http/Controllers/Zerone/CompanyController.php
@@ -141,7 +141,7 @@ class CompanyController extends Controller{
                 CompanyApply::editCompanyApply([['id',$id]],['status'=>$sta]);//申请通过
                 //添加服务商
                 $listdata = ['organization_name'=>$companylist['company_name'],'parent_id'=>$parent_id,'parent_tree'=>$parent_tree,'program_id'=>3,'type'=>3,'status'=>1];
-                $organization_id = Organization::addProgram($listdata); //返回值为商户的id
+                $organization_id = Organization::addorganization($listdata); //返回值为商户的id
 
                 $user = Account::max('account');
                 $account  = $user+1;//用户账号
