ab1012d012d6ed3c89b8ae866e0a7d6f78062a0b
zeo <234521890@qq.com>
2018-01-20 14:03:21 +0800

    byzeo

--- app/Helper/Common.php
+++ app/Helper/Common.php
@@ -4,10 +4,13 @@
  * key_id  - 目前以用户ID作为Redis的key的关键字
  * admin_data - 需要缓存的数据
  */
-function create_account_cache($key_id,$admin_data){
-    $admin_data = serialize($admin_data);//序列化数组数据
-    Redis::connection('zeo');//连接到我的redis服务器
-    $data_key = 'zerone_system_admin_data_'.$key_id;
-    Redis::set($data_key,$admin_data);
+if (!function_exists('create_account_cache')) {
+    function create_account_cache($key_id, $admin_data)
+    {
+        $admin_data = serialize($admin_data);//序列化数组数据
+        Redis::connection('zeo');//连接到我的redis服务器
+        $data_key = 'zerone_system_admin_data_' . $key_id;
+        Redis::set($data_key, $admin_data);
+    }
 }
 ?>
