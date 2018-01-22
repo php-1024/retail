d54ae8fa0f56f38e903b7e6ad406509f029e5de4
kbzz <463661663@qq.com>
2018-01-22 17:23:16 +0800

    1

--- resources/views/Zerone/Company/company_program.blade.php
+++ resources/views/Zerone/Company/company_program.blade.php
@@ -93,12 +93,12 @@
                                         @endforeach
                                     </td>
                                     <td>{{$value->created_at}}</td>
+                                    @foreach($value->programs as $k=>$v)
                                     <td class="text-right">
-                                        @foreach($value->programs as $k=>$v)
                                         <button class="btn btn-info btn-xs" onclick="getAssetsAdd('{{$value->id}}')"><i class="icon-arrow-down"></i>&nbsp;&nbsp;程序划入</button>
                                         <button class="btn btn-primary btn-xs"><i class="icon-arrow-up"></i>&nbsp;&nbsp;程序划出</button>
-                                        @endforeach
                                     </td>
+                                    @endforeach
                                 </tr>
                                 @endforeach
                                 </tbody>
