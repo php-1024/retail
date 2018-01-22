61640df3ea3c765aa5d4992d55b89cdaf02c07c8
mail@54zm.com <mail@54zm.com>
2018-01-22 17:32:30 +0800

    1

--- resources/views/Zerone/Company/company_program.blade.php
+++ resources/views/Zerone/Company/company_program.blade.php
@@ -94,10 +94,12 @@
                                     </td>
                                     <td>{{$value->created_at}}</td>
                                     <td class="text-right">
+                                        @foreach($value->programs as $k=>$v)
                                         <div>
-                                        <button class="btn btn-info btn-xs" onclick="getAssetsAdd('{{$value->id}}')"><i class="icon-arrow-down"></i>&nbsp;&nbsp;程序划入</button>
-                                        <button class="btn btn-primary btn-xs"><i class="icon-arrow-up"></i>&nbsp;&nbsp;程序划出</button>
+                                            <button class="btn btn-info btn-xs" onclick="getAssetsAdd('{{$value->id}}')"><i class="icon-arrow-down"></i>&nbsp;&nbsp;程序划入</button>
+                                            <button class="btn btn-primary btn-xs"><i class="icon-arrow-up"></i>&nbsp;&nbsp;程序划出</button>
                                         </div>
+                                        @endforeach
                                     </td>
                                 </tr>
                                 @endforeach
