dea4550ae3db3f7a7da7ce9c978480e768619399
mail@54zm.com <mail@54zm.com>
2018-01-22 16:46:02 +0800

    1

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -259,6 +259,7 @@ class DashboardController extends Controller{
         //获取重Admin开始的的所有人员
         $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.'0,1,'.'%']],0,'id','asc')->toArray();
         //根据获取的人员组成结构树
+        dump($list);
         $structure = $this->create_structure($list,1);
         return view('Zerone/Dashboard/structure',['structure'=>$structure ,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
