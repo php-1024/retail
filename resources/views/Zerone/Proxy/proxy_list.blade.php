26ab4d7907055b3d2061870613673a9dfff613b3
mail@54zm.com <mail@54zm.com>
2018-01-22 14:34:47 +0800

    1

--- resources/views/Zerone/Proxy/proxy_list.blade.php
+++ resources/views/Zerone/Proxy/proxy_list.blade.php
@@ -51,24 +51,24 @@
                                 <input type="text" id="amount" name="organization_name" value="{{ $search_data['organization_name'] }}" placeholder="请输入服务商名称" class="form-control">
                             </div>
                         </div>
-                        {{--<div class="col-sm-3">--}}
-                        {{--<div class="form-group">--}}
-                        {{--<label class="control-label" for="amount">手机号码</label>--}}
-                        {{--<input type="text" id="amount" name="proxy_owner_mobile" value="{{ $search_data['proxy_owner_mobile'] }}" placeholder="手机号码" class="form-control">--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-sm-3">--}}
-                        {{--<div class="form-group">--}}
-                        {{--<label class="control-label" for="amount">所在战区</label>--}}
-                        {{--<select class="form-control m-b" name="account">--}}
-                        {{--<option>东部战区</option>--}}
-                        {{--<option>西部战区</option>--}}
-                        {{--<option>南部战区</option>--}}
-                        {{--<option>北部战区</option>--}}
-                        {{--<option>中部战区</option>--}}
-                        {{--</select>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
+                        <div class="col-sm-3">
+                        <div class="form-group">
+                        <label class="control-label" for="amount">手机号码</label>
+                        <input type="text" id="amount" name="proxy_owner_mobile" value="{{ $search_data['proxy_owner_mobile'] }}" placeholder="手机号码" class="form-control">
+                        </div>
+                        </div>
+                        <div class="col-sm-3">
+                        <div class="form-group">
+                        <label class="control-label" for="amount">所在战区</label>
+                        <select class="form-control m-b" name="account">
+                        <option>东部战区</option>
+                        <option>西部战区</option>
+                        <option>南部战区</option>
+                        <option>北部战区</option>
+                        <option>中部战区</option>
+                        </select>
+                        </div>
+                        </div>
                         <div class="col-sm-3">
                             <div class="form-group">
                                 <label class="control-label" for="amount"> &nbsp;</label>
