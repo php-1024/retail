9050a63b6cb6445baefc50da573ff5248b96eadf
mail@54zm.com <mail@54zm.com>
2018-01-22 17:29:56 +0800

    1

--- app/Http/Controllers/Zerone/CompanyController.php
+++ app/Http/Controllers/Zerone/CompanyController.php
@@ -358,7 +358,7 @@ class CompanyController extends Controller{
         $listOrg = Organization::getOne([['id',$organization_id]]);
 
         $list = Package::getPaginage([],15,'id');
-
+        dump($list);
         return view('Zerone/Company/company_program',['list'=>$list,'listOrg'=>$listOrg,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //商户资产页面划入js显示
