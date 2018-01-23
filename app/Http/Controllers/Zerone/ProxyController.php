2269545ddcbb74a3d70ebebdb0c7c2de63ff8053
mail@54zm.com <mail@54zm.com>
2018-01-23 09:49:08 +0800

    1

--- app/Http/Controllers/Zerone/ProxyController.php
+++ app/Http/Controllers/Zerone/ProxyController.php
@@ -370,7 +370,8 @@ class ProxyController extends Controller{
         $program_id = $request->input('program_id');//程序id
         $num = $request->input('num');//数量
         $status = $request->input('status');//判断划入或者划出
-
+        echo $status;
+        exit();
         DB::beginTransaction();
         try{
             if($status == '1'){
