b75947739c0b71403346fc02dd9302c31dd1e803
kbzz <463661663@qq.com>
2018-01-23 10:31:15 +0800

    1

--- resources/views/Zerone/Company/company_program.blade.php
+++ resources/views/Zerone/Company/company_program.blade.php
@@ -87,7 +87,7 @@
                                         <div>
                                             <span class="label label-danger"><i class="icon-code"></i> {{$v->program_name}}</span> &nbsp;&nbsp;
                                             <span class="label label-primary">剩余：@if(!empty($v->program_spare_num)){{$v->program_spare_num}}@else 0 @endif套</span>&nbsp;&nbsp;
-                                            <span class="label label-warning">已用：@if(!empty($v->program_use_num)){{$v->program_use_num}}@else 0 @endif 套</span>&nbsp;&nbsp;
+                                            <span class="label label-warning">已用：@if(!empty($v->program_use_num)){{$v->program_use_num}}@else 0 @endif套</span>&nbsp;&nbsp;
                                         </div>
                                         <div style=" margin-top: 20px;"></div>
                                         @endforeach
