2fe49db9d70db2b998d9b7a4c4b85408e5dcfe55
kbzz <463661663@qq.com>
2018-01-23 10:11:58 +0800

    1

--- resources/views/Zerone/Company/company_program.blade.php
+++ resources/views/Zerone/Company/company_program.blade.php
@@ -95,7 +95,7 @@
                                     <td>{{$value->created_at}}</td>
                                     <td class="text-right">
                                             <button class="btn btn-info btn-xs" onclick="getAssetsAdd('{{$v->id}}','1')"><i class="icon-arrow-down"></i>&nbsp;&nbsp;程序划入</button>
-                                            <button class="btn btn-primary btn-xs"><i class="icon-arrow-up"></i>&nbsp;&nbsp;程序划出</button>
+                                            <button class="btn btn-primary btn-xs" onclick="getAssetsReduce('{{$v->id}}','0')"><i class="icon-arrow-up"></i>&nbsp;&nbsp;程序划出</button>
                                     </td>
                                 </tr>
                                 @endforeach
@@ -135,6 +135,43 @@
 
     //程序划入
     function getAssetsAdd(package_id,status) {
+
+        var url = $('#company_assets_add').val();
+        var token = $('#_token').val();
+        var organization_id = $('#organization_id').val();
+        if (package_id == '') {
+            swal({
+                title: "提示信息",
+                text: '数据传输错误',
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+            }, function () {
+                window.location.reload();
+            });
+            return;
+        }
+
+        var data = {'package_id': package_id, 'status':status, 'organization_id':organization_id, '_token': token};
+        $.post(url, data, function (response) {
+            if (response.status == '-1') {
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                }, function () {
+                    window.location.reload();
+                });
+                return;
+            } else {
+
+                $('#myModal').html(response);
+                $('#myModal').modal();
+            }
+        });
+    }
+    //程序划入
+    function getAssetsReduce(package_id,status) {
 
         var url = $('#company_assets_add').val();
         var token = $('#_token').val();
