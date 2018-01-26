bf1524dccd969e6f0fd0a2c73313befe3907b75d
kbzz <463661663@qq.com>
2018-01-26 10:35:40 +0800

    1

--- resources/views/Proxy/Login/display.blade.php
+++ resources/views/Proxy/Login/display.blade.php
@@ -37,7 +37,6 @@
 
 </div>
 
-<script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
 <script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
 <script>
     $(function(){
