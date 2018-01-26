3a25f275307cef85427940a8864616e92a5b5d9f
kbzz <463661663@qq.com>
2018-01-26 10:30:38 +0800

    1

--- resources/views/Proxy/Login/display.blade.php
+++ resources/views/Proxy/Login/display.blade.php
@@ -60,6 +60,7 @@
         var target = $("#currentForm");
         var url = target.attr("action");
         var data = target.serialize();
+        console.log(data);
 //        $.post(url,data,function(json){
 //            if(json.status==1){
 //                window.location.reload();
