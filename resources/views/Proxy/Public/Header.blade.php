6962e65c1db21ee484a49bed8524531bf39cc67d
kbzz <463661663@qq.com>
2018-01-26 17:24:29 +0800

    1

--- resources/views/Proxy/Public/Header.blade.php
+++ resources/views/Proxy/Public/Header.blade.php
@@ -1,30 +1,22 @@
-<!--header start-->
+<div class="top-nav ">
+    <!--search & user info start-->
+    <ul class="nav pull-right top-menu">
+        <li class="dropdown">
+            <a class="dropdown-toggle" href="#">
+                <i class="icon-user"></i>
+                <span class="username">admin-超级管理员</span>
+            </a>
 
-    <div class="sidebar-toggle-box">
-        <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
-    </div>
-    <!--logo start-->
-    <a href="#" class="logo">ZER<span>O</span>NE</a>
+        </li>
+        <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
 
-    <div class="top-nav ">
-        <!--search & user info start-->
-        <ul class="nav pull-right top-menu">
-            <li class="dropdown">
-                <a class="dropdown-toggle" href="#">
-                    <i class="icon-user"></i>
-                    <span class="username">admin-超级管理员</span>
-                </a>
+                <span class="username">退出登陆</span>
+                <i class="icon-arrow-right"></i>
+            </a>
 
-            </li>
-            <li class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-
-                    <span class="username">退出登陆</span>
-                    <i class="icon-arrow-right"></i>
-                </a>
-
-            </li>
-            <!-- user login dropdown end -->
-        </ul>
-        <!--search & user info end-->
-    </div>
+        </li>
+        <!-- user login dropdown end -->
+    </ul>
+    <!--search & user info end-->
+</div>
