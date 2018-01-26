34444f8008170efca2bfed767122b5acecaf3fef
kbzz <463661663@qq.com>
2018-01-26 11:27:07 +0800

    1

--- resources/views/Proxy/System/index.blade.php
+++ resources/views/Proxy/System/index.blade.php
@@ -24,188 +24,132 @@
 
     <div id="page-wrapper" class="gray-bg">
         @include('Zerone/Public/Header')
-        <div class="row wrapper border-bottom white-bg page-heading">
-            <div class="col-sm-4">
-                <h2>管理首页</h2>
-                <ol class="breadcrumb">
-                    <li>
-                        <a href="JavaScript:;">系统管理</a>
-                    </li>
-                    <li class="active">
-                        <strong>管理首页</strong>
-                    </li>
-                </ol>
-            </div>
-
-        </div>
-
-        <div class="wrapper wrapper-content animated fadeInRight">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <span class="label label-info pull-right">零壹管理系统</span>
-                            <h5>管理人员</h5>
-                        </div>
-                        <div class="ibox-content">
-                            <h1 class="no-margins">{{$zerone['system_personnel']}}人</h1>
-                            <div class="stat-percent font-bold text-info">20人 <i class="fa fa-level-up"></i></div>
-                            <small>今日</small>
-                        </div>
+        <section id="main-content">
+            <section class="wrapper">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!--breadcrumbs start -->
+                        <ul class="breadcrumb">
+                            <li><a href="#"><i class="icon-desktop"></i> 系统管理</a></li>
+                            <li class="active">系统首页</li>
+                        </ul>
+                        <!--breadcrumbs end -->
                     </div>
                 </div>
-                <div class="col-lg-3">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <span class="label label-primary pull-right">服务商系统</span>
-                            <h5>管理人员</h5>
-                        </div>
-                        <div class="ibox-content">
-                            <h1 class="no-margins">{{$zerone['service_providers']}}人</h1>
-                            <div class="stat-percent font-bold text-navy">88人 <i class="fa fa-level-up"></i></div>
-                            <small>今日</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <span class="label label-danger pull-right">商户系统</span>
-                            <h5>管理人员</h5>
-                        </div>
-                        <div class="ibox-content">
-                            <h1 class="no-margins">{{$zerone['merchant_system']}}人</h1>
-                            <div class="stat-percent font-bold text-danger">18人 <i class="fa fa-level-up"></i></div>
-                            <small>今日</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <span class="label label-success pull-right">所有业务系统</span>
-                            <h5>管理人员</h5>
-                        </div>
-                        <div class="ibox-content">
-                            <h1 class="no-margins">{{$zerone['all_system_personnel']}}人</h1>
-                            <div class="stat-percent font-bold text-success">1680人<i class="fa fa-level-up"></i></div>
-                            <small>今日</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row ">
-                <div class="col-lg-12">
-                    <div class="ibox-content forum-container">
 
-                        <div class="forum-title">
-
-                            <h3>服务商统计</h3>
-                        </div>
-
-                        <div class="forum-item active">
-                            <div class="row">
-                                <div class="col-md-9">
-                                    <div class="forum-icon">
-                                        <i class="fa fa-line-chart"></i>
-                                    </div>
-                                    <a href="javascript:;" class="forum-item-title">服务商数量</a>
-                                    <div class="forum-sub-title">进驻零壹平台的服务商的数量</div>
-                                </div>
-
-
-                                <div class="col-md-3 forum-info">
-                                            <span class="views-number">
-                                                {{$zerone['service_provider']}}家
-                                            </span>
-                                    <div>
-                                        <small>总计</small>
-                                    </div>
-                                </div>
+                <div class="row state-overview">
+                    <div class="col-lg-6 col-sm-6">
+                        <section class="panel">
+                            <div class="symbol red">
+                                <i class="icon-user"></i>
                             </div>
-                        </div>
-                        <div class="forum-item active">
-                            <div class="row">
-                                <div class="col-md-9">
-                                    <div class="forum-icon">
-                                        <i class="fa fa-line-chart"></i>
-                                    </div>
-                                    <a href="javascript:;" class="forum-item-title">商户数量</a>
-                                    <div class="forum-sub-title">注册零壹平台的商户的数量</div>
-                                </div>
-
-                                <div class="col-md-3 forum-info">
-                                            <span class="views-number">
-                                                {{$zerone['merchant']}}家
-                                            </span>
-                                    <div>
-                                        <small>总计</small>
-                                    </div>
-                                </div>
+                            <div class="value">
+                                <h1>22</h1>
+                                <p>公司人员</p>
                             </div>
-                        </div>
-                        <div class="forum-item active">
-                            <div class="row">
-                                <div class="col-md-9">
-                                    <div class="forum-icon">
-                                        <i class="fa fa-line-chart"></i>
-                                    </div>
-                                    <a href="javascript:;" class="forum-item-title">店铺数量</a>
-                                    <div class="forum-sub-title">商户在零壹平台开设的店铺数量</div>
-                                </div>
-                                <div class="col-md-3 forum-info">
-                                            <span class="views-number">
-                                                {{$zerone['shop']}}
-                                            </span>
-                                    <div>
-                                        <small>总计</small>
-                                    </div>
-                                </div>
+                        </section>
+                    </div>
+
+                    <div class="col-lg-6 col-sm-6">
+                        <section class="panel">
+                            <div class="symbol blue">
+                                <i class="icon-sitemap"></i>
                             </div>
-                        </div>
-
-
+                            <div class="value">
+                                <h1>300</h1>
+                                <p>商户</p>
+                            </div>
+                        </section>
                     </div>
                 </div>
 
-            </div>
-            <div class="row wrapper wrapper-content animated fadeInRight">
-                <div class="col-lg-7">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <h5>用户登录日志</h5>
-                        </div>
-                        <div class="ibox-content">
-                            <table class="table table-hover no-margins">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <section class="panel">
+                            <header class="panel-heading">
+                                最新登陆记录
+                            </header>
+                            <table class="table table-striped">
                                 <thead>
                                 <tr>
                                     <th>用户</th>
-                                    <th>登录IP</th>
-                                    <th>登录地址</th>
-                                    <th>登录时间</th>
+                                    <th>登陆IP</th>
+                                    <th>登陆地址</th>
+                                    <th>登陆时间</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($login_log_list as $key=>$val)
-                                    <tr>
-                                        <td>{{  $val->accounts->account }}</td>
-                                        <td>{{  long2ip($val->ip) }}</td>
-                                        <td>{{  $val->ip_position }}</td>
-                                        <td>{{  $val->created_at }}</td>
-                                    </tr>
-                                @endforeach
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>14.127.254.249</td>
+                                    <td>中国广东深圳</td>
+                                    <td>2018-01-16 09:37:37</td>
+                                </tr>
                                 </tbody>
                             </table>
-                        </div>
+                        </section>
                     </div>
-                </div>
-                <div class="col-lg-5">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <h5>用户操作日志</h5>
-                        </div>
-                        <div class="ibox-content">
-                            <table class="table table-hover no-margins">
+
+                    <div class="col-sm-6">
+                        <section class="panel">
+                            <header class="panel-heading">
+                                最新操作记录
+                            </header>
+                            <table class="table table-striped">
                                 <thead>
                                 <tr>
                                     <th>用户</th>
@@ -214,24 +158,67 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($operation_log_list as $key=>$val)
-                                    <tr>
-                                        <td>{{ $val->accounts->account }}</td>
-                                        <td>{{ $val->operation_info }}</td>
-                                        <td>{{ $val->created_at }}</td>
-                                    </tr>
-                                @endforeach
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
+                                <tr>
+                                    <td>admin</td>
+                                    <td>编辑了菜单系统管理</td>
+                                    <td>2018-01-15 22:14:16</td>
+                                </tr>
                                 </tbody>
                             </table>
-                        </div>
+                        </section>
                     </div>
                 </div>
-            </div>
-        </div>
-        @include('Zerone/Public/Footer')
+            </section>
+        </section>
+        <!--main content end-->
+        </section>
     </div>
 </div>
-
 <!-- Mainly scripts -->
 <script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
 <script src="{{asset('public/Zerone/library/bootstrap')}}/js/bootstrap.min.js"></script>
