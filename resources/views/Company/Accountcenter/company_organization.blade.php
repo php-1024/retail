ad572345f503c85fd391cf9bf6b234b02bbbdba6
mail@54zm.com <mail@54zm.com>
2018-01-26 19:15:07 +0800

    1

--- resources/views/Company/Accountcenter/company_organization.blade.php
+++ resources/views/Company/Accountcenter/company_organization.blade.php
@@ -10,15 +10,17 @@
     <title>选择服务商 | 零壹服务商管理平台</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="{{asset('public/Company')}}/css/bootstrap.min.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Company')}}/css/bootstrap-reset.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/bootstrap.min.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/bootstrap-reset.css" type="text/css"/>
     <!--external css-->
-    <link rel="stylesheet" href="{{asset('public/Company/library/font-awesome')}}/css/font-awesome.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Company/library/jquery-easy-pie-chart')}}/jquery.easy-pie-chart.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company/library/font-awesome')}}/css/font-awesome.css"
+          type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Company/library/jquery-easy-pie-chart')}}/jquery.easy-pie-chart.css"
+          type="text/css"/>
     <link rel="stylesheet" href="{{asset('public/Company')}}/css/owl.carousel.css" type="text/css">
     <!-- Custom styles for this template -->
-    <link rel="stylesheet" href="{{asset('public/Company')}}/css/style.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Company')}}/css/style-responsive.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/style-responsive.css" type="text/css"/>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
     <!--[if lt IE 9]>
     <script src="{{asset('public/Company/library/ie')}}/html5shiv.js"></script>
@@ -43,7 +45,8 @@
     <div class="row state-overview" style="margin: 10px;">
         <div class="form-group">
             <div class="col-lg-12">
-                <div class="input-group m-bot15 col-lg-3 "> <span class="input-group-addon"><i class="icon-search"></i></span>
+                <div class="input-group m-bot15 col-lg-3 "><span class="input-group-addon"><i
+                                class="icon-search"></i></span>
                     <input type="text" class="form-control " placeholder="服务商名称">
                 </div>
                 <div class="input-group m-bot15 col-lg-2 ">
@@ -55,38 +58,19 @@
     </div>
     <!--state overview start-->
     <div class="row state-overview" style="margin: 10px;">
-        <div class="col-lg-3 col-sm-6"> <a href="index.html">
+        @foreach($organization as $key=>$val)
+        <div class="col-lg-3 col-sm-6">
+            <a href="index.html">
                 <section class="panel">
-                    <div class="symbol terques"> <i class="icon-arrow-right"></i> </div>
-                    <div class="value"> <b>零壹新科技（四平）有限公司</b>
+                    <div class="symbol terques"><i class="icon-arrow-right"></i></div>
+                    <div class="value"><b>{{ $val->organization_name }}</b>
                         <p>东北战区</p>
                     </div>
                 </section>
-            </a> </div>
-        <div class="col-lg-3 col-sm-6"> <a href="index.html">
-                <section class="panel">
-                    <div class="symbol terques"> <i class="icon-arrow-right"></i> </div>
-                    <div class="value"> <b>零壹新科技（吉林）有限公司</b>
-                        <p>东北战区</p>
-                    </div>
-                </section>
-            </a> </div>
-        <div class="col-lg-3 col-sm-6"> <a href="index.html">
-                <section class="panel">
-                    <div class="symbol terques"> <i class="icon-arrow-right"></i> </div>
-                    <div class="value"> <b>零壹新科技（沈阳）有限公司</b>
-                        <p>东北战区</p>
-                    </div>
-                </section>
-            </a> </div>
-        <div class="col-lg-3 col-sm-6"> <a href="index.html">
-                <section class="panel">
-                    <div class="symbol terques"> <i class="icon-arrow-right"></i> </div>
-                    <div class="value"> <b>零壹新科技（哈尔滨）有限公司</b>
-                        <p>东北战区</p>
-                    </div>
-                </section>
-            </a> </div>
+            </a>
+        </div>
+        @endforeach
+
     </div>
     <!--state overview end-->
 </section>
@@ -99,8 +83,8 @@
 <script src="{{asset('public/Company')}}/js/jquery.nicescroll.js" type="text/javascript"></script>
 <script src="{{asset('public/Company')}}/js/jquery.sparkline.js" type="text/javascript"></script>
 <script src="{{asset('public/Company/library/jquery-easy-pie-chart')}}/jquery.easy-pie-chart.js"></script>
-<script src="{{asset('public/Company')}}/js/owl.carousel.js" ></script>
-<script src="{{asset('public/Company')}}/js/jquery.customSelect.min.js" ></script>
+<script src="{{asset('public/Company')}}/js/owl.carousel.js"></script>
+<script src="{{asset('public/Company')}}/js/jquery.customSelect.min.js"></script>
 
 <!--common script for all pages-->
 <script src="{{asset('public/Company')}}/js/common-scripts.js"></script>
@@ -112,19 +96,19 @@
 
     //owl carousel
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $("#owl-demo").owlCarousel({
-            navigation : true,
-            slideSpeed : 300,
-            paginationSpeed : 400,
-            singleItem : true
+            navigation: true,
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            singleItem: true
 
         });
     });
 
     //custom select box
 
-    $(function(){
+    $(function () {
         $('select.styled').customSelect();
     });
 
