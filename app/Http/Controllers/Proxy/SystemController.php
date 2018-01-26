74d6b99a0398bd0343dec8c9ecae3fe491a0540d
kbzz <463661663@qq.com>
2018-01-26 16:35:31 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -17,11 +17,14 @@ class SystemController extends Controller{
     //添加服务商
     public function display(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dump($admin_data);
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        return view('Proxy/System/index',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        if($admin_data['super_id'] == 1){
+            return view('Proxy/System/select_proxy');
+        }else{
+            return view('Proxy/System/index',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        }
     }
     //注册提交商户数据
     public function company_add_check(Request $request){
