00abaaa4c64f1be8e0fedfa008ef489bd9ba2d46
kbzz <463661663@qq.com>
2018-01-26 15:38:46 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -19,10 +19,7 @@ class SystemController extends Controller{
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
-        dd($son_menu_data);
-
         $route_name = $request->path();//获取当前的页面路由
-
         return view('Proxy/System/index',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //注册提交商户数据
