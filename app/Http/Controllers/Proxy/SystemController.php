d93311559fee014490885138f7e4938fafdfa54b
kbzz <463661663@qq.com>
2018-01-26 16:54:06 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -8,6 +8,7 @@ class SystemController extends Controller{
     public function display(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
+        dd($menu_data);
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         if($admin_data['super_id'] == 1){
