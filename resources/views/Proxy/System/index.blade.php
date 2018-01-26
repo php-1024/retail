af9490dcc4320ca36bdd83be09cab6b686f23769
kbzz <463661663@qq.com>
2018-01-26 14:48:43 +0800

    1

--- resources/views/Proxy/System/index.blade.php
+++ resources/views/Proxy/System/index.blade.php
@@ -345,11 +345,11 @@
 </section>
 
 <!-- js placed at the end of the document so the pages load faster -->
-<script src="js/jquery.js"></script>
-<script src="js/jquery-1.8.3.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.scrollTo.min.js"></script>
-<script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery-1.8.3.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.scrollTo.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.nicescroll.js" type="text/javascript"></script>
 
 <!--common script for all pages-->
 <script src="js/common-scripts.js"></script>
