276a98221055a4ad292fefaf028140ffbc719f4a
kbzz <463661663@qq.com>
2018-01-26 18:19:15 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -10,11 +10,11 @@ class SystemController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        if($admin_data['super_id'] == 1){
-            return view('Proxy/System/select_proxy');
-        }else{
+//        if($admin_data['super_id'] == 1){
+//            return view('Proxy/System/select_proxy');
+//        }else{
             return view('Proxy/System/index',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
-        }
+//        }
     }
 
     //退出登录
