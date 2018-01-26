fe6a82b44a064b367af2dae089538e3fe47f7e4b
kbzz <463661663@qq.com>
2018-01-26 14:58:34 +0800

    1

--- resources/views/Proxy/System/index.blade.php
+++ resources/views/Proxy/System/index.blade.php
@@ -1,301 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>零壹新科技服务商管理平台</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="{{asset('public/Proxy')}}/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{asset('public/Proxy')}}/css/bootstrap-reset.css" rel="stylesheet">
-    <!--external css-->
-    <link href="{{asset('public/Proxy')}}/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link href="{{asset('public/Proxy')}}/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>
-    <link rel="stylesheet" href="{{asset('public/Proxy')}}/css/owl.carousel.css" type="text/css">
-    <!-- Custom styles for this template -->
-    <link href="{{asset('public/Proxy')}}/css/style.css" rel="stylesheet">
-    <link href="{{asset('public/Proxy')}}/css/style-responsive.css" rel="stylesheet" />
+    <title>零壹新科技程序管理平台</title>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
-    <!--[if lt IE 9]>
-    <script src="{{asset('public/Proxy')}}/js/html5shiv.js"></script>
-    <script src="{{asset('public/Proxy')}}/js/respond.min.js"></script>
-    <![endif]-->
+    <link href="{{asset('public/Zerone/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/font')}}/css/font-awesome.css" rel="stylesheet">
+
+    <link href="{{asset('public/Zerone')}}/css/animate.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone')}}/css/style.css" rel="stylesheet">
+
 </head>
 
-<body>
+<body class="">
 
-<section id="container" class="">
-    <!--header start-->
-    <header class="header white-bg">
-        <div class="sidebar-toggle-box">
-            <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
-        </div>
-        <!--logo start-->
-        <a href="#" class="logo">ZER<span>O</span>NE</a>
+<div id="wrapper">
 
-        <div class="top-nav ">
-            <!--search & user info start-->
-            <ul class="nav pull-right top-menu">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" href="#">
-                        <i class="icon-user"></i>
-                        <span class="username">admin-超级管理员</span>
-                    </a>
+    @include('Zerone/Public/Nav')
 
-                </li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-
-                        <span class="username">退出登陆</span>
-                        <i class="icon-arrow-right"></i>
-                    </a>
-
-                </li>
-                <!-- user login dropdown end -->
-            </ul>
-            <!--search & user info end-->
-        </div>
-    </header>
-    <!--header end-->
-    <!--sidebar start-->
-    <aside>
-        <div id="sidebar"  class="nav-collapse ">
-            <!-- sidebar menu start-->
-            <ul class="sidebar-menu">
-                @include('Tooling/Public/Nav')
-            </ul>
-            <!-- sidebar menu end-->
-        </div>
-    </aside>
-    <!--sidebar end-->
-    <!--main content start-->
-    <section id="main-content">
-        <section class="wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <!--breadcrumbs start -->
-                    <ul class="breadcrumb">
-                        <li><a href="#"><i class="icon-desktop"></i> 系统管理</a></li>
-                        <li class="active">系统首页</li>
-                    </ul>
-                    <!--breadcrumbs end -->
-                </div>
+    <div id="page-wrapper" class="gray-bg">
+        @include('Zerone/Public/Header')
+        <div class="row wrapper border-bottom white-bg page-heading">
+            <div class="col-sm-4">
+                <h2>管理首页</h2>
+                <ol class="breadcrumb">
+                    <li>
+                        <a href="JavaScript:;">系统管理</a>
+                    </li>
+                    <li class="active">
+                        <strong>管理首页</strong>
+                    </li>
+                </ol>
             </div>
 
-            <div class="row state-overview">
-                <div class="col-lg-6 col-sm-6">
-                    <section class="panel">
-                        <div class="symbol red">
-                            <i class="icon-user"></i>
-                        </div>
-                        <div class="value">
-                            <h1>22</h1>
-                            <p>公司人员</p>
-                        </div>
-                    </section>
-                </div>
+        </div>
 
-                <div class="col-lg-6 col-sm-6">
-                    <section class="panel">
-                        <div class="symbol blue">
-                            <i class="icon-sitemap"></i>
-                        </div>
-                        <div class="value">
-                            <h1>300</h1>
-                            <p>商户</p>
-                        </div>
-                    </section>
-                </div>
-            </div>
+     
+        @include('Zerone/Public/Footer')
+    </div>
+</div>
 
-            <div class="row">
-                <div class="col-sm-6">
-                    <section class="panel">
-                        <header class="panel-heading">
-                            最新登陆记录
-                        </header>
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th>用户</th>
-                                <th>登陆IP</th>
-                                <th>登陆地址</th>
-                                <th>登陆时间</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>14.127.254.249</td>
-                                <td>中国广东深圳</td>
-                                <td>2018-01-16 09:37:37</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </section>
-                </div>
+<!-- Mainly scripts -->
+<script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
+<script src="{{asset('public/Zerone/library/bootstrap')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Zerone/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
+<script src="{{asset('public/Zerone/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
 
-                <div class="col-sm-6">
-                    <section class="panel">
-                        <header class="panel-heading">
-                            最新操作记录
-                        </header>
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th>用户</th>
-                                <th>操作</th>
-                                <th>时间</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            <tr>
-                                <td>admin</td>
-                                <td>编辑了菜单系统管理</td>
-                                <td>2018-01-15 22:14:16</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </section>
-                </div>
-            </div>
-        </section>
-    </section>
-    <!--main content end-->
-</section>
-
-<!-- js placed at the end of the document so the pages load faster -->
-<script src="{{asset('public/Proxy')}}/js/jquery.js"></script>
-<script src="{{asset('public/Proxy')}}/js/jquery-1.8.3.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/jquery.scrollTo.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/jquery.nicescroll.js" type="text/javascript"></script>
-
-<!--common script for all pages-->
-<script src="{{asset('public/Proxy')}}/js/common-scripts.js"></script>
-
-
-<script>
-
-    //owl carousel
-
-    $(document).ready(function() {
-        $("#owl-demo").owlCarousel({
-            navigation : true,
-            slideSpeed : 300,
-            paginationSpeed : 400,
-            singleItem : true
-
-        });
-    });
-
-    //custom select box
-
-    $(function(){
-        $('select.styled').customSelect();
-    });
-
-</script>
+<!-- Custom and plugin javascript -->
+<script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
+<script src="{{asset('public/Zerone/library/pace')}}/js/pace.min.js"></script>
 
 </body>
+
 </html>
