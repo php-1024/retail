0362b5bafff60cd97c41be1c6d0536e725b00508
kbzz <463661663@qq.com>
2018-01-26 17:22:50 +0800

    1

--- resources/views/Proxy/System/index.blade.php
+++ resources/views/Proxy/System/index.blade.php
@@ -27,7 +27,34 @@
 
 <section id="container" class="">
     <header class="header white-bg">
-        @include('Zerone/Public/Header')
+        <div class="sidebar-toggle-box">
+            <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
+        </div>
+        <!--logo start-->
+        <a href="#" class="logo">ZER<span>O</span>NE</a>
+
+        <div class="top-nav ">
+            <!--search & user info start-->
+            <ul class="nav pull-right top-menu">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" href="#">
+                        <i class="icon-user"></i>
+                        <span class="username">admin-超级管理员</span>
+                    </a>
+
+                </li>
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+
+                        <span class="username">退出登陆</span>
+                        <i class="icon-arrow-right"></i>
+                    </a>
+
+                </li>
+                <!-- user login dropdown end -->
+            </ul>
+            <!--search & user info end-->
+        </div>
     </header>
     <aside>
         <div id="sidebar"  class="nav-collapse ">
