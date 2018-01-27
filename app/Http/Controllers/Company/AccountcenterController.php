91be79af8afd14b16e7b08ed7e786de64b40f4a3
mail@54zm.com <mail@54zm.com>
2018-01-27 10:58:10 +0800

    1

--- app/Http/Controllers/Company/AccountcenterController.php
+++ app/Http/Controllers/Company/AccountcenterController.php
@@ -19,7 +19,7 @@ class AccountcenterController extends Controller{
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
         $route_name = $request->path();                     //获取当前的页面路由
         dump($request);
-        dump($request->organization);
+        dump($request->organization_id);
         if(!empty($admin_data['super_id']) && $admin_data['super_id'] == 1){
             $organization = Organization::getlist(['type'=>'3']); //如何是admin则获取所有组织信息
             if (!empty($request->organization_id)){
