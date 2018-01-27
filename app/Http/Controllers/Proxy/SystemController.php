9dc8cbbb96c7b204a0a384934d1229d1a432ad9a
kbzz <463661663@qq.com>
2018-01-27 15:16:40 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -38,6 +38,7 @@ class SystemController extends Controller{
     public function select_proxy(Request $request){
         $organization_id = $request->input('organization_id');//中间件产生的管理员数据参数
         $account_info = Account::getOneAccount([['organization_id',$organization_id],['parent_id','1']]);//根据账号查询
+        dd($account_info);exit;
         if(!empty($account_info)){
             //重新生成缓存的登录信息
             $admin_data = [
