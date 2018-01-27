90b2a9de3c38e6f1669a814520ed6af66b45a178
kbzz <463661663@qq.com>
2018-01-27 16:52:27 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -71,6 +71,7 @@ class SystemController extends Controller{
             \ZeroneRedis::create_proxy_account_cache(1,$admin_data);//生成账号数据的Redis缓存
             \ZeroneRedis::create_proxy_menu_cache(1);//生成对应账号的系统菜单
         }else{
+            echo 1;exit;
             return response()->json(['data' => '操作失败', 'status' => '0']);
         }
     }
