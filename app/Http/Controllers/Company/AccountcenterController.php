e830a46d62c088fec65a748ebe7324154eca691d
mail@54zm.com <mail@54zm.com>
2018-01-27 10:45:14 +0800

    1

--- app/Http/Controllers/Company/AccountcenterController.php
+++ app/Http/Controllers/Company/AccountcenterController.php
@@ -22,7 +22,11 @@ class AccountcenterController extends Controller{
         dump($request->organization);
         dump($organization);
         if(!empty($admin_data['super_id']) && $admin_data['super_id'] == 1){
-            return  view('Company/Accountcenter/company_organization',['organization'=>$organization]);
+            if (!empty($request->organization){
+                return view('Company/Accountcenter/display',['organization_id'=>$request->organization,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+            }else{
+                return  view('Company/Accountcenter/company_organization',['organization'=>$organization]);
+            }
         }else{
             return view('Company/Accountcenter/display',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
         }
