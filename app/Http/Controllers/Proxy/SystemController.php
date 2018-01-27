88a6db30804e0bdeac56ca185da5560fcdd0bb05
kbzz <463661663@qq.com>
2018-01-27 10:43:56 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -10,11 +10,18 @@ class SystemController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-//        if($admin_data['super_id'] == 1){
-//            return view('Proxy/System/select_proxy');
-//        }else{
-            return view('Proxy/System/index',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
-//        }
+        if($admin_data['super_id'] == 1){
+            return view('Proxy/System/select_proxy');
+        }else{
+
+            $where = [];
+            if($admin_data['id']<>1){   //不是超级管理员的时候，只查询自己相关的数据【后期考虑转为查询自己及自己管理的下级人员的所有操作记录】
+                $where = [['account_id',$admin_data['id']]];
+            }
+            $login_log_list = LoginLog::getList($where,10,'id');//登录记录
+            $operation_log_list = OperationLog::getList($where,10,'id');//操作记录
+            return view('Proxy/System/index',['login_log_list'=>$login_log_list,'operation_log_list'=>$operation_log_list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        }
     }
 
     //退出登录
