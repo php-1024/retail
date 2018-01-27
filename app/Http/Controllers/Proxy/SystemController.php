c37b07b211fbee08c93abf47176c418e7fe254b7
kbzz <463661663@qq.com>
2018-01-27 15:37:11 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -12,9 +12,9 @@ use Session;
 class SystemController extends Controller{
     //添加服务商
     public function display(Request $request){
-        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dd($admin_data);
+        Session::put('proxy_account_id','');
 
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
