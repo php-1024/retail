12f7089b620e530e931ca24a23e16de84ec9e41d
kbzz <463661663@qq.com>
2018-01-27 14:36:56 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -38,7 +38,7 @@ class SystemController extends Controller{
     public function select_proxy(Request $request){
         $organization_id = $request->input('organization_id');//中间件产生的管理员数据参数
         $account_info = Account::getOneAccount([['organization_id',$organization_id],['parent_id','1']]);//根据账号查询
-        if(!empty($list)){
+        if(!empty($account_info)){
             //登录成功要生成缓存的登录信息
             $admin_data = [
                 'id'=>$account_info->id,    //用户ID
@@ -53,7 +53,6 @@ class SystemController extends Controller{
                 'account_status'=>$account_info->status,//用户状态
                 'super_id' => '2' //超级管理员进入后切换身份用
             ];
-            dd($admin_data);
             //构造用户缓存数据
             if(!empty( $account_info->account_info->realname)) {
                 $admin_data['realname'] = $account_info->account_info->realname;
