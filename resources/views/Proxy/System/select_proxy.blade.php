40f41842dbac7bfe2c65f3cd6b4bd9be3aac88ed
kbzz <463661663@qq.com>
2018-01-27 14:02:09 +0800

    1

--- resources/views/Proxy/System/select_proxy.blade.php
+++ resources/views/Proxy/System/select_proxy.blade.php
@@ -73,46 +73,5 @@
     <!--state overview end-->
 </section>
 
-
-<!-- js placed at the end of the document so the pages load faster -->
-<script src="{{asset('public/Proxy')}}/js/jquery.js"></script>
-<script src="{{asset('public/Proxy')}}/js/jquery-1.8.3.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/jquery.scrollTo.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/jquery.nicescroll.js" type="text/javascript"></script>
-<script src="{{asset('public/Proxy')}}/js/jquery.sparkline.js" type="text/javascript"></script>
-<script src="{{asset('public/Proxy')}}/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-<script src="{{asset('public/Proxy')}}/js/owl.carousel.js" ></script>
-<script src="{{asset('public/Proxy')}}/js/jquery.customSelect.min.js" ></script>
-
-<!--common script for all pages-->
-<script src="{{asset('public/Proxy')}}/js/common-scripts.js"></script>
-
-<!--script for this page-->
-<script src="{{asset('public/Proxy')}}/js/sparkline-chart.js"></script>
-<script src="{{asset('public/Proxy')}}/js/easy-pie-chart.js"></script>
-
-<script>
-
-    //owl carousel
-
-    $(document).ready(function() {
-        $("#owl-demo").owlCarousel({
-            navigation : true,
-            slideSpeed : 300,
-            paginationSpeed : 400,
-            singleItem : true
-
-        });
-    });
-
-    //custom select box
-
-    $(function(){
-        $('select.styled').customSelect();
-    });
-
-</script>
-
 </body>
 </html>
