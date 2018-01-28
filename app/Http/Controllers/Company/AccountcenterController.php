28c89f84baddd83ac4826810778a5e065d02b4d5
ismzxw <mail@54zm.com>
2018-01-28 20:39:30 +0800

    1

--- app/Http/Controllers/Company/AccountcenterController.php
+++ app/Http/Controllers/Company/AccountcenterController.php
@@ -29,9 +29,9 @@ class AccountcenterController extends Controller{
 
         if($admin_data['is_super'] == 1 && $admin_data['organization_id'] == 0){    //如果是超级管理员并且组织ID等于零则进入选择组织页面
             $organization = Organization::getlist(['type'=>'3']);                   //如何是admin则获取所有组织信息
+            dump($request);
             return  view('Company/Accountcenter/company_organization',['organization'=>$organization]);
         }
-        dump($request);
         $accountInfo = AccountInfo::getOne(['id' => $admin_data['id']]);
         $organization = Organization::getOne(['id' => $admin_data['organization_id']]);
         return view('Company/Accountcenter/display',['organization'=>$organization,'account_info'=>$accountInfo,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
