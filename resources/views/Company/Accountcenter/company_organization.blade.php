e2cc7703eee164c29ba45fda528bd11af19e575b
ismzxw <mail@54zm.com>
2018-01-28 21:45:02 +0800

    1

--- resources/views/Company/Accountcenter/company_organization.blade.php
+++ resources/views/Company/Accountcenter/company_organization.blade.php
@@ -117,7 +117,8 @@
         var url = target.attr("action");
         var data = target.serialize();
         $.post(url, data, function (json) {
-            window.location.reload();
+            console.log(json);
+//            window.location.reload();
         });
     }
     @endforeach
