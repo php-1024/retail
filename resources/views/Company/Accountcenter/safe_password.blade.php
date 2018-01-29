e5331ba5f3729bda7e82fefd80fc41b957c3b296
mail@54zm.com <mail@54zm.com>
2018-01-29 15:13:40 +0800

    1

--- resources/views/Company/Accountcenter/safe_password.blade.php
+++ resources/views/Company/Accountcenter/safe_password.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" class="app">
 <head>
     <meta charset="utf-8" />
-    <title>密码设置 | 零壹云管理平台 | 商户管理系统</title>
+    <title>安全密码设置 | 零壹云管理平台 | 商户管理系统</title>
     <link rel="stylesheet" href="{{asset('public/Company/library/jPlayer')}}/jplayer.flat.css" type="text/css" />
     <link rel="stylesheet" href="{{asset('public/Company')}}/css/bootstrap.css" type="text/css" />
     <link rel="stylesheet" href="{{asset('public/Company')}}/css/animate.css" type="text/css" />
@@ -130,13 +130,13 @@
                                                     <span>账号信息</span>
                                                 </a>
                                             </li>
-                                            <li  class="active">
+                                            <li>
                                                 <a href="password.html" class="auto">
                                                     <i class="fa fa-angle-right text-info"></i>
                                                     <span>登陆密码修改</span>
                                                 </a>
                                             </li>
-                                            <li >
+                                            <li class="active">
                                                 <a href="safe_password.html" class="auto">
                                                     <i class="fa fa-angle-right text-info"></i>
                                                     <span>安全密码设置</span>
@@ -202,12 +202,12 @@
                 <section class="vbox">
                     <section class="scrollable padder">
                         <div class="m-b-md">
-                            <h3 class="m-b-none">登陆密码</h3>
+                            <h3 class="m-b-none">安全密码</h3>
                         </div>
                         <section class="panel panel-default">
 
                             <header class="panel-heading font-bold">
-                                登陆密码修改
+                                安全密码设置
                             </header>
                             <div class="panel-body">
                                 <form class="form-horizontal" method="get">
@@ -221,37 +221,72 @@
 
                                     <div class="line line-dashed b-b line-lg pull-in"></div>
                                     <div class="form-group">
-                                        <label class="col-sm-2 control-label" for="input-id-1">原登陆密码</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="input-id-1" value="">
-                                        </div>
-                                    </div>
-
-                                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label" for="input-id-1">新登陆密码</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="input-id-1" value="">
-                                        </div>
-                                    </div>
-
-                                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label" for="input-id-1">重复新密码</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="input-id-1" value="">
-                                        </div>
-                                    </div>
-
-                                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                                    <div class="form-group">
                                         <label class="col-sm-2 control-label" for="input-id-1">安全密码</label>
                                         <div class="col-sm-10">
                                             <input type="text" class="form-control" id="input-id-1" value="">
                                         </div>
                                     </div>
-                                    <div class="line line-dashed b-b line-lg pull-in"></div>
 
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">重复安全密码</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <div class="col-sm-12 col-sm-offset-6">
+
+                                            <button type="button" class="btn btn-success" id="addBtn">保存信息</button>
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                </form>
+                            </div>
+                        </section>
+
+                        <section class="panel panel-default">
+
+                            <header class="panel-heading font-bold">
+                                安全密码修改
+                            </header>
+                            <div class="panel-body">
+                                <form class="form-horizontal" method="get">
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">登陆账号</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" disabled="" value="200307">
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">原安全密码</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">新安全密码</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">重复安全密码</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
                                     <div class="form-group">
                                         <div class="col-sm-12 col-sm-offset-6">
 
