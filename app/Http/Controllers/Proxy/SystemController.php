bbeb022c3574ee424aa93d0aaad967dc12e20afb
kbzz <463661663@qq.com>
2018-01-29 19:36:45 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -195,8 +195,10 @@ class SystemController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
 
         $account = $request->input('account');//通过登录页账号查询
-
-        $search_data = ['operation_log.organization_id'=>$admin_data['organization_id']];
+        $search_data = [['operation_log.organization_id',$admin_data['organization_id']]];
+        if(!empty($account)){
+            $search_data[] = ['$account',$account];
+        }
         $list = OperationLog::getProxyPaginate($search_data,10,'id');
         $roles = [];
         foreach($list as $key=>$val){
