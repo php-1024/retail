e46fd5fc2f237505ebf5b9e577bd8a2cd9468878
kbzz <463661663@qq.com>
2018-01-29 18:18:38 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -156,6 +156,8 @@ class SystemController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = $admin_data['organization_id'];//当前组织ID，零壹管理平台组织只能为1
+        $oneAcc = Account::getOne([['organization_id',$organization_id],['parent_id',1]]);
+        dd($oneAcc);
         //获取重Admin开始的的所有人员
         $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.'0,1,'.'%']],0,'id','asc')->toArray();
         //根据获取的人员组成结构树
