bf003c568e140e748f1e0945bb88f9be835c5fda
kbzz <463661663@qq.com>
2018-01-29 11:39:39 +0800

    1

--- app/Http/Controllers/Proxy/PersonaController.php
+++ app/Http/Controllers/Proxy/PersonaController.php
@@ -6,7 +6,6 @@ use Session;
 class PersonaController extends Controller{
     //修改安全密码
     public function safe_password(Request $request){
-        dd(1);
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
