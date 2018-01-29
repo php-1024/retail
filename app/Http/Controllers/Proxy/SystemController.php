6ce87c2f045490fa74cc34050a1b4c7300cf1591
kbzz <463661663@qq.com>
2018-01-29 09:50:55 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -13,12 +13,11 @@ class SystemController extends Controller{
     //添加服务商
     public function display(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dump($admin_data);
-
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         if($admin_data['super_id'] == 1){
+            dump($admin_data);
             $listOrg = Organization::getPaginage([['program_id','2']],20,'id');
             foreach ($listOrg as $k=>$v){
                 $zone_id = $v['warzoneProxy']['zone_id'];
