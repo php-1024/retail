25d7bd202aaf089eb6fea04018bdac2bec5d34ca
kbzz <463661663@qq.com>
2018-01-29 18:01:08 +0800

    1

--- resources/views/Proxy/System/select_structure.blade.php
+++ resources/views/Proxy/System/select_structure.blade.php
@@ -18,7 +18,7 @@
     <link href="{{asset('public/Proxy')}}/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>
     <link rel="stylesheet" href="{{asset('public/Proxy')}}/css/owl.carousel.css" type="text/css">
     <link href="{{asset('public/Zerone/library/sweetalert')}}/css/sweetalert.css" rel="stylesheet">
- 
+
     <!-- Custom styles for this template -->
     <link href="{{asset('public/Proxy')}}/css/style.css" rel="stylesheet">
     <link href="{{asset('public/Proxy')}}/css/style-responsive.css" rel="stylesheet" />
@@ -42,131 +42,7 @@
     <aside>
         @include('Proxy/Public/Nav')
     </aside>
-    <!--sidebar end-->
-    <!--main content start-->
-    <section id="container" class="">
-        <!--header start-->
-        <header class="header white-bg">
-            <div class="sidebar-toggle-box">
-                <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
-            </div>
-            <!--logo start-->
-            <a href="#" class="logo">ZER<span>O</span>NE</a>
 
-            <div class="top-nav ">
-                <!--search & user info start-->
-                <ul class="nav pull-right top-menu">
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" href="#">
-                            <i class="icon-user"></i>
-                            <span class="username">admin-超级管理员</span>
-                        </a>
-
-                    </li>
-                    <li class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-
-                            <span class="username">退出登陆</span>
-                            <i class="icon-arrow-right"></i>
-                        </a>
-
-                    </li>
-                    <!-- user login dropdown end -->
-                </ul>
-                <!--search & user info end-->
-            </div>
-        </header>
-        <!--header end-->
-        <!--sidebar start-->
-        <aside>
-            <div id="sidebar"  class="nav-collapse ">
-                <!-- sidebar menu start-->
-                <ul class="sidebar-menu">
-                    <li class="sub-menu">
-                        <a href="javascript:;">
-                            <i class="icon-desktop"></i>
-                            <span>系统管理</span>
-                            <span class="arrow"></span>
-                        </a>
-                        <ul class="sub">
-                            <li><a href="index.html">系统首页</a></li>
-                            <li><a href="config.html">参数设置</a></li>
-                            <li><a href="proxy_info.html">公司信息设置</a></li>
-                            <li><a href="structure.html">公司人员结构</a></li>
-                            <li><a href="operationlog.html">操作日志查询</a></li>
-                            <li><a href="loginlog.html">登陆日志查询</a></li>
-                        </ul>
-                    </li>
-                    <li class="sub-menu">
-                        <a href="javascript:;">
-                            <i class="icon-user"></i>
-                            <span>个人信息</span>
-                            <span class="arrow"></span>
-                        </a>
-                        <ul class="sub">
-                            <li><a href="account_info.html">个人信息</a></li>
-                            <li><a href="password.html">登陆密码修改</a></li>
-                            <li><a href="safe_password.html">安全密码修改</a></li>
-                            <li><a href="myoperationlog.html">我的操作日志</a></li>
-                            <li><a href="myloginlog.html">我的登陆日志</a></li>
-                        </ul>
-                    </li>
-                    <li class="sub-menu">
-                        <a href="javascript:;">
-                            <i class="icon-group"></i>
-                            <span>下级人员管理</span>
-                            <span class="arrow"></span>
-                        </a>
-                        <ul class="sub">
-                            <li><a href="role_add.html">权限角色添加</a></li>
-                            <li><a href="role_list.html">权限角色列表</a></li>
-                            <li><a href="subordinate_add.html">下级人员添加</a></li>
-                            <li><a href="subordinate_list.html">下级人员列表</a></li>
-                            <li><a href="subordinate_structure.html">下级人员结构</a></li>
-                        </ul>
-                    </li>
-                    <li class="sub-menu">
-                        <a href="javascript:;">
-                            <i class="icon-code"></i>
-                            <span>系统资产管理</span>
-                            <span class="arrow"></span>
-                        </a>
-                        <ul class="sub">
-                            <li><a href="program_list.html">系统资产列表</a></li>
-                            <li><a href="program_log.html">资产划拨记录</a></li>
-                        </ul>
-                    </li>
-                    <li class="sub-menu">
-                        <a href="javascript:;">
-                            <i class="icon-sitemap"></i>
-                            <span>下辖商户管理</span>
-                            <span class="arrow open"></span>
-                        </a>
-                        <ul class="sub">
-                            <li><a href="register_setting.html">商户注册列表</a></li>
-                            <li><a href="company_list.html">商户列表</a></li>
-                        </ul>
-                    </li>
-                    <li class="sub-menu active">
-                        <a href="javascript:;">
-                            <i class="icon-cloud"></i>
-                            <span>商户店铺管理</span>
-                            <span class="arrow open"></span>
-                        </a>
-                        <ul class="sub">
-                            <li class="active"><a href="store_list.html">店铺列表</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="login.html">
-                            <i class="icon-user"></i>
-                            <span>退出登陆</span>
-                        </a>
-                    </li>
-                </ul>
-                <!-- sidebar menu end-->
-            </div>
-        </aside>
         <!--sidebar end-->
         <!--main content start-->
         <section id="main-content">
@@ -338,8 +214,6 @@
             </section>
         </section>
         <!--main content end-->
-    </section>
-
 
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="{{asset('public/Proxy')}}/js/jquery.js"></script>
