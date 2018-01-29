c90ca40ba29d7c7602524c64c4f160cb80c63f55
kbzz <463661663@qq.com>
2018-01-29 15:36:29 +0800

    1

--- app/Http/Controllers/Proxy/PersonaController.php
+++ app/Http/Controllers/Proxy/PersonaController.php
@@ -59,7 +59,7 @@ class PersonaController extends Controller{
                 try {
                     if($admin_data['super_id'] == 2){
                         Account::editAccount([['id',1]],['safe_password' => $encryptPwd]);
-                        OperationLog::addOperationLog('1','1','1',$route_name,'修改了安全密码');//保存操作记录
+                        OperationLog::addOperationLog('1','1','1',$route_name,'在服务商系统修改了安全密码');//保存操作记录
                     }else{
                         Account::editAccount([['id',$admin_data['id']]],['safe_password' => $encryptPwd]);
                         OperationLog::addOperationLog('2',$admin_data['organization_id'],$admin_data['id'],$route_name,'修改了安全密码');//保存操作记录
