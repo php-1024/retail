8592f96f5c637dcf4bddf6b710b60287edc51e70
kbzz <463661663@qq.com>
2018-01-29 18:44:14 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -160,6 +160,7 @@ class SystemController extends Controller{
         $parent_tree = $oneAcc['parent_tree'];//组织树
         //获取重Admin开始的的所有人员
         $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.$parent_tree.$oneAcc['id'].',%']],0,'id','asc')->toArray();
+        dd($parent_tree.$oneAcc['id']);
         //根据获取的人员组成结构树
         $structure = $this->create_structure($list,$oneAcc['parent_id']);
         return view('Proxy/System/select_structure',['oneAcc'=>$oneAcc,'structure'=>$structure,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
