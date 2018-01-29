362d87ca942944f8317063ad522dedd62ef86c34
kbzz <463661663@qq.com>
2018-01-29 19:33:30 +0800

    1

--- app/Models/OperationLog.php
+++ app/Models/OperationLog.php
@@ -63,12 +63,10 @@ class OperationLog extends Model{
     }
     //获取联表的分页数据
     public static function getProxyPaginate($where,$paginate,$orderby,$sort='DESC'){
-        $model = self::join('account',function($join){
+        $model = self::where($where)->join('account',function($join){
             $join->on('operation_log.account_id','account.id');
         })->select('account.account','operation_log.*');
-        if(!empty($where)){
-            $model =$model->where($where);
-        }
+   
         return $model->orderBy($orderby,$sort)->paginate($paginate);
     }
 }
