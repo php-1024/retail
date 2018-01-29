f03ae5eeba690e52e6fcbc15a9acb4e200b9b835
mail@54zm.com <mail@54zm.com>
2018-01-29 15:39:25 +0800

    1

--- app/Services/ZeroneRedis/ZeroneRedis.php
+++ app/Services/ZeroneRedis/ZeroneRedis.php
@@ -26,15 +26,6 @@ class ZeroneRedis
     }
 
     /*
-     * 商户平台超级管理员使用
-     */
-    public static function create_super_company_account_cache($key_id,$admin_data){
-        $admin_data = serialize($admin_data);//序列化数组数据
-        Redis::connection('company');//连接到我的redis服务器-商户平台使用
-        $data_key = 'super_company_system_admin_data_'.$key_id;
-        Redis::set($data_key,$admin_data);
-    }
-    /*
      * 商户平台
      */
     public static function create_company_account_cache($key_id,$admin_data){
@@ -114,30 +105,6 @@ class ZeroneRedis
         Redis::connection('company');//连接到我的redis服务器——商户平台使用
         $menu_key = 'company_system_menu_'.$id;  //一级菜单的Redis主键。
         $son_menu_key = 'company_system_son_menu_'.$id;//子菜单的Redis主键
-        Redis::set($menu_key,$menu);
-        Redis::set($son_menu_key,$son_menu);
-    }
-
-    //内部方法，生成商户系统账号的菜单
-    /*
-     * id - 用户的ID
-     */
-    public static function create_super_company_menu_cache($id){
-        $menu = ProgramMenu::getList([[ 'parent_id',0],['program_id','3']],0,'id','asc');//获取商户系统的一级菜单
-        $son_menu = [];
-        foreach($menu as $key=>$val){//获取一级菜单下的子菜单
-            $son_menu[$val->id] = ProgramMenu::son_menu($val->id);
-        }
-        if($id <> 1){
-            /**
-             * 未完成，这里准备查询用户权限。
-             */
-        }
-        $menu = serialize($menu);
-        $son_menu = serialize($son_menu);
-        Redis::connection('company');//连接到我的redis服务器——商户平台使用
-        $menu_key = 'super_company_system_menu_'.$id;  //一级菜单的Redis主键。
-        $son_menu_key = 'super_company_system_son_menu_'.$id;//子菜单的Redis主键
         Redis::set($menu_key,$menu);
         Redis::set($son_menu_key,$son_menu);
     }
