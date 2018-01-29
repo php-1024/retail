5c371bc2b29046cecf99c34f8ee7501f2f6982f1
mail@54zm.com <mail@54zm.com>
2018-01-29 16:12:05 +0800

    1

--- resources/views/Company/Accountcenter/password.blade.php
+++ resources/views/Company/Accountcenter/password.blade.php
@@ -125,7 +125,7 @@
 <script src="{{asset('public/Company')}}/js/bootstrap.js"></script>
 <!-- App -->
 <script src="{{asset('public/Company')}}/js/app.js"></script>
-<script src="{{asset('public/Company/library/slimscroll')}}/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Company/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
 <script src="{{asset('public/Company')}}/js/app.plugin.js"></script>
 <script src="{{asset('public/Company/library/jPlayer')}}/jquery.jplayer.min.js"></script>
 <script src="{{asset('public/Company/library/jPlayer')}}/add-on/jplayer.playlist.min.js"></script>
