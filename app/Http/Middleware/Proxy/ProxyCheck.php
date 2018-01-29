cdc7547aa3a42c5e9c961024d20deaf5e0d85542
kbzz <463661663@qq.com>
2018-01-29 13:02:32 +0800

    1

--- app/Http/Middleware/Proxy/ProxyCheck.php
+++ app/Http/Middleware/Proxy/ProxyCheck.php
@@ -63,8 +63,8 @@ class ProxyCheck{
                 }
                 $admin_data['super_id'] = 1; //切换权限
                 \ZeroneRedis::create_proxy_account_cache(1,$admin_data);//生成账号数据的Redis缓存
-                dd($admin_data);
-
+                
+                return self::res(1,$re2['response']);
             }
         }
     }
