331c63c4c5f540da2ad415a7ee292a480f473c5a
kbzz <463661663@qq.com>
2018-01-30 10:33:55 +0800

    1

--- app/Http/Controllers/Proxy/PersonaController.php
+++ app/Http/Controllers/Proxy/PersonaController.php
@@ -19,7 +19,7 @@ class PersonaController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
         $account_id = $admin_data['id'];//当前登录账号ID
         $user = Account::getOne([['id',$admin_data['id']]]);
-        if($account_id == 1) {//如果是超级管理员
+        if($admin_data['super_id'] == 2) {//如果是超级管理员
             $module_node_list = Module::getListProgram(1, [], 0, 'id');//获取当前系统的所有模块和节点
         }else{
             $account_node_list = ProgramModuleNode::getAccountModuleNodes(1,$admin_data['id']);//获取当前用户具有权限的节点
@@ -41,7 +41,7 @@ class PersonaController extends Controller{
                 unset($module);
             }
         }
-        dump($user);
+        dump($module_node_list);
         return view('Proxy/Persona/account_info',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
 
     }
