6ab203fa6d2e49bc9063054b8a76d1b528addd29
kbzz <463661663@qq.com>
2018-01-30 17:02:01 +0800

    1

--- resources/views/Proxy/Public/Nav.blade.php
+++ resources/views/Proxy/Public/Nav.blade.php
@@ -4,7 +4,7 @@
         <ul class="sidebar-menu">
             @foreach($menu_data as $key=>$val)
             <li @if(in_array($route_name,explode(',',$val->menu_routes_bind))) class="active sub-menu" @endif>
-                <a href="javascript:;">
+                <a href="index.html">
                     <i class="{{ $val->icon_class }}"></i>
                     <span>{{ $val->menu_name }}</span>
                     <span class="arrow open"></span>
