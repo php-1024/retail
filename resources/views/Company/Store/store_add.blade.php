44a109f7b3ce534bc774769e12cb387d5acc3b70
mail@54zm.com <mail@54zm.com>
2018-01-30 11:34:47 +0800

    1

--- resources/views/Company/Store/store_add.blade.php
+++ resources/views/Company/Store/store_add.blade.php
@@ -42,7 +42,7 @@
                                         <div class="clear">
                                             <div class=" m-t-xs m-b-xs">
                                                 {{$v['package_name']}}
-                                                <i class="fa fa-rmb text-success text-lg pull-right">价值{{$v['package_price']}}</i>
+                                                <i class="fa fa-rmb text-success text-lg pull-left">{{$v['package_price']}}</i>
                                                 <i class="fa fa-cutlery text-success text-lg pull-right"></i>
                                             </div>
 
