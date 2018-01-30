63f77232eb716a96ac00e67472bad0a475b86345
kbzz <463661663@qq.com>
2018-01-30 19:39:58 +0800

    1

--- app/Http/Controllers/Proxy/CompanyController.php
+++ app/Http/Controllers/Proxy/CompanyController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers\Proxy;
 use App\Http\Controllers\Controller;
+use App\Models\Organization;
+use App\Models\ProxyApply;
 use Illuminate\Http\Request;
 use Session;
 class CompanyController extends Controller{
@@ -10,8 +12,20 @@ class CompanyController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
+        $proxy_name = $request->input('proxy_name');
+        $proxy_owner_mobile = $request->input('proxy_owner_mobile');
+        $search_data = ['proxy_name'=>$proxy_name,'proxy_owner_mobile'=>$proxy_owner_mobile];
 
-        return view('Proxy/Company/company_register',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        $where = [['parent_id',$admin_data['organization']]];
+        if(!empty($proxy_name)){
+            $where[] = ['proxy_name','like','%'.$proxy_name.'%'];
+        }
+        if(!empty($proxy_owner_mobile)){
+            $where[] = ['proxy_owner_mobile',$proxy_owner_mobile];
+        }
+        $list = ProxyApply::getPaginage($where,'15','id');
+        dump($list);
+        return view('Proxy/Company/company_register',['list'=>$list,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //商户列表
     public function company_list(Request $request){
