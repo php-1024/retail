0e234ac0772c123077f9dd66054e501e85cccfd6
kbzz <463661663@qq.com>
2018-01-30 16:28:13 +0800

    1

--- resources/views/Proxy/Role/role_add.blade.php
+++ resources/views/Proxy/Role/role_add.blade.php
@@ -20,6 +20,7 @@
     <!-- Custom styles for this template -->
     <link href="{{asset('public/Proxy')}}/css/style.css" rel="stylesheet">
     <link href="{{asset('public/Proxy')}}/css/style-responsive.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('public/Proxy')}}/css/owl.carousel.css" type="text/css">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
     <!--[if lt IE 9]>
