bb7c2ad490c4c48654ba72044816f1ba4e8fec36
kbzz <463661663@qq.com>
2018-01-30 16:40:21 +0800

    1

--- resources/views/Proxy/Role/role_add.blade.php
+++ resources/views/Proxy/Role/role_add.blade.php
@@ -216,9 +216,16 @@
     </section>
     <!--main content end-->
 </section>
-
 <!-- js placed at the end of the document so the pages load faster -->
 <script src="{{asset('public/Proxy')}}/js/jquery.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery-1.8.3.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.scrollTo.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.nicescroll.js" type="text/javascript"></script>
+
+<!--common script for all pages-->
+<script src="{{asset('public/Proxy')}}/js/common-scripts.js"></script>
+<!-- js placed at the end of the document so the pages load faster -->
 <!--common script for all pages-->
 <script src="{{asset('public/Proxy')}}/js/icheck.min.js"></script>
 
