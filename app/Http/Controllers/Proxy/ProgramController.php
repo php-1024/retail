e40b3be3aa1e570c4e87e091e519eada6544963c
kbzz <463661663@qq.com>
2018-01-30 17:40:04 +0800

    1

--- app/Http/Controllers/Proxy/ProgramController.php
+++ app/Http/Controllers/Proxy/ProgramController.php
@@ -10,7 +10,18 @@ class ProgramController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
+        $organization_id = $admin_data['organization_id'];//服务商id
+        $listOrg = Organization::getOneProxy([['id',$organization_id]]);
 
+        $list = Package::getPaginage([],15,'id');
+        foreach ($list as $key=>$value) {
+            foreach ($value['programs'] as $k => $v) {
+                $re = Assets::getOne([['organization_id', $organization_id], ['package_id', $value['id']], ['program_id', $v['id']]]);
+                $list[$key]['programs'][$k]['program_spare_num'] = $re['program_spare_num'];
+                $list[$key]['programs'][$k]['program_use_num'] = $re['program_use_num'];
+            }
+        }
+        dump($listOrg);
         return view('Proxy/Program/program_list',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //添加服务商
