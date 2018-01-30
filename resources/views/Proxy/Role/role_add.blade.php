bc7ab214c6bd639ae643f79b53fda47c53ba0259
kbzz <463661663@qq.com>
2018-01-30 17:12:41 +0800

    1

--- resources/views/Proxy/Role/role_add.blade.php
+++ resources/views/Proxy/Role/role_add.blade.php
@@ -39,6 +39,26 @@
     <!--main content end-->
 </section>
 
+<!-- js placed at the end of the document so the pages load faster -->
+<script src="{{asset('public/Proxy')}}/js/jquery.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery-1.8.3.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.scrollTo.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.nicescroll.js" type="text/javascript"></script>
+
+<!--common script for all pages-->
+<script src="{{asset('public/Proxy')}}/js/common-scripts.js"></script>
+<script src="{{asset('public/Proxy')}}/js/icheck.min.js"></script>
+
+<script>
+    $(function(){
+        $('.i-checks').iCheck({
+            checkboxClass: 'icheckbox_square-green',
+            radioClass: 'iradio_square-green',
+        });
+        $('select.styled').customSelect();
+    });
+</script>
 </body>
 </html>
 
