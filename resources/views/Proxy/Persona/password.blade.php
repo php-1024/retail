d6f38b436a8c6be289d0610b845ae7b48761cc5b
mail@54zm.com <mail@54zm.com>
2018-01-31 11:16:21 +0800

    1

--- resources/views/Proxy/Persona/password.blade.php
+++ resources/views/Proxy/Persona/password.blade.php
@@ -111,9 +111,16 @@
     </section>
     <!--main content end-->
 </section>
+<!-- js placed at the end of the document so the pages load faster -->
+<script src="{{asset('public/Proxy')}}/js/jquery-1.8.3.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.scrollTo.min.js"></script>
+<script src="{{asset('public/Proxy')}}/js/jquery.nicescroll.js" type="text/javascript"></script>
+
+<!--common script for all pages-->
+<script src="{{asset('public/Proxy')}}/js/common-scripts.js"></script>
 <!-- Custom and plugin javascript -->
 <script src="{{asset('public/Proxy/library/jquery')}}/js/jquery-2.1.1.js"></script>
-<script src="{{asset('public/Proxy')}}/js/bootstrap.min.js"></script>
 <script src="{{asset('public/Proxy/library/sweetalert')}}/js/sweetalert.min.js"></script>
 
 
