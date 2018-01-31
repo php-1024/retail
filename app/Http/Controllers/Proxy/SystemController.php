ec39a2a1611211732562bea8399c0bf8236d9afa
mail@54zm.com <mail@54zm.com>
2018-01-31 18:52:53 +0800

    1

--- app/Http/Controllers/Proxy/SystemController.php
+++ app/Http/Controllers/Proxy/SystemController.php
@@ -81,6 +81,7 @@ class SystemController extends Controller{
             return response()->json(['data' => '操作成功', 'status' => '1']);
 
         }else{
+            dd($request);
             return response()->json(['data' => '操作失败', 'status' => '0']);
         }
     }
