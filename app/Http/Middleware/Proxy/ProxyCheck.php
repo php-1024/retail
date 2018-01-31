66b33257ba87c98d4562af7c6a625f88d5487e99
mail@54zm.com <mail@54zm.com>
2018-01-31 16:41:41 +0800

    1

--- app/Http/Middleware/Proxy/ProxyCheck.php
+++ app/Http/Middleware/Proxy/ProxyCheck.php
@@ -48,7 +48,7 @@ class ProxyCheck{
             /****下级人员管理--权限角色****/
             case "proxy/role/role_add":                 //权限角色添加
             case "proxy/role/role_list":                //权限角色列表
-            case "proxy/system/subordinate_add":        //下级人员添加
+            case "proxy/role/subordinate_add":          //下级人员添加
             /****下级人员管理--权限角色****/
 
             /****系统资产管理****/
