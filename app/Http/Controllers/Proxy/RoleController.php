20947d9a7f7264e2a6207603a4efc95a25c3c157
mail@54zm.com <mail@54zm.com>
2018-01-31 17:45:36 +0800

    1

--- app/Http/Controllers/Proxy/RoleController.php
+++ app/Http/Controllers/Proxy/RoleController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers\Proxy;
 use App\Http\Controllers\Controller;
+use App\Models\Module;
+use App\Models\ProgramModuleNode;
 use Illuminate\Http\Request;
 use Session;
 class RoleController extends Controller{
@@ -11,7 +13,30 @@ class RoleController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
+        if($admin_data['id'] == 1) {
+            $module_node_list = Module::getListProgram(1, [], 0, 'id');//获取当前系统的所有模块和节点
+        }else{
+            $account_node_list = ProgramModuleNode::getAccountModuleNodes(1,$admin_data['id']);//获取当前用户具有权限的节点
 
+            $modules = [];
+            $nodes = [];
+            $module_node_list = [];
+            //过滤重复选出的节点和模块
+            foreach($account_node_list as $key=>$val){
+                $modules[$val->module_id] = $val->module_name;
+                $nodes[$val->module_id][$val->node_id] = $val->node_name;
+            }
+            //遍历，整理为合适的格式
+            foreach($modules as $key=>$val){
+                $module = ['id'=>$key,'module_name'=>$val];
+                foreach($nodes[$key] as $k=>$v){
+                    $module['program_nodes'][] = array('id'=>$k,'node_name'=>$v);
+                }
+                $module_node_list[] = $module;
+                unset($module);
+            }
+        }
+        dump($module_node_list);
         return view('Proxy/Role/role_add',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
