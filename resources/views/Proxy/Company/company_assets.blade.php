46ce600bb96500fa93a75f292ccaa51c7d3ba585
mail@54zm.com <mail@54zm.com>
2018-01-31 11:42:52 +0800

    1

--- resources/views/Proxy/Company/company_assets.blade.php
+++ resources/views/Proxy/Company/company_assets.blade.php
@@ -73,7 +73,7 @@
                     title: "提示信息",
                     text: json.data,
                     confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "确定",
+                    confirmButtonText: "确定"
                 },function(){
                     window.location.reload();
                 });
@@ -82,8 +82,7 @@
                     title: "提示信息",
                     text: json.data,
                     confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "确定",
-                    //type: "warning"
+                    confirmButtonText: "确定"
                 });
             }
         });
