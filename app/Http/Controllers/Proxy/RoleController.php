054a4349c5d57b01e8211b45ba8829ab9ad28842
kbzz <463661663@qq.com>
2018-01-31 20:46:23 +0800

    1

--- app/Http/Controllers/Proxy/RoleController.php
+++ app/Http/Controllers/Proxy/RoleController.php
@@ -80,7 +80,6 @@ class RoleController extends Controller{
     //下级人员管理权限角色列表
     public function role_list(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dd($admin_data);
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
