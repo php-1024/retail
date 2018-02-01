ef4cefa81090825b365339d349ad9b852fd25d7d
mail@54zm.com <mail@54zm.com>
2018-02-01 19:31:28 +0800

    1

--- resources/views/Company/Store/store_list.blade.php
+++ resources/views/Company/Store/store_list.blade.php
@@ -61,9 +61,7 @@
                                     <div class="line line-dashed b-b line-lg pull-in"></div>
                                     <div>
                                         主账号：<label class="label label-info pull-right">
-                                            @foreach($val as $kk=>$vv)
-                                                @php(var_dump($vv))
-                                            @endforeach
+                                            {{$val->account->account}}
                                         </label>
                                     </div>
                                 </div>
