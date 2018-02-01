80a024dc90b0585c8aa42bed0d92e53451d8f7fe
mail@54zm.com <mail@54zm.com>
2018-02-01 16:09:42 +0800

    1

--- app/Http/Controllers/Company/StoreController.php
+++ app/Http/Controllers/Company/StoreController.php
@@ -20,7 +20,7 @@ class StoreController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();                 //获取当前的页面路由
         $program = Program::getList(['id'=>3],0,'id','DESC');//查询所有商户系统套餐程序
-        $package = Package::getList(['id'=>3],0,'id','DESC');//查询所有商户系统套餐程序
+        $package = Package::getList(['id'=>1],0,'id','DESC');//查询所有商户系统套餐程序
         dump($package);
         return view('Company/Store/store_add',['program'=>$program,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
