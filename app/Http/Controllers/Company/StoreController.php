3bf7d5591874e2ac372453b8d835d12bb0d85ae9
mail@54zm.com <mail@54zm.com>
2018-02-01 18:27:06 +0800

    1

--- app/Http/Controllers/Company/StoreController.php
+++ app/Http/Controllers/Company/StoreController.php
@@ -106,6 +106,7 @@ class StoreController extends Controller{
         $menu_data = $request->get('menu_data');        //中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();                 //获取当前的页面路由
+        dump($admin_data);
         return view('Company/Store/store_list',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 }
