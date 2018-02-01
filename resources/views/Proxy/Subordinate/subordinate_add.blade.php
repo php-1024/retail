3abd52ad8a6797bb8fbde602e3b21f08b3deb6a0
mail@54zm.com <mail@54zm.com>
2018-02-01 15:21:06 +0800

    1

--- resources/views/Proxy/Subordinate/subordinate_add.blade.php
+++ resources/views/Proxy/Subordinate/subordinate_add.blade.php
@@ -108,30 +108,30 @@
                                                     </button>
                                                 </div>
                                             </div>
-                                            <div class="form-group">
-                                                <label class="col-sm-2 control-label">用户权限</label>
-                                                <div class="col-sm-10">
-                                                    @foreach($module_node_list as $key=>$val)
-                                                        <group class="checked_box_group_{{ $val['id'] }}">
-                                                            <div>
-                                                                <label class="i-checks">
-                                                                    <input type="checkbox" @if(in_array($val['id'],$selected_modules)) checked="checked" @endif class="checkbox_module_name checkbox_module_name_{{ $val['id'] }}" value="{{ $val['id'] }}"> {{ $val['module_name'] }}
-                                                                </label>
-                                                            </div>
-                                                            <div>
-                                                                @foreach($val['program_nodes'] as $kk=>$vv)
-                                                                    <label class="i-checks">
-                                                                        <input type="checkbox" @if(in_array($vv['id'],$selected_nodes)) checked="checked" @endif  data-group_id="{{  $val['id'] }}" class="checkbox_node_name checkbox_node_name_{{ $val['id'] }}" name="module_node_ids[]" value="{{ $vv['id'] }}"> {{ $vv['node_name'] }}
-                                                                    </label>
-                                                                    &nbsp;&nbsp;
+                                            {{--<div class="form-group">--}}
+                                                {{--<label class="col-sm-2 control-label">用户权限</label>--}}
+                                                {{--<div class="col-sm-10">--}}
+                                                    {{--@foreach($module_node_list as $key=>$val)--}}
+                                                        {{--<group class="checked_box_group_{{ $val['id'] }}">--}}
+                                                            {{--<div>--}}
+                                                                {{--<label class="i-checks">--}}
+                                                                    {{--<input type="checkbox" @if(in_array($val['id'],$selected_modules)) checked="checked" @endif class="checkbox_module_name checkbox_module_name_{{ $val['id'] }}" value="{{ $val['id'] }}"> {{ $val['module_name'] }}--}}
+                                                                {{--</label>--}}
+                                                            {{--</div>--}}
+                                                            {{--<div>--}}
+                                                                {{--@foreach($val['program_nodes'] as $kk=>$vv)--}}
+                                                                    {{--<label class="i-checks">--}}
+                                                                        {{--<input type="checkbox" @if(in_array($vv['id'],$selected_nodes)) checked="checked" @endif  data-group_id="{{  $val['id'] }}" class="checkbox_node_name checkbox_node_name_{{ $val['id'] }}" name="module_node_ids[]" value="{{ $vv['id'] }}"> {{ $vv['node_name'] }}--}}
+                                                                    {{--</label>--}}
+                                                                    {{--&nbsp;&nbsp;--}}
 
-                                                                @endforeach
-                                                            </div>
-                                                        </group>
-                                                        <div style="margin-top: 20px;"></div>
-                                                    @endforeach
-                                                </div>
-                                            </div>
+                                                                {{--@endforeach--}}
+                                                            {{--</div>--}}
+                                                        {{--</group>--}}
+                                                        {{--<div style="margin-top: 20px;"></div>--}}
+                                                    {{--@endforeach--}}
+                                                {{--</div>--}}
+                                            {{--</div>--}}
                                             <div class="form-group" id="module_node_box"></div>
                                             <div class="form-group">
                                                 <label class="col-sm-2 control-label">安全密码</label>
