3f0ebde7b5e70fb459bc49661ef3e3e8e0001a0f
kbzz <463661663@qq.com>
2018-02-01 17:51:51 +0800

    1

--- resources/views/Proxy/Subordinate/subordinate_lock.blade.php
+++ resources/views/Proxy/Subordinate/subordinate_lock.blade.php
@@ -1,34 +1,39 @@
-<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/subordinate_lock') }}">
+<form class="form-horizontal tasi-form" method="post" role="form" id="currentForm" action="{{ url('proxy/ajax/subordinate_lock') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" id="id" value="{{$id}}">
     <input type="hidden" name="account" id="account" value="{{$account}}">
     <input type="hidden" name="status" id="status" value="{{$status}}">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content animated fadeIn">
+    <div class="modal-dialog">
+        <div class="modal-content">
             <div class="modal-header">
-                确认冻结下级人员
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">确认冻结用户</h4>
             </div>
             <div class="modal-body">
                 <div class="form-group">
                     <label class="col-sm-2 control-label">用户账号</label>
-                    <div class="col-sm-10">{{ $account }}</div>
+                    <div class="col-sm-10"><input type="text" value="{{ $account }}" disabled="" class="form-control"></div>
                 </div>
-                <div style="clear:both"></div>
-                <div class="hr-line-dashed"></div>
+
                 <div class="form-group">
                     <label class="col-sm-2 control-label">安全密码</label>
                     <div class="col-sm-10"><input type="password" class="form-control" id="safe_password" name="safe_password"></div>
                 </div>
-                <div style="clear:both"></div>
             </div>
-
             <div class="modal-footer">
-                <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
-                <button type="button" class="btn btn-primary" onclick="return postForm();">确定</button>
+                <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
+                <button class="btn btn-success" type="button"  onclick="return postForm();">确认</button>
             </div>
         </div>
     </div>
 </form>
+
+
+
+
+
+
+
 <script>
     //提交表单
     function postForm() {
@@ -48,13 +53,14 @@
                     window.location.reload();
                 });
             }else{
-                swal({
-                    title: "提示信息",
-                    text: json.data,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "确定",
-                    //type: "warning"
-                });
+                console.log(json);
+//                swal({
+//                    title: "提示信息",
+//                    text: json.data,
+//                    confirmButtonColor: "#DD6B55",
+//                    confirmButtonText: "确定",
+//                    //type: "warning"
+//                });
             }
         });
     }
