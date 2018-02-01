89e66f3d6bceb466a06dd4800374d8e4407015e2
kbzz <463661663@qq.com>
2018-02-01 18:07:52 +0800

    1

--- resources/views/Proxy/Subordinate/selected_rule.blade.php
+++ resources/views/Proxy/Subordinate/selected_rule.blade.php
@@ -10,7 +10,7 @@
             </div>
             <div>
                 @foreach($val['program_nodes'] as $kk=>$vv)
-                    <label class="checkbox-inline i-checks">
+                    <label class="i-checks">
                         <input type="checkbox"  data-group_id="{{ $val['id'] }}" @if(in_array($vv['id'],$selected_nodes)) checked="checked" @endif class="checkbox_node_name checkbox_node_name_{{ $val['id'] }}" name="module_node_ids[]" value="{{ $vv['id'] }}"> {{ $vv['node_name'] }}
                     </label>
                 @endforeach
