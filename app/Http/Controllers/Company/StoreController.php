079b973cb2608a665f7365187cf1b3b43b064efa
mail@54zm.com <mail@54zm.com>
2018-02-01 16:11:50 +0800

    1

--- app/Http/Controllers/Company/StoreController.php
+++ app/Http/Controllers/Company/StoreController.php
@@ -20,8 +20,7 @@ class StoreController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();                 //获取当前的页面路由
         $program = Program::getList(['id'=>3],0,'id','DESC');//查询所有商户系统套餐程序
-        $package = Package::getList(['id'=>1],0,'id','DESC');//查询所有商户系统套餐程序
-        dump($package);
+        dump($program);
         return view('Company/Store/store_add',['program'=>$program,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
@@ -34,7 +33,7 @@ class StoreController extends Controller{
         $route_name = $request->path();                 //获取当前的页面路由
         $package_id = $request->package_id;             //套餐id
         $program = Program::getList(['complete_id'=>'3'],0,'id','DESC');
-        $package_program = PackageProgram::getlist(['package_id'=>$package_id]);
+        $package_program = Package::getList(['id'=>$package_id],0,'id','DESC');   //查询当前所选餐包含的程序
         dump($package_program);
         return view('Company/Store/store_add_second',['program'=>$program,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
