b11e5b0bf8726df292d564c93e27c079bf2716dd
mail@54zm.com <mail@54zm.com>
2018-02-01 10:38:34 +0800

    1

--- resources/views/Zerone/Dashboard/operation_log.blade.php
+++ resources/views/Zerone/Dashboard/operation_log.blade.php
@@ -94,7 +94,7 @@
                                         <td>{{ $val->accounts->id }}</td>
                                         <td>{{ $val->accounts->account }}</td>
 
-                                        <td>@if(empty($roles[$val->id]))角色未设置@else{{ $roles[$val->id] }}@endif</td>
+                                        <td>@if(empty($roles[$val->id]))角色未设置@else{{ $roles[$val->id] }}{{$val->id}}@endif</td>
 
                                         <td>{{ $val->operation_info }}</td>
                                         <td>{{ $val->created_at }}</td>
