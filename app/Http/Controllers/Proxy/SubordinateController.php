067ce7a5bfe6112600e0032b9de8e9a0a5adbd55
mail@54zm.com <mail@54zm.com>
2018-02-01 14:29:54 +0800

    1

--- app/Http/Controllers/Proxy/SubordinateController.php
+++ app/Http/Controllers/Proxy/SubordinateController.php
@@ -21,7 +21,8 @@ class SubordinateController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
         //获取当前用户添加的权限角色
         $role_list = OrganizationRole::getList([['program_id',2],['created_by',$admin_data['id']]],0,'id');
-        return view('Proxy/Subordinate/subordinate_add',['role_list'=>$role_list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        $module_node_list = Module::getListProgram(2, [], 0, 'id');//获取当前系统的所有模块和节点
+        return view('Proxy/Subordinate/subordinate_add',['module_node_list'=>$module_node_list,'role_list'=>$role_list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
     //快速授权功能
