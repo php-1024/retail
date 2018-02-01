27bf3274569f04ef43bbb39a22ea0cf8ef8d1314
kbzz <463661663@qq.com>
2018-02-01 15:28:10 +0800

    1

--- resources/views/Zerone/Subordinate/subordinate_edit.blade.php
+++ resources/views/Zerone/Subordinate/subordinate_edit.blade.php
@@ -1,58 +1,40 @@
-<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/subordinate_edit_check') }}">
+<form class="form-horizontal tasi-form" method="post" role="form" id="currentForm" action="{{ url('proxy/ajax/subordinate_edit_check') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" id="id" value="{{ $info->id }}">
     <input type="hidden" name="account" id="account" value="{{ $info->account }}">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content animated fadeIn">
+    <div class="modal-dialog">
+        <div class="modal-content">
             <div class="modal-header">
-                修改用户
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">编辑用户</h4>
             </div>
             <div class="modal-body">
-
                 <div class="form-group">
                     <label class="col-sm-2 control-label">用户账号</label>
-                    <div class="col-sm-10">{{ $info->account }}</div>
+                    <div class="col-sm-10"><input type="text" value="{{ $info->account }}" disabled="" class="form-control"></div>
                 </div>
-
-                <div style="clear:both"></div>
-                <div class="hr-line-dashed"></div>
 
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">登录密码</label>
-                    <div class="col-sm-10"><input type="password" class="form-control" name="password" value=""></div>
+                    <label class="col-sm-2 control-label">用户密码</label>
+                    <div class="col-sm-10"><input type="password" class="form-control" name="password"  value=""></div>
                 </div>
-                <div style="clear:both"></div>
-                <div class="hr-line-dashed"></div>
-
                 <div class="form-group">
                     <label class="col-sm-2 control-label">真实姓名</label>
                     <div class="col-sm-10"><input type="text" class="form-control" name="realname" value="@if(!empty($info->account_info)){{ $info->account_info->realname }}@endif"></div>
                 </div>
-                <div style="clear:both"></div>
-                <div class="hr-line-dashed"></div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">联系方式</label>
-                    <div class="col-sm-10"><input type="text" class="form-control" name="mobile" value="{{ $info->mobile }}"></div>
-                </div>
-                <div style="clear:both"></div>
-                <div class="hr-line-dashed"></div>
-
                 <div class="form-group">
                     <label class="col-sm-2 control-label">安全密码</label>
-                    <div class="col-sm-10"><input type="password" class="form-control" id="safe_password" name="safe_password"></div>
+                    <div class="col-sm-10"><input type="password" class="form-control"  name="safe_password"></div>
                 </div>
-                <div style="clear:both"></div>
-                <div class="hr-line-dashed"></div>
             </div>
-
             <div class="modal-footer">
-                <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
-                <button type="button" class="btn btn-primary" onclick="return postForm();">保存</button>
+                <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
+                <button class="btn btn-success" type="button" id="save_btn" onclick="return postForm();">保存</button>
             </div>
         </div>
     </div>
 </form>
+
 <script>
     //提交表单
     function postForm() {
@@ -72,13 +54,14 @@
                     window.location.reload();
                 });
             }else{
-                swal({
-                    title: "提示信息",
-                    text: json.data,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "确定",
-                    //type: "warning"
-                });
+                console.log(json);
+//                swal({
+//                    title: "提示信息",
+//                    text: json.data,
+//                    confirmButtonColor: "#DD6B55",
+//                    confirmButtonText: "确定",
+//                    //type: "warning"
+//                });
             }
         });
     }
