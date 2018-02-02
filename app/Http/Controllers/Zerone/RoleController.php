674375913b053fb79d1eb0041b11c2a0b47a9c0d
mail@54zm.com <mail@54zm.com>
2018-02-02 18:07:53 +0800

    1

--- app/Http/Controllers/Zerone/RoleController.php
+++ app/Http/Controllers/Zerone/RoleController.php
@@ -19,7 +19,7 @@ class RoleController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
 
-        $re = ProgramModuleNode::getModuleNodes('1');
+        $re = ProgramModuleNode::getModuleNodes([['id'=>'1']]);
         dump($re);
 
         if($admin_data['id'] == 1) {
