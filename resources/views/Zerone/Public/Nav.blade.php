1a99e4433642f586a23636f307d693a7e1ee0114
mail@54zm.com <mail@54zm.com>
2018-02-02 17:43:01 +0800

    1

--- resources/views/Zerone/Public/Nav.blade.php
+++ resources/views/Zerone/Public/Nav.blade.php
@@ -14,7 +14,7 @@
             </li>
             @foreach($menu_data as $key=>$val)
             <li @if(in_array($route_name,explode(',',$val->menu_routes_bind))) class="active" @endif>
-                <a href="index.html"><i class="{{ $val->icon_class }}"></i> <span class="nav-label">{{ $val->menu_name }}</span> <span class="fa arrow"></span></a>
+                <a href="javascript:;"><i class="{{ $val->icon_class }}"></i> <span class="nav-label">{{ $val->menu_name }}</span> <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level collapse">
                     @foreach($son_menu_data[$val->id] as $k=>$v)
                     <li @if($route_name == $v->menu_route)class="active"@endif><a href="{{ url($v->menu_route) }}">{{ $v->menu_name }}</a></li>
