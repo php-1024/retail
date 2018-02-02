d493156ca6ab72b44cc4d44cc56928a49b59fb31
kbzz <463661663@qq.com>
2018-02-02 16:52:51 +0800

    1

--- resources/views/Proxy/System/loginlog.blade.php
+++ resources/views/Proxy/System/loginlog.blade.php
@@ -62,7 +62,7 @@
                                 <div class="form-group">
                                     <label class="control-label col-lg-1" for="inputSuccess">用户账号</label>
                                     <div class="col-lg-2">
-                                        <input type="text" class="form-control" placeholder="用户账号"  name="account" value="{{ $search_data['operation_log.account'] }}">
+                                        <input type="text" class="form-control" placeholder="用户账号"  name="account" value="{{ $search_data['account.account'] }}">
                                     </div>
                                     <div class="col-lg-2">
                                         <button type="submit" class="btn btn-primary"><i class="icon-search"></i> 查询</button>
