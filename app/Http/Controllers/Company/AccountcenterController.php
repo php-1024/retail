21d30ffd7b155d5e9624fe122e1ecf9af1f7a2cb
mail@54zm.com <mail@54zm.com>
2018-02-02 16:51:12 +0800

    1

--- app/Http/Controllers/Company/AccountcenterController.php
+++ app/Http/Controllers/Company/AccountcenterController.php
@@ -290,7 +290,7 @@ class AccountcenterController extends Controller{
             $time_st_format = strtotime($time_st . ' 00:00:00');//开始时间转时间戳
             $time_nd_format = strtotime($time_nd . ' 23:59:59');//结束时间转时间戳
         }
-        dump($admin_data);
+        dump($request);
         //只查询自己相关的数据
         $where = [
             ['account_id',$admin_data['id']],
