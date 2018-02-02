7b1a06b1f447ae82a24db9268d6e42c22f45545e
kbzz <463661663@qq.com>
2018-02-02 15:36:16 +0800

    1

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -43,7 +43,7 @@ class Organization extends Model{
     }
     //和WarzoneProxy表 warzone表 一对一的关系
     public function warzone(){
-        return $this->belongsToMany('App\Models\Warzone','WarzoneProxy','organization_id','zone_id');
+        return $this->belongsToMany('App\Models\Warzone','warzone_proxy','organization_id','zone_id');
     }
 
     //获取分页数据-服务商
