7afcb99b28a69948385c37cf435e8865fd0d00cb
kbzz <463661663@qq.com>
2018-02-02 14:29:23 +0800

    1

--- app/Http/Middleware/Proxy/ProxyCheck.php
+++ app/Http/Middleware/Proxy/ProxyCheck.php
@@ -90,10 +90,10 @@ class ProxyCheck{
                 return $re2;
             }else{
                 $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-                if($admin_data['super_id'] != 2){ //防止直接输入地址访问
+                if($admin_data['is_super'] != 2){ //防止直接输入地址访问
                     return self::res(0,$request);
                 }
-                $admin_data['super_id'] = 1; //切换权限
+                $admin_data['is_super'] = 1; //切换权限
                 \ZeroneRedis::create_proxy_account_cache(1,$admin_data);//生成账号数据的Redis缓存
                 return self::res(1,$request);
 
