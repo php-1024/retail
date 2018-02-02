392a4986bc96daca1073f45651d0df870be5b68a
mail@54zm.com <mail@54zm.com>
2018-02-02 15:52:44 +0800

    1

--- app/Http/Controllers/Company/AccountcenterController.php
+++ app/Http/Controllers/Company/AccountcenterController.php
@@ -126,9 +126,9 @@ class AccountcenterController extends Controller{
             AccountInfo::editAccountInfo([['account_id',$admin_data['id']]],['realname'=>$realname]);
             //添加操作日志
             if ($admin_data['is_super'] == 1){//超级管理员操作商户的记录
-                OperationLog::addOperationLog('1','1','1',$route_name,'在商户系统修改了商户的个人账号信息！');//保存操作记录
+                OperationLog::addOperationLog('3','1','1',$route_name,'在商户系统修改了商户的个人账号信息！');//保存操作记录
             }else{//商户本人操作记录
-                OperationLog::addOperationLog('1',$admin_data['organization_id'],$admin_data['id'],$route_name, '修改了个人账号信息');//保存操作记录
+                OperationLog::addOperationLog('3',$admin_data['organization_id'],$admin_data['id'],$route_name, '修改了个人账号信息');//保存操作记录
             }
             DB::commit();
         } catch (\Exception $e) {
