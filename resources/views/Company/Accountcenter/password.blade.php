6eaa597e913ffa609a110b9a6275bb8c79d5a825
mail@54zm.com <mail@54zm.com>
2018-02-03 15:26:38 +0800

    By John Yep

--- resources/views/Company/Accountcenter/password.blade.php
+++ resources/views/Company/Accountcenter/password.blade.php
@@ -107,7 +107,7 @@
         var data = target.serialize();
         $.post(url, data, function (json) {
             if (json.status == -1) {
-                window.location.reload();
+//                window.location.reload();
             } else if(json.status == 1) {
                 swal({
                     title: "提示信息",
