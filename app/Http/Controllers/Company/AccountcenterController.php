3ed4686e7e7318a971704e337de6d1d73e4e8fad
mail@54zm.com <mail@54zm.com>
2018-02-03 18:00:02 +0800

    By John Yep

--- app/Http/Controllers/Company/AccountcenterController.php
+++ app/Http/Controllers/Company/AccountcenterController.php
@@ -46,6 +46,12 @@ class AccountcenterController extends Controller{
         $organization_name  = $request->organization_name;
         $where = ['type'=>'3'];
         $organization = Organization::getCompanyAndWarzone($organization_name,$where,20,'id','ASC');
+        $listOrg = Organization::getWarzoneProxyAndWarzone([['program_id','3']],20,'id');
+        dump($listOrg);
+        foreach ($organization as $key=>$val){
+//            $val->id
+
+        }
         dump($organization);
         return  view('Company/Accountcenter/company_list',['organization'=>$organization,'organization_name'=>$organization_name]);
     }
