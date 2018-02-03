c8519355c9da3db88949e485709cbda2921d4c27
mail@54zm.com <mail@54zm.com>
2018-02-03 16:23:04 +0800

    By John Yep

--- resources/views/Company/Accountcenter/password.blade.php
+++ resources/views/Company/Accountcenter/password.blade.php
@@ -93,13 +93,12 @@
 <!-- Bootstrap -->
 <script src="{{asset('public/Company')}}/js/bootstrap.js"></script>
 <!-- App -->
-{{--<script src="{{asset('public/Company')}}/js/app.js"></script>--}}
+<script src="{{asset('public/Company')}}/js/app.js"></script>
 <script src="{{asset('public/Company/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
 <script src="{{asset('public/Company')}}/js/app.plugin.js"></script>
 <script src="{{asset('public/Company/library/jPlayer')}}/jquery.jplayer.min.js"></script>
 <script src="{{asset('public/Company/library/jPlayer')}}/add-on/jplayer.playlist.min.js"></script>
 <script src="{{asset('public/Company/library/sweetalert')}}/sweetalert.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/common-scripts.js"></script>
 <script>
     //提交表单
     function postForm() {
