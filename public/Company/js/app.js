b77bde003dc19b13d893a298357b50868fb0a295
mail@54zm.com <mail@54zm.com>
2018-02-03 16:33:38 +0800

    By John Yep

--- public/Company/js/app.js
+++ public/Company/js/app.js
@@ -81,14 +81,14 @@ Modernizr.addTest('ios7 ipad',function(){return!!navigator.userAgent.match(/iPad
     , url: ''
   }
 
-  Bjax.prototype.start = function () {
-    var that = this;
-    // this.backdrop();
-    $.ajax(this.options.url).done(function(r){
-      that.$content = r;
-      that.complete();
-    });
-  }
+  // Bjax.prototype.start = function () {
+  //   var that = this;
+  //   this.backdrop();
+  //   $.ajax(this.options.url).done(function(r){
+  //     that.$content = r;
+  //     that.complete();
+  //   });
+  // }
 
   Bjax.prototype.complete = function (){
     var that = this;
@@ -104,18 +104,18 @@ Modernizr.addTest('ios7 ipad',function(){return!!navigator.userAgent.match(/iPad
   }
 
   //网页头部加载进度条（暂时屏蔽掉，没什么用）
-  // Bjax.prototype.backdrop = function(){
-  //   this.$element.css('position','relative')
-  //   this.$backdrop = $('<div class="backdrop fade bg-white"></div>')
-  //     .appendTo(this.$element);
-  //   if(!this.options.backdrop) this.$backdrop.css('height', '2');
-  //   this.$backdrop[0].offsetWidth; // force reflow
-  //   this.$backdrop.addClass('in');
-  //
-  //   this.$bar = $('<div class="bar b-t b-2x b-info"></div>')
-  //     .width(0)
-  //     .appendTo(this.$backdrop);
-  // }
+  Bjax.prototype.backdrop = function(){
+    this.$element.css('position','relative')
+    this.$backdrop = $('<div class="backdrop fade bg-white"></div>')
+      .appendTo(this.$element);
+    if(!this.options.backdrop) this.$backdrop.css('height', '2');
+    this.$backdrop[0].offsetWidth; // force reflow
+    this.$backdrop.addClass('in');
+
+    this.$bar = $('<div class="bar b-t b-2x b-info"></div>')
+      .width(0)
+      .appendTo(this.$backdrop);
+  }
 
   Bjax.prototype.update = function (){
     this.$element.css('position','');
