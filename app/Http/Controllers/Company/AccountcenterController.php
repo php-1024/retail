4913e4fcdc2861eb9c87072e3c09dfb4c74eab19
mail@54zm.com <mail@54zm.com>
2018-02-06 10:04:16 +0800

    1

--- app/Http/Controllers/Company/AccountcenterController.php
+++ app/Http/Controllers/Company/AccountcenterController.php
@@ -46,14 +46,13 @@ class AccountcenterController extends Controller{
         $organization_name  = $request->organization_name;
         $where = ['type'=>'3'];
         $organization = Organization::getCompanyAndWarzone($organization_name,$where,20,'id','ASC');
+        foreach ($organization as $key=>$val){
+            $res[] = $val->parent_id;
+        }
+        dump($res);
+//        dump($organization);
         $listOrg = Organization::getWarzoneProxyAndWarzone([['program_id','3']],20,'id');
         dump($listOrg);
-        dump($organization);
-        foreach ($organization as $key=>$val){
-            $res[] = Organization::getArrayCompany(['id'=>$val->id]);
-        }
-//        dump($res);
-//        dump($organization);
         return  view('Company/Accountcenter/company_list',['organization'=>$organization,'organization_name'=>$organization_name]);
     }
 
