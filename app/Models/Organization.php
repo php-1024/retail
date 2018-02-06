feebd3891c8cb6167d9d1564feeee407ec823cec
mail@54zm.com <mail@54zm.com>
2018-02-06 13:06:37 +0800

    1

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -132,11 +132,10 @@ class Organization extends Model{
         if(!empty($organization_name)){
             $model =$model->where('organization_name','like','%'.$organization_name.'%');
         }
-        $model = $model->where($where)->orderBy($orderby,$sort);
         $model = $model->join('organization as iszmxw',function($join){
             $join->on('iszmxw.parent_id','organization.id');
-        })->where(['id'=>'2'])->select('organization.organization_name as iszmxw','organization.organization_name');
-        return $model->paginate($paginate);
+        })->where(['id'=>'2'],$where)->select('organization.organization_name as iszmxw','organization.organization_name');
+        return $model->orderBy($orderby,$sort)->paginate($paginate);
     }
 
 }
