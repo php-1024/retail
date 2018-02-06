dd498453c0fa88f2bde5c227e1cb34e655b0e597
mail@54zm.com <mail@54zm.com>
2018-02-06 11:31:49 +0800

    1

--- resources/views/Company/Accountcenter/company_list.blade.php
+++ resources/views/Company/Accountcenter/company_list.blade.php
@@ -67,7 +67,7 @@
                     <div class="symbol terques"><i class="icon-arrow-right"></i></div>
                     <div class="value"><b>{{ $val->organization_name }}</b>
                         <p>
-                            @if(in_array($val->parent_id,$organization_all)) {{$organization_all['organization_name']}} @endif
+                            {{--所属服务商--}}
                         </p>
                     </div>
                 </section>
