9c4491747a4718db6804522b26ab196eab909388
zeo <234521890@qq.com>
2018-02-11 11:33:42 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -16,10 +16,10 @@ class WechatController extends Controller{
     public function open(Request $request){
         file_put_contents('testopen.txt','123456');
 
-        $timeStamp    =$request->input('timestamp');
-        $nonce        =$request->input('nonce');
-        $encrypt_type =$request->input('encrypt_type');
-        $msg_sign     =$request->input('msg_signature');
+        $timeStamp    =$_GET['timestamp'];
+        $nonce        =$_GET['nonce'];
+        $encrypt_type =$_GET['encrypt_type'];
+        $msg_sign     =$_GET['msg_signature'];
         $encryptMsg   =file_get_contents('php://input');
         file_put_contents('testopen.txt',$encryptMsg);
         $result = \Wechat::getVerify_Ticket($timeStamp,$nonce,$encrypt_type,$msg_sign,$encryptMsg);
