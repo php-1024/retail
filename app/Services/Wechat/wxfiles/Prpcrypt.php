8fee00a59982b8a37e8336b3b5b04f8ac72dd82f
zeo <234521890@qq.com>
2018-02-11 13:56:25 +0800

    by zeo

--- app/Services/Wechat/wxfiles/Prpcrypt.php
+++ app/Services/Wechat/wxfiles/Prpcrypt.php
@@ -70,7 +70,7 @@ class Prpcrypt
             mcrypt_module_close($module);*/
             //$ciphertext_dec = base64_decode($encrypted);
             $iv = substr($this->key, 0, 16);
-            $decrypted = openssl_decrypt($encrypted,'AES-128-CBC',$this->key,OPENSSL_ZERO_PADDING);
+            $decrypted = openssl_decrypt($encrypted,'AES-128-CBC',$this->key,OPENSSL_RAW_DATA);
             dump($decrypted);
         } catch (Exception $e) {
             return array(ErrorCode::$DecryptAESError, null);
