4ceb7f1829e87dbdc7d8dcc9531384efd4f84d2b
zeo <234521890@qq.com>
2018-02-13 11:52:39 +0800

    by zeo

--- app/Models/WechatAuthorization.php
+++ app/Models/WechatAuthorization.php
@@ -21,6 +21,7 @@ class WechatAuthorization extends Model{
     }
 
     public static function addAuthorization($param){
+        dump($param);
         $model = new WechatAuthorization();
         $model->organization_id = $param['organization_id'];
         $model->authorizer_appid = $param['authorizer_appid'];
