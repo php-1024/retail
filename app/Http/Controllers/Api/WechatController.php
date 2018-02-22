b05e2231afe85ef6d33b58934e43a6389d212d9f
zeo <234521890@qq.com>
2018-02-22 16:13:54 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -52,6 +52,7 @@ class WechatController extends Controller{
         $code = trim($_GET['code']);
         $state = trim($_GET['state']);
         dump($code);
+        dump($_GET['openid']);
         exit();
 
     }
