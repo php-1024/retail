04d30bb4c60518bf28b8bc3656b7c05f9e4d18c2
zeo <234521890@qq.com>
2018-02-22 16:10:55 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -37,7 +37,7 @@ class WechatController extends Controller{
             dump($re);
             $appid = 'wxab6d2b312939eb01';
             $redirect_url = 'http://o2o.01nnt.com/api/wechat/open_web_redirect?open_id='.$re['openid'];
-            $url = \Wechat::get_open_web_auth_url($appid);
+            $url = \Wechat::get_open_web_auth_url($appid,$redirect_url);
             dump($url);
         }else{
             exit('无效的的回调链接');
