bd40a461c070abef590f56e7200b7a9803e1233c
zeo <234521890@qq.com>
2018-02-23 15:30:08 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -190,7 +190,7 @@ class WechatController extends Controller{
             //点击事件触发关键字回复
             elseif ($param['Event']=='CLICK' && $param['EventKey'] == "1234") {
                 $contentStr = $openid.'||'.$param['FromUserName'].'||'.$param['ToUserName']."||测试内容2";
-            }else{
+            }elseif ($keyword=='aaa'){
                 $contentStr = '你好吗，世界';
             }
             $result = '';
