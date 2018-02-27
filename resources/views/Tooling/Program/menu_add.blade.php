1cdd7450547f73941c40a17d1e53c4cdda894023
zeo <234521890@qq.com>
2018-02-27 15:51:38 +0800

    by zeo

--- resources/views/Tooling/Program/menu_add.blade.php
+++ resources/views/Tooling/Program/menu_add.blade.php
@@ -30,8 +30,6 @@
                     <button type="button" class="btn btn-info" id="first_btn" onclick="showSecondBox();">下一级&nbsp;&nbsp;<i class="fa fa-arrow-circle-down"></i></button>
                 </div>
             </div>
-            <div style="clear:both"></div>
-            <div class="hr-line-dashed"></div>
 
             <div class="form-group" id="second_box" style="display:none;">
                 <label class="col-sm-2 control-label"></label>
