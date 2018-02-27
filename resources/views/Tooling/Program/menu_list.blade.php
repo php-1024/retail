51628716e89a9b1383e20321c6c8f1f7ec6ed5a0
zeo <234521890@qq.com>
2018-02-27 17:16:08 +0800

    by zeo

--- resources/views/Tooling/Program/menu_list.blade.php
+++ resources/views/Tooling/Program/menu_list.blade.php
@@ -96,7 +96,7 @@
                                         <div class="dd-handle">
                                             <span class="label label-primary"><i class="{{ $val->icon_class }}"></i></span>
                                             <span class="pull-right">
-                                                <input type="text" class="pull-left" value="{{ $val->sort }}" style="width: 50px; text-align: center;">
+                                                <input type="text" class="pull-left form-control" value="{{ $val->sort }}" style="width: 50px; text-align: center;">
                                                     &nbsp;&nbsp;
                                                 <div class="btn-group">
 
