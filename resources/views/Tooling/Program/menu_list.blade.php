37a1fd7e64246db21e4ea119b2326087526c2f78
zeo <234521890@qq.com>
2018-02-27 17:07:24 +0800

    by zeo

--- resources/views/Tooling/Program/menu_list.blade.php
+++ resources/views/Tooling/Program/menu_list.blade.php
@@ -97,7 +97,8 @@
                                             <span class="label label-primary"><i class="{{ $val->icon_class }}"></i></span>
                                             <span class="pull-right">
                                                 <div class="btn-group">
-                                                    <input type="text" value="{{ $val->sort }}">
+                                                    <input type="text" class="block" value="{{ $val->sort }}">
+
                                                     <button type="button" id="editBtn" onclick="return getEditForm('{{ $val->id }}');" class="block btn btn-xs btn-info"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑菜单</button>
                                                     <button type="button" id="deleteBtn" onclick="deleteData({{ $val->id }})" class="block btn btn-xs btn-warning"><i class="fa fa-remove"></i>&nbsp;&nbsp;删除菜单</button>
                                                     <button type="button" id="deleteBtn" onclick="removeData({{ $val->id }})" class="block btn btn-xs btn-danger"><i class="fa fa-remove"></i>&nbsp;&nbsp;彻底删除</button>
