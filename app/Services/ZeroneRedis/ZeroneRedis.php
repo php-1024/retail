a911b06a960f0b017907f051d70fa3b884d1dc61
zeo <234521890@qq.com>
2018-02-28 16:52:02 +0800

    by zeo

--- app/Services/ZeroneRedis/ZeroneRedis.php
+++ app/Services/ZeroneRedis/ZeroneRedis.php
@@ -57,7 +57,7 @@ class ZeroneRedis
      * id - 用户的ID
      */
     public static function create_menu_cache($id){
-        $menu = ProgramMenu::getList([[ 'parent_id',0],['program_id','1']],0,'id','asc');//获取零壹管理系统的一级菜单
+        $menu = ProgramMenu::getList([[ 'parent_id',0],['program_id','1']],0,'sort','asc');//获取零壹管理系统的一级菜单
         $son_menu = [];
         foreach($menu as $key=>$val){//获取一级菜单下的子菜单
             $son_menu[$val->id] = ProgramMenu::son_menu($val->id);
