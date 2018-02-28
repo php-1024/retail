badf71d35cdc75af5e802b64c0bd285c23c449e1
kbzz <463661663@qq.com>
2018-02-28 15:14:06 +0800

    1

--- resources/views/Catering/Login/display.blade.php
+++ resources/views/Catering/Login/display.blade.php
@@ -55,11 +55,11 @@
     </div>
 </footer>
 <!-- / footer -->
-<script src="{{asset('public/Catering')}}/js/jquery.min.js"></script>
+{{--<script src="{{asset('public/Catering')}}/js/jquery.min.js"></script>--}}
 <!-- Bootstrap -->
 <script src="{{asset('public/Catering')}}/js/bootstrap.js"></script>
 <!-- App -->
-<script src="{{asset('public/Catering')}}/js/app.js"></script>
+{{--<script src="{{asset('public/Catering')}}/js/app.js"></script>--}}
 <script src="{{asset('public/Catering')}}/js/slimscroll/jquery.slimscroll.min.js"></script>
 <script src="{{asset('public/Catering')}}/js/app.plugin.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/jquery.jplayer.min.js"></script>
