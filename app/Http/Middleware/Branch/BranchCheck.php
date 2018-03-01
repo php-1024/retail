7510deb515e7f0f3babfa3b32bb7c3710938ef1d
mail@54zm.com <mail@54zm.com>
2018-03-01 17:15:28 +0800

    1

--- app/Http/Middleware/Branch/BranchCheck.php
+++ app/Http/Middleware/Branch/BranchCheck.php
@@ -33,6 +33,7 @@ class BranchCheck{
             case "company/store/store_add_second":      //店铺管理立即开店
             case "company/store/store_list":            //店铺管理
                 $re = $this->checkLoginAndRule($request);//判断是否登录
+                dd($re['response']);
                 return $next($re['response']);
 //                return self::format_response($re,$next);
                 break;
