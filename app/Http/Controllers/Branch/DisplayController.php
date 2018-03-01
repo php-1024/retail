a4621f08d1e96f69ededbd3906b0a064c4f0da63
mail@54zm.com <mail@54zm.com>
2018-03-01 17:43:14 +0800

    1

--- app/Http/Controllers/Branch/DisplayController.php
+++ app/Http/Controllers/Branch/DisplayController.php
@@ -23,11 +23,11 @@ class DisplayController extends Controller
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
         $route_name = $request->path();                     //获取当前的页面路由
         if($admin_data['is_super'] == 1 && $admin_data['organization_id'] == 0){    //如果是超级管理员并且组织ID等于零则进入选择组织页面
-            return redirect('Branch/company_list');
+            return redirect('branch/branch_list');
         }
         $organization = Organization::getOneCompany(['id' => $admin_data['organization_id']]);
 //        if (empty($admin_data['safe_password'])){           //先设置安全密码
-//            return redirect('Branch/account/password');
+//            return redirect('branch/account/password');
 //        }else{
             return view('Branch/Display/display',['organization'=>$organization,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
 //        }
