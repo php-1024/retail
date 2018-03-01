c5cbb080ae6fdbab168dde6dd108ed2d54172a88
kbzz <463661663@qq.com>
2018-03-01 11:00:02 +0800

    1

--- app/Http/Controllers/Catering/ShopController.php
+++ app/Http/Controllers/Catering/ShopController.php
@@ -40,7 +40,6 @@ class ShopController extends Controller{
         $admin_this = $request->get('admin_data');//中间件产生的管理员数据参数
         $organization_id = $request->input('organization_id');//中间件产生的管理员数据参数
         $account_info = Account::getOneAccount([['organization_id',$organization_id],['parent_id','1']]);//根据账号查询
-        dd($account_info);
         if(!empty($account_info)){
             //重新生成缓存的登录信息
             $admin_data = [
@@ -55,6 +54,7 @@ class ShopController extends Controller{
                 'safe_password'=>$admin_this['safe_password'],//安全密码-超级管理员
                 'account_status'=>$account_info->status,//用户状态
             ];
+            dd($admin_data);
             Session::put('catering_account_id',encrypt(1));//存储登录session_id为当前用户ID
             //构造用户缓存数据
             if(!empty( $account_info->account_info->realname)) {
