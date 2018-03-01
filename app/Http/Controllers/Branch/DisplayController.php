55bb91353cab02e51bc0d2f51db8e7b882b44102
mail@54zm.com <mail@54zm.com>
2018-03-01 17:24:38 +0800

    1

--- app/Http/Controllers/Branch/DisplayController.php
+++ app/Http/Controllers/Branch/DisplayController.php
@@ -21,17 +21,19 @@ class DisplayController extends Controller
      */
     public function display(Request $request)
     {
-        $key = config("app.branch_encrypt_key");//获取加密盐（分店专用）
-        $password = 'admin';
-        $encrypted = md5($password);//加密密码第一重
-        $encryptPwd = md5("lingyikeji" . $encrypted . $key);//加密密码第二重
-        dump($encryptPwd);
-        dump($request);
-//        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-//        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
-//        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
-//        $route_name = $request->path();                     //获取当前的页面路由
-        return view('Branch/Display/display');
+        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
+        $route_name = $request->path();                     //获取当前的页面路由
+        if($admin_data['is_super'] == 1 && $admin_data['organization_id'] == 0){    //如果是超级管理员并且组织ID等于零则进入选择组织页面
+            return redirect('Branch/company_list');
+        }
+        $organization = Organization::getOneCompany(['id' => $admin_data['organization_id']]);
+        if (empty($admin_data['safe_password'])){           //先设置安全密码
+            return redirect('Branch/account/password');
+        }else{
+            return view('Branch/Display/display',['organization'=>$organization,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        }
     }
 }
 
