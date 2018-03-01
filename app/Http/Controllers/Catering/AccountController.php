efe5507e6dcc94e54f2384431dd60f5895ad5f96
kbzz <463661663@qq.com>
2018-03-01 14:53:06 +0800

    1

--- app/Http/Controllers/Catering/AccountController.php
+++ app/Http/Controllers/Catering/AccountController.php
@@ -17,7 +17,6 @@ use Session;
 class AccountController extends Controller{
     //个人信息
     public function profile(Request $request){
-        dd(1);
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
