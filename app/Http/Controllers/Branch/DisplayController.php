5e8030c4180a8cb1047380fcd3239e73fdaebc9d
mail@54zm.com <mail@54zm.com>
2018-03-01 15:43:14 +0800

    1

--- app/Http/Controllers/Branch/DisplayController.php
+++ app/Http/Controllers/Branch/DisplayController.php
@@ -21,13 +21,17 @@ class DisplayController extends Controller
      */
     public function display(Request $request)
     {
-//        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-//        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
-//        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
-//        $route_name = $request->path();                     //获取当前的页面路由
-        dump($request);
-        return view('Branch/Display/display');
+        dd($request);
     }
+//    public function display(Request $request)
+//    {
+////        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
+////        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
+////        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
+////        $route_name = $request->path();                     //获取当前的页面路由
+//        dump($request);
+//        return view('Branch/Display/display');
+//    }
 }
 
 ?>
