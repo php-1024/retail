2faf0b8e64d1052dc82c32b428b1f5975e980695
kbzz <463661663@qq.com>
2018-03-01 11:05:47 +0800

    1

--- app/Http/Middleware/Catering/CateringCheck.php
+++ app/Http/Middleware/Catering/CateringCheck.php
@@ -30,7 +30,7 @@ class CateringCheck{
             /****系统管理****/
             case "catering/system/setup":                  //参数设置
             case "catering/system/proxy_info":             //公司信息设置
-            case "catering":                            //服务商后台首页
+            case "catering":                               //服务商后台首页
             case "catering/select_shop":                   //店铺超级管员进入操作
             case "catering/system/proxy_structure":        //人员结构
             case "catering/system/operationlog":           //操作日记
--- resources/views/Catering/Shop/select_shop.blade.php
+++ resources/views/Catering/Shop/select_shop.blade.php
@@ -107,14 +107,14 @@
                     window.location.reload();
                 });
             }else{
-                swal({
-                    title: "提示信息",
-                    text: json.data,
-                    confirmButtonColor:"#DD6B55",
-                    confirmButtonText: "确定"
-                },function () {
-                    window.location.reload();
-                });
+//                swal({
+//                    title: "提示信息",
+//                    text: json.data,
+//                    confirmButtonColor:"#DD6B55",
+//                    confirmButtonText: "确定"
+//                },function () {
+//                    window.location.reload();
+//                });
             }
         });
     }
