8bed371523d93c308d4604a9a3b521f971741e19
kbzz <463661663@qq.com>
2018-03-01 15:10:56 +0800

    1

--- resources/views/Catering/Account/profile.blade.php
+++ resources/views/Catering/Account/profile.blade.php
@@ -35,78 +35,201 @@
             </aside>
             <!-- /.aside -->
             <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">账号信息</h3>
+                        </div>
 
+                        <div class="col-lg-4">
+                            <section class="panel panel-default">
+                                <header class="panel-heading font-bold">
+                                    个人账号信息修改
+                                </header>
+                                <div class="panel-body">
+                                    <form class="form-horizontal" method="get">
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label" for="input-id-1">用户账号</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" class="form-control" id="input-id-1" disabled="" value="200307">
+                                            </div>
+                                        </div>
+
+                                        <div class="line line-dashed b-b line-lg pull-in"></div>
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label" for="input-id-1">真实姓名</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" class="form-control" id="input-id-1"  value="薛志豪">
+                                            </div>
+                                        </div>
+
+                                        <div class="line line-dashed b-b line-lg pull-in"></div>
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label" for="input-id-1">手机号码</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" class="form-control" id="input-id-1" value="13123456789">
+                                                <span class="help-block m-b-none">该手机号码可用作登录，与负责人手机号码可分别设置</span>
+                                            </div>
+
+                                        </div>
+
+                                        <div class="line line-dashed b-b line-lg pull-in"></div>
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label" for="input-id-1">安全密码</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" class="form-control" id="input-id-1" value="">
+                                            </div>
+                                        </div>
+                                        <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                        <div class="form-group">
+                                            <div class="col-sm-12 col-sm-offset-5">
+
+                                                <button type="button" class="btn btn-success" id="addBtn">保存信息</button>
+                                            </div>
+                                        </div>
+                                        <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    </form>
+                                </div>
+                            </section>
+                        </div>
+
+                        <div class="col-lg-8">
+                            <section class="panel panel-default">
+                                <header class="panel-heading font-bold">
+                                    个人系统权限
+                                </header>
+                                <div class="panel-body">
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单模块
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单编辑
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option2" id="inlineCheckbox2" checked="checked" disabled=""> 订单查询
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单添加
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单删除
+                                        </label>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单模块
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单编辑
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option2" id="inlineCheckbox2" checked="checked" disabled=""> 订单查询
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单添加
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单删除
+                                        </label>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单模块
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单编辑
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option2" id="inlineCheckbox2" checked="checked" disabled=""> 订单查询
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单添加
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单删除
+                                        </label>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单模块
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单编辑
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option2" id="inlineCheckbox2" checked="checked" disabled=""> 订单查询
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单添加
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单删除
+                                        </label>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单模块
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option1" id="inlineCheckbox1" checked="checked" disabled=""> 订单编辑
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option2" id="inlineCheckbox2" checked="checked" disabled=""> 订单查询
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单添加
+                                        </label>
+                                        &nbsp;&nbsp;
+                                        <label class="i-checks">
+                                            <input type="checkbox" value="option3" id="inlineCheckbox3" checked="checked" disabled=""> 订单删除
+                                        </label>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                </div>
+                            </section>
+                        </div>
+                    </section>
+                </section>
             </section>
         </section>
     </section>
 </section>
 
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <form class="form-horizontal tasi-form" method="get">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">店铺信息编辑</h4>
-                </div>
-                <div class="modal-body">
-                    <form class="form-horizontal" method="get">
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">店铺名称</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="刘记鸡煲王" placeholder="店铺名称" class="form-control">
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
 
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">负责人</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="张老三" placeholder="负责人" class="form-control">
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">手机号码</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="13123456789" placeholder="手机号码" class="form-control">
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">店铺LOGO</label>
-                            <div class="col-sm-10">
-                                <input type="file" class="filestyle" style="display: none;" data-icon="false" data-classButton="btn btn-default" data-classInput="form-control inline v-middle input-s">
-                            </div>
-                        </div>
-
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">安全密码</label>
-                            <div class="col-sm-10">
-                                <input type="password" value="" placeholder="安全密码" class="form-control" >
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
-                    <button class="btn btn-success" type="button" id="save_btn">确定</button>
-                </div>
-            </div>
-        </div>
-    </form>
-</div>
 <script src="{{asset('public/Catering')}}/js/jquery.min.js"></script>
 <!-- Bootstrap -->
 <script src="{{asset('public/Catering')}}/js/bootstrap.js"></script>
