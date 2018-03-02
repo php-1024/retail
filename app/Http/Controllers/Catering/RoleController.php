a05fd213c84d793d218bc4b60f1ee96922252a70
kbzz <463661663@qq.com>
2018-03-02 15:20:09 +0800

    1

--- app/Http/Controllers/Catering/RoleController.php
+++ app/Http/Controllers/Catering/RoleController.php
@@ -95,7 +95,7 @@ class RoleController extends Controller{
         foreach($list as $key=>$val){
             $role_module_nodes[$val->id] = $this->getModuleNode($val->id);//获取角色拥有的所有模块和节点
         }
-        return view('Proxy/Role/role_list',['list'=>$list,'role_module_nodes'=>$role_module_nodes,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        return view('Catering/Role/role_list',['list'=>$list,'role_module_nodes'=>$role_module_nodes,'search_data'=>$search_data,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
     /***
