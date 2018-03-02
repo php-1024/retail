debd0cb21712aba727376eefd953720e8eab07d2
kbzz <463661663@qq.com>
2018-03-02 15:03:16 +0800

    1

--- resources/views/Catering/Role/role_add.blade.php
+++ resources/views/Catering/Role/role_add.blade.php
@@ -119,7 +119,6 @@
 <script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/demo.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/sweetalert/sweetalert.min.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/iCheck/js/icheck.min.js"></script>
-<script src="{{asset('public/Proxy')}}/js/common-scripts.js"></script>
 
 <!--common script for all pages-->
 <script>
@@ -179,7 +178,7 @@
         });
     }
 
-    
+
 </script>
 </body>
 </html>
