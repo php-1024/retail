dc5a7147529c31fecb4350b0a5b43e30cfe2d84d
kbzz <463661663@qq.com>
2018-03-02 11:09:03 +0800

    1

--- app/Http/Controllers/Catering/AccountController.php
+++ app/Http/Controllers/Catering/AccountController.php
@@ -197,7 +197,7 @@ class AccountController extends Controller{
         }else{
             $oneAcc = Account::getOne([['id',$id]]);
         }
-        return view('Proxy/Persona/password',['oneAcc'=>$oneAcc,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        return view('Catering/Account/password',['oneAcc'=>$oneAcc,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
 
     }
     //修改登入密码功能提交
