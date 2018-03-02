874c05d2fd12b1327272fba525b0b8c6ead98248
kbzz <463661663@qq.com>
2018-03-02 11:25:38 +0800

    1

---
+++ resources/views/Catering/Account/message_setting.blade.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en" class="app">
+<head>
+    <meta charset="utf-8" />
+    <title>零壹云管理平台 | 总分店管理系统</title>
+    <link rel="stylesheet" href="{{asset('public/Catering')}}/js/jPlayer/jplayer.flat.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/font.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/app.css" type="text/css" />
+    <link href="{{asset('public/Catering')}}/sweetalert/sweetalert.css" rel="stylesheet" />
+    <!--[if lt IE 9]>
+    <script src="{{asset('public/Catering')}}/js/ie/html5shiv.js"></script>
+    <script src="{{asset('public/Catering')}}/js/ie/respond.min.js"></script>
+    <script src="{{asset('public/Catering')}}/js/ie/excanvas.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<section class="vbox">
+    <header class="bg-white-only header header-md navbar navbar-fixed-top-xs">
+        @include('Catering/Public/Header')
+    </header>
+    <section>
+        <section class="hbox stretch">
+
+            <!-- .aside -->
+            <aside class="bg-black dk aside hidden-print" id="nav">
+                <section class="vbox">
+                    <section class="w-f-md scrollable">
+                        @include('Catering/Public/Nav')
+                    </section>
+                </section>
+            </aside>
+            <!-- /.aside -->
+            <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">消息接受设置</h3>
+                        </div>
+
+                        <section class="panel panel-default">
+
+                            <header class="panel-heading font-bold">
+                                消息接受设置
+                            </header>
+                            <div class="panel-body">
+                                <form class="form-horizontal" method="get">
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label">用户关注消息推送</label>
+                                        <div class="col-sm-10">
+                                            <label class="switch">
+                                                <input type="checkbox">
+                                                <span></span>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label">用户下单消息推送</label>
+                                        <div class="col-sm-10">
+                                            <label class="switch">
+                                                <input type="checkbox">
+                                                <span></span>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label">用户付款消息推送</label>
+                                        <div class="col-sm-10 ">
+                                            <label class="switch">
+                                                <input type="checkbox">
+                                                <span></span>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label">订单完成消息推送</label>
+                                        <div class="col-sm-10">
+                                            <label class="switch">
+                                                <input type="checkbox">
+                                                <span></span>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label">用户充值消息推送</label>
+                                        <div class="col-sm-10">
+                                            <label class="switch">
+                                                <input type="checkbox">
+                                                <span></span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">安全密码</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <div class="col-sm-12 col-sm-offset-6">
+
+                                            <button type="button" class="btn btn-success" id="addBtn">保存信息</button>
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                </form>
+                            </div>
+                        </section>
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+</section>
+<script src="{{asset('public/Catering')}}/js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('public/Catering')}}/js/bootstrap.js"></script>
+<!-- App -->
+<script src="{{asset('public/Catering')}}/js/app.js"></script>
+<script src="{{asset('public/Catering')}}/js/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Catering')}}/js/app.plugin.js"></script>
+<script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/add-on/jplayer.playlist.min.js"></script>
+<script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/demo.js"></script>
+<script type="text/javascript" src="{{asset('public/Catering')}}/sweetalert/sweetalert.min.js"></script>
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#editBtn').click(function(){
+            $('#myModal').modal();
+        });
+        $('#save_btn').click(function(){
+            swal({
+                title: "温馨提示",
+                text: "操作成功",
+                type: "success"
+            });
+        });
+    });
+</script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
