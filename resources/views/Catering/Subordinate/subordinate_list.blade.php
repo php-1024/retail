52a4aadca757b7c7f2e34a22e09ce08c4095ddb7
kbzz <463661663@qq.com>
2018-03-03 10:58:06 +0800

    1

--- resources/views/Catering/Subordinate/subordinate_list.blade.php
+++ resources/views/Catering/Subordinate/subordinate_list.blade.php
@@ -35,7 +35,147 @@
             </aside>
             <!-- /.aside -->
             <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">下属列表</h3>
+                        </div>
+                        <section class="panel panel-default">
+                            <header class="panel-heading">
+                                下属列表
+                            </header>
+                            <div class="row wrapper">
+                                <form class="form-horizontal" method="get">
+                                    <label class="col-sm-1 control-label">下属账号</label>
 
+                                    <div class="col-sm-2">
+                                        <input class="input-sm form-control" size="16" type="text" value="">
+                                    </div>
+
+                                    <div class="col-sm-3">
+                                        <button type="button" class="btn btn-s-md btn-info"><i class="fa fa-search"></i>&nbsp;&nbsp;搜索</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table table-striped b-t b-light">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>用户账号</th>
+                                        <th>用户角色</th>
+                                        <th>微信头像</th>
+                                        <th>微信昵称</th>
+                                        <th>真实姓名</th>
+                                        <th>手机号码</th>
+                                        <th>用户状态</th>
+                                        <th>添加时间</th>
+                                        <th>操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>100021</td>
+                                        <td>
+                                            财务管理员
+                                        </td>
+                                        <td>
+                                            <img src="images/m1.jpg" alt="" class="r r-2x img-full" style="width: 50px; height: 50px;">
+                                        </td>
+                                        <td>
+                                            时光取名叫无心
+                                        </td>
+                                        <td>
+                                            李健瑚
+                                        </td>
+                                        <td>
+                                            13123456789
+                                        </td>
+                                        <td>
+                                            <label class="label label-success">正常</label>
+                                        </td>
+                                        <td>2017-08-09 11:11:11</td>
+                                        <td>
+                                            <button class="btn btn-info btn-xs" id="editBtn"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>
+                                            <button type="button" id="ruleBtn" class="btn  btn-xs btn-primary"><i class="fa fa-certificate"></i>&nbsp;&nbsp;授权</button>
+                                            <button type="button" id="lockBtn" class="btn  btn-xs btn-warning"><i class="icon icon-lock"></i>&nbsp;&nbsp;冻结</button>
+                                            <button class="btn btn-danger btn-xs" id="deleteBtn"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>100022</td>
+                                        <td>
+                                            财务管理员
+                                        </td>
+                                        <td>
+                                            <img src="images/m1.jpg" alt="" class="r r-2x img-full" style="width: 50px; height: 50px;">
+                                        </td>
+                                        <td>
+                                            <label class="label label-danger">未绑定</label>
+                                        </td>
+                                        <td>
+                                            <label class="label label-danger">未绑定</label>
+                                        </td>
+                                        <td>
+                                            13123456789
+                                        </td>
+                                        <td>
+                                            <label class="label label-warning">冻结</label>
+                                        </td>
+                                        <td>2017-08-09 11:11:11</td>
+                                        <td>
+                                            <button class="btn btn-info btn-xs" id="editBtn"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>
+                                            <button type="button" id="ruleBtn" class="btn  btn-xs btn-primary"><i class="fa fa-certificate"></i>&nbsp;&nbsp;授权</button>
+                                            <button type="button" id="lockBtn" class="btn  btn-xs btn-success"><i class="icon icon-lock"></i>&nbsp;&nbsp;解冻</button>
+                                            <button class="btn btn-danger btn-xs" id="deleteBtn"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <footer class="panel-footer">
+                                <div class="row">
+
+                                    <div class="col-sm-12 text-right text-center-xs">
+                                        <ul class="pagination pull-right">
+                                            <li class="footable-page-arrow disabled">
+                                                <a data-page="first" href="#first">«</a>
+                                            </li>
+
+                                            <li class="footable-page-arrow disabled">
+                                                <a data-page="prev" href="#prev">‹</a>
+                                            </li>
+                                            <li class="footable-page active">
+                                                <a data-page="0" href="#">1</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">2</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">3</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">4</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">5</a>
+                                            </li>
+                                            <li class="footable-page-arrow">
+                                                <a data-page="next" href="#next">›</a>
+                                            </li>
+                                            <li class="footable-page-arrow">
+                                                <a data-page="last" href="#last">»</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </footer>
+                        </section>
+
+                    </section>
+                </section>
             </section>
         </section>
     </section>
