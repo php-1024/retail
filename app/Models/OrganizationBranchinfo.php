eab1262535d7c83e3cd46d775a5d2880ebb82bf1
kbzz <463661663@qq.com>
2018-03-03 18:07:17 +0800

    1

--- app/Models/OrganizationBranchinfo.php
+++ app/Models/OrganizationBranchinfo.php
@@ -23,9 +23,9 @@ class OrganizationBranchinfo extends Model{
     public static function addOrganizationBranchinfo($param){
         $program = new OrganizationBranchinfo();//实例化程序模型
         $program->organization_id = $param['organization_id'];//组织id
-        $program->proxy_owner = $param['branch_owner'];//分店负责人姓名
-        $program->proxy_owner_idcard = $param['branch_owner_idcard'];//分店负责人身份证
-        $program->proxy_owner_mobile = $param['branch_owne_mobile'];//分店负责人手机号
+        $program->branch_owner = $param['branch_owner'];//分店负责人姓名
+        $program->branch_owner_idcard = $param['branch_owner_idcard'];//分店负责人身份证
+        $program->branch_owner_mobile = $param['branch_owne_mobile'];//分店负责人手机号
         $program->type = $param['type'];//0为总店 1为分店
         $program->save();
         return $program->id;
