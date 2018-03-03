3ea4f95f6b90aeef6da5c493cc775e0a0e7a1098
kbzz <463661663@qq.com>
2018-03-03 18:24:21 +0800

    1

--- app/Http/Controllers/Catering/StoreController.php
+++ app/Http/Controllers/Catering/StoreController.php
@@ -14,11 +14,12 @@ class StoreController extends Controller{
     //创建总分店
     public function branch_create(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dump($admin_data);
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = $admin_data['organization_id'];
+        $aa =Organization::getOneCatering([['organization_id',$organization_id]]);
+        dump($aa);
         $onebranch = Organization::checkRowExists([['parent_id',$organization_id],['type',5]]); //查询有没有总店，如果有，接下来创建的都是分店
         $package_program = Package::getList(['id'=>1],0,'id','DESC');   //查询当前所选餐包含的程序 1为餐饮系统
         return view('Catering/Store/branch_create',['onebranch'=>$onebranch,'onebranch'=>$onebranch,'package_program'=>$package_program,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
@@ -29,7 +30,8 @@ class StoreController extends Controller{
 
         $admin_data = $request->get('admin_data');      //中间件产生的管理员数据参数
         $route_name = $request->path();                 //获取当前的页面路由
-        $parent_id = $admin_data['id'];                 //上级id
+
+        $organization_parent_id = $admin_data['id'];                 //上级id
         $parent_tree = $admin_data['parent_tree'].$parent_id.',';//树型关系
 
         $program_id = '5';                              //分店登入程序为5
