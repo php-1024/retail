6b128975440f791389b0841afc89de3e520929d7
kbzz <463661663@qq.com>
2018-03-03 11:25:03 +0800

    1

--- resources/views/Catering/Subordinate/subordinate_edit.blade.php
+++ resources/views/Catering/Subordinate/subordinate_edit.blade.php
@@ -1,4 +1,4 @@
-<form class="form-horizontal tasi-form" method="post" role="form" id="currentForm" action="{{ url('catering/ajax/subordinate_edit_check') }}">
+<form class="form-horizontal tasi-form"method="post" role="form" id="currentForm" action="{{ url('catering/ajax/subordinate_edit_check') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" id="id" value="{{ $info->id }}">
     <input type="hidden" name="account" id="account" value="{{ $info->account }}">
@@ -6,39 +6,47 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">编辑用户</h4>
+                <h4 class="modal-title">编辑用户基本信息</h4>
             </div>
             <div class="modal-body">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">用户账号</label>
-                    <div class="col-sm-10"><input type="text" value="{{ $info->account }}" disabled="" class="form-control"></div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">用户密码</label>
-                    <div class="col-sm-10"><input type="password" class="form-control" name="password"  value=""></div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">真实姓名</label>
-                    <div class="col-sm-10"><input type="text" class="form-control" name="realname" value="@if(!empty($info->account_info)){{ $info->account_info->realname }}@endif"></div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">联系方式</label>
-                    <div class="col-sm-10"><input type="text" class="form-control"  name="mobile" value="{{ $info->mobile }}"></div>
-                </div>
+                <form class="form-horizontal" method="get">
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label" for="input-id-1">用户账号</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="input-id-1" value="{{ $info->account }}">
+                        </div>
+                    </div>
+                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label" for="input-id-1">真实姓名</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="input-id-1" name="realname" value="@if(!empty($info->account_info)){{ $info->account_info->realname }}@endif">
+                        </div>
+                    </div>
+                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label" for="input-id-1">手机号码</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="input-id-1"  name="mobile" value="{{ $info->mobile }}">
+                        </div>
+                    </div>
+                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label" for="input-id-1">安全密码</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="input-id-1" value=""  name="safe_password">
+                        </div>
+                    </div>
 
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">安全密码</label>
-                    <div class="col-sm-10"><input type="password" class="form-control"  name="safe_password"></div>
-                </div>
+                </form>
             </div>
             <div class="modal-footer">
                 <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
-                <button class="btn btn-success" type="button" id="save_btn" onclick="return postForm();">保存</button>
+                <button class="btn btn-success" type="button" id="addBtn" onclick="return postForm();">确定</button>
             </div>
         </div>
     </div>
 </form>
-
 <script>
     //提交表单
     function postForm() {
