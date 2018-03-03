e2bf9d723882f0168748589e7973a61c5afe5af6
kbzz <463661663@qq.com>
2018-03-03 14:41:25 +0800

    1

--- resources/views/Catering/Subordinate/subordinate_authorize.blade.php
+++ resources/views/Catering/Subordinate/subordinate_authorize.blade.php
@@ -5,6 +5,59 @@
     <input type="hidden" id="quick_rule_url" value="{{ url('catering/ajax/quick_rule') }}">
     <input type="hidden" id="selected_rule_url" value="{{ url('catering/ajax/selected_rule') }}">
     <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">编辑用户基本信息</h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" method="get">
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">用户账号</label>
+                        <div class="col-sm-3">{{ $info->account }}</div>
+                    </div>
+                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">权限角色</label>
+                        <div class="col-sm-3">
+                            <select class="form-control m-b" name="account">
+                                <option value="0">请选择</option>
+                                @foreach($role_list as $k=>$v)
+                                    <option @if($info->account_role == $v->id) selected @endif value="{{ $v->id }}">{{ $v->role_name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-sm-2"><button type="button" class="btn btn-success" onclick="get_quick_rule('#role_id');"><i class="icon-arrow-down"></i>&nbsp;&nbsp;快速授权</button></div>
+                        <div class="col-sm-2"><button type="button" class="btn btn-success" onclick="get_selected_rule();"><i class="icon-arrow-down"></i>&nbsp;&nbsp;恢复默认</button></div>
+                    </div>
+                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                    <div class="form-group" id="module_node_box"></div>
+                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">安全密码</label>
+                        <div class="col-sm-10"><input type="text" class="form-control"  name="safe_password"></div>
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
+                <button class="btn btn-success" type="button" id="addBtn" onclick="return postForm();">确定</button>
+            </div>
+        </div>
+    </div>
+</form>
+
+
+<form class="form-horizontal tasi-form" method="post" role="form" id="currentForm" action="{{ url('proxy/ajax/subordinate_authorize_check') }}">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <input type="hidden" name="id" id="id" value="{{ $info->id }}">
+    <input type="hidden" name="account" id="account" value="{{ $info->account }}">
+    <input type="hidden" id="quick_rule_url" value="{{ url('proxy/ajax/quick_rule') }}">
+    <input type="hidden" id="selected_rule_url" value="{{ url('proxy/ajax/selected_rule') }}">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content animated fadeIn">
             <div class="modal-header">
                 修改用户
