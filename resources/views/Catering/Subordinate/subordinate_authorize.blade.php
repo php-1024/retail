fe96cda9bad3400961a855aa38caee94da7e7441
kbzz <463661663@qq.com>
2018-03-03 14:05:18 +0800

    1

--- resources/views/Catering/Subordinate/subordinate_authorize.blade.php
+++ resources/views/Catering/Subordinate/subordinate_authorize.blade.php
@@ -3,7 +3,6 @@
     <input type="hidden" name="id" id="id" value="{{ $info->id }}">
     <input type="hidden" name="account" id="account" value="{{ $info->account }}">
     <input type="hidden" id="quick_rule_url" value="{{ url('catering/ajax/quick_rule') }}">
-    <input type="hidden" id="selected_rule_url" value="{{ url('catering/ajax/selected_rule') }}">
     <div class="modal-dialog modal-lg">
         <div class="modal-content animated fadeIn">
             <div class="modal-header">
@@ -30,7 +29,6 @@
                         </select>
                     </div>
                     <div class="col-sm-2"><button type="button" class="btn btn-primary" onclick="get_quick_rule('#role_id');"><i class="fa fa-arrow-circle-down"></i>&nbsp;&nbsp;快速授权</button></div>
-                    <div class="col-sm-2"><button type="button" class="btn btn-primary" onclick="get_selected_rule();"><i class="fa fa-repeat"></i>&nbsp;&nbsp;恢复默认</button></div>
                 </div>
                 <div style="clear:both"></div>
                 <div class="hr-line-dashed"></div>
@@ -55,10 +53,6 @@
     </div>
 </form>
 <script>
-    $(document).ready(function() {
-        get_selected_rule();
-    });
-
     //获取上级程序节点
     function get_quick_rule(obj){
         var url =  $('#quick_rule_url').val();
@@ -69,7 +63,7 @@
             $('#module_node_box').html(response);
         });
     }
-    
+
     //提交表单
     function postForm() {
         var target = $("#currentForm");
