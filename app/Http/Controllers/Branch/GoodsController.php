79ea8a731b1538653ddbb155f55d795890f03b3c
mail@54zm.com <mail@54zm.com>
2018-03-03 16:03:28 +0800

    '1'

--- app/Http/Controllers/Branch/GoodsController.php
+++ app/Http/Controllers/Branch/GoodsController.php
@@ -45,6 +45,16 @@ class GoodsController extends Controller
         return view('Branch/Goods/goods_add',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
+    //编辑商品
+    public function goods_edit(Request $request)
+    {
+        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
+        $route_name = $request->path();                         //获取当前的页面路由
+        return view('Branch/Goods/goods_edit',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+    }
+
     //商品列表
     public function goods_list(Request $request)
     {
