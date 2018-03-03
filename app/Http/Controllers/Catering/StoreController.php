9c56705b4df456fd6a4a4ddbabb55c201652ea2d
kbzz <463661663@qq.com>
2018-03-03 17:04:47 +0800

    1

--- app/Http/Controllers/Catering/StoreController.php
+++ app/Http/Controllers/Catering/StoreController.php
@@ -13,10 +13,10 @@ class StoreController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = $admin_data['organization_id'];
-        $onebranch = Organization::getOneBranch([['parent_id',$organization_id]]);
+        $onebranch = Organization::checkRowExists([['parent_id',$organization_id],['type',5]]);
         dump($onebranch);
         $package_program = Package::getList(['id'=>1],0,'id','DESC');   //查询当前所选餐包含的程序 1为餐饮系统
-        return view('Catering/Store/branch_create',['package_program'=>$package_program,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        return view('Catering/Store/branch_create',['onebranch'=>$onebranch,'package_program'=>$package_program,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
         }
 
     //分店店管理
