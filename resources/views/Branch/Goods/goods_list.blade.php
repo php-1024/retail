5a873e17b64183ca8dcdfd438d3c24b9e2eaf1d2
mail@54zm.com <mail@54zm.com>
2018-03-03 14:21:10 +0800

    '1'

---
+++ resources/views/Branch/Goods/goods_list.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en" class="app">
+<head>
+    <meta charset="utf-8" />
+    <title>登录密码修改 | 零壹云管理平台 | 商户管理系统</title>
+    <link rel="stylesheet" href="{{asset('public/Company/library/jPlayer')}}/jplayer.flat.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/font.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Company')}}/css/app.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Zerone/library/sweetalert')}}/css/sweetalert.css">
+    <!--[if lt IE 9]>
+    <script src="{{asset('public/Company/library/ie')}}/html5shiv.js"></script>
+    <script src="{{asset('public/Company/library/ie')}}/respond.min.js"></script>
+    <script src="{{asset('public/Company/library/ie')}}/excanvas.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<section class="vbox">
+    @include('Branch/Public/Header')
+    <section>
+        <section class="hbox stretch">
+            <!-- .aside -->
+            @include('Branch/Public/Nav')
+            <!-- /.aside -->
+            <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">添加商品分类</h3>
+                        </div>
+                        <section class="panel panel-default">
+
+                            <header class="panel-heading font-bold">
+                                添加商品分类
+                            </header>
+                            <div class="panel-body">
+                                <form class="form-horizontal" method="get">
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">分类名称</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">分类排序</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">安全密码</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" value="">
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <div class="col-sm-12 col-sm-offset-6">
+
+                                            <button type="button" class="btn btn-success" id="addBtn">保存信息</button>
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                </form>
+                            </div>
+                        </section>
+
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+</section>
+<script src="{{asset('public/Company')}}/js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('public/Company')}}/js/bootstrap.js"></script>
+<!-- App -->
+<script src="{{asset('public/Company')}}/js/app.js"></script>
+<script src="{{asset('public/Company/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Company')}}/js/app.plugin.js"></script>
+<script src="{{asset('public/Company/library/jPlayer')}}/jquery.jplayer.min.js"></script>
+<script src="{{asset('public/Company/library/jPlayer')}}/add-on/jplayer.playlist.min.js"></script>
+<script src="{{asset('public/Company/library/sweetalert')}}/sweetalert.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#editBtn').click(function(){
+            $('#myModal').modal();
+        });
+        $('#save_btn').click(function(){
+            swal({
+                title: "温馨提示",
+                text: "操作成功",
+                type: "success"
+            });
+        });
+    });
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+//                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    //type: "warning"
+                });
+            }
+        });
+    }
+</script>
+</body>
+</html>
