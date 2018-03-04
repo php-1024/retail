4869917c15bd25d2f199c2c2a8e270d106931b26
kbzz <463661663@qq.com>
2018-03-04 11:25:36 +0800

    1

--- app/Http/Controllers/Catering/SubscriptionController.php
+++ app/Http/Controllers/Catering/SubscriptionController.php
@@ -6,7 +6,6 @@ use Session;
 class SubscriptionController extends Controller{
     //公众号设置
     public function subscription_setting(Request $request){
-        dd(1);
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
