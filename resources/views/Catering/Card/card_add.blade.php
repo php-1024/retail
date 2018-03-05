aa3e985cc33c2152198223308aa2094430ddddcc
kbzz <463661663@qq.com>
2018-03-05 18:27:29 +0800

    1

--- resources/views/Catering/Card/card_add.blade.php
+++ resources/views/Catering/Card/card_add.blade.php
@@ -35,7 +35,91 @@
             </aside>
             <!-- /.aside -->
             <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">添加会员卡</h3>
+                        </div>
+                        <section class="panel panel-default">
+                            <header class="panel-heading font-bold">
+                                添加会员卡
+                            </header>
+                            <div class="panel-body">
+                                <form class="form-horizontal" method="get">
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">会员卡名称</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" placeholder="会员卡名称">
+                                        </div>
+                                    </div>
 
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">折扣比率</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" placeholder="0 - 1 间的 2位小数">
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">发行数量</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" >
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">会员卡价格</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="input-id-1" >
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">有效时间</label>
+                                        <div class="col-sm-3">
+                                            <input class="input-sm datepicker-input form-control" size="16" type="text" value="" placeholder="开始时间" data-date-format="yyyy-mm-dd">
+                                        </div>
+                                        <div class="col-sm-1 text-center" style="padding-top: 7px;">
+                                            到
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <input class="input-sm datepicker-input form-control" size="16" type="text" value="" placeholder="结束时间" data-date-format="yyyy-mm-dd">
+                                        </div>
+                                    </div>
+
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <label class= "col-sm-2 control-label" for="input-id-1">适用店铺范围</label>
+                                        <div class="col-sm-10">
+                                            <select multiple class="chosen-select col-sm-12">
+                                                <option value="AK" selected="selected">所有店铺</option>
+                                                <option value="HI">刘记鸡煲王（总店）</option>
+                                                <option value="CA">刘记鸡煲王（宝能店）</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <div class="col-sm-12 col-sm-offset-5">
+                                            <button type="button" class="btn btn-success" id="addBtn">保存信息</button>
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                </form>
+                            </div>
+                        </section>
+                    </section>
+                </section>
             </section>
         </section>
     </section>
