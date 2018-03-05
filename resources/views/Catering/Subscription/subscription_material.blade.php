5be066a2e0fab0f0fc88ac9e957864b93625e3c7
kbzz <463661663@qq.com>
2018-03-05 18:52:13 +0800

    1

--- resources/views/Catering/Subscription/subscription_material.blade.php
+++ resources/views/Catering/Subscription/subscription_material.blade.php
@@ -212,6 +212,12 @@
 <script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/add-on/jplayer.playlist.min.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/demo.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/sweetalert/sweetalert.min.js"></script>
+<script src="{{asset('public/Catering')}}/js/file-input/bootstrap-filestyle.min.js"></script>
+
+
+
+
+
 
 <!-- Ladda -->
 <script src="{{asset('public/Catering')}}/ladda/spin.min.js"></script>
