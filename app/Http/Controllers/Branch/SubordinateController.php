2071121e1bc91ce565ec3c94b86f4e6158ec28c4
mail@54zm.com <mail@54zm.com>
2018-03-05 17:48:08 +0800

    '1'

--- app/Http/Controllers/Branch/SubordinateController.php
+++ app/Http/Controllers/Branch/SubordinateController.php
@@ -138,7 +138,7 @@ class SubordinateController extends Controller
     public function subordinate_edit(Request $request){
         $id = $request->input('id');
         $info = Account::getOne([['id',$id]]);
-        return view('Catering/Subordinate/subordinate_edit',['info'=>$info]);
+        return view('Branch/Subordinate/subordinate_edit',['info'=>$info]);
     }
 
     //编辑下级人员数据提交
