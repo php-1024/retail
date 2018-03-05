12f1b78a997fd589cc70b207149efa97cafa4d83
mail@54zm.com <mail@54zm.com>
2018-03-05 18:04:32 +0800

    '1'

--- app/Http/Controllers/Branch/SubordinateController.php
+++ app/Http/Controllers/Branch/SubordinateController.php
@@ -204,6 +204,44 @@ class SubordinateController extends Controller
         return view('Branch/Subordinate/subordinate_authorize',['info'=>$info,'role_list'=>$role_list]);
     }
 
+    //获取下级人员当前已经选取的节点
+    public function selected_rule(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $id = $request->input('id');
+        $account_id = Account::getPluck([['organization_id',$admin_data['organization_id']],['parent_id',1]],'id')->first();
+        if($account_id == $admin_data['id']) {
+            $module_node_list = Module::getListProgram(5, [], 0, 'id');//获取当前系统的所有模块和节点
+        }else{
+            $account_node_list = ProgramModuleNode::getAccountModuleNodes(5,$admin_data['id']);//获取当前用户具有权限的节点
+
+            $modules = [];
+            $nodes = [];
+            $module_node_list = [];
+            //过滤重复选出的节点和模块
+            foreach($account_node_list as $key=>$val){
+                $modules[$val->module_id] = $val->module_name;
+                $nodes[$val->module_id][$val->node_id] = $val->node_name;
+            }
+            //遍历，整理为合适的格式
+            foreach($modules as $key=>$val){
+                $module = ['id'=>$key,'module_name'=>$val];
+                foreach($nodes[$key] as $k=>$v){
+                    $module['program_nodes'][] = array('id'=>$k,'node_name'=>$v);
+                }
+                $module_node_list[] = $module;
+                unset($module);
+            }
+        }
+        $selected_nodes = [];//选中的节点
+        $selected_modules = [];//选中的模块
+        $selected_node_list = ProgramModuleNode::getAccountModuleNodes(5,$id);//获取要操作的用户有的节点
+        foreach($selected_node_list as $key=>$val){
+            $selected_modules[] = $val->module_id;
+            $selected_nodes[] = $val->node_id;
+        }
+        return view('Branch/Subordinate/selected_rule',['module_node_list'=>$module_node_list,'selected_nodes'=>$selected_nodes,'selected_modules'=>$selected_modules]);
+    }
+
     //下级人员列表
     public function subordinate_list(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
