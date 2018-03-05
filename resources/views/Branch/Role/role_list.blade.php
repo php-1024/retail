1aaceeb32fbaa8be9261c015a8db3545e8c51c33
mail@54zm.com <mail@54zm.com>
2018-03-05 16:34:47 +0800

    '1'

--- resources/views/Branch/Role/role_list.blade.php
+++ resources/views/Branch/Role/role_list.blade.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="utf-8"/>
     <title>角色列表 | 零壹云管理平台 | 总分店管理系统</title>
-    <link rel="stylesheet" href="{{asset('public/Branch/library')}}/jPlayer/jplayer.flat.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/bootstrap.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/animate.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/font-awesome.min.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/simple-line-icons.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/font.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/app.css" type="text/css"/>
-    <link href="{{asset('public/Branch/library')}}/sweetalert/sweetalert.css" rel="stylesheet"/>
-    <link href="{{asset('public/Branch/library')}}/iCheck/css/custom.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{asset('public/Branch/library')}}/jPlayer/jplayer.flat.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/font.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/app.css" type="text/css" />
+    <link href="{{asset('public/Branch/library')}}/sweetalert/sweetalert.css" rel="stylesheet" />
+    <link href="{{asset('public/Branch/library')}}/iCheck/css/custom.css" rel="stylesheet" />
     <!--[if lt IE 9]>
     <script src="{{asset('public/Branch/library')}}/ie/html5shiv.js"></script>
     <script src="{{asset('public/Branch/library')}}/ie/respond.min.js"></script>
@@ -71,16 +71,10 @@
                                         <tr>
                                             <td>{{ $val->id }}</td>
                                             <td>{{ $val->role_name }}</td>
-                                            {{--<td>--}}
-                                                {{--@foreach($role_module_nodes[$val->id] as $k=>$v)--}}
-                                                    {{--<button data-original-title="{{$k}}" data-content="@foreach($v as $kk=>$vv) {{$vv}} @endforeach" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">{{$k}}</button>&nbsp;&nbsp;&nbsp;--}}
-                                                {{--@endforeach--}}
-                                            {{--</td>--}}
                                             <td>
-                                                <button data-original-title="订单模块" data-content="订单添加  订单删除  订单查看 订单编辑" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">订单模块</button>&nbsp;&nbsp;
-                                                <button data-original-title="订单模块" data-content="订单添加  订单删除  订单查看 订单编辑" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">订单模块</button>&nbsp;&nbsp;
-                                                <button data-original-title="订单模块" data-content="订单添加  订单删除  订单查看 订单编辑" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">订单模块</button>&nbsp;&nbsp;
-                                                <button data-original-title="订单模块" data-content="订单添加  订单删除  订单查看 订单编辑" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">订单模块</button>&nbsp;&nbsp;
+                                                @foreach($role_module_nodes[$val->id] as $k=>$v)
+                                                    <button data-original-title="{{$k}}" data-content="@foreach($v as $kk=>$vv) {{$vv}} @endforeach" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">{{$k}}</button>&nbsp;&nbsp;&nbsp;
+                                                @endforeach
                                             </td>
                                             <td>{{ $val->created_at }}</td>
                                             <td>
@@ -114,8 +108,8 @@
         </section>
     </section>
 </section>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-     aria-hidden="true"></div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"></div>
+
 <script src="{{asset('public/Branch')}}/js/jquery.min.js"></script>
 <!-- Bootstrap -->
 <script src="{{asset('public/Branch')}}/js/bootstrap.js"></script>
@@ -126,9 +120,34 @@
 <script src="{{asset('public/Branch/library')}}/file-input/bootstrap-filestyle.min.js"></script>
 <script src="{{asset('public/Branch/library')}}/jPlayer/jquery.jplayer.min.js"></script>
 <script src="{{asset('public/Branch/library')}}/jPlayer/add-on/jplayer.playlist.min.js"></script>
-<script src="{{asset('public/Branch/library')}}/sweetalert/sweetalert.min.js"></script>
+<script src="{{asset('public/Branch/library')}}/jPlayer/demo.js"></script>
 <script src="{{asset('public/Branch/library')}}/iCheck/js/icheck.min.js"></script>
+
+
 <script type="text/javascript">
+    $(function(){
+        $('#addBtn').click(function(){
+            swal({
+                title: "温馨提示",
+                text: "操作成功",
+                type: "success"
+            });
+        });
+
+        $('.i-checks').iCheck({
+            checkboxClass: 'icheckbox_square-green',
+            radioClass: 'iradio_square-green',
+        });
+
+        $('#editBtn').click(function(){
+            $('#myModal').modal();
+        });
+        $('#deleteBtn').click(function(){
+            $('#myModal2').modal();
+        });
+        $('.popovers').popover();
+    });
+
     //获取删除权限角色删除密码确认框
     function getDeleteComfirmForm(id) {
         var url = $('#role_delete_comfirm_url').val();
