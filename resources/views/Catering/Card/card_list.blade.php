2ed91b5bc045174721f99fffb300f9328737a071
kbzz <463661663@qq.com>
2018-03-05 18:31:46 +0800

    1

--- resources/views/Catering/Card/card_list.blade.php
+++ resources/views/Catering/Card/card_list.blade.php
@@ -35,7 +35,171 @@
             </aside>
             <!-- /.aside -->
             <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">会员卡管理</h3>
+                        </div>
+                        <section class="panel panel-default">
+                            <header class="panel-heading">
+                                会员卡列表
+                            </header>
+                            <div class="row wrapper">
 
+                                <div class="well">
+                                    <h3>温馨提示</h3>
+                                    <p class="text-danger">1.新生成的会员卡，默认对适用店铺中的所有商品有效，若想调整，请及时调整</p>
+                                    <p class="text-danger">2.新生成的会员卡，初始状态为暂停销售，待设置好商品后可以点击开始销售会员卡才会在前端显示</p>
+                                    <p class="text-danger">3.已经销售出部分的会员卡将无法删除</p>
+                                </div>
+
+                            </div>
+                            <div class="row wrapper">
+                                <form class="form-horizontal" method="get">
+                                    <div class="col-sm-2">
+                                        <select name="account" class="form-control m-b">
+                                            <option>所有分店</option>
+                                            <option>刘记鸡煲王（总店）</option>
+                                            <option>刘记鸡煲王（宝能店）</option>
+
+                                        </select>
+                                    </div>
+                                    <label class="col-sm-1 control-label">会员卡名称</label>
+
+                                    <div class="col-sm-2">
+                                        <input class="input-sm form-control" size="16" type="text" value="">
+                                    </div>
+
+                                    <div class="col-sm-3">
+                                        <button type="button" class="btn btn-s-md btn-info"><i class="fa fa-search"></i>&nbsp;&nbsp;搜索</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table table-striped b-t b-light">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>名称</th>
+                                        <th>售价</th>
+                                        <th>折扣</th>
+                                        <th>适用分店</th>
+                                        <th>发行总数</th>
+                                        <th>剩余总数</th>
+                                        <th>有效期</th>
+                                        <th>状态</th>
+                                        <th>发行时间</th>
+
+                                        <th>操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>黄金会员卡</td>
+                                        <td>
+                                            100.00
+                                        </td>
+                                        <td>
+                                            <label class="label label-primary">0.6</label>
+                                        </td>
+                                        <td>
+                                            <p><button data-original-title="适用分店" data-content="所有分店" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">4个分店</button></p>
+                                        </td>
+                                        <td>
+                                            999
+                                        </td>
+                                        <td>
+                                            666
+                                        </td>
+                                        <td>
+                                            2017-08-09 到 2018-08-09
+                                        </td>
+                                        <td>
+                                            <label class="label label-success">销售中</label>
+                                        </td>
+                                        <td>2017-08-09 11:11:11</td>
+                                        <td>
+                                            <button class="btn btn-info btn-xs" id="editBtn" type="button"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>&nbsp;&nbsp;
+                                            <button class="btn btn-primary btn-xs" onclick="location.href='card_goods.html'" id="goodsBtn" type="button"><i class="fa fa-list"></i>&nbsp;&nbsp;调整适用商品</button>&nbsp;&nbsp;
+                                            <button class="btn btn-warning btn-xs" id="lockBtn" type="button"><i class="fa fa-lock"></i>&nbsp;&nbsp;停止销售</button>&nbsp;&nbsp;
+                                            <button class="btn btn-danger btn-xs" id="deleteBtn" type="button"><i class="fa fa-lock"></i>&nbsp;&nbsp;删除会员卡</button>&nbsp;&nbsp;
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>黄金会员卡</td>
+                                        <td>
+                                            100.00
+                                        </td>
+                                        <td>
+                                            <label class="label label-primary">0.6</label>
+                                        </td>
+                                        <td>
+                                            <p><button data-original-title="适用分店" data-content="刘记鸡煲王（宝能店），刘记鸡煲王（总店）" data-placement="top" data-trigger="hover" class="btn btn-info btn-xs popovers">2个分店</button></p>
+                                        </td>
+                                        <td>
+                                            999
+                                        </td>
+                                        <td>
+                                            666
+                                        </td>
+                                        <td>
+                                            2017-08-09 到 2018-08-09
+                                        </td>
+                                        <td>
+                                            <label class="label label-warning">销售停止</label>
+                                        </td>
+                                        <td>2017-08-09 11:11:11</td>
+                                        <td>
+                                            <button class="btn btn-info btn-xs" type="button"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>&nbsp;&nbsp;
+                                            <button class="btn btn-primary btn-xs" type="button"><i class="fa fa-list"></i>&nbsp;&nbsp;调整适用商品</button>&nbsp;&nbsp;
+                                            <button class="btn btn-success btn-xs" type="button"><i class="fa fa-lock"></i>&nbsp;&nbsp;开始销售</button>&nbsp;&nbsp;
+                                            <button class="btn btn-danger btn-xs" type="button"><i class="fa fa-lock"></i>&nbsp;&nbsp;删除会员卡</button>&nbsp;&nbsp;
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <footer class="panel-footer">
+                                <div class="row">
+                                    <div class="col-sm-12 text-right text-center-xs">
+                                        <ul class="pagination pull-right">
+                                            <li class="footable-page-arrow disabled">
+                                                <a data-page="first" href="#first">«</a>
+                                            </li>
+
+                                            <li class="footable-page-arrow disabled">
+                                                <a data-page="prev" href="#prev">‹</a>
+                                            </li>
+                                            <li class="footable-page active">
+                                                <a data-page="0" href="#">1</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">2</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">3</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">4</a>
+                                            </li>
+                                            <li class="footable-page">
+                                                <a data-page="1" href="#">5</a>
+                                            </li>
+                                            <li class="footable-page-arrow">
+                                                <a data-page="next" href="#next">›</a>
+                                            </li>
+                                            <li class="footable-page-arrow">
+                                                <a data-page="last" href="#last">»</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </footer>
+                        </section>
+                    </section>
+                </section>
             </section>
         </section>
     </section>
