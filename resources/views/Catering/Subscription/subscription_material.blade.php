ca784c98ce060020345793c42f021d920c99b661
kbzz <463661663@qq.com>
2018-03-05 18:46:59 +0800

    1

--- resources/views/Catering/Subscription/subscription_material.blade.php
+++ resources/views/Catering/Subscription/subscription_material.blade.php
@@ -212,6 +212,10 @@
 <script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/demo.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/sweetalert/sweetalert.min.js"></script>
 
+<!-- Ladda -->
+<script src="{{asset('public/Catering')}}/ladda/spin.min.js"></script>
+<script src="{{asset('public/Catering')}}/ladda/ladda.min.js"></script>
+<script src="{{asset('public/Catering')}}/ladda/ladda.jquery.min.js"></script>
 
 <script type="text/javascript">
     $(function(){
