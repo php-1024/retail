542fbcc7d9c008817f5328ccb9498b71ad5e9b03
mail@54zm.com <mail@54zm.com>
2018-03-06 12:20:17 +0800

    '1'

--- app/Http/Controllers/Branch/DeviceController.php
+++ app/Http/Controllers/Branch/DeviceController.php
@@ -71,6 +71,16 @@ class DeviceController extends Controller
         return view('Branch/Device/printer_list',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
+    //设备管理-打印机关联商品
+    public function printer_goods(Request $request)
+    {
+        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
+        $route_name = $request->path();                         //获取当前的页面路由
+        return view('Branch/Device/printer_goods',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+    }
+
 }
 
 ?>
