66dc9fb015762e4ef770fdb378d8080cffe769e0
kbzz <463661663@qq.com>
2018-03-06 15:29:34 +0800

    1

--- app/Http/Controllers/Proxy/RoleController.php
+++ app/Http/Controllers/Proxy/RoleController.php
@@ -103,7 +103,7 @@ class RoleController extends Controller{
      * $role_id 当前角色的ID
      */
     private function getModuleNode($role_id){
-        $list = ProgramModuleNode::getRoleModuleNodes(1,$role_id);
+        $list = ProgramModuleNode::getRoleModuleNodes(2,$role_id);
         $module_nodes = [];
         foreach($list as $key=>$val){
             $module_nodes[$val['module_name']][] = $val['node_name'];
