6b6c20aba848ebc7de2c01c9f5125d66086752fc
zeo <234521890@qq.com>
2018-03-06 17:03:45 +0800

    by zeo

--- app/Services/ZeroneRedis/ZeroneRedis.php
+++ app/Services/ZeroneRedis/ZeroneRedis.php
@@ -122,8 +122,7 @@ class ZeroneRedis
 
     public static function create_proxy_menu_cache($id){
         $menu = ProgramMenu::getList([[ 'parent_id',0],['program_id','2']],0,'sort','asc')->toArray();//获取零壹管理系统的一级菜单
-        dump($menu);
-        exit;
+
         if($id <> 1){
             //查询用户所具备的所有节点的路由
             $account_info = Account::getOne([['id',$id]]);
