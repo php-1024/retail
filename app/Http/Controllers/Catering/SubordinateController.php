ad79ce0fa21ba954c3bb80d7df4855d16f917188
kbzz <463661663@qq.com>
2018-03-06 09:34:25 +0800

    1

--- app/Http/Controllers/Catering/SubordinateController.php
+++ app/Http/Controllers/Catering/SubordinateController.php
@@ -20,8 +20,8 @@ class SubordinateController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         //获取当前用户添加的权限角色
-        $role_list = OrganizationRole::getList([['program_id',7],['created_by',$admin_data['id']]],0,'id');
-        $module_node_list = Module::getListProgram(7, [], 0, 'id');//获取当前系统的所有模块和节点
+        $role_list = OrganizationRole::getList([['program_id',4],['created_by',$admin_data['id']]],0,'id');
+        $module_node_list = Module::getListProgram(4, [], 0, 'id');//获取当前系统的所有模块和节点
         $selected_modules = [];//选中的模块
         $selected_nodes = [];//选中的节点
         return view('Catering/Subordinate/subordinate_add',['selected_nodes'=>$selected_nodes,'selected_modules'=>$selected_modules,'module_node_list'=>$module_node_list,'role_list'=>$role_list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
@@ -33,9 +33,9 @@ class SubordinateController extends Controller{
         $role_id = $request->input('role_id');
         $account_id = Account::getPluck([['organization_id',$admin_data['organization_id']],['parent_id',1]],'id')->first();
         if($account_id == $admin_data['id']) {
-            $module_node_list = Module::getListProgram(7, [], 0, 'id');//获取当前系统的所有模块和节点
+            $module_node_list = Module::getListProgram(4, [], 0, 'id');//获取当前系统的所有模块和节点
         }else{
-            $account_node_list = ProgramModuleNode::getAccountModuleNodes(7,$admin_data['id']);//获取当前用户具有权限的节点
+            $account_node_list = ProgramModuleNode::getAccountModuleNodes(4,$admin_data['id']);//获取当前用户具有权限的节点
 
             $modules = [];
             $nodes = [];
