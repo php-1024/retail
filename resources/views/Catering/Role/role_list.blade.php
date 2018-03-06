b1a67fc0261b2d9f12fd08380287a7f0648da70d
kbzz <463661663@qq.com>
2018-03-06 15:38:11 +0800

    1

--- resources/views/Catering/Role/role_list.blade.php
+++ resources/views/Catering/Role/role_list.blade.php
@@ -195,9 +195,13 @@
             }
         });
     }
-    $('.i-checks').iCheck({
-        checkboxClass: 'icheckbox_square-green',
-        radioClass: 'iradio_square-green',
+
+    $(function(){
+        $('.i-checks').iCheck({
+            checkboxClass: 'icheckbox_square-green',
+            radioClass: 'iradio_square-green',
+        });
+        $('.popovers').popover();
     });
 </script>
 </body>
