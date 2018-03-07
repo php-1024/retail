37eb0a4adebe8410210ce6357e5de9c5133a9fb6
mail@54zm.com <mail@54zm.com>
2018-03-07 14:11:23 +0800

    '1'

--- resources/views/Zerone/Dashboard/display.blade.php
+++ resources/views/Zerone/Dashboard/display.blade.php
@@ -188,7 +188,7 @@
                                 <tbody>
                                 @foreach($login_log_list as $key=>$val)
                                 <tr>
-                                    <td>{{  $val->accounts->account }}</td>
+{{--                                    <td>{{  $val->accounts->account }}</td>--}}
                                     <td>{{  long2ip($val->ip) }}</td>
                                     <td>{{  $val->ip_position }}</td>
                                     <td>{{  $val->created_at }}</td>
