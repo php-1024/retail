c13f170e03633617e52a29d2d912ab06f9e367e4
mail@54zm.com <mail@54zm.com>
2018-03-07 11:40:25 +0800

    '1'

--- app/Http/Controllers/Branch/DisplayController.php
+++ app/Http/Controllers/Branch/DisplayController.php
@@ -55,6 +55,7 @@ class DisplayController extends Controller
         $organization_name  = $request->organization_name;
         $where = ['type'=>'5'];//type=5分店组织
         $organization = Organization::getBranchAndWarzone($organization_name,$where,20,'id','ASC'); //查询分店
+        dump($organization);
         foreach ($organization as $key=>$val){
             $catering = Organization::getOneCatering(['id'=>$val->parent_id]);
             $val->cateringname = $catering->organization_name;
