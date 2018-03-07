aa7243981aee166d6eddb55515cf98e5257f35d3
kbzz <463661663@qq.com>
2018-03-07 19:09:48 +0800

    1

--- app/Http/Controllers/Zerone/StoreController.php
+++ app/Http/Controllers/Zerone/StoreController.php
@@ -30,7 +30,7 @@ class StoreController extends Controller{
             $oneAssets = Assets::getOne([['organization_id',$val->id]]);
             $branch_munber[$val->id]=!empty($oneAssets->program_spare_num)?$oneAssets->program_spare_num:0;
         }
-        dd($branch_munber);
+        dd($branch_munber['23']);
         return view('Zerone/Store/store_list',['listStore'=>$listStore,'branch_munbe'=>$branch_munber,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
