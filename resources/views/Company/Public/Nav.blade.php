a937496f8557d195aa855334c6e567b9e8a57c51
zeo <234521890@qq.com>
2018-03-08 10:07:31 +0800

    by zeo

--- resources/views/Company/Public/Nav.blade.php
+++ resources/views/Company/Public/Nav.blade.php
@@ -7,7 +7,7 @@
                 <nav class="nav-primary hidden-xs">
                     <ul class="nav" data-ride="collapse">
                         @foreach($menu_data as $key=>$val)
-                            @dump($val)
+                            <?php dump($val);?>
                         <li @if(in_array($route_name,explode(',',$val['menu_routes_bind']))) class="active" @endif>
                             <a href="javascript:;" class="auto">
                                 <span class="pull-right text-muted">
