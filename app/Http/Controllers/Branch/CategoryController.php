e2c15c72044bf0842013fafe5b1dee094e91b49d
mail@54zm.com <mail@54zm.com>
2018-03-09 13:58:41 +0800

    '1'

--- app/Http/Controllers/Branch/CategoryController.php
+++ app/Http/Controllers/Branch/CategoryController.php
@@ -69,9 +69,13 @@ class CategoryController extends Controller
         $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
         $route_name = $request->path();                         //获取当前的页面路由
-        dump($request);
-//        OrganizationCategory::getPaginage();
-        return view('Branch/Category/category_list',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+        $where = [
+            'program_id' => '5',
+            'organization_id' => $admin_data['organization_id'],
+        ];
+        $category = OrganizationCategory::getPaginage($where,'1','category_sort','DESC');
+        dump($category);
+        return view('Branch/Category/category_list',['category'=>$category,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 }
 
