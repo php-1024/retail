fe0ff8333491ee28aeb8c2f7eb5f82a5f183c50b
mail@54zm.com <mail@54zm.com>
2018-03-09 18:30:48 +0800

    '1'

--- resources/views/Branch/Goods/goods_edit.blade.php
+++ resources/views/Branch/Goods/goods_edit.blade.php
@@ -114,7 +114,7 @@
                                         </form>
                                     </div>
                                     <div class="tab-pane fade in" id="picture">
-                                        <button type="button" class="btn btn-success" id="addBtn"><i class="fa fa-plus"></i>&nbsp;&nbsp;添加图片</button>
+                                        <button type="button" class="btn btn-success" id="thumb"><i class="fa fa-plus"></i>&nbsp;&nbsp;添加图片</button>
                                         <div class="line line-dashed b-b line-lg pull-in"></div>
                                         <div class="table-responsive">
                                             <table class="table table-bordered table-stripped">
@@ -482,6 +482,44 @@
     </section>
 </section>
 
+
+<div class="modal fade" id="thumb" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <form class="form-horizontal tasi-form" method="get">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">添加规格</h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal" method="get">
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="input-id-1">规格名称</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="input-id-1" value="">
+                            </div>
+                        </div>
+                        <div class="line line-dashed b-b line-lg pull-in"></div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="input-id-1">安全密码</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="input-id-1" value="">
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
+                    <button class="btn btn-success" type="button" id="addBtn">确定</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <form class="form-horizontal tasi-form" method="get">
         <div class="modal-dialog">
@@ -516,6 +554,7 @@
         </div>
     </form>
 </div>
+
 
 <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <form class="form-horizontal tasi-form" method="get">
@@ -607,6 +646,9 @@
         });
         $(".addBtn").click(function(){
             $('#myModal').modal();
+        });
+        $(".addBtnthumb").click(function(){
+            $('#thumb').modal();
         });
         $(".deleteBtn").click(function(){
             $('#myModal1').modal();
