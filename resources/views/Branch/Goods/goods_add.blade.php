f5b5de80144dc1b0c87485c42dd3314308da0152
mail@54zm.com <mail@54zm.com>
2018-03-09 16:06:09 +0800

    '1'

--- resources/views/Branch/Goods/goods_add.blade.php
+++ resources/views/Branch/Goods/goods_add.blade.php
@@ -103,8 +103,7 @@
                                             <div class="line line-dashed b-b line-lg pull-in"></div>
                                             <div class="form-group">
                                                 <div class="col-sm-12 col-sm-offset-6">
-
-                                                    <button type="button" class="btn btn-success" id="addBtn">保存信息</button>
+                                                    <button type="button" class="btn btn-success" id="addBtn" onclick="return postForm();">保存信息</button>
                                                 </div>
                                             </div>
 
