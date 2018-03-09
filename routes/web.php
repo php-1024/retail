7eef00d0f0fbf32f79fb2b3b07d383ad4f78f30c
kbzz <463661663@qq.com>
2018-03-09 14:40:58 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -612,7 +612,7 @@ Route::group(['prefix'=>'catering'],function(){
         Route::post('selected_rule','Catering\SubordinateController@selected_rule')->middleware('CateringCheckAjax');//下级人员已经选中的权限出框
 
         //用户管理
-        Route::post('member_label_add','Catering\UserController@member_label_add')->middleware('ZeroneCheckAjax');      //添加h会员标签ajax显示页面
+        Route::post('member_label_add','Catering\UserController@member_label_add')->middleware('CateringCheckAjax');    //添加h会员标签ajax显示页面
 
         //总分店管理
         Route::post('branch_create_check','Catering\StoreController@branch_create_check')->middleware('CateringCheckAjax');//总分店添加功能提交
