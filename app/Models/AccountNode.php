aee5d02c25dc73159977cb6eb1b4e1235c330aeb
zeo <234521890@qq.com>
2018-03-09 19:27:49 +0800

    by zeo

--- app/Models/AccountNode.php
+++ app/Models/AccountNode.php
@@ -50,7 +50,8 @@ class AccountNode extends Model
     public static function addNewsNode($program_id,$nodes){
         $organization_list = Organization::where('program_id',$program_id)->get();
         foreach($organization_list as $key=>$val){
-            dump($val);
+            $main_account = Account::where('organization_id',$val['id'])->where('parent_id',0)->first();
+            dump($main_account);
         }
     }
 
