a2c743b34c3de3830890263de6ae06439ab42613
kbzz <463661663@qq.com>
2018-03-09 14:29:51 +0800

    1

--- resources/views/Catering/User/user_tag.blade.php
+++ resources/views/Catering/User/user_tag.blade.php
@@ -242,6 +242,8 @@
 <script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/add-on/jplayer.playlist.min.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/demo.js"></script>
 <script type="text/javascript" src="{{asset('public/Catering')}}/sweetalert/sweetalert.min.js"></script>
+<script src="{{asset('public/Catering')}}/iCheck/js/icheck.min.js"></script>
+
 <script type="text/javascript">
     //添加会员标签
     function getAddForm(){
