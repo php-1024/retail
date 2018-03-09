5629779bdff4c75f716b8f6ac314fa619fdf4306
mail@54zm.com <mail@54zm.com>
2018-03-09 16:51:04 +0800

    '1'

--- app/Http/Controllers/Branch/GoodsController.php
+++ app/Http/Controllers/Branch/GoodsController.php
@@ -68,6 +68,7 @@ class GoodsController extends Controller
             }
             DB::commit();
         } catch (\Exception $e) {
+            dd($e);
             DB::rollBack();//事件回滚
             return response()->json(['data' => '添加分类失败，请检查', 'status' => '0']);
         }
