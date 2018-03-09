54ae64cf10d4409d7e55e240ca86b649a99c8955
mail@54zm.com <mail@54zm.com>
2018-03-09 11:09:44 +0800

    '1'

--- app/Http/Controllers/Branch/CategoryController.php
+++ app/Http/Controllers/Branch/CategoryController.php
@@ -32,7 +32,7 @@ class CategoryController extends Controller
         $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
         $route_name = $request->path();                         //获取当前的页面路由
-        return view('Branch/Category/category_add',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+        dd($request);
     }
 
     //商品分类列表
