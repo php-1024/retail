32b2b26383356ef812bcf5323fbb0c3ef983e09e
kbzz <463661663@qq.com>
2018-03-10 17:23:51 +0800

    1

--- resources/views/Catering/User/user_list_edit.blade.php
+++ resources/views/Catering/User/user_list_edit.blade.php
@@ -58,7 +58,7 @@
                     <div class="form-group">
                         <label class="col-sm-2 text-right">手机号码</label>
                         <div class="col-sm-10">
-                            <input type="text" value="{{$userInfo['mobile']}}" placeholder="手机号码" class="form-control" name="mobile">
+                            <input type="text" value="{{$data['mobile']}}" placeholder="手机号码" class="form-control" name="mobile">
                         </div>
                     </div>
                     <div style="clear:both;"></div>
