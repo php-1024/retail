ca00868caa39f64db0cc04bde3f29e0667757718
mail@54zm.com <mail@54zm.com>
2018-03-10 17:34:03 +0800

    '1'

--- resources/views/Branch/Goods/goods_spec_comfirm.blade.php
+++ resources/views/Branch/Goods/goods_spec_comfirm.blade.php
@@ -52,7 +52,23 @@
 //                    alert('添加子规格类成功！');
 //                    $('#spec_content').html(json);
 //                    $('#spec_content').modal();
-                    window.location.reload();
+//                    window.location.reload();
+
+                    $.ajax({
+                        url:'{{url('branch/ajax/goods_spec')}}',//你对数据库的操作路径
+                        data:{//这是参数
+                            id:1,
+                            name:'iszmxw',
+                        },
+                        type:'post',//提交方式
+                        success:function(data){//后台处理数据成功后的回调函数
+                            $("#spec_content").html(data);
+                        },
+                        error:function(data){//后台处理数据失败后的回调函数
+                            //   alert(data)
+                        }
+                    })
+
                 });
             }else{
                 swal({
