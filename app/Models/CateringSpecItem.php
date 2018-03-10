7c25d2946737fc3024ad65c327f7ce4c59f357d9
mail@54zm.com <mail@54zm.com>
2018-03-10 19:19:14 +0800

    '1'

--- app/Models/CateringSpecItem.php
+++ app/Models/CateringSpecItem.php
@@ -6,26 +6,21 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-class SpecItem extends Model{
+class CateringSpecItem extends Model{
     use SoftDeletes;
-    protected $table = 'spec_item';
+    protected $table = 'catering_spec_item';
     protected $primaryKey = 'id';
     public $timestamps = true;
     public $dateFormat = 'U';//设置保存的created_at updated_at为时间戳格式
 
-    //和创建者CateringGoods表多对一的关系
-    public function spec(){
-        return $this->belongsto('App\Models\Spec','spec_id');
-    }
-
-    //获取单条餐饮商品信息
-    public static function getOne($where){
-        return self::with('spec')->where($where)->first();
+    //和餐饮规格表CateringSpec一对多的关系
+    public function catering_spec(){
+        return $this->belongsto('App\Models\CateringSpec','spec_id');
     }
 
     //获取餐饮商品规格列表
     public static function getList($where,$limit=0,$orderby,$sort='DESC'){
-        $model = new SpecItem();
+        $model = new CateringSpecItem();
         if(!empty($limit)){
             $model = $model->limit($limit);
         }
@@ -34,7 +29,7 @@ class SpecItem extends Model{
 
     //添加餐饮商品子规格
     public static function addSpecItem($param){
-        $model = new SpecItem();
+        $model = new CateringSpecItem();
         $model->name = $param['name'];
         $model->spec_id = $param['spec_id'];
         $model->save();
@@ -49,11 +44,6 @@ class SpecItem extends Model{
             }
             $model->save();
         }
-    }
-
-    //获取分页列表
-    public static function getPaginage($where,$paginate,$orderby,$sort='DESC'){
-        return self::with('spec')->where($where)->orderBy($orderby,$sort)->paginate($paginate);
     }
 }
 ?>
