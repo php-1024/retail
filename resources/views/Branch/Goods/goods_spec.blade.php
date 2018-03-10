cdcaff4a071f15f19d8e4426c382a2d755ced392
mail@54zm.com <mail@54zm.com>
2018-03-10 12:35:45 +0800

    '1'

--- resources/views/Branch/Goods/goods_spec.blade.php
+++ resources/views/Branch/Goods/goods_spec.blade.php
@@ -1,62 +1,63 @@
+@foreach($spec as $key=>$val)
+    <div class="m-t">
+        <label class="label label-primary">主食</label>
+        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
+        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+    </div>
+    <div class="m-t">
+        <div class="m-t col-lg-2">
+            <label class="label label-success">米饭</label>
+            <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
+            <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+        </div>
+        <div class="m-t col-lg-2">
+            <label class="label label-success">拉面</label>
+            <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
+            <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+        </div>
+        <div class="m-t col-lg-2">
+            <label class="label label-success">餐包</label>
+            <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
+            <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
+        </div>
+        <div class="m-t col-lg-2">
+            <button type="button" class="btn addBtn btn-info btn-xs"><i class="fa fa-plus"></i>&nbsp;&nbsp;添加规格子项</button>
+        </div>
+    </div>
+    <div style="clear: both;"></div>
+    <div class="line line-dashed b-b line-lg pull-in"></div>
+@endforeach
 
-<div class="m-t">
-    <label class="label label-primary">主食</label>
-    <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-    <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-</div>
-<div class="m-t">
-    <div class="m-t col-lg-2">
-        <label class="label label-success">米饭</label>
-        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-    </div>
-    <div class="m-t col-lg-2">
-        <label class="label label-success">拉面</label>
-        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-    </div>
-    <div class="m-t col-lg-2">
-        <label class="label label-success">餐包</label>
-        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-    </div>
-    <div class="m-t col-lg-2">
-        <button type="button" class="btn addBtn btn-info btn-xs"><i class="fa fa-plus"></i>&nbsp;&nbsp;添加规格子项</button>
-    </div>
-</div>
-<div style="clear: both;"></div>
-<div class="line line-dashed b-b line-lg pull-in"></div>
 
-
-<div class="m-t">
-    <label class="label label-primary">味道</label>
-    <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-    <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-</div>
-<div class="m-t">
-    <div class="m-t col-lg-2">
-        <label class="label label-success">酸</label>
-        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-    </div>
-    <div class="m-t col-lg-2">
-        <label class="label label-success">甜</label>
-        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-    </div>
-    <div class="m-t col-lg-2">
-        <label class="label label-success">苦</label>
-        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-    </div>
-    <div class="m-t col-lg-2">
-        <label class="label label-success">辣</label>
-        <button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>
-        <button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>
-    </div>
-    <div class="m-t col-lg-2">
-        <button type="button" class="btn addBtn btn-info btn-xs"><i class="fa fa-plus"></i>&nbsp;&nbsp;添加规格子项</button>
-    </div>
-</div>
-<div style="clear: both;"></div>
-<div class="line line-dashed b-b line-lg pull-in"></div>
+{{--<div class="m-t">--}}
+    {{--<label class="label label-primary">味道</label>--}}
+    {{--<button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>--}}
+    {{--<button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>--}}
+{{--</div>--}}
+{{--<div class="m-t">--}}
+    {{--<div class="m-t col-lg-2">--}}
+        {{--<label class="label label-success">酸</label>--}}
+        {{--<button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>--}}
+        {{--<button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>--}}
+    {{--</div>--}}
+    {{--<div class="m-t col-lg-2">--}}
+        {{--<label class="label label-success">甜</label>--}}
+        {{--<button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>--}}
+        {{--<button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>--}}
+    {{--</div>--}}
+    {{--<div class="m-t col-lg-2">--}}
+        {{--<label class="label label-success">苦</label>--}}
+        {{--<button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>--}}
+        {{--<button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>--}}
+    {{--</div>--}}
+    {{--<div class="m-t col-lg-2">--}}
+        {{--<label class="label label-success">辣</label>--}}
+        {{--<button type="button" class="btn editBtn btn-info btn-xs"><i class="fa fa-edit"></i></button>--}}
+        {{--<button type="button" class="btn deleteBtn btn-danger btn-xs"><i class="fa fa-times"></i></button>--}}
+    {{--</div>--}}
+    {{--<div class="m-t col-lg-2">--}}
+        {{--<button type="button" class="btn addBtn btn-info btn-xs"><i class="fa fa-plus"></i>&nbsp;&nbsp;添加规格子项</button>--}}
+    {{--</div>--}}
+{{--</div>--}}
+{{--<div style="clear: both;"></div>--}}
+{{--<div class="line line-dashed b-b line-lg pull-in"></div>--}}
