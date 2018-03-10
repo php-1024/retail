85eb29644d201003ddf2afedc1190e51298b14f0
kbzz <463661663@qq.com>
2018-03-10 19:26:00 +0800

    1

--- app/Http/Controllers/Catering/UserController.php
+++ app/Http/Controllers/Catering/UserController.php
@@ -280,7 +280,7 @@ class UserController extends Controller{
         $store_id = $admin_data['organization_id'];//组织id
         $list = StoreUserLog::getPaginage([['store_id',$store_id]],'5','id');
         dump($list);
-        return view('Catering/User/user_timeline',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        return view('Catering/User/user_timeline',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 }
 ?>
