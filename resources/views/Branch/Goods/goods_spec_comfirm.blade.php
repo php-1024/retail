2537b28a8bc35a647ab7f9f26aa063c82aca1856
mail@54zm.com <mail@54zm.com>
2018-03-10 17:35:57 +0800

    '1'

--- resources/views/Branch/Goods/goods_spec_comfirm.blade.php
+++ resources/views/Branch/Goods/goods_spec_comfirm.blade.php
@@ -1,6 +1,7 @@
  <form method="post" class="form-horizontal"  role="form" id="spec_item_add_check" action="{{ url('branch/ajax/spec_item_add_check') }}">
         <input type="hidden" name="_token" value="{{csrf_token()}}">
         <input type="hidden" name="spec_id" value="{{$spec_id}}">
+        <input type="hidden" name="spec" value="{{$spec}}">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -57,8 +58,7 @@
                     $.ajax({
                         url:'{{url('branch/ajax/goods_spec')}}',//你对数据库的操作路径
                         data:{//这是参数
-                            id:1,
-                            name:'iszmxw',
+                            spec:'{{$spec}}',
                         },
                         type:'post',//提交方式
                         success:function(data){//后台处理数据成功后的回调函数
