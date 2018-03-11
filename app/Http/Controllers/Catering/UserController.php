0ae12d012312e52e4ebbcdd36b46195d9d015995
kbzz <463661663@qq.com>
2018-03-11 11:40:15 +0800

    1

--- app/Http/Controllers/Catering/UserController.php
+++ app/Http/Controllers/Catering/UserController.php
@@ -168,15 +168,13 @@ class UserController extends Controller{
                     //减少原粉丝标签的人数
                     $label_number = Label::getPluck([['id',$oneData->label_id]],'label_number')->first();//获取原粉丝标签的人数
                     $number = $label_number-1;
-                    dd($number);
-
                     Label::editLabel([['id',$label_id]],['label_number'=>$number]);//修改粉丝标签的人数
                 }
                  if($label_id != 0){
                      //增加现有的粉丝标签人数
-                     $label_number = Label::getPluck([['id',$label_id]],'label_number')->first();//获取粉丝标签的人数
-                     $number = $label_number+1;
-                     Label::editLabel([['id',$label_id]],['label_number'=>$number]);//修改粉丝标签的人数
+                     $add_label_number = Label::getPluck([['id',$label_id]],'label_number')->first();//获取粉丝标签的人数
+                     $add_number = $add_label_number+1;
+                     Label::editLabel([['id',$label_id]],['label_number'=>$add_number]);//修改粉丝标签的人数
                  }
                 UserLabel::editUserLabel([['id',$oneData->id]],['label_id'=>$label_id]);//修改粉丝标签关联表Label_id
 
