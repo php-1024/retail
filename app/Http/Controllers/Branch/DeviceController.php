d901c33b2c2f28c32aeb1ee3f7f2ebafc38e9682
mail@54zm.com <mail@54zm.com>
2018-03-12 12:27:20 +0800

    '1'

--- app/Http/Controllers/Branch/DeviceController.php
+++ app/Http/Controllers/Branch/DeviceController.php
@@ -28,7 +28,6 @@ class DeviceController extends Controller
     public function room_add_check(Request $request)
     {
         $organization_id = $request->get('organization_id');      //中间件产生的管理员数据参数
-        dd($admin_data);
         $route_name = $request->path();                         //获取当前的页面路由
         $room_name = $request->get('room_name');    //栏目名称
         $room_data = [
