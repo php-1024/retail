7a79cc4897b48f3e8f01069b4cfe671e07f03af4
zeo <234521890@qq.com>
2018-03-13 17:48:49 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -272,12 +272,13 @@ class WechatController extends Controller{
 
     //授权回调链接
     public function redirect(Request $request){
-        $origanization_id = $_GET['organization_id'];//中间件产生的管理员数据参数
-        /*
+        $organization_id = $_GET['organization_id'];//中间件产生的管理员数据参数
+
+
         $auth_code = $_GET['auth_code'];//授权码
         $auth_info = \Wechat::get_authorization_info($auth_code);//获取授权
         $auth_data = array(
-            'organization_id'=>$origanization_id,
+            'organization_id'=>$organization_id,
             'authorizer_appid'=>$auth_info['authorizer_appid'],
             'authorizer_access_token'=>$auth_info['authorizer_access_token'],
             'authorizer_refresh_token'=>$auth_info['authorizer_refresh_token'],
@@ -286,9 +287,9 @@ class WechatController extends Controller{
             'expire_time'=>time()+7200,
         );
         WechatAuthorization::addAuthorization($auth_data);
-        */
 
-        ///return redirect('store_auth');
+
+        return redirect('store_auth');
     }
 }
 ?>
