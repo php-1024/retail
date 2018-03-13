68bc9a7214c0acb427ffe669898d188bf0857558
mail@54zm.com <mail@54zm.com>
2018-03-13 15:38:09 +0800

    '1'

--- app/Http/Controllers/Branch/OrderController.php
+++ app/Http/Controllers/Branch/OrderController.php
@@ -6,6 +6,7 @@
 namespace App\Http\Controllers\Branch;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
 use App\Models\CateringOrder;
 use App\Models\Organization;
 use Illuminate\Http\Request;
@@ -26,7 +27,10 @@ class OrderController extends Controller
             'branch_id' => $admin_data['organization_id'],
         ];
         $list = CateringOrder::getPaginage($where,10,'created_at','DESC');
-        dump($where);
+        foreach ( $list as $key=>$val){
+            $account = Account::getOne(['id'=>$val->account_id]);
+            $key->account = $account;
+        }
         dump($list);
         return view('Branch/Order/order_spot',['list'=>$list,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
