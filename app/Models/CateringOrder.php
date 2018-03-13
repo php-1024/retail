dbc93c750a437ee539025449b801b88e4c1275ab
mail@54zm.com <mail@54zm.com>
2018-03-13 15:30:17 +0800

    '1'

--- app/Models/CateringOrder.php
+++ app/Models/CateringOrder.php
@@ -49,7 +49,7 @@ class CateringOrder extends Model{
 
     //获取分页列表
     public static function getPaginage($where,$paginate,$orderby,$sort='DESC'){
-        return self::with('account')->first()->where($where)->orderBy($orderby,$sort)->paginate($paginate);
+        return self::with('account')->where($where)->orderBy($orderby,$sort)->paginate($paginate);
     }
 }
 ?>
