5143847a0953d572d72333c2c96424a75ae41535
mail@54zm.com <mail@54zm.com>
2018-03-13 15:08:11 +0800

    '1'

--- app/Http/Controllers/Branch/OrderController.php
+++ app/Http/Controllers/Branch/OrderController.php
@@ -20,9 +20,9 @@ class OrderController extends Controller
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
         $route_name = $request->path();                         //获取当前的页面路由
         $where = [
-            'branch_id',$admin_data['organization_id']
+            'branch_id' => $admin_data['organization_id'],
         ];
-        $list = CateringOrder::getPaginage([],10,'created_at','DESC');
+        $list = CateringOrder::getPaginage($where,10,'created_at','DESC');
         dump($list);
         return view('Branch/Order/order_spot',['list'=>$list,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
