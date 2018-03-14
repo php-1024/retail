34fa4cd517dc04a36b4e8d2712eb27e7104b4dcd
kbzz <463661663@qq.com>
2018-03-14 15:56:34 +0800

    1

--- app/Http/Controllers/Retail/NewsController.php
+++ app/Http/Controllers/Retail/NewsController.php
@@ -34,16 +34,5 @@ class NewsController extends Controller{
 
         return view('Retail/News/message_default',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
-
-    //消息群发
-    public function message_mass(Request $request){
-        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
-        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
-        $route_name = $request->path();//获取当前的页面路由
-
-        return view('Retail/News/message_mass',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
-    }
-
 }
 ?>
--- app/Http/Controllers/Retail/UserController.php
+++ app/Http/Controllers/Retail/UserController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Http\Controllers\Catering;
+namespace App\Http\Controllers\Retail;
 use App\Http\Controllers\Controller;
 use App\Models\Label;
 use App\Models\OperationLog;
@@ -15,120 +15,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Session;
 class UserController extends Controller{
-    //粉丝标签管理
-    public function user_tag(Request $request){
-        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
-        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
-        $route_name = $request->path();//获取当前的页面路由
-
-        $store_id = $admin_data['organization_id'];//组织id
-        $list = Label::getPaginage([['store_id',$store_id]],'10','id');
-
-        return view('Catering/User/user_tag',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
-    }
-    //添加会员标签ajax显示页面
-    public function label_add(Request $request){
-
-        return view('Catering/User/label_add');
-    }
-    //添加会员标签功能提交
-    public function label_add_check(Request $request){
-        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        $route_name = $request->path();//获取当前的页面路由
-
-        $label_name = $request->label_name; //会员标签名称
-        $store_id = $admin_data['organization_id'];//组织id
-
-        $re = Label::checkRowExists([['store_id',$store_id],['label_name',$label_name]]);
-        if($re == 'true'){
-            return response()->json(['data' => '会员标签名称已存在！', 'status' => '0']);
-        }
-
-        DB::beginTransaction();
-        try {
-            $dataLabel = [
-                'store_id'=>$store_id,
-                'branch_id'=>0,
-                'label_name'=>$label_name,
-                'label_number'=>0,
-            ];
-           Label::addLabel($dataLabel);
-            if($admin_data['is_super'] != 2){
-                OperationLog::addOperationLog('4',$admin_data['organization_id'],$admin_data['id'],$route_name,'创建会员标签成功：'.$label_name);//保存操作记录
-            }
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();//事件回滚
-            return response()->json(['data' => '创建会员标签失败！', 'status' => '0']);
-        }
-        return response()->json(['data' => '创建会员标签成功！', 'status' => '1']);
-
-
-
-    }
-    //编辑会员标签ajax显示页面
-    public function label_edit(Request $request){
-        $id = $request->id; //会员标签id
-        $oneLabel = Label::getOneLabel([['id',$id]]);
-        return view('Catering/User/label_edit',['oneLabel'=>$oneLabel]);
-    }
-    //编辑会员标签功能提交
-    public function label_edit_check(Request $request){
-
-        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        $route_name = $request->path();//获取当前的页面路由
-
-        $store_id = $admin_data['organization_id'];//组织id
-
-        $id = $request->id; //会员标签id
-        $label_name = $request->label_name; //会员标签名称
-        $re = Label::checkRowExists([['store_id',$store_id],['label_name',$label_name]]);
-        if($re == 'true'){
-            return response()->json(['data' => '会员标签名称已存在！', 'status' => '0']);
-        }
-        DB::beginTransaction();
-        try {
-            Label::editLabel(['id'=>$id],['label_name'=>$label_name]);
-            if($admin_data['is_super'] != 2){
-                OperationLog::addOperationLog('4',$admin_data['organization_id'],$admin_data['id'],$route_name,'修改会员标签成功：'.$label_name);//保存操作记录
-            }
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();//事件回滚
-            return response()->json(['data' => '修改会员标签失败！', 'status' => '0']);
-        }
-        return response()->json(['data' => '修改会员标签成功！', 'status' => '1']);
-
-    }
-
-    //删除会员标签ajax显示页面
-    public function label_delete(Request $request){
-        $id = $request->id; //会员标签id
-        $oneLabel = Label::getOneLabel([['id',$id]]);
-        return view('Catering/User/label_delete',['oneLabel'=>$oneLabel]);
-    }
-    //删除会员标签ajax显示页面
-    public function label_delete_check(Request $request){
-
-        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        $route_name = $request->path();//获取当前的页面路由
-
-        $id = $request->id; //会员标签id
-        $label_name = $request->label_name; //会员标签名称
-        DB::beginTransaction();
-        try {
-            Label::where('id',$id)->forceDelete();
-            if($admin_data['is_super'] != 2){
-                OperationLog::addOperationLog('4',$admin_data['organization_id'],$admin_data['id'],$route_name,'删除会员标签：'.$label_name);//保存操作记录
-            }
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();//事件回滚
-            return response()->json(['data' => '删除会员标签失败！', 'status' => '0']);
-        }
-        return response()->json(['data' => '删除会员标签成功！', 'status' => '1']);
-    }
     //粉丝用户管理
     public function user_list(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
@@ -146,7 +32,7 @@ class UserController extends Controller{
             $list[$key]['label_id']  = UserLabel::getPluck([['user_id',$value->user_id],['store_id',$organization_id]],'label_id')->first();//粉丝对应的标签id
         }
         $label = Label::ListLabel([['store_id',$organization_id]]);//会员标签
-        return view('Catering/User/user_list',['list'=>$list,'store_name'=>$store_name,'label'=>$label,'organization_id'=>$organization_id,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        return view('Retail/User/user_list',['list'=>$list,'store_name'=>$store_name,'label'=>$label,'organization_id'=>$organization_id,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     //粉丝用户管理
     public function store_label_add_check(Request $request){
@@ -215,7 +101,7 @@ class UserController extends Controller{
             $list =  User::getOneUser([['id',$recommender_id]]);
             $data['recommender_name'] = $list->UserInfo->nickname;
         }
-        return view('Catering/User/user_list_edit',['data'=>$data,'userInfo'=>$userInfo]);
+        return view('Retail/User/user_list_edit',['data'=>$data,'userInfo'=>$userInfo]);
 
     }
     //粉丝用户管理编辑功能提交
@@ -255,7 +141,7 @@ class UserController extends Controller{
         $status = $request->status;//冻结或者解锁
         $nickname =  UserInfo::getPluck([['user_id',$user_id]],'nickname')->first();//微信昵称
 
-        return view('Catering/User/user_list_wallet',['user_id'=>$user_id,'nickname'=>$nickname,'status'=>$status]);
+        return view('Retail/User/user_list_wallet',['user_id'=>$user_id,'nickname'=>$nickname,'status'=>$status]);
 
     }
     //粉丝用户管理冻结功能显示
@@ -265,7 +151,7 @@ class UserController extends Controller{
         $status = $request->status;//冻结或者解锁
         $nickname =  UserInfo::getPluck([['user_id',$user_id]],'nickname')->first();//微信昵称
 
-        return view('Catering/User/user_list_lock',['user_id'=>$user_id,'nickname'=>$nickname,'status'=>$status]);
+        return view('Retail/User/user_list_lock',['user_id'=>$user_id,'nickname'=>$nickname,'status'=>$status]);
 
     }
     //粉丝用户管理冻结功能提交
@@ -312,7 +198,7 @@ class UserController extends Controller{
         foreach($list as $key=>$value){
             $list[$key]['nickname'] = UserInfo::getPluck([['user_id', $value->user_id]],'nickname')->first();//微信昵称
         }
-        return view('Catering/User/user_timeline',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        return view('Retail/User/user_timeline',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 }
 ?>
