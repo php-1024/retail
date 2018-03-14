ef77da1d80ae653c9683f4b0e6426a9833abaf11
zeo <234521890@qq.com>
2018-03-14 11:13:57 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -301,12 +301,8 @@ class WechatController extends Controller{
         $id = $request->input('id');
         $auth_info = \Wechat::refresh_authorization_info($organization_id);//刷新并获取授权令牌
         //$this->pull_authorizer_info($id,$auth_info);
-        /*
-        $fans_list = \Wechat::get_fans_list($auth_info['authorizer_access_token']);//粉丝列表
-        foreach($fans_list as $key=>$val){
-            dump($val);
-        }
-        */
+        $this->pull_fans_list($organization_id,$auth_info);
+
 
     }
 
@@ -335,7 +331,10 @@ class WechatController extends Controller{
      * 拉取公众号粉丝信息
      */
     private function pull_fans_list($id,$organization_id,$auth_info){
-
+        $fans_list = \Wechat::get_fans_list($auth_info['authorizer_access_token']);//粉丝列表
+        foreach($fans_list as $key=>$val){
+            dump($val);
+        }
     }
 }
 ?>
