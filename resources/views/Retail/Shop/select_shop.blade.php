ef3bc9adc2c621cf9b3f5bc4ff90a0a1ec3308d0
kbzz <463661663@qq.com>
2018-03-14 15:00:38 +0800

    1

--- resources/views/Retail/Shop/select_shop.blade.php
+++ resources/views/Retail/Shop/select_shop.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>选择服务商 | 零壹服务商管理平台</title>
+    <title>选择总店 | 零壹零售总店管理平台</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{asset('public/Proxy')}}/css/bootstrap.min.css" rel="stylesheet">
@@ -32,7 +32,7 @@
         <div class="col-lg-12 col-sm-6">
             <!--breadcrumbs start -->
             <ul class="breadcrumb">
-                <li><h3 style="margin-top: 10px;"><i class="icon-desktop"></i> 选择要进入的店铺</h3></li>
+                <li><h3 style="margin-top: 10px;"><i class="icon-desktop"></i> 选择要进入的总店</h3></li>
             </ul>
             <!--breadcrumbs end -->
         </div>
@@ -43,7 +43,7 @@
             <div class="col-lg-12">
                 <div class="input-group m-bot15 col-lg-3 ">
                     <span class="input-group-addon"><i class="icon-search"></i></span>
-                    <input type="text" class="form-control " placeholder="店铺名称">
+                    <input type="text" class="form-control " placeholder="总店名称">
 
                 </div>
                 <div class="input-group m-bot15 col-lg-2 ">
