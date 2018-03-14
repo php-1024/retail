0e5d3ff9c094d05cd3e4e6f21ae6116d6f3789f5
zeo <234521890@qq.com>
2018-03-14 16:50:39 +0800

    by zeo

--- app/Services/Wechat/WechatApi.php
+++ app/Services/Wechat/WechatApi.php
@@ -355,7 +355,6 @@ class WechatApi{
 
         $re = \HttpCurl::doPost($url, $data);
         $re = json_decode($re,true);
-        dump($re);
         if(empty($re['ticket'])){
             return false;
         }else{
