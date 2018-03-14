afc2d197127a6e14e2a32b8ca426f06b0b939d80
kbzz <463661663@qq.com>
2018-03-14 17:56:31 +0800

    1

--- resources/views/Retail/Shop/operation_log.blade.php
+++ resources/views/Retail/Shop/operation_log.blade.php
@@ -110,6 +110,8 @@
 <script type="text/javascript" src="{{asset('public/Retail')}}/js/jPlayer/jquery.jplayer.min.js"></script>
 <script type="text/javascript" src="{{asset('public/Retail')}}/js/jPlayer/add-on/jplayer.playlist.min.js"></script>
 <script type="text/javascript" src="{{asset('public/Retail')}}/js/jPlayer/demo.js"></script>
+<script type="text/javascript" src="{{asset('public/Retail')}}/js/datepicker/bootstrap-datepicker.js"></script>
+
 </body>
 </html>
 
