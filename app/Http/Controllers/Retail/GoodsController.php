6b9dbc2a4be1a5ea80664afda748402adb98be77
kbzz <463661663@qq.com>
2018-03-15 10:53:44 +0800

    1

--- app/Http/Controllers/Retail/GoodsController.php
+++ app/Http/Controllers/Retail/GoodsController.php
@@ -50,7 +50,12 @@ class GoodsController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
 
-        return view('Retail/Goods/goods_detail',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        $goods_id = $request->goods_id;//商品id
+
+        $onedata = CateringGoods::getOne([['id',$goods_id]]);
+
+        dump($onedata);
+        return view('Retail/Goods/goods_detail',['onedata'=>$onedata,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 }
 ?>
