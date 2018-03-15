6755cf68d130fa2464f23fc799a602618d96c1f0
zeo <234521890@qq.com>
2018-03-15 11:09:10 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -45,8 +45,13 @@ class WechatController extends Controller{
     /*
      * 图片素材
      */
-    public function material_image(){
-        echo "这里是图片素材";
+    public function material_image(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
+        $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+
+        return view('Wechat/Catering/material_image',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
     public function test(){
