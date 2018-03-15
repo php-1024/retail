fb95d115f338847356b6829100a2e5cc89a169b2
mail@54zm.com <mail@54zm.com>
2018-03-15 18:50:58 +0800

    '2'

--- resources/views/Retail/Goods/goods_edit.blade.php
+++ resources/views/Retail/Goods/goods_edit.blade.php
@@ -219,7 +219,7 @@
                     confirmButtonColor: "#DD6B55",
                     confirmButtonText: "确定",
                 },function(){
-                    window.location.href = "{{asset("retailbranch/goods/goods_list")}}";
+                    window.location.href = "{{asset("retail/goods/goods_list")}}";
                 });
             }else{
                 swal({
@@ -254,7 +254,7 @@
                         confirmButtonText: "确定",
                     },function(){
                         //图片添加成功后异步刷新图片列表部分
-                        var url = '{{url('retailbranch/ajax/goods_thumb')}}';//需要异步加载的页面
+                        var url = '{{url('retail/ajax/goods_thumb')}}';//需要异步加载的页面
                         var goods_id = $("#goods_id").val();
                         var token = $("#_token").val();
                         var data = {'goods_id':goods_id,'_token':token};
