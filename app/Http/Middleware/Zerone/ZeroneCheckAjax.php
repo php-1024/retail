3a8633c967b6f20bb10e06e9644983c40ec1c56d
kbzz <463661663@qq.com>
2018-03-15 17:32:57 +0800

    1

--- app/Http/Middleware/Zerone/ZeroneCheckAjax.php
+++ app/Http/Middleware/Zerone/ZeroneCheckAjax.php
@@ -650,14 +650,13 @@ class ZeroneCheckAjax
         if (empty($request->input('captcha'))) {
             return self::res(0, response()->json(['data' => '请输入验证码', 'status' => '0']));
         }
-        
-//        if (Session::get('zerone_system_captcha') == $request->input('captcha')) {
+        if (Session::get('zerone_system_captcha') == $request->input('captcha')) {
             //把参数传递到下一个程序
             return self::res(1, $request);
-//        } else {
-//            //用户输入验证码错误
-//            return self::res(0, response()->json(['data' => '验证码错误', 'status' => '0']));
-//        }
+        } else {
+            //用户输入验证码错误
+            return self::res(0, response()->json(['data' => '验证码错误', 'status' => '0']));
+        }
     }
     //检测服务商申请表信息
     public function checkProxyAdd($request){
