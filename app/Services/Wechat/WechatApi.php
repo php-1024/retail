3afda195bb1549472987a0b65be4b93d74caaf16
zeo <234521890@qq.com>
2018-03-15 18:57:16 +0800

    by zeo

--- app/Services/Wechat/WechatApi.php
+++ app/Services/Wechat/WechatApi.php
@@ -410,7 +410,7 @@ class WechatApi{
     public function uploadimg($authorizer_access_token,$file){
         $url = 'https://api.weixin.qq.com/cgi-bin/media/uploadimg?access_token='.$authorizer_access_token;
         $data = [
-            'media'=>'@'.$file,
+            'media'=>'@'.$file.'r\n',
         ];
         //$data = json_encode($data, JSON_UNESCAPED_UNICODE);
         $re = \HttpCurl::doPost($url, $data);
