a287e939cc766ca5e43b03fc7115163ba24a41e8
mail@54zm.com <mail@54zm.com>
2018-03-15 17:25:25 +0800

    '2'

--- app/Http/Controllers/Retail/LoginController.php
+++ app/Http/Controllers/Retail/LoginController.php
@@ -100,7 +100,7 @@ class LoginController extends Controller
                             ErrorLog::clearErrorTimes($ip);//清除掉错误记录
                             //插入登录记录
                             if (LoginLog::addLoginLog($account_info['id'], 10, $account_info->organization_id, $ip, $addr)) {//写入登录日志
-                                Session::put('retail_branch_account_id', encrypt($account_info->id));//存储登录session_id为当前用户ID
+                                Session::put('retail_account_id', encrypt($account_info->id));//存储登录session_id为当前用户ID
                                 //构造用户缓存数据
                                 if (!empty($account_info->account_info->realname)) {
                                     $admin_data['realname'] = $account_info->account_info->realname;
@@ -124,7 +124,7 @@ class LoginController extends Controller
                         }
                     } else {
                         ErrorLog::clearErrorTimes($ip);//清除掉错误记录
-                        Session::put('retail_branch_account_id', encrypt($account_info->id));//存储登录session_id为当前用户ID
+                        Session::put('retail_account_id', encrypt($account_info->id));//存储登录session_id为当前用户ID
                         $admin_data['realname'] = '系统管理员';
                         $admin_data['role_name'] = '系统管理员';
                         //构造用户缓存数据
@@ -144,7 +144,7 @@ class LoginController extends Controller
 
     //退出登录
     public function quit(){
-        Session::put('retail_branch_account_id','');
+        Session::put('retail_account_id','');
         return redirect('retail/login');
     }
 }
