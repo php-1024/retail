e1b2e868a3065249a3c8fa50d97bae63ee66307e
kbzz <463661663@qq.com>
2018-03-15 17:23:37 +0800

    1

---
+++ resources/views/Zerone/Company/company_examinelist.blade.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>零壹新科技程序管理平台</title>
+    <link href="{{asset('public/Zerone/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/font')}}/css/font-awesome.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/sweetalert')}}/css/sweetalert.css" rel="stylesheet">
+
+    <link href="{{asset('public/Zerone')}}/css/animate.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone')}}/css/style.css" rel="stylesheet">
+</head>
+
+<body class="">
+
+
+<div id="wrapper">
+    @include('Zerone/Public/Nav')
+
+    <div id="page-wrapper" class="gray-bg">
+        @include('Zerone/Public/Header')
+        <div class="row wrapper border-bottom white-bg page-heading">
+            <div class="col-sm-4">
+                <h2>商户注册审核</h2>
+                <ol class="breadcrumb">
+                    <li class="active">
+                        <a href="JavaScript:;">商户管理</a>
+                    </li>
+                    <li >
+                        <strong>商户注册审核</strong>
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <div class="wrapper wrapper-content animated fadeInRight ecommerce">
+
+
+            <div class="ibox-content m-b-sm border-bottom">
+
+                <div class="row">
+                    <form method="get" role="form" id="searchForm" action="">
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label class="control-label" for="amount">商户名称</label>
+                            <input type="text" id="proxy_name" name="company_name" value="{{ $search_data['company_name'] }}" placeholder="请输入服务商名称" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label class="control-label" for="amount">手机号码</label>
+                            <input type="text" id="proxy_owner_mobile" name="company_owner_mobile" value="{{ $search_data['company_owner_mobile'] }}" placeholder="手机号码" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label class="control-label" for="amount"> &nbsp;</label>
+                            <button type="submit" class="block btn btn-info"><i class="fa fa-search"></i>搜索</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
+                            <input type="hidden" id="company_examine" value="{{ url('zerone/ajax/company_examine') }}">
+                            <table class="table table-stripped toggle-arrow-tiny" data-page-size="15">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>商户名称</th>
+                                    <th>归属服务商</th>
+                                    <th>负责人姓名</th>
+                                    <th>身份证号</th>
+                                    <th>手机号码</th>
+                                    <th>申请状态</th>
+                                    <th class="col-sm-1">注册时间</th>
+                                    <th class="col-sm-2 text-right" >操作</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($list as $key=>$value)
+                                <tr>
+                                    <td>{{$value->id}}</td>
+                                    <td>{{$value->company_name}}</td>
+                                    <td>{{$value->organization->organization_name}}</td>
+                                    <td>{{$value->company_owner}}</td>
+
+                                    <td>{{$value->company_owner_idcard}}</td>
+                                    <td>{{$value->company_owner_mobile}}</td>
+                                    <td>@if($value->status == 0)<label class="label label-warning">待审核</label>
+                                        @elseif($value->status == 1)<label class="label label-primary">已通过</label>
+                                        @elseif($value->status == -1)<label class="label label-danger">未通过</label>
+                                        @endif
+                                    </td>
+                                    <td>{{$value->created_at}}</td>
+                                    <td class="text-right">
+                                        @if($value->status == 0)
+                                        <button type="button" id="okBtn" class="btn  btn-xs btn-primary" onclick="getEditForm({{ $value->id }},this.value)" value="1"><i class="fa fa-check"></i>&nbsp;&nbsp;审核通过</button>
+                                        <button type="button" id="notokBtn" class="btn  btn-xs btn-danger" onclick="getEditForm({{ $value->id }},this.value)" value="-1"><i class="fa fa-remove"></i>&nbsp;&nbsp;拒绝通过</button>
+                                        @else
+                                            <label class="label label-primary">已审核</label>
+                                        @endif
+                                    </td>
+                                </tr>
+                                @endforeach
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="9" class="footable-visible">
+                                        {{ $list->appends($search_data)->links() }}
+                                    </td>
+                                </tr>
+                                </tfoot>
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        @include('Zerone/Public/Footer')
+
+        <div class="modal inmodal" id="myModal" tabindex="-1" role="dialog" aria-hidden="true"></div>
+    </div>
+</div>
+
+{{--<!-- Page-Level Scripts -->--}}
+<script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
+<script src="{{asset('public/Zerone/library/bootstrap')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Zerone/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
+<script src="{{asset('public/Zerone/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
+<script src="{{asset('public/Zerone/library/pace')}}/js/pace.min.js"></script>
+<script src="{{asset('public/Zerone/library/iCheck')}}/js/icheck.min.js"></script>
+<script src="{{asset('public/Zerone/library/sweetalert')}}/js/sweetalert.min.js"></script>
+
+<script src="{{asset('public/Zerone')}}/js/switchery.js"></script>
+<script src="{{asset('public/Zerone')}}/js/footable.all.min.js"></script>
+<script src="{{asset('public/Zerone')}}/js/bootstrap-datepicker.js"></script>
+
+
+<script>
+$(function(){
+
+    //设置CSRF令牌
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+});
+
+//审核
+function getEditForm(id,sta){
+
+    var url = $('#company_examine').val();
+    var token = $('#_token').val();
+    if(id==''){
+        swal({
+            title: "提示信息",
+            text: '数据传输错误',
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "确定",
+        },function(){
+            window.location.reload();
+        });
+        return;
+    }
+
+    var data = {'id':id,'sta':sta,'_token':token};
+    $.post(url,data,function(response){
+        if(response.status=='-1'){
+            swal({
+                title: "提示信息",
+                text: response.data,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+            },function(){
+                window.location.reload();
+            });
+            return;
+        }else{
+
+            $('#myModal').html(response);
+            $('#myModal').modal();
+        }
+    });
+}
+</script>
+</body>
+
+</html>
---
+++ resources/views/Zerone/Proxy/proxy_add.blade.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>零壹新科技程序管理平台</title>
+    <link href="{{asset('public/Zerone/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/font')}}/css/font-awesome.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone/library/sweetalert')}}/css/sweetalert.css" rel="stylesheet">
+
+    <link href="{{asset('public/Zerone')}}/css/animate.css" rel="stylesheet">
+    <link href="{{asset('public/Zerone')}}/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body class="">
+
+
+<div id="wrapper">
+    @include('Zerone/Public/Nav')
+
+    <div id="page-wrapper" class="gray-bg">
+        @include('Zerone/Public/Header')
+        <div class="row wrapper border-bottom white-bg page-heading">
+            <div class="col-sm-4">
+                <h2>添加服务商</h2>
+                <ol class="breadcrumb">
+                    <li class="active">
+                        <a href="JavaScript:;">服务商管理</a>
+                    </li>
+                    <li >
+                        <strong>添加服务商</strong>
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <div class="wrapper wrapper-content animated fadeInRight ecommerce">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>添加服务商</h5>
+
+                        </div>
+                        <div class="ibox-content">
+                            <form method="post" class="form-horizontal"  role="form" id="currentForm" action="{{ url('zerone/ajax/proxy_add_check') }}">
+                                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <div class="form-group"><label class="col-sm-2 control-label">所在战区</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-control m-b" name="zone_id">
+                                            @foreach($warzone_list as $key=>$value)
+                                                <option value="{{$value->id}}">{{$value->zone_name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group"><label class="col-sm-2 control-label">服务商名称</label>
+                                    <div class="col-sm-10"><input type="text" name="organization_name" class="form-control"></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">负责人姓名</label>
+                                    <div class="col-sm-10"><input type="text" name="realname" class="form-control"></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">负责人身份证号</label>
+                                    <div class="col-sm-10"><input type="text" name="idcard" class="form-control"></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">手机号码</label>
+                                    <div class="col-sm-10"><input type="text" name="mobile" class="form-control"></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">服务商登录密码</label>
+                                    <div class="col-sm-10"><input type="password" name="proxy_password" class="form-control"></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">重复登录密码</label>
+                                    <div class="col-sm-10"><input type="password" name="re_proxy_password" class="form-control"></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">安全密码</label>
+                                    <div class="col-sm-10"><input type="password" name="safe_password" class="form-control"></div>
+                                </div>
+                                <div class="hr-line-dashed"></div>
+
+                                <div class="form-group ">
+                                    <div class="col-sm-4 col-sm-offset-5">
+                                        <button class="btn btn-primary" id="addbtn" onclick="return postForm();" type="button">确认申请</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('Zerone/Public/Footer')
+    </div>
+</div>
+<script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
+<script src="{{asset('public/Zerone/library/bootstrap')}}/js/bootstrap.min.js"></script>
+<script src="{{asset('public/Zerone/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
+<script src="{{asset('public/Zerone/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
+<script src="{{asset('public/Zerone/library/pace')}}/js/pace.min.js"></script>
+<script src="{{asset('public/Zerone/library/iCheck')}}/js/icheck.min.js"></script>
+<script src="{{asset('public/Zerone/library/sweetalert')}}/js/sweetalert.min.js"></script>
+<!-- Page-Level Scripts -->
+<script>
+    $(function(){
+        //设置CSRF令牌
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    });
+
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    //type: "warning"
+                });
+            }
+        });
+    }
+</script>
+</body>
+
+</html>
