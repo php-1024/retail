7ffa028fc50ab465e0899e6ae85da6575155349b
zeo <234521890@qq.com>
2018-03-16 17:11:27 +0800

    by zeo

--- resources/views/Wechat/Catering/material_image_select.blade.php
+++ resources/views/Wechat/Catering/material_image_select.blade.php
@@ -7,16 +7,18 @@
             </div>
             <div class="modal-body">
                 <div class="row row-sm">
-                    <div class="col-lg-2">
-                        <div class="item">
-                            <div class="pos-rlt">
-                                <a href="javascript:;"><img src="images/m1.jpg" alt="" class="r r-2x img-full"></a>
-                            </div>
-                            <div class="padder-v">
-                                <span>414631616.JPG</span>
+                    @foreach($list as $key=>$val)
+                        <div class="col-lg-2">
+                            <div class="item">
+                                <div class="pos-rlt">
+                                    <a href="javascript:;"><img src="{{asset('uploads/wechat/'.$admin_data['organization_id'].'/'.$val->filename)}}" alt="" style="height: 200px; width: 200px;"></a>
+                                </div>
+                                <div class="padder-v">
+                                    <span>{{ $val->filename }}</span>&nbsp;&nbsp;<button class="btn btn-xs btn-danger" onclick="return getDeleteComfirmForm('{{$val->id}}');"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
             <div class="modal-footer">
