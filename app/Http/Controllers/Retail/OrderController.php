0a81c33461dc74e9d7cfbb7bc96d24e1dcadc3c7
mail@54zm.com <mail@54zm.com>
2018-03-16 18:00:03 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -37,16 +37,6 @@ class OrderController extends Controller
         return view('Retail/Order/order_spot',['list'=>$list,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
-    //订单管理-外卖订单详情
-    public function order_takeout_detail(Request $request)
-    {
-        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
-        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
-        $route_name = $request->path();                         //获取当前的页面路由
-        return view('Retail/Order/order_takeout_detail',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
-    }
-
     //订单管理-现场订单详情
     public function order_spot_detail(Request $request)
     {
@@ -66,26 +56,6 @@ class OrderController extends Controller
             $order_price += $val->price;        //计算订单总价
         }
         return view('Retail/Order/order_spot_detail',['order_price'=>$order_price,'order_goods'=>$order_goods,'order'=>$order,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
-    }
-
-    //订单管理-外卖订单
-    public function order_takeout(Request $request)
-    {
-        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
-        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
-        $route_name = $request->path();                         //获取当前的页面路由
-        return view('Retail/Order/order_takeout',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
-    }
-
-    //预约管理
-    public function order_appointment(Request $request)
-    {
-        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
-        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
-        $route_name = $request->path();                         //获取当前的页面路由
-        return view('Retail/Order/order_appointment',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
 }
