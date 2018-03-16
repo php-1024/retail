a99d0dbfd7de4a9bcb8c028e3624a96fd52f3db1
kbzz <463661663@qq.com>
2018-03-16 15:34:48 +0800

    1

--- app/Http/Controllers/Zerone/LoginController.php
+++ app/Http/Controllers/Zerone/LoginController.php
@@ -44,7 +44,6 @@ class LoginController extends Controller{
 
     //检测登录
     public function login_check(){
-        exit(1234);
         $ip = Request::getClientIp();//获取访问者IP
         $addr_arr = \IP2Attr::find($ip);//获取访问者地址
         $addr = $addr_arr[0].$addr_arr[1].$addr_arr[2].$addr_arr[3];//获取访问者地址
