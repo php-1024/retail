31e7276ec43c463e09feef91b7675668446e7119
mail@54zm.com <mail@54zm.com>
2018-03-16 16:58:56 +0800

    '1'

--- app/Http/Controllers/Retail/DisplayController.php
+++ app/Http/Controllers/Retail/DisplayController.php
@@ -35,7 +35,7 @@ class DisplayController extends Controller
         $login_log_list = LoginLog::getList($where,10,'created_at','DESC');
         $operation_log_list = OperationLog::getList($where,10,'created_at','DESC');//操作记录
         if($admin_data['is_super'] == 1 && $admin_data['organization_id'] == 0){    //如果是超级管理员并且组织ID等于零则进入选择组织页面
-            return redirect('retail/branch_list');
+            return redirect('retail/list');
         }
         if (empty($admin_data['safe_password'])){           //先设置安全密码
             return redirect('retail/account/password');
@@ -58,7 +58,7 @@ class DisplayController extends Controller
             $catering = Organization::getOneCatering(['id'=>$val->parent_id]);
             $val->cateringname = $catering->organization_name;
         }
-        return  view('Retail/Account/branch_list',['organization'=>$organization,'organization_name'=>$organization_name]);
+        return  view('Retail/Account/list',['organization'=>$organization,'organization_name'=>$organization_name]);
     }
 
     //选择店铺
@@ -119,7 +119,7 @@ class DisplayController extends Controller
             $admin_data['role_name'] = '角色未设置';
         }
         ZeroneRedis::create_retail_account_cache(1, $admin_data);//生成账号数据的Redis缓存
-        ZeroneRedis::create_branch_menu_cache(1);//生成对应账号的商户系统菜单
+        ZeroneRedis::create_retail_menu_cache(1);//生成对应账号的商户系统菜单
     }
 }
 
