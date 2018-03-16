190864fce291a1dff5f5ad0719ad297ede1bc588
zeo <234521890@qq.com>
2018-03-16 19:24:51 +0800

    by zeo

--- resources/views/Wechat/Catering/material_articles_add.blade.php
+++ resources/views/Wechat/Catering/material_articles_add.blade.php
@@ -307,6 +307,34 @@
             }
         });
     }
+
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                },function(){
+                    window.location.href = $('#material_article_url').val();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                });
+            }
+        });
+    }
 </script>
 </body>
 </html>
