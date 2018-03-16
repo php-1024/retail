bd1c734cade53371ab2b40d7a20d554349e8f845
mail@54zm.com <mail@54zm.com>
2018-03-16 18:51:08 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -86,6 +86,7 @@ class OrderController extends Controller
             }
             DB::commit();
         } catch (\Exception $e) {
+            dd($e);
             DB::rollBack();//事件回滚
             return response()->json(['data' => '修改订单状态失败，请检查', 'status' => '0']);
         }
