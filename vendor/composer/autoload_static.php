ca91da0158315bb3e51a61b6006a623c01ea57fa
kbzz <463661663@qq.com>
2018-03-16 15:57:55 +0800

    1

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -387,7 +387,7 @@ class ComposerStaticInit1708003cd15a9f552ca4c1b8ad90d78a
         'App\\Models\\Account' => __DIR__ . '/../..' . '/app/Models/Account.php',
         'App\\Models\\AccountInfo' => __DIR__ . '/../..' . '/app/Models/AccountInfo.php',
         'App\\Models\\AccountNode' => __DIR__ . '/../..' . '/app/Models/AccountNode.php',
-        'App\\Models\\Assets' => __DIR__ . '/../..' . '/app/Models/Assets.php',
+        'App\\Models\\Assets' => __DIR__ . '/../..',
         'App\\Models\\AssetsOperation' => __DIR__ . '/../..',
         'App\\Models\\CompanyApply' => __DIR__ . '/../..' . '/app/Models/CompanyApply.php',
         'App\\Models\\ErrorLog' => __DIR__ . '/../..' . '/app/Models/ErrorLog.php',
