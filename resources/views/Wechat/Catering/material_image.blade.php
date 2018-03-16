5cae08ed499e5c0023df7d2b88521e9b022564c1
zeo <234521890@qq.com>
2018-03-16 11:45:11 +0800

    by zeo

--- resources/views/Wechat/Catering/material_image.blade.php
+++ resources/views/Wechat/Catering/material_image.blade.php
@@ -68,12 +68,13 @@
                                     @foreach($list as $key=>$val)
                                     <div class="col-lg-2">
                                         <div class="item">
-                                            <div class="item-overlay opacity r r-2x bg-black">
-                                                <div class="center text-center m-t-n">
-                                                    <a href="#"><i class="fa fa-play-circle i-2x"></i></a>
-                                                </div>
-                                            </div>
+
                                             <div class="pos-rlt">
+                                                <div class="item-overlay opacity r r-2x bg-black">
+                                                    <div class="center text-center m-t-n">
+                                                        <a href="#"><i class="fa fa-play-circle i-2x"></i></a>
+                                                    </div>
+                                                </div>
                                                 <a href="javascript:;"><img src="{{asset('uploads/wechat/'.$admin_data['organization_id'].'/'.$val->filename)}}" alt="" style="height: 200px; width: 200px;"></a>
                                             </div>
                                             <div class="padder-v">
