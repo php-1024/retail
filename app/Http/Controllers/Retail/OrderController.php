f1cbbb6b6ffe63746017eca3d40c00e6a55369b2
mail@54zm.com <mail@54zm.com>
2018-03-16 11:03:19 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -59,16 +59,13 @@ class OrderController extends Controller
         $account = Account::getOne([['id',$order->user_id]]);    //查询处理订单信息和用户信息
         $order->account = $account;
         $order_goods = CateringOrderGoods::getList([['order_id',$order->id]],0,'id','DESC');
+        $order_price = 0;
         foreach ($order_goods as $key=>$val){
             $goods = CateringGoods::getOne([['id',$val->goods_id]]);
             $val->order_goods = $goods;
-            $order_price[] = $val->price;
+            $order_price += $val->price;
         }
-        $orders_price = 0;
-        foreach ($order_price as $key=>$val){
-            $orders_price += $val->price;
-        }
-        dump($orders_price);
+        dump($order_price);
         return view('Retail/Order/order_spot_detail',['order_goods'=>$order_goods,'order'=>$order,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
