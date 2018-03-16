890271a3f08c1b3788ec9a57a0bb096ff6299b84
zeo <234521890@qq.com>
2018-03-16 15:59:17 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -18,11 +18,12 @@ class WechatController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $url = \Wechat::get_auth_url($admin_data['organization_id'],$route_name);
+
 
         $wechat_info = [];
         $org_info = Organization::where('id',$admin_data['organization_id'])->first();
         if(isset($org_info->wechatAuthorization)) {//如果该组织授权了公众号
+            $url = \Wechat::get_auth_url($admin_data['organization_id'],$route_name);
             $wechat_info = $org_info->wechatAuthorization->wechatAuthorizerInfo;//获取公众号信息
 
             //如果没有带参数的二维码
