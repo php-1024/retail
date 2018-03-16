db6cd266c953a67599e599a72d837787a4f9290e
zeo <234521890@qq.com>
2018-03-16 11:50:52 +0800

    by zeo

--- resources/views/Wechat/Catering/material_image.blade.php
+++ resources/views/Wechat/Catering/material_image.blade.php
@@ -72,7 +72,7 @@
                                                 <a href="javascript:;"><img src="{{asset('uploads/wechat/'.$admin_data['organization_id'].'/'.$val->filename)}}" alt="" style="height: 200px; width: 200px;"></a>
                                             </div>
                                             <div class="padder-v">
-                                                <span>{{ $val->filename }}</span>&nbsp;&nbsp;<span class="text-danger"><i class="fa fa-times"></i>删除</span>
+                                                <span>{{ $val->filename }}</span>&nbsp;&nbsp;<button class="btn btn-danger"><i class="fa fa-times"></i>删除</button>
                                             </div>
                                         </div>
                                     </div>
