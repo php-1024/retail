0e44ebe832b9faa2f787ea007dcbd40b90af29f3
zeo <234521890@qq.com>
2018-03-16 11:14:29 +0800

    by zeo

--- routes/web.php
+++ routes/web.php
@@ -619,6 +619,7 @@ Route::group(['prefix'=>'api'],function() {
     Route::group(['prefix' => 'catering'] , function(){
         Route::any('store_auth', 'Api\WechatController@store_auth')->middleware('CateringCheck');//开放平台控制公众平台回复函数
         Route::any('material_image', 'Api\WechatController@material_image')->middleware('CateringCheck');//图片素材
+        Route::any('material_article','Api\WechatController@material_article')->middleware('CateringCheck');
     });
 
     //微信接口中页面使用Ajax的部分
