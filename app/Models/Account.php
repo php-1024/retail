a310942c989af34a642ceb151a8f4175ca0c0ce4
mail@54zm.com <mail@54zm.com>
2018-03-16 19:46:49 +0800

    '1'

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -38,9 +38,9 @@ class Account extends Model{
         return $this->hasMany('App\Models\OrganizationRole', 'created_by');
     }
 
-    //和餐饮分类表CateringCategory一对一的关系
+    //和餐饮分类表CateringCategory一对多的关系
     public function CateringCategory(){
-        return $this->hasOne('App\Models\CateringCategory', 'created_by');
+        return $this->hasMany('App\Models\CateringCategory', 'created_by');
     }
 
     //简易型查询单条数据关联查询
