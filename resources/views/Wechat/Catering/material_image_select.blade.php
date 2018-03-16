61b850077b647a6289bdc386ff3f4954c5a30377
zeo <234521890@qq.com>
2018-03-16 17:13:00 +0800

    by zeo

--- resources/views/Wechat/Catering/material_image_select.blade.php
+++ resources/views/Wechat/Catering/material_image_select.blade.php
@@ -11,7 +11,7 @@
                         <div class="col-lg-2">
                             <div class="item">
                                 <div class="pos-rlt">
-                                    <a href="javascript:;"><img src="{{asset('uploads/wechat/'.$admin_data['organization_id'].'/'.$val->filename)}}" alt="" style="height: 200px; width: 200px;"></a>
+                                    <a href="javascript:;"><img src="{{asset('uploads/wechat/'.$val['organization_id'].'/'.$val->filename)}}" alt="" style="height: 200px; width: 200px;"></a>
                                 </div>
                                 <div class="padder-v">
                                     <span>{{ $val->filename }}</span>
