e3104b21245fe7eeb34dc952b1716c087c9a7283
zeo <234521890@qq.com>
2018-03-16 14:08:00 +0800

    by zeo

--- resources/views/Wechat/Catering/meterial_image_delete_comfirm.blade.php
+++ resources/views/Wechat/Catering/meterial_image_delete_comfirm.blade.php
@@ -1,4 +1,4 @@
-<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/meterial_image_delete') }}">
+<form method="post" role="form" id="currentForm" action="{{ url('api/ajax/meterial_image_delete') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" id="id" value="{{$id}}">
     <div class="modal-dialog modal-lg">
