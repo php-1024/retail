5149a661479f93bb82854b5548553621ed23698d
zeo <234521890@qq.com>
2018-03-16 17:53:37 +0800

    by zeo

--- resources/views/Wechat/Catering/material_article_add.blade.php
+++ resources/views/Wechat/Catering/material_article_add.blade.php
@@ -110,7 +110,7 @@
                                             <div class="form-group">
                                                 <label class="col-sm-2 control-label" for="input-id-1">原文地址</label>
                                                 <div class="col-sm-10">
-                                                    <input type="text" class="form-control" id="input-id-1" name="url" value="">
+                                                    <input type="text" class="form-control" id="input-id-1" name="origin_url" value="">
                                                 </div>
                                             </div>
 
