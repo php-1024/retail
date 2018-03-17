eeb35a132405462507bcdea184b62065626dd961
zeo <234521890@qq.com>
2018-03-17 16:27:46 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -370,7 +370,7 @@ class WechatController extends Controller{
             $articles[$key]['image_info'] = $image_info;
             unset($image_info);
         }
-        dump($article_info);
+        dump($articles);
         $num = count($article_info);
 
         return view('Wechat/Catering/material_articles_edit',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
