39d4bb5ac30002e5c9c3ce28542a60819f87a5fd
kbzz <463661663@qq.com>
2018-03-17 11:01:09 +0800

    1

--- app/Http/Controllers/Zerone/AgentController.php
+++ app/Http/Controllers/Zerone/AgentController.php
@@ -506,8 +506,8 @@ class AgentController extends Controller{
                 }
                 if($status == 1){//消耗程序数量
                     $number = count($datastore);//计算店铺数量
-                    dd($number);
                     $Assets = OrganizationAssets::getOne([['organization_id',$organization_id],['program_id',$asset_id]]);//查询服务商程序数量信息
+                    dd($Assets);
                     if($Assets->program_balance >= $number){//如果服务商剩余程序数量足够
                         $program_balance = $Assets->program_balance - $number;//剩余数量
                         $program_used_num = $Assets->program_used_num + $number;//使用数量
