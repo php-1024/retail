68a2c5ddbb7cf3b0beb15d82fca513d815c83df8
mail@54zm.com <mail@54zm.com>
2018-03-17 16:03:42 +0800

    '1'

--- resources/views/Retail/Display/display.blade.php
+++ resources/views/Retail/Display/display.blade.php
@@ -309,7 +309,7 @@
                     <div class="form-group">
                         <label class="col-sm-2 text-right">手机号码</label>
                         <div class="col-sm-10">
-                            <input type="text" value="{{$organization->OrganizationRetailinfo->retail_mobile}}" name="mobile" placeholder="手机号码" class="form-control">
+                            <input type="text" value="{{$organization->OrganizationRetailinfo->retail_owner_mobile}}" name="mobile" placeholder="手机号码" class="form-control">
                         </div>
                     </div>
                     <div style="clear:both;"></div>
