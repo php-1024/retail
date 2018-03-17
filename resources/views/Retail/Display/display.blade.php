f2f6e5628a0acc8a5afd0c7268a79b5e094f3809
mail@54zm.com <mail@54zm.com>
2018-03-17 09:43:07 +0800

    '1'

--- resources/views/Retail/Display/display.blade.php
+++ resources/views/Retail/Display/display.blade.php
@@ -39,9 +39,7 @@
 
                                 <header class="panel-heading font-bold">
                                     概况
-                                    <input type="hidden" id="store_edit_comfirm_url" value="{{ url('retail/ajax/store_edit') }}">
-                                    <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
-                                    <button class="btn btn-default btn-xs pull-right" onclick="getEditForm('{{$admin_data['organization_id']}}')"><i class="fa fa-edit "></i>&nbsp;编辑</button>
+                                    <button class="btn btn-default btn-xs pull-right" id="editBtn"><i class="fa fa-edit "></i>&nbsp;编辑</button>
                                 </header>
                                 <div class="panel-body">
                                     <form class="form-horizontal" method="get">
@@ -252,7 +250,9 @@
 </section>
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <form class="form-horizontal tasi-form" method="get">
+    <form class="form-horizontal" id="store_edit_check" method="post" action="{{url('retail/ajax/store_edit_check')}}">
+        <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
+        <input type="hidden" name="organization_id" id="organization_id" value="{{$admin_data['organization_id']}}">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -262,9 +262,9 @@
                 <div class="modal-body">
                     <form class="form-horizontal" method="get">
                         <div class="form-group">
-                            <label class="col-sm-2 text-right">分店名称</label>
+                            <label class="col-sm-2 text-right">店铺名称</label>
                             <div class="col-sm-10">
-                                <input type="text" value="刘记鸡煲王【龙岗店】" placeholder="店铺名称" class="form-control">
+                                <input type="text" value="刘记鸡煲王【龙岗店】" name="organization_name" placeholder="店铺名称" class="form-control">
                             </div>
                         </div>
                         <div style="clear:both;"></div>
@@ -274,7 +274,7 @@
                         <div class="form-group">
                             <label class="col-sm-2 text-right">负责人</label>
                             <div class="col-sm-10">
-                                <input type="text" value="张老三" placeholder="负责人" class="form-control">
+                                <input type="text" value="张老三" name="owner" placeholder="负责人" class="form-control">
                             </div>
                         </div>
                         <div style="clear:both;"></div>
@@ -283,7 +283,7 @@
                         <div class="form-group">
                             <label class="col-sm-2 text-right">手机号码</label>
                             <div class="col-sm-10">
-                                <input type="text" value="13123456789" placeholder="手机号码" class="form-control">
+                                <input type="text" value="13123456789" name="mobile" placeholder="手机号码" class="form-control">
                             </div>
                         </div>
                         <div style="clear:both;"></div>
@@ -297,7 +297,6 @@
                         </div>
 
                         <div style="clear:both;"></div>
-
                         <div class="line line-dashed b-b line-lg pull-in"></div>
                         <div class="form-group">
                             <label class="col-sm-2 text-right">店铺地址</label>
@@ -312,7 +311,7 @@
                         <div class="form-group">
                             <label class="col-sm-2 text-right">安全密码</label>
                             <div class="col-sm-10">
-                                <input type="password" value="" placeholder="安全密码" class="form-control" >
+                                <input type="password" value="" name="safe_password" placeholder="安全密码" class="form-control" >
                             </div>
                         </div>
                         <div style="clear:both;"></div>
@@ -321,7 +320,7 @@
                 </div>
                 <div class="modal-footer">
                     <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
-                    <button class="btn btn-success" type="button" id="save_btn">确定</button>
+                    <button class="btn btn-success" type="button" onclick="EditStore('{{$admin_data['organization_id']}}')">确定</button>
                 </div>
             </div>
         </div>
@@ -341,40 +340,33 @@
 <script type="text/javascript" src="{{asset('public/Branch')}}/library/jPlayer/demo.js"></script>
 <script type="text/javascript" src="{{asset('public/Branch')}}/library/sweetalert/sweetalert.min.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#editBtn').click(function(){
-            $('#myModal').modal();
+    //编辑店铺信息
+    function EditStore(){
+        var target = $("#store_edit_check");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url,data,function(response){
+            if(response.status=='-1'){
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+                return;
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }
         });
-        $('#save_btn').click(function(){
-            swal({
-                title: "温馨提示",
-                text: "操作成功",
-                type: "success"
-            });
-        });
-    });
-    //删除商品信息
-    function getEditForm(id){
-        $('#myModal').modal();
-//        var url = $('#store_edit_comfirm_url').val();
-//        var token = $('#_token').val();
-//        var data = {'_token':token,'id':id};
-//        $.post(url,data,function(response){
-//            if(response.status=='-1'){
-//                swal({
-//                    title: "提示信息",
-//                    text: response.data,
-//                    confirmButtonColor: "#DD6B55",
-//                    confirmButtonText: "确定",
-//                },function(){
-//                    window.location.reload();
-//                });
-//                return;
-//            }else{
-//                $('#myModal').html(response);
-//                $('#myModal').modal();
-//            }
-//        });
     }
 </script>
 </body>
