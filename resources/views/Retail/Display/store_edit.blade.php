b148e15e80c1de1d49a79eb80caf03900f75a4dd
mail@54zm.com <mail@54zm.com>
2018-03-17 09:33:51 +0800

    '1'

--- resources/views/Retail/Display/store_edit.blade.php
+++ resources/views/Retail/Display/store_edit.blade.php
@@ -1,6 +1,80 @@
-<form class="form-horizontal" id="store_edit_check" method="post" action="{{url('retail/ajax/store_edit_check')}}">
-    <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
-    <input type="hidden" name="organization_id" id="organization_id" value="{{$organization_id}}">
+{{--<form class="form-horizontal" id="store_edit_check" method="post" action="{{url('retail/ajax/store_edit_check')}}">--}}
+    {{--<input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">--}}
+    {{--<input type="hidden" name="organization_id" id="organization_id" value="{{$organization_id}}">--}}
+    {{--<div class="modal-dialog">--}}
+        {{--<div class="modal-content">--}}
+            {{--<div class="modal-header">--}}
+                {{--<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>--}}
+                {{--<h4 class="modal-title">店铺信息编辑</h4>--}}
+            {{--</div>--}}
+            {{--<div class="modal-body">--}}
+                {{--<form class="form-horizontal" method="get">--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label class="col-sm-2 text-right">店铺名称</label>--}}
+                        {{--<div class="col-sm-10">--}}
+                            {{--<input type="text" value="刘记鸡煲王【龙岗店】" name="organization_name" placeholder="店铺名称" class="form-control">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div style="clear:both;"></div>--}}
+                    {{--<div class="line line-dashed b-b line-lg pull-in"></div>--}}
+
+
+                    {{--<div class="form-group">--}}
+                        {{--<label class="col-sm-2 text-right">负责人</label>--}}
+                        {{--<div class="col-sm-10">--}}
+                            {{--<input type="text" value="张老三" name="owner" placeholder="负责人" class="form-control">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div style="clear:both;"></div>--}}
+                    {{--<div class="line line-dashed b-b line-lg pull-in"></div>--}}
+
+                    {{--<div class="form-group">--}}
+                        {{--<label class="col-sm-2 text-right">手机号码</label>--}}
+                        {{--<div class="col-sm-10">--}}
+                            {{--<input type="text" value="13123456789" name="mobile" placeholder="手机号码" class="form-control">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div style="clear:both;"></div>--}}
+                    {{--<div class="line line-dashed b-b line-lg pull-in"></div>--}}
+
+                    {{--<div class="form-group">--}}
+                        {{--<label class="col-sm-2 text-right">店铺LOGO</label>--}}
+                        {{--<div class="col-sm-10">--}}
+                            {{--<input type="file" class="filestyle" style="display: none;" data-icon="false" data-classButton="btn btn-default" data-classInput="form-control inline v-middle input-s">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+
+                    {{--<div style="clear:both;"></div>--}}
+                    {{--<div class="line line-dashed b-b line-lg pull-in"></div>--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label class="col-sm-2 text-right">店铺地址</label>--}}
+                        {{--<div class="col-sm-10">--}}
+                            {{--<input type="text" value="广东省深圳市龙岗区万汇大厦1606" placeholder="店铺地址" class="form-control">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+
+                    {{--<div style="clear:both;"></div>--}}
+                    {{--<div class="line line-dashed b-b line-lg pull-in"></div>--}}
+
+                    {{--<div class="form-group">--}}
+                        {{--<label class="col-sm-2 text-right">安全密码</label>--}}
+                        {{--<div class="col-sm-10">--}}
+                            {{--<input type="password" value="" name="safe_password" placeholder="安全密码" class="form-control" >--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div style="clear:both;"></div>--}}
+
+                {{--</form>--}}
+            {{--</div>--}}
+            {{--<div class="modal-footer">--}}
+                {{--<button data-dismiss="modal" class="btn btn-default" type="button">取消</button>--}}
+                {{--<button class="btn btn-success" type="button" onclick="EditStore()">确定</button>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</form>--}}
+
+<form class="form-horizontal tasi-form" method="get">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -10,9 +84,9 @@
             <div class="modal-body">
                 <form class="form-horizontal" method="get">
                     <div class="form-group">
-                        <label class="col-sm-2 text-right">店铺名称</label>
+                        <label class="col-sm-2 text-right">分店名称</label>
                         <div class="col-sm-10">
-                            <input type="text" value="刘记鸡煲王【龙岗店】" name="organization_name" placeholder="店铺名称" class="form-control">
+                            <input type="text" value="刘记鸡煲王【龙岗店】" placeholder="店铺名称" class="form-control">
                         </div>
                     </div>
                     <div style="clear:both;"></div>
@@ -22,7 +96,7 @@
                     <div class="form-group">
                         <label class="col-sm-2 text-right">负责人</label>
                         <div class="col-sm-10">
-                            <input type="text" value="张老三" name="owner" placeholder="负责人" class="form-control">
+                            <input type="text" value="张老三" placeholder="负责人" class="form-control">
                         </div>
                     </div>
                     <div style="clear:both;"></div>
@@ -31,7 +105,7 @@
                     <div class="form-group">
                         <label class="col-sm-2 text-right">手机号码</label>
                         <div class="col-sm-10">
-                            <input type="text" value="13123456789" name="mobile" placeholder="手机号码" class="form-control">
+                            <input type="text" value="13123456789" placeholder="手机号码" class="form-control">
                         </div>
                     </div>
                     <div style="clear:both;"></div>
@@ -44,14 +118,15 @@
                         </div>
                     </div>
 
-                    {{--<div style="clear:both;"></div>--}}
-                    {{--<div class="line line-dashed b-b line-lg pull-in"></div>--}}
-                    {{--<div class="form-group">--}}
-                        {{--<label class="col-sm-2 text-right">店铺地址</label>--}}
-                        {{--<div class="col-sm-10">--}}
-                            {{--<input type="text" value="广东省深圳市龙岗区万汇大厦1606" placeholder="店铺地址" class="form-control">--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
+                    <div style="clear:both;"></div>
+
+                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                    <div class="form-group">
+                        <label class="col-sm-2 text-right">店铺地址</label>
+                        <div class="col-sm-10">
+                            <input type="text" value="广东省深圳市龙岗区万汇大厦1606" placeholder="店铺地址" class="form-control">
+                        </div>
+                    </div>
 
                     <div style="clear:both;"></div>
                     <div class="line line-dashed b-b line-lg pull-in"></div>
@@ -59,7 +134,7 @@
                     <div class="form-group">
                         <label class="col-sm-2 text-right">安全密码</label>
                         <div class="col-sm-10">
-                            <input type="password" value="" name="safe_password" placeholder="安全密码" class="form-control" >
+                            <input type="password" value="" placeholder="安全密码" class="form-control" >
                         </div>
                     </div>
                     <div style="clear:both;"></div>
@@ -68,7 +143,7 @@
             </div>
             <div class="modal-footer">
                 <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
-                <button class="btn btn-success" type="button" onclick="EditStore()">确定</button>
+                <button class="btn btn-success" type="button" id="save_btn">确定</button>
             </div>
         </div>
     </div>
