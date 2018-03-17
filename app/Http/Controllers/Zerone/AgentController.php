c8b813a0f6f06095c2b1e6906826ed15581a1c5a
kbzz <463661663@qq.com>
2018-03-17 10:48:14 +0800

    1

--- app/Http/Controllers/Zerone/AgentController.php
+++ app/Http/Controllers/Zerone/AgentController.php
@@ -492,8 +492,7 @@ class AgentController extends Controller{
             $parent_tree = $oneAgent['parent_tree'].$organization_id.',';//组织树
             Organization::editOrganization([['id',$fansmanage_id]],['parent_id'=>$organization_id,'parent_tree'=>$parent_tree]);
             $datastore = Organization::getList([['parent_id',$fansmanage_id]]);//商户信息下级分店信息
-            if(is_array($datastore)){//如果有店铺
-                dd(1);
+            if(!empty($datastore->toArray())){//如果有店铺
                 foreach($datastore as $key=>$value){
                     $asset_id = $value->program_id;//店铺用的程序id
                     $storeParent_tree = $parent_tree.$fansmanage_id.',';//商户店铺的组织树
