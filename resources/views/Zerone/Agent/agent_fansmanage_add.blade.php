0821141465edb3155d1c8a93693d4af1119f2af0
kbzz <463661663@qq.com>
2018-03-17 15:04:34 +0800

    1

--- resources/views/Zerone/Agent/agent_fansmanage_add.blade.php
+++ resources/views/Zerone/Agent/agent_fansmanage_add.blade.php
@@ -23,7 +23,7 @@
                 <div class="hr-line-dashed"></div>
 
                 <div class="form-group">
-                    <label class="col-sm-3 control-label" style="padding-top: 7px;">消耗程序与分店数量</label>
+                    <label class="col-sm-3 control-label" style="padding-top: 7px;">是否消耗程序</label>
                     <div class="col-sm-9">
                         <input type="checkbox" class="js-switch" checked  value="1" name="status"/>
                     </div>
