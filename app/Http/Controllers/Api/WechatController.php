c0959e8e84e67e1f15d38ba15194e7d6334f4f5e
zeo <234521890@qq.com>
2018-03-17 15:30:50 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -293,6 +293,11 @@ class WechatController extends Controller{
         return view('Wechat/Catering/material_article_edit',['info'=>$info,'id'=>$id,'image_info'=>$image_info,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
+    public function material_article_edit_check(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        $route_name = $request->path();//获取当前的页面路由
+    }
+
     /*
      * 图片选择页面
      */
