6ab58f71149f4798cc21a1ac838313b2688b5e9d
mail@54zm.com <mail@54zm.com>
2018-03-17 12:39:18 +0800

    '1'

--- resources/views/Retail/Display/display.blade.php
+++ resources/views/Retail/Display/display.blade.php
@@ -355,7 +355,7 @@
 
     //编辑店铺信息
     function EditStore() {
-        var formData = $( "#_token" ).serialize();
+        var formData = $( "#_token" ).val();
         console.log(formData);
         $.ajax({
             url: '{{ url('retail/ajax/store_edit') }}',
