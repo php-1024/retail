679c6924d87c7a439e1bc7a949d0f7132ff08538
zeo <234521890@qq.com>
2018-03-18 17:18:23 +0800

    by zeo

--- resources/views/Wechat/Catering/auto_reply_edit.blade.php
+++ resources/views/Wechat/Catering/auto_reply_edit.blade.php
@@ -1,4 +1,4 @@
-<form class="form-horizontal tasi-form" method="post" id="currentForm" action="{{ url('api/ajax/auto_reply_edit_article_check') }}">
+<form class="form-horizontal tasi-form" method="post" id="currentForm" action="{{ url('api/ajax/auto_reply_edit_check') }}">
     <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
     <input type="hidden" name="id" id="id" value="{{$id}}">
     <div class="modal-dialog">
