0a2fa60650e7ab03d445c5182fea032b4cf4a852
zeo <234521890@qq.com>
2018-03-18 15:52:04 +0800

    by zeo

--- resources/views/Wechat/Catering/auto_reply.blade.php
+++ resources/views/Wechat/Catering/auto_reply.blade.php
@@ -65,6 +65,7 @@
                                     <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
                                     <input type="hidden" name="_token" id="auto_reply_add_url" value="{{ url('api/ajax/auto_reply_add') }}">
                                     <input type="hidden" name="_token" id="auto_reply_edit_text_url" value="{{ url('api/ajax/auto_reply_edit_text') }}">
+                                    <input type="hidden" name="_token" id="auto_reply_edit_image_url" value="{{ url('api/ajax/auto_reply_image_text') }}">
                                     <div class="line line-dashed b-b line-lg pull-in"></div>
                                 </div>
                                 <div class="table-responsive">
@@ -392,9 +393,43 @@
             }
         });
     }
-    //弹出图片上传框
+    //弹出文本输入框
     function getAutoEditTextForm(id){
         var url = $('#auto_reply_edit_text_url').val();
+        var token = $('#_token').val();
+        if(id==''){
+            swal({
+                title: "提示信息",
+                text: '数据传输错误',
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+            },function(){
+                window.location.reload();
+            });
+            return;
+        }
+
+        var data = {'id':id,'_token':token};
+        $.post(url,data,function(response){
+            if(response.status=='-1'){
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+                return;
+            }else{
+                $('#myModal').html(response);
+                $('#myModal').modal();
+            }
+        });
+    }
+    //弹出图片选择框
+    function getAutoEditImageForm(id){
+        var url = $('#auto_reply_edit_image_url').val();
         var token = $('#_token').val();
         if(id==''){
             swal({
