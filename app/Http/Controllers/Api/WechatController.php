d454b2512ca65b3b39fb36af813dc6ba57c6d034
zeo <234521890@qq.com>
2018-03-18 14:40:29 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -593,8 +593,8 @@ class WechatController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-
-        return view('Wechat/Catering/auto_reply',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
+        $list = WechatReply::getPaginage([['organization_id',$admin_data['organization_id']]],15,'id','desc');
+        return view('Wechat/Catering/auto_reply',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
     /*
      * 添加关键字
