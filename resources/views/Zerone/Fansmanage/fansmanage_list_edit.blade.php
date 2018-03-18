2c96d29a0a551c22b4806e50523f1f18cd7128c1
kbzz <463661663@qq.com>
2018-03-18 09:39:55 +0800

    1

--- resources/views/Zerone/Fansmanage/fansmanage_list_edit.blade.php
+++ resources/views/Zerone/Fansmanage/fansmanage_list_edit.blade.php
@@ -10,7 +10,7 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label">归属服务商</label>
                     <div class="col-sm-10">
-                        <div class="col-sm-10">测试服务商</div>
+                        <div class="col-sm-10"><input type="text" class="form-control" value="测试服务商" disabled></div>
                     </div>
                 </div>
                 <div style="clear:both"></div>
