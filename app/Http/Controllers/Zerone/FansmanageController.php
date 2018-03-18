2c0c0859643c65d0846db7bdf11eb468d04bf71e
kbzz <463661663@qq.com>
2018-03-18 08:34:18 +0800

    1

--- app/Http/Controllers/Zerone/FansmanageController.php
+++ app/Http/Controllers/Zerone/FansmanageController.php
@@ -142,8 +142,8 @@ class FansmanageController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $list = Organization::where([['status','1'],['type',[1,2]]])->get();
-        
+        $list = Organization::where([['status','1'],['type','in',[1,2]]])->get();
+
         return view('Zerone/Fansmanage/fansmanage_add',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'list'=>$list]);
     }
     //注册提交商户数据
