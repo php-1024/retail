1c48df45a5f0a9fa53e9267aa03347f1dce2adb2
zeo <234521890@qq.com>
2018-03-18 19:33:33 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\WechatReply;
 use App\Models\WechatSubscribeReply;
+use App\Models\WechatDefaultReply;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\WechatOpenSetting;
