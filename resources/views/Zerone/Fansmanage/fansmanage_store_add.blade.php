42d1fc3e69d8f452f54afc84b5dcdb45974cca85
kbzz <463661663@qq.com>
2018-03-18 15:39:29 +0800

    1

--- resources/views/Zerone/Fansmanage/fansmanage_store_add.blade.php
+++ resources/views/Zerone/Fansmanage/fansmanage_store_add.blade.php
@@ -1,12 +1,4 @@
-<link href="{{asset('public/Zerone/library/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet">
-<link href="{{asset('public/Zerone/library/font')}}/css/font-awesome.css" rel="stylesheet">
-<link href="{{asset('public/Zerone/library/sweetalert')}}/css/sweetalert.css" rel="stylesheet">
-<link href="{{asset('public/Zerone')}}/css/animate.css" rel="stylesheet">
-<link href="{{asset('public/Zerone')}}/css/style.css" rel="stylesheet">
-<link href="{{asset('public/Zerone/library/chosen')}}/css/chosen.css" rel="stylesheet">
-<link href="{{asset('public/Zerone/library/sweetalert')}}/css/sweetalert.css" rel="stylesheet">
-<link href="{{asset('public/Zerone/library/iCheck')}}/css/custom.css" rel="stylesheet">
-<link href="{{asset('public/Zerone/library/switchery')}}/css/switchery.css" rel="stylesheet">
+<div class="modal inmodal">
 <div class="modal-dialog modal-lg">
     <div class="modal-content animated fadeIn">
         <div class="modal-header">
@@ -55,16 +47,4 @@
         </div>
     </div>
 </div>
-<script src="{{asset('public/Zerone/library/jquery')}}/js/jquery-2.1.1.js"></script>
-<script src="{{asset('public/Zerone/library/bootstrap')}}/js/bootstrap.min.js"></script>
-<script src="{{asset('public/Zerone/library/metisMenu')}}/js/jquery.metisMenu.js"></script>
-<script src="{{asset('public/Zerone/library/slimscroll')}}/js/jquery.slimscroll.min.js"></script>
-
-<!-- Custom and plugin javascript -->
-<script src="{{asset('public/Zerone')}}/js/inspinia.js"></script>
-<script src="{{asset('public/Zerone/library/pace')}}/js/pace.min.js"></script>
-<script src="{{asset('public/Zerone/library/iCheck')}}/js/icheck.min.js"></script>
-<script src="{{asset('public/Zerone/library/sweetalert')}}/js/sweetalert.min.js"></script>
-<script src="{{asset('public/Zerone/library/chosen')}}/js/chosen.jquery.js"></script>
-<script src="{{asset('public/Tooling/library/switchery')}}/js/switchery.js"></script>
-<script src="{{asset('public/Zerone/library/sweetalert')}}/js/sweetalert.min.js"></script>
+</div>
