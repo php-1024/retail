1844c4f78b23f87517355b3f28c5a628af9df1b9
kbzz <463661663@qq.com>
2018-03-18 16:05:47 +0800

    1

--- resources/views/Zerone/Fansmanage/fansmanage_store_add.blade.php
+++ resources/views/Zerone/Fansmanage/fansmanage_store_add.blade.php
@@ -1,47 +1,82 @@
-<div class="modal-dialog modal-lg">
-    <div class="modal-content animated fadeIn">
-        <div class="modal-header">
-            <h3>“刘记餐饮集团”店铺划入</h3>
-        </div>
-        <div class="modal-body">
-            <div class="form-group">
-                <label class="col-sm-3 control-label" style="padding-top: 7px;">请选择要划入的店铺</label>
-                <div class="col-sm-9">
-                    <select data-placeholder="请店铺" class="chosen-select" style="width:350px;" tabindex="4" name="store_id">
-                       @foreach($list as $key=>$value)
-                        <option value="{{$value->id}}">{{$value->organization_name}}</option>
-                       @endforeach
-                    </select>
+<form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/fansmanage_store_add_check') }}">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <input type="hidden" name="organization_id" value="{{$organization_id}}">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content animated fadeIn">
+            <div class="modal-header">
+                <h3>“刘记新科技有限公司”商户划入</h3>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label class="col-sm-3 control-label" style="padding-top: 7px;">请选择要划入的店铺</label>
+                    <div class="col-sm-9">
+                        <select data-placeholder="请选择省份" class="chosen-select" style="width:350px;" tabindex="4" name="fansmanage_id">
+                            @foreach($list as $key=>$value)
+                                <option value="{{$value->id}}">{{$value->organization_name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
                 </div>
 
-            </div>
+                <div style="clear:both"></div>
+                <div class="hr-line-dashed"></div>
 
-            <div style="clear:both"></div>
-            <div class="hr-line-dashed"></div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label" style="padding-top: 7px;">是否消耗程序</label>
+                    <div class="col-sm-9">
+                        <input type="checkbox" class="js-switch" checked  value="1" name="status"/>
+                    </div>
 
-            <div class="form-group">
-                <label class="col-sm-3 control-label" style="padding-top: 7px;">消耗程序与分店数量</label>
-                <div class="col-sm-9">
-                    <input type="checkbox" class="js-switch" checked  value="1" name="status"/>
                 </div>
 
+                <div style="clear:both"></div>
+                <div class="hr-line-dashed"></div>
+
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">安全密码</label>
+                    <div class="col-sm-9"><input type="password" class="form-control" value="" name="safe_password"></div>
+                </div>
+                <div style="clear:both"></div>
+                <div class="hr-line-dashed"></div>
+
             </div>
 
-            <div style="clear:both"></div>
-            <div class="hr-line-dashed"></div>
-
-            <div class="form-group">
-                <label class="col-sm-3 control-label">安全密码</label>
-                <div class="col-sm-9"><input type="text" class="form-control" value=""></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
+                <button type="button" class="btn btn-primary saveBtn" onclick="postForm()">保存</button>
             </div>
-            <div style="clear:both"></div>
-            <div class="hr-line-dashed"></div>
-
-        </div>
-
-        <div class="modal-footer">
-            <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
-            <button type="button" class="btn btn-primary saveBtn">保存</button>
         </div>
     </div>
-</div>
+</form>
+<script>
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    //type: "warning"
+                });
+            }
+        });
+    }
+</script>
