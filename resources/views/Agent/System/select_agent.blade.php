1092dbd89744730283d2140df0e7470199067eb3
kbzz <463661663@qq.com>
2018-03-19 13:50:32 +0800

    1

--- resources/views/Agent/System/select_agent.blade.php
+++ resources/views/Agent/System/select_agent.blade.php
@@ -106,14 +106,15 @@
                     window.location.reload();
                 });
             }else{
-                swal({
-                    title: "提示信息",
-                    text: json.data,
-                    confirmButtonColor:"#DD6B55",
-                    confirmButtonText: "确定"
-                },function () {
-                    window.location.reload();
-                });
+                console.log(json);
+//                swal({
+//                    title: "提示信息",
+//                    text: json.data,
+//                    confirmButtonColor:"#DD6B55",
+//                    confirmButtonText: "确定"
+//                },function () {
+//                    window.location.reload();
+//                });
             }
         });
     }
