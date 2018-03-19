f4f2828a95b370c04bf4c71c1b29316ffdb7a39c
kbzz <463661663@qq.com>
2018-03-19 15:39:36 +0800

    1

--- app/Http/Controllers/Agent/SystemController.php
+++ app/Http/Controllers/Agent/SystemController.php
@@ -158,7 +158,6 @@ class SystemController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = $admin_data['organization_id'];//当前组织ID，零壹管理平台组织只能为1
         $oneAcc = Account::getOne([['organization_id',$organization_id],['deepth',1]]);//查找服务商对应的负责人信息
-        dd($oneAcc);
         $parent_tree = $oneAcc['parent_tree'];//组织树
         //获取重Admin开始的的所有人员
         $list = Account::getList([['organization_id',$organization_id],['parent_tree','like','%'.$parent_tree.$oneAcc['id'].',%']],0,'id','asc')->toArray();
