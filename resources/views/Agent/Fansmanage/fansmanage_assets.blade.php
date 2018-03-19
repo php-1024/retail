8f4cd8542a3f2d2e34e911089f4836c2f8dc3b31
kbzz <463661663@qq.com>
2018-03-20 03:04:20 +0800

    1

--- resources/views/Agent/Fansmanage/fansmanage_assets.blade.php
+++ resources/views/Agent/Fansmanage/fansmanage_assets.blade.php
@@ -1,4 +1,4 @@
-<form class="form-horizontal tasi-form" method="post" role="form" id="currentForm" action="{{ url('aent/ajax/fansmanage_assets_check') }}">
+<form class="form-horizontal tasi-form" method="post" role="form" id="currentForm" action="{{ url('agent/ajax/fansmanage_assets_check') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <input type="hidden" name="organization_id" value="{{$oneFansmanage->id}}">
     <input type="hidden" name="program_id" value="{{$oneProgram->id}}">
