0d759f0092f3bea51795d5c2382b44f824cacd0c
mail@54zm.com <mail@54zm.com>
2018-03-19 10:32:32 +0800

    '1'

--- app/Http/Controllers/Catering/UserController.php
+++ app/Http/Controllers/Catering/UserController.php
@@ -22,8 +22,8 @@ class UserController extends Controller{
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
 
-        $store_id = $admin_data['organization_id'];//组织id
-        $list = Label::getPaginage([['store_id',$store_id]],'10','id');
+        $fansmanage_id = $admin_data['organization_id'];//组织id
+        $list = Label::getPaginage([['fansmanage_id',$fansmanage_id]],'10','id');
 
         return view('Catering/User/user_tag',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
