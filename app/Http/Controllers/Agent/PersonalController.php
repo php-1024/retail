947198ff5cf8487904d043f794b87fd70eb88332
kbzz <463661663@qq.com>
2018-03-19 16:38:12 +0800

    1

--- app/Http/Controllers/Agent/PersonalController.php
+++ app/Http/Controllers/Agent/PersonalController.php
@@ -160,7 +160,7 @@ class PersonalController extends Controller{
     //修改安全密码
     public function safe_password(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dd($admin_data);
+        dump($admin_data);
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
