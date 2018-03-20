e59e5986f6ae91053782989674e5596741e81dbf
zeo <234521890@qq.com>
2018-03-20 19:23:36 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -1220,7 +1220,7 @@ class WechatController extends Controller{
                                 $result = $this->zerone_response_article($param,$article_data);
                                 break;
                         }
-                    }else{
+                    }else{//默认回复
                         $result = $this->zerone_response_text($param,'这里是默认回复');
                     }
                 }
