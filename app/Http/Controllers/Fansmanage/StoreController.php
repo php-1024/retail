4f2de829f37321637927fec17bd944ab444b88ca
mail@54zm.com <mail@54zm.com>
2018-03-20 17:08:51 +0800

    '1'

--- app/Http/Controllers/Fansmanage/StoreController.php
+++ app/Http/Controllers/Fansmanage/StoreController.php
@@ -90,10 +90,11 @@ class StoreController extends Controller{
             AccountInfo::addAccountInfo($accdatainfo);
             //创建后减少程序剩余数量
             $num = $organization_assets['program_balance'] - 1;
+            $used_num = $organization_assets['program_used_num'] + 1;
             if ($num<0){
                 return response()->json(['data' => '创建店铺失败，您暂无剩余的资产程序了！', 'status' => '0']);
             }
-            OrganizationAssets::editAssets([['id', $organization_assets['id']]], ['program_balance' => $num]);
+            OrganizationAssets::editAssets([['id', $organization_assets['id']]], ['program_balance' => $num],['program_used_num'=>$used_num]);
             //添加操作日志
             if ($admin_data['is_super'] == 2){//超级管理员操作商户的记录
                 OperationLog::addOperationLog('1','1','1',$route_name,'在粉丝管理系统创建了店铺：'.$organization_name);    //保存操作记录
