ac73a22b0e46285c7fa7a793a2c3e3aec9fdc75c
mail@54zm.com <mail@54zm.com>
2018-03-21 14:15:24 +0800

    '1'

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -458,9 +458,8 @@ class WechatController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $re = WechatAuthorization::getOne([['organization_id',$admin_data['organization_id']]]);
-        dump($re);
-        $list = WechatDefinedMenu::getList([['organization_id',$admin_data['organization_id']]],0,'id','DESC');
+        $authorization = WechatAuthorization::getOne([['organization_id',$admin_data['organization_id']]]);
+        $list = WechatDefinedMenu::getList([['organization_id',$admin_data['organization_id'],['authorizer_appid',$authorization['authorizer_appid']]]],0,'id','DESC');
         dump($list);
         return view('Wechat/Catering/defined_menu',['list'=>$list,'admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
