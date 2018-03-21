eb31461c63441397898a5bdc1f01dc0e333eded5
zeo <234521890@qq.com>
2018-03-21 19:32:46 +0800

    by zeo

--- public/Tooling/js/inspinia.js
+++ public/Tooling/js/inspinia.js
@@ -9,17 +9,24 @@
 $(document).ready(function () {
 
 
-    // Add body-small class if window less than 768px
-    if ($(this).width() < 769) {
-        $('body').addClass('body-small')
-    } else {
-        $('body').removeClass('body-small')
-    }
+
 
     // MetsiMenu
     $('#side-menu').metisMenu();
 
-
+    // Collapse ibox function
+    $('.collapse-link').click(function () {
+        var ibox = $(this).closest('div.ibox');
+        var button = $(this).find('i');
+        var content = ibox.find('div.ibox-content');
+        content.slideToggle(200);
+        button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+        ibox.toggleClass('').toggleClass('border-bottom');
+        setTimeout(function () {
+            ibox.resize();
+            ibox.find('[id^=map-]').resize();
+        }, 50);
+    });
 
     // Close ibox function
     $('.close-link').click(function () {
