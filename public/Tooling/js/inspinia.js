4b1775ccf0e63f237bb20075fd1a5669c52ed90b
zeo <234521890@qq.com>
2018-03-21 19:34:23 +0800

    by zeo

--- public/Tooling/js/inspinia.js
+++ public/Tooling/js/inspinia.js
@@ -257,7 +257,26 @@ function animationHover(element, animation) {
         });
 }
 
-
+function SmoothlyMenu() {
+    if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+        // Hide menu in order to smoothly turn on when maximize menu
+        $('#side-menu').hide();
+        // For smoothly turn on menu
+        setTimeout(
+            function () {
+                $('#side-menu').fadeIn(400);
+            }, 200);
+    } else if ($('body').hasClass('fixed-sidebar')) {
+        $('#side-menu').hide();
+        setTimeout(
+            function () {
+                $('#side-menu').fadeIn(400);
+            }, 100);
+    } else {
+        // Remove all inline style from jquery fadeIn function to reset menu state
+        $('#side-menu').removeAttr('style');
+    }
+}
 
 // Dragable panels
 function WinMove() {
