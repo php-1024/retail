54d74d7660418015a439a2970be2f3e385cfde6b
mail@54zm.com <mail@54zm.com>
2018-03-21 17:37:49 +0800

    '1'

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -547,9 +547,6 @@ class WechatController extends Controller{
             if ($val['parent_id'] == 0){
                 $menu_data['id'] = $val['id'];
                 $menu_data['menu_name'] = $val['menu_name'];
-            }else{
-                $menu_data['son_menu_data']['id'] = $val['id'];
-                $menu_data['son_menu_data']['menu_name'] = $val['menu_name'];
             }
         }
         dump($menu_data);
