e9a06ffa3defc772b09ebe0ce9759809baa24d94
zeo <234521890@qq.com>
2018-03-21 19:34:08 +0800

    by zeo

--- public/Tooling/js/inspinia.js
+++ public/Tooling/js/inspinia.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
     // Close menu in canvas mode
     $('.close-canvas-menu').click(function () {
         $("body").toggleClass("mini-navbar");
-
+        SmoothlyMenu();
     });
 
     // Run menu of canvas
@@ -257,26 +257,7 @@ function animationHover(element, animation) {
         });
 }
 
-function SmoothlyMenu() {
-    if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
-        // Hide menu in order to smoothly turn on when maximize menu
-        $('#side-menu').hide();
-        // For smoothly turn on menu
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(400);
-            }, 200);
-    } else if ($('body').hasClass('fixed-sidebar')) {
-        $('#side-menu').hide();
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(400);
-            }, 100);
-    } else {
-        // Remove all inline style from jquery fadeIn function to reset menu state
-        $('#side-menu').removeAttr('style');
-    }
-}
+
 
 // Dragable panels
 function WinMove() {
