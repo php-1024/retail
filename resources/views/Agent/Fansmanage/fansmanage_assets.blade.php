8c5c8ca187cd620fbe707c2be5df4b0487bc25c6
kbzz <463661663@qq.com>
2018-03-21 16:28:52 +0800

    1

--- resources/views/Agent/Fansmanage/fansmanage_assets.blade.php
+++ resources/views/Agent/Fansmanage/fansmanage_assets.blade.php
@@ -65,21 +65,12 @@
                     window.location.reload();
                 });
             }else{
-<<<<<<< HEAD
                 swal({
                     title: "提示信息",
                     text: json.data,
                     confirmButtonColor: "#DD6B55",
                     confirmButtonText: "确定"
                 });
-=======
-               swal({
-                   title: "提示信息",
-                   text: json.data,
-                   confirmButtonColor: "#DD6B55",
-                   confirmButtonText: "确定"
-               });
->>>>>>> bbd8c00e66a810e6ddc9bf96e3ad901731ad2f85
             }
         });
     }
