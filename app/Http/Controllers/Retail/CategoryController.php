932fa0358e2287349ca583ab9bdf6eb3d141b30f
mail@54zm.com <mail@54zm.com>
2018-03-22 17:12:35 +0800

    '1'

--- app/Http/Controllers/Retail/CategoryController.php
+++ app/Http/Controllers/Retail/CategoryController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 零售版店铺
- * 栏目管理
+ * 栏目分类管理
  **/
 
 namespace App\Http\Controllers\Retail;
@@ -20,8 +20,8 @@ class CategoryController extends Controller
     public function category_add(Request $request)
     {
         $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
-        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');            //中间件产生的菜单数据参数
+        $son_menu_data = $request->get('son_menu_data');    //中间件产生的子菜单数据参数
         $route_name = $request->path();                         //获取当前的页面路由
         return view('Retail/Category/category_add',['admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
@@ -29,7 +29,7 @@ class CategoryController extends Controller
     //添加商品分类操作
     public function category_add_check(Request $request)
     {
-        $admin_data = $request->get('admin_data');      //中间件产生的管理员数据参数
+        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
         $route_name = $request->path();                         //获取当前的页面路由
         $category_name = $request->get('category_name');    //栏目名称
         $category_sort = $request->get('category_sort');    //栏目排序
@@ -48,10 +48,10 @@ class CategoryController extends Controller
         try {
             RetailCategory::addCategory($category_data);
             //添加操作日志
-            if ($admin_data['is_super'] == 1){//超级管理员操作商户的记录
-                OperationLog::addOperationLog('1','1','1',$route_name,'在餐饮分店管理系统添加了栏目分类！');//保存操作记录
-            }else{//商户本人操作记录
-                OperationLog::addOperationLog('5',$admin_data['organization_id'],$admin_data['id'],$route_name, '添加了栏目分类！');//保存操作记录
+            if ($admin_data['is_super'] == 1){//超级管理员添加零售店铺分类的记录
+                OperationLog::addOperationLog('1','1','1',$route_name,'在零售管理系统添加了栏目分类！');//保存操作记录
+            }else{//零售店铺本人操作记录
+                OperationLog::addOperationLog('10',$admin_data['organization_id'],$admin_data['id'],$route_name, '添加了栏目分类！');//保存操作记录
             }
             DB::commit();
         } catch (\Exception $e) {
@@ -65,8 +65,8 @@ class CategoryController extends Controller
     public function category_list(Request $request)
     {
         $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-        $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
-        $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');            //中间件产生的菜单数据参数
+        $son_menu_data = $request->get('son_menu_data');    //中间件产生的子菜单数据参数
         $route_name = $request->path();                         //获取当前的页面路由
         $category_name = $request->get('name');
         $where = [
@@ -93,8 +93,8 @@ class CategoryController extends Controller
         try {
             RetailCategory::select_delete($category_id);
             //添加操作日志
-            if ($admin_data['is_super'] == 1) {//超级管理员操作商户的记录
-                OperationLog::addOperationLog('1', '1', '1', $route_name, '在餐饮分店管理系统删除了商品分类！');//保存操作记录
+            if ($admin_data['is_super'] == 1) {//超级管理员删除零售店铺商品分类的记录
+                OperationLog::addOperationLog('1', '1', '1', $route_name, '在零售店铺管理系统删除了商品分类！');//保存操作记录
             } else {//分店本人操作记录
                 OperationLog::addOperationLog('10', $admin_data['organization_id'], $admin_data['id'], $route_name, '删除了商品分类！');//保存操作记录
             }
@@ -110,7 +110,7 @@ class CategoryController extends Controller
     public function category_edit(Request $request)
     {
         $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-        $category_id = $request->get('id');
+        $category_id = $request->get('id');                 //获取栏目id
         $where = [
             'retail_id' => $admin_data['organization_id'],
             'id' => $category_id,
@@ -123,10 +123,10 @@ class CategoryController extends Controller
     public function category_edit_check(Request $request)
     {
         $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
-        $route_name = $request->path();                     //获取当前的页面路由
-        $name = $request->get('category_name');                      //栏目名称
-        $displayorder = $request->get('displayorder');      //栏目排序
-        $category_id = $request->get('category_id');         //栏目id
+        $route_name = $request->path();                         //获取当前的页面路由
+        $name = $request->get('category_name');            //栏目名称
+        $displayorder = $request->get('displayorder');     //栏目排序
+        $category_id = $request->get('category_id');       //栏目id
         if (empty($displayorder)){
             $displayorder = '0';
         }
@@ -142,10 +142,10 @@ class CategoryController extends Controller
         try {
             RetailCategory::editCategory($where,$category_data);
             //添加操作日志
-            if ($admin_data['is_super'] == 1){//超级管理员操作商户的记录
-                OperationLog::addOperationLog('1','1','1',$route_name,'在餐饮分店管理系统修改了栏目分类！');//保存操作记录
-            }else{//商户本人操作记录
-                OperationLog::addOperationLog('5',$admin_data['organization_id'],$admin_data['id'],$route_name, '修改了栏目分类！');//保存操作记录
+            if ($admin_data['is_super'] == 1){//超级管理员操作零售店铺的记录
+                OperationLog::addOperationLog('1','1','1',$route_name,'在零售店铺管理系统修改了栏目分类！');//保存操作记录
+            }else{//零售店铺本人操作记录
+                OperationLog::addOperationLog('10',$admin_data['organization_id'],$admin_data['id'],$route_name, '修改了栏目分类！');//保存操作记录
             }
             DB::commit();
         } catch (\Exception $e) {
