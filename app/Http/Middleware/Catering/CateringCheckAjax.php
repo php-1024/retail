eec175b0ad5726d4c9fa8a982afd9ae85a8b1eb1
zeo <234521890@qq.com>
2018-03-22 18:10:11 +0800

    by zeo

--- app/Http/Middleware/Catering/CateringCheckAjax.php
+++ app/Http/Middleware/Catering/CateringCheckAjax.php
@@ -945,7 +945,9 @@ class CateringCheckAjax
         if(empty($request->input('menu_name'))){
             return self::res(0,response()->json(['data' => '请输入菜单名称！', 'status' => '0']));
         }
-        dump(strlen('张三李四'));
+        if(strlen($request->input('menu_name'))>12){
+            return self::res(0,response()->json(['data' => '您输入的菜单名称超长', 'status' => '0']));
+        }
         if(empty($request->input('event_type'))){
             return self::res(0,response()->json(['data' => '请选择事件类型！', 'status' => '0']));
         }
