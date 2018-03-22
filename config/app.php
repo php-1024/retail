8028f2f0d3f4841f9bb0d5d35f0b8c165fe48a05
kbzz <463661663@qq.com>
2018-03-22 16:24:10 +0800

    1

--- config/app.php
+++ config/app.php
@@ -82,6 +82,8 @@ return [
     'zerone_route_except'=>[
         'zerone',//零壹管理平台首页
         'zerone/ajax/proxy_list_frozen',//冻结服务商弹出安全密码输入框
+        'zerone/agent/agent_fansmanage',//冻结服务商弹出安全密码输入框
+
     ],
 
     /*
