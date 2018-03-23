200e1cf28ce51597898c58e633ade45aab9bd94b
mail@54zm.com <mail@54zm.com>
2018-03-23 10:15:34 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -25,16 +25,11 @@ class OrderController extends Controller
         $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
         $route_name = $request->path();                         //获取当前的页面路由
-        $restaurant_id = Organization::getPluck(['id'=>$admin_data['organization_id']],'parent_id')->first();
         $where = [
-            'fansmanage_id' => $restaurant_id,
             'order_type' => '1',    //0为未知订单，1为现场订单，2为外卖订单，3为预约订单
             'retail_id' => $admin_data['organization_id'],
         ];
         $list = RetailOrder::getPaginage($where,10,'created_at','DESC');
-        dump($request);
-        dump($restaurant_id);
-        dump($list);
         foreach ( $list as $key=>$val){
             $user = User::getOneUser([['id',$val->user_id]]);
             $val->user = $user;
