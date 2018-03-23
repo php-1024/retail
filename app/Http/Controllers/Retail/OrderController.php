823066f28053d2416a24cb7aa4a04ae05343933b
mail@54zm.com <mail@54zm.com>
2018-03-23 10:09:38 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -21,6 +21,7 @@ class OrderController extends Controller
     //订单管理-现场订单
     public function order_spot(Request $request)
     {
+        dump($request);
         $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');            //中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的管理员数据参数
