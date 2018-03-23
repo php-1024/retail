84dc0ac062eaa4f0f615cd3ce134b0b93042da84
mail@54zm.com <mail@54zm.com>
2018-03-23 18:23:21 +0800

    '1'

--- app/Http/Middleware/Retail/RetailCheck.php
+++ app/Http/Middleware/Retail/RetailCheck.php
@@ -86,6 +86,7 @@ class RetailCheck{
             //计算数组差集，获取用户所没有的权限
             $unset_routes = array_diff($program_routes,$account_routes);
             //如果跳转的路由不在该程序的所有节点中。则报错
+            dd(config('app.retail_route_except'));
             if(!in_array($route_name,$program_routes) && !in_array($route_name,config('app.retail_route_except'))){
                 return self::res(0, response()->json(['data' => '对不起，您不具备权限', 'status' => '0']));
             }
