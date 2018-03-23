32e885f7ca70af3328744be3bf48a684916ac496
mail@54zm.com <mail@54zm.com>
2018-03-23 10:54:49 +0800

    '1'

--- app/Http/Controllers/Retail/GoodsController.php
+++ app/Http/Controllers/Retail/GoodsController.php
@@ -40,6 +40,8 @@ class GoodsController extends Controller
         $category_id = $request->get('category_id');        //栏目ID
         $name = $request->get('name');                      //商品名称
         $price = $request->get('price');                    //商品价格
+        $barcode = $request->get('barcode');                //商品条码
+        dd($barcode);
         $stock = $request->get('stock');                    //商品库存
         $displayorder = $request->get('displayorder');      //商品排序
         $details = $request->get('details');                //商品详情
@@ -49,7 +51,7 @@ class GoodsController extends Controller
         $fansmanage_id = Organization::getPluck(['id'=>$admin_data['organization_id']],'parent_id')->first();
 
         //商品数据
-        $goods_data = ['fansmanage_id' => $fansmanage_id,'retail_id' => $admin_data['organization_id'],'created_by' => $admin_data['id'],'category_id' => $category_id,'name' => $name, 'price' => $price, 'stock' => $stock, 'displayorder' => $displayorder, 'details' => $details];
+        $goods_data = ['fansmanage_id' => $fansmanage_id,'retail_id' => $admin_data['organization_id'],'created_by' => $admin_data['id'],'category_id' => $category_id,'name' => $name, 'price' => $price, 'stock' => $stock, 'barcode'=>$barcode,'displayorder' => $displayorder, 'details' => $details];
         DB::beginTransaction();
         try {
             $goods_id = RetailGoods::addRetailGoods($goods_data);
