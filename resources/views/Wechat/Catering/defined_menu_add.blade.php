263c2cea6aaf5849ccae0bdd6b97ba44b1389e84
kbzz <463661663@qq.com>
2018-03-24 16:52:33 +0800

    1

--- resources/views/Wechat/Catering/defined_menu_add.blade.php
+++ resources/views/Wechat/Catering/defined_menu_add.blade.php
@@ -76,8 +76,8 @@
                 <section class="panel panel-default">
                     <header class="panel-heading text-right bg-light">
                         <ul class="nav nav-tabs pull-left">
-                            <li id="link_type" class="active"><a href="#link_response" onclick="$('#response_type').val(1)" data-toggle="tab"><i class="fa fa-file-text-o text-muted"></i>&nbsp;&nbsp;跳转链接</a></li>
-                            <li id="text_type"><a href="#text_response" onclick="$('#response_type').val(2)" data-toggle="tab"><i class="icon icon-picture text-muted"></i>&nbsp;&nbsp;关键字回复</a></li>
+                            <li id="link_type" ><a href="#link_response" onclick="$('#response_type').val(1)" data-toggle="tab"><i class="fa fa-file-text-o text-muted"></i>&nbsp;&nbsp;跳转链接</a></li>
+                            <li id="text_type" class="active"><a href="#text_response" onclick="$('#response_type').val(2)" data-toggle="tab"><i class="icon icon-picture text-muted"></i>&nbsp;&nbsp;关键字回复</a></li>
                         </ul>
                         <span class="hidden-sm">&nbsp;</span>
                     </header>
