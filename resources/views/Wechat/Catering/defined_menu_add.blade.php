d3cd355aed40748495df6da47e69f4f99af1aca4
kbzz <463661663@qq.com>
2018-03-24 16:59:14 +0800

    1

--- resources/views/Wechat/Catering/defined_menu_add.blade.php
+++ resources/views/Wechat/Catering/defined_menu_add.blade.php
@@ -93,7 +93,7 @@
                                 <select style="width:260px" name="response_keyword" class="chosen-select2">
                                     <option value ="">请选择关键字</option>
                                     @foreach($wechatreply as $key=>$val)
-                                        <option value ="{{$val->id}}">{{$val->keyword}}</option>
+                                        <option value ="{{$val->keyword}}">{{$val->keyword}}</option>
                                     @endforeach
                                 </select>
                                  <span class="help-block m-b-none">
