70bab75404bab975c5d0110b0538403cfd2a2a3a
zeo <234521890@qq.com>
2018-03-24 18:59:04 +0800

    by zeo

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -557,6 +557,7 @@ class WechatController extends Controller{
             }
         }
         $auth_info = \Wechat::refresh_authorization_info($organization_id);//刷新并获取授权令牌
+        dump($data);
         $re = \Wechat::create_menu($auth_info['authorizer_access_token'],$data);
         dd($re);
     }
