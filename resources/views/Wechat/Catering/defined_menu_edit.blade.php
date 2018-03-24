1ea6d13ca2a2744e946847e5c2728e72d304efab
kbzz <463661663@qq.com>
2018-03-24 11:11:11 +0800

    1

--- resources/views/Wechat/Catering/defined_menu_edit.blade.php
+++ resources/views/Wechat/Catering/defined_menu_edit.blade.php
@@ -125,7 +125,7 @@
         var url = target.attr("action");
         var data = target.serialize();
         var data = target.serializeArray();
-        console.log(data[4][event_type]);
+        console.log(data[4]);
 //        $.post(url,data,function(json){
 //            if(json.status==1){
 //                swal({
