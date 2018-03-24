6b9dc5644a67fd839ef10b6808351c7c4d6143dc
kbzz <463661663@qq.com>
2018-03-24 15:47:34 +0800

    1

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -467,7 +467,7 @@ class WechatController extends Controller{
                 $list[$key]['sub_button'] =$re;
             }
         }
-        dump($list);
+        dump(json_decode($list));
         return view('Wechat/Catering/defined_menu',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
