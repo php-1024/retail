148866a249630c8eb73ba0ea56d7931efd56de13
kbzz <463661663@qq.com>
2018-03-24 11:33:28 +0800

    1

--- resources/views/Wechat/Catering/defined_menu_add.blade.php
+++ resources/views/Wechat/Catering/defined_menu_add.blade.php
@@ -141,25 +141,24 @@
         var target = $("#defined_menu_add_check");
         var url = target.attr("action");
         var data = target.serialize();
-        console.log(data);
-//        $.post(url,data,function(json){
-//            if(json.status==1){
-//                swal({
-//                    title: "提示信息",
-//                    text: json.data,
-//                    confirmButtonColor: "#DD6B55",
-//                    confirmButtonText: "确定"
-//                },function(){
-//                    window.location.reload();
-//                });
-//            }else{
-//                swal({
-//                    title: "提示信息",
-//                    text: json.data,
-//                    confirmButtonColor:"#DD6B55",
-//                    confirmButtonText: "确定"
-//                });
-//            }
-//        });
+        $.post(url,data,function(json){
+            if(json.status==1){
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor:"#DD6B55",
+                    confirmButtonText: "确定"
+                });
+            }
+        });
     }
 </script>
