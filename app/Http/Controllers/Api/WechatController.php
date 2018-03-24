a9d0f9a892bc62a7a2d82a8498bbb8f35af96bda
kbzz <463661663@qq.com>
2018-03-24 19:10:43 +0800

    1

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -504,9 +504,9 @@ class WechatController extends Controller{
                             $type='location_select';
                             break;
                     }
-                    $data['button']['name'] = $value['menu_name'];
+                    $data[$key]['button']['name'] = $value['menu_name'];
                     if($v['event_type']==1){
-                        $data['button']['sub_button'][] = [
+                        $data[$key]['button']['sub_button'][] = [
                             'name'=>$v['menu_name'],
                             'type'=>$type,
                             'url' =>$v['response_url']
@@ -547,12 +547,12 @@ class WechatController extends Controller{
                         $type='location_select';
                         break;
                 }
-                $data['button']['name'] = $value['menu_name'];
-                $data['button']['type'] = $type;
+                $data[$key]['button']['name'] = $value['menu_name'];
+                $data[$key]['button']['type'] = $type;
                 if($value['event_type'] == 1){
-                    $data['button']['url']= $value['response_url'];
+                    $data[$key]['button']['url']= $value['response_url'];
                 }else{
-                    $data['button']['key']= $value['response_keyword'];
+                    $data[$key]['button']['key']= $value['response_keyword'];
                 }
             }
         }
