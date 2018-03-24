14a899c12f48e92b5fbbbe32737318b5a9bb5a0a
kbzz <463661663@qq.com>
2018-03-24 19:22:41 +0800

    1

--- app/Services/Wechat/WechatApi.php
+++ app/Services/Wechat/WechatApi.php
@@ -66,7 +66,6 @@ class WechatApi{
     public function create_menu($authorizer_access_token,$menu_data){
         $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$authorizer_access_token;
         $data = json_encode($menu_data, JSON_UNESCAPED_UNICODE);
-        dump($data);
         $re = \HttpCurl::doPost($url,$data);
         return $re;
     }
