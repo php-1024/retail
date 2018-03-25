2af7aa87ba954f79fc836f67fdfe8cb2559840cd
kbzz <463661663@qq.com>
2018-03-25 11:24:31 +0800

    1

--- app/Http/Controllers/Catering/UserController.php
+++ app/Http/Controllers/Catering/UserController.php
@@ -18,8 +18,6 @@ class UserController extends Controller{
     //粉丝标签管理
     public function user_tag(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dump($admin_data);
-
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
@@ -71,6 +69,8 @@ class UserController extends Controller{
     }
     //编辑会员标签ajax显示页面
     public function label_edit(Request $request){
+        $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
+        dump($admin_data);
         $id = $request->id; //会员标签id
         $oneLabel = Label::getOneLabel([['id',$id]]);
         return view('Catering/User/label_edit',['oneLabel'=>$oneLabel]);
