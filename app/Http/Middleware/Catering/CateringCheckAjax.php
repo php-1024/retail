347d82fd9b6d2b24f93ac0936259745a3038892c
kbzz <463661663@qq.com>
2018-03-25 13:18:01 +0800

    1

--- app/Http/Middleware/Catering/CateringCheckAjax.php
+++ app/Http/Middleware/Catering/CateringCheckAjax.php
@@ -169,6 +169,9 @@ class CateringCheckAjax
             case "catering/ajax/subordinate_lock":          //添加下级人员快速授权
             case "catering/ajax/subordinate_authorize":     //添加下级人员用户授权显示页面
             case "catering/ajax/subordinate_delete":        //添加下级人员用户授权显示页面
+            case "catering/ajax/label_add":                 //添加会员标签显示页面
+            case "catering/ajax/label_edit":                //编辑会员标签显示页面
+            case "catering/ajax/label_delete":              //删除会员标签显示页面
             case "catering/ajax/label_wechat":              //同步粉丝标签
             case "catering/ajax/store_label_add_check":     //粉丝会员标签改变显示页面
             case "catering/ajax/user_list_edit":            //会员列表编辑显示页面
