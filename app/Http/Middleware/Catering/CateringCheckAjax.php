912cfc26a922b40cf765d415554600df1b7afbc0
kbzz <463661663@qq.com>
2018-03-25 13:39:44 +0800

    1

--- app/Http/Middleware/Catering/CateringCheckAjax.php
+++ app/Http/Middleware/Catering/CateringCheckAjax.php
@@ -157,8 +157,7 @@ class CateringCheckAjax
             case "api/ajax/auto_reply_delete_check"://检测是否登陆 权限 安全密码--删除关键字
             case "api/ajax/defined_menu_delete_check"://检测是否登陆 权限 安全密码--删除自定义菜单
             case "api/ajax/wechat_menu_add_check"://检测是否登陆 权限 安全密码--一键同步到微信菜单
-            case "catering/user/label_wechat_check"://检测是否登陆 权限 安全密码--同步粉丝标签
-                dd(1);
+            case "catering/ajax/label_wechat_check"://检测是否登陆 权限 安全密码--同步粉丝标签
                 $re = $this->checkLoginAndRuleAndSafe($request);
                 return self::format_response($re,$next);
                 break;
