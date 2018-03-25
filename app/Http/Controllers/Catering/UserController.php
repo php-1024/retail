13a43c911d726f7a648e5766f6aaa03d3c9fe6c8
kbzz <463661663@qq.com>
2018-03-25 12:01:57 +0800

    1

--- app/Http/Controllers/Catering/UserController.php
+++ app/Http/Controllers/Catering/UserController.php
@@ -56,6 +56,7 @@ class UserController extends Controller{
 
             $auth_info = \Wechat::refresh_authorization_info('6');//刷新并获取授权令牌
             $re = \Wechat::create_fans_tag($auth_info['authorizer_access_token'],$label_name);
+            $re = json_decode($re,true);
             if($re['name'] == $label_name) {
                 if ($admin_data['is_super'] != 2) {
                     OperationLog::addOperationLog('4', $admin_data['organization_id'], $admin_data['id'], $route_name, '创建会员标签成功：' . $label_name);//保存操作记录
