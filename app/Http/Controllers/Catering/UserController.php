9e5379bdbe70046baa75af1af6a787d0a6322fc1
kbzz <463661663@qq.com>
2018-03-25 13:24:37 +0800

    1

--- app/Http/Controllers/Catering/UserController.php
+++ app/Http/Controllers/Catering/UserController.php
@@ -48,8 +48,8 @@ class UserController extends Controller{
         try {
             $auth_info = \Wechat::refresh_authorization_info($fansmanage_id);//刷新并获取授权令牌
             $re = \Wechat::create_fans_tag($auth_info['authorizer_access_token'],$label_name);
-            dd($re);
             $re = json_decode($re,true);
+            dd($re);
             $dataLabel = [
                 'fansmanage_id'=>'6',
                 'store_id'=>0,
