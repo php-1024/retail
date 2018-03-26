d173d446a1f506944e3f029e8858c14a42736767
kbzz <463661663@qq.com>
2018-03-26 17:07:15 +0800

    1

--- resources/views/Fansmanage/Message/auto_reply.blade.php
+++ resources/views/Fansmanage/Message/auto_reply.blade.php
@@ -3,25 +3,25 @@
 <head>
     <meta charset="utf-8" />
     <title>零壹云管理平台 | 总分店管理系统</title>
-    <link rel="stylesheet" href="{{asset('public/Catering')}}/js/jPlayer/jplayer.flat.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/bootstrap.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/animate.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/font-awesome.min.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/simple-line-icons.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/font.css" type="text/css" />
-    <link rel="stylesheet" href="{{asset('public/Catering')}}/css/app.css" type="text/css" />
-    <link href="{{asset('public/Catering')}}/sweetalert/sweetalert.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('public/Fansmanage')}}/js/jPlayer/jplayer.flat.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Fansmanage')}}/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Fansmanage')}}/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Fansmanage')}}/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Fansmanage')}}/css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Fansmanage')}}/css/font.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Fansmanage')}}/css/app.css" type="text/css" />
+    <link href="{{asset('public/Fansmanage')}}/sweetalert/sweetalert.css" rel="stylesheet" />
 
     <!--[if lt IE 9]>
-    <script src="{{asset('public/Catering')}}/js/ie/html5shiv.js"></script>
-    <script src="{{asset('public/Catering')}}/js/ie/respond.min.js"></script>
-    <script src="{{asset('public/Catering')}}/js/ie/excanvas.js"></script>
+    <script src="{{asset('public/Fansmanage')}}/js/ie/html5shiv.js"></script>
+    <script src="{{asset('public/Fansmanage')}}/js/ie/respond.min.js"></script>
+    <script src="{{asset('public/Fansmanage')}}/js/ie/excanvas.js"></script>
     <![endif]-->
 </head>
 <body class="">
 <section class="vbox">
     <header class="bg-white-only header header-md navbar navbar-fixed-top-xs">
-        @include('Catering/Public/Header')
+        @include('Fansmanage/Public/Header')
     </header>
     <section>
         <section class="hbox stretch">
@@ -30,7 +30,7 @@
             <aside class="bg-black dk aside hidden-print" id="nav">
                 <section class="vbox">
                     <section class="w-f-md scrollable">
-                        @include('Catering/Public/Nav')
+                        @include('Fansmanage/Public/Nav')
                     </section>
                 </section>
             </aside>
@@ -42,13 +42,13 @@
                         <section class="vbox animated fadeInUp">
                             <section class="scrollable hover">
                                 <div class="list-group no-radius no-border no-bg m-t-n-xxs m-b-none auto">
-                                    <a href="{{url('api/catering/auto_reply')}}" class="list-group-item active">
+                                    <a href="{{url('fansmanage/message/auto_reply')}}" class="list-group-item active">
                                         关键词自动回复
                                     </a>
-                                    <a href="{{url('api/catering/subscribe_reply')}}" class="list-group-item">
+                                    <a href="{{url('fansmanage/message/subscribe_reply')}}" class="list-group-item">
                                         关注后自动回复
                                     </a>
-                                    <a href="{{url('api/catering/default_reply')}}" class="list-group-item ">
+                                    <a href="{{url('fansmanage/message/default_reply')}}" class="list-group-item ">
                                         默认回复
                                     </a>
                                 </div>
@@ -63,12 +63,12 @@
                                 <div class="row row-sm">
                                     <button class="btn btn-success" id="addKeyWord" onclick="return getAddForm();">新建关键字 &nbsp;&nbsp;<i class="fa fa-plus"></i></button>
                                     <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
-                                    <input type="hidden" name="_token" id="auto_reply_add_url" value="{{ url('api/ajax/auto_reply_add') }}">
-                                    <input type="hidden" name="_token" id="auto_reply_edit_text_url" value="{{ url('api/ajax/auto_reply_edit_text') }}">
-                                    <input type="hidden" name="_token" id="auto_reply_edit_image_url" value="{{ url('api/ajax/auto_reply_edit_image') }}">
-                                    <input type="hidden" name="_token" id="auto_reply_edit_article_url" value="{{ url('api/ajax/auto_reply_edit_article') }}">
-                                    <input type="hidden" name="_token" id="auto_reply_edit_url" value="{{ url('api/ajax/auto_reply_edit') }}">
-                                    <input type="hidden" name="_token" id="auto_reply_delete_url" value="{{ url('api/ajax/auto_reply_delete_confirm') }}">
+                                    <input type="hidden" name="_token" id="auto_reply_add_url" value="{{ url('fansmanage/ajax/auto_reply_add') }}">
+                                    <input type="hidden" name="_token" id="auto_reply_edit_text_url" value="{{ url('fansmanage/ajax/auto_reply_edit_text') }}">
+                                    <input type="hidden" name="_token" id="auto_reply_edit_image_url" value="{{ url('fansmanage/ajax/auto_reply_edit_image') }}">
+                                    <input type="hidden" name="_token" id="auto_reply_edit_article_url" value="{{ url('fansmanage/ajax/auto_reply_edit_article') }}">
+                                    <input type="hidden" name="_token" id="auto_reply_edit_url" value="{{ url('fansmanage/ajax/auto_reply_edit') }}">
+                                    <input type="hidden" name="_token" id="auto_reply_delete_url" value="{{ url('fansmanage/ajax/auto_reply_delete_confirm') }}">
                                     <div class="line line-dashed b-b line-lg pull-in"></div>
                                 </div>
                                 <div class="table-responsive">
@@ -149,47 +149,17 @@
 </section>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"></div>
 
-
-<div class="modal fade" id="myModal6" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <form class="form-horizontal tasi-form" method="get">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">确认删除关键字</h4>
-                </div>
-                <div class="modal-body">
-                    <form class="form-horizontal" method="get">
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">安全密码</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="" placeholder="安全密码" class="form-control">
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
-                    <button class="btn btn-success" type="button" id="save_btn">确定</button>
-                </div>
-            </div>
-        </div>
-    </form>
-</div>
-
-
-<script src="{{asset('public/Catering')}}/js/jquery.min.js"></script>
+<script src="{{asset('public/Fansmanage')}}/js/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="{{asset('public/Catering')}}/js/bootstrap.js"></script>
+<script src="{{asset('public/Fansmanage')}}/js/bootstrap.js"></script>
 <!-- App -->
-<script src="{{asset('public/Catering')}}/js/app.js"></script>
-<script src="{{asset('public/Catering')}}/js/slimscroll/jquery.slimscroll.min.js"></script>
-<script src="{{asset('public/Catering')}}/js/app.plugin.js"></script>
-<script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/jquery.jplayer.min.js"></script>
-<script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/add-on/jplayer.playlist.min.js"></script>
-<script type="text/javascript" src="{{asset('public/Catering')}}/js/jPlayer/demo.js"></script>
-<script type="text/javascript" src="{{asset('public/Catering')}}/sweetalert/sweetalert.min.js"></script>
+<script src="{{asset('public/Fansmanage')}}/js/app.js"></script>
+<script src="{{asset('public/Fansmanage')}}/js/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Fansmanage')}}/js/app.plugin.js"></script>
+<script type="text/javascript" src="{{asset('public/Fansmanage')}}/js/jPlayer/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="{{asset('public/Fansmanage')}}/js/jPlayer/add-on/jplayer.playlist.min.js"></script>
+<script type="text/javascript" src="{{asset('public/Fansmanage')}}/js/jPlayer/demo.js"></script>
+<script type="text/javascript" src="{{asset('public/Fansmanage')}}/sweetalert/sweetalert.min.js"></script>
 
 
 <script type="text/javascript">
