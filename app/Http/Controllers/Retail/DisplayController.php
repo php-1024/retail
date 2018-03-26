edc08d296ef20ebbaa9be416a2dca4f4ab10c4dd
mail@54zm.com <mail@54zm.com>
2018-03-26 11:34:36 +0800

    '1'

--- app/Http/Controllers/Retail/DisplayController.php
+++ app/Http/Controllers/Retail/DisplayController.php
@@ -37,6 +37,7 @@ class DisplayController extends Controller
         $fansmanage_id = Organization::getPluck(['id'=>$admin_data['organization_id']],'parent_id');
         $fans = FansmanageUser::getCount(['store_id'=>$admin_data['organization_id'],'fansmanage_id'=>$fansmanage_id]);//查询当前店铺粉丝数量
         $order = RetailOrder::getList(['retail_id'=>$admin_data['organization_id'],'fansmanage_id'=>$fansmanage_id],'0','id','DESC');
+        $order_spot = RetailOrder::getList(['retail_id'=>$admin_data['organization_id'],'fansmanage_id'=>$fansmanage_id,'order_type'=>'1'],'0','id','DESC')->count();
         $goods = RetailGoods::getList(['retail_id'=>$admin_data['organization_id'],'fansmanage_id'=>$fansmanage_id],'0','id','DESC')->count();
         $operating_receipt = 0.00;//营业收入
         foreach ($order as $key=>$val){
@@ -46,6 +47,7 @@ class DisplayController extends Controller
             'fans' => $fans,
             'operating_receipt' => $operating_receipt,
             'goods' => $goods,
+            'order_spot' => $order_spot,
         ];
         dump($statistics);
         $login_log_list = LoginLog::getList($where,10,'created_at','DESC');
