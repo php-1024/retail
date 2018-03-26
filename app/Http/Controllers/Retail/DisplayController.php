defae1dc5c6748f40b4b59b2b3e970f99fdc55f2
mail@54zm.com <mail@54zm.com>
2018-03-26 10:35:45 +0800

    '1'

--- app/Http/Controllers/Retail/DisplayController.php
+++ app/Http/Controllers/Retail/DisplayController.php
@@ -21,6 +21,7 @@ class DisplayController extends Controller
     //首页
     public function display(Request $request)
     {
+        dump($request);
         $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');            //中间件产生的菜单数据参数
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的子菜单数据参数
