63a7d637f4d86bf60ee3eef197b581a610dd88eb
kbzz <463661663@qq.com>
2018-03-27 14:38:49 +0800

    1

--- app/Http/Controllers/Agent/FansmanageController.php
+++ app/Http/Controllers/Agent/FansmanageController.php
@@ -97,6 +97,7 @@ class FansmanageController extends Controller{
         $route_name = $request->path();//获取当前的页面路由
         $organization_id = $request->input('organization_id');//服务商id
         $oneFansmanage = Organization::getOneFansmanage([['id',$organization_id]]);
+        dump($oneFansmanage);
         $list = Program::getPaginage([['is_asset','1']],15,'id');
         foreach ($list as $key=>$value) {
             $re = OrganizationAssets::getOne([['organization_id', $organization_id], ['program_id',$value['id']]]);
