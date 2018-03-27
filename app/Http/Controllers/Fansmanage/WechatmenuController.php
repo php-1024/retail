2c2fa1f87d68a3621b7643a8910447d1960319b4
kbzz <463661663@qq.com>
2018-03-27 19:46:26 +0800

    1

--- app/Http/Controllers/Fansmanage/WechatmenuController.php
+++ app/Http/Controllers/Fansmanage/WechatmenuController.php
@@ -546,10 +546,10 @@ class WechatmenuController extends Controller{
 
     public function conditional_menu_get(Request $request){
         $admin_data = $request->get('admin_data');//中间件产生的管理员数据参数
-        dd(1);
         $tag_id = $request->label_id;//会员标签id
         //获取菜单列表
         $list = WechatConditionalMenu::getList([['organization_id',$admin_data['organization_id'],['tag_id',$tag_id]],['parent_id','0']],0,'id','asc');
+        dd($list);
         $son_menu = [];
         foreach ($list as $key=>$val){
             $sm = WechatConditionalMenu::getList([['organization_id',$admin_data['organization_id'],['tag_id',$tag_id]],['parent_id',$val->id]],0,'id','asc');
