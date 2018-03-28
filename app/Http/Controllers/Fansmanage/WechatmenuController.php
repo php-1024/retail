3603c6f51c24ce402eb63dd106959c353a54ec57
kbzz <463661663@qq.com>
2018-03-28 10:30:47 +0800

    1

--- app/Http/Controllers/Fansmanage/WechatmenuController.php
+++ app/Http/Controllers/Fansmanage/WechatmenuController.php
@@ -495,6 +495,7 @@ class WechatmenuController extends Controller{
         $response_type = $request->get('response_type'); //获取响应类型
         $organization_id = $admin_data['organization_id'];  //组织ID
         $tag_id = $admin_data['label_id'];  //会员标签id
+        dd($tag_id);
         $authorization = WechatAuthorization::getOne([['organization_id',$admin_data['organization_id']]]); //获取授权APPID
         $menu_name = $request->get('menu_name');                //获取菜单名称
         $parent_id = $request->get('parent_id');                //获取上级菜单ID
