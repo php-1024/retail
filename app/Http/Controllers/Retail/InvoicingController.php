40ba4373b139e86432b558bc1ecf6c3c21a8151f
mail@54zm.com <mail@54zm.com>
2018-03-28 17:17:00 +0800

    '1'

--- app/Http/Controllers/Retail/InvoicingController.php
+++ app/Http/Controllers/Retail/InvoicingController.php
@@ -25,7 +25,7 @@ class InvoicingController extends Controller
     //零售进销存开单--供应商到货开单
     public function purchase_goods(Request $request)
     {
-        dd($request);
+        dump(Session::all());
         $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
         $menu_data = $request->get('menu_data');            //中间件产生的菜单数据参数
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的子菜单数据参数
