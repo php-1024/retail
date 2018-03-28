e3cdf695cf78df47f5558d04f64026bf94a086c3
kbzz <463661663@qq.com>
2018-03-28 18:29:28 +0800

    1

--- app/Http/Controllers/Fansmanage/WechatmenuController.php
+++ app/Http/Controllers/Fansmanage/WechatmenuController.php
@@ -324,13 +324,13 @@ class WechatmenuController extends Controller{
 
 
 
-        $data = [
-            'openid_list'=>['oyhbt1PNT38bzuM5rvwF71ePtUFI'],
-            'tagid' =>'35'
-        ];
-        $auth_info = \Wechat::refresh_authorization_info($admin_data['organization_id']);//刷新并获取授权令牌
-        $re = \Wechat::add_fans_tag_label($auth_info['authorizer_access_token'],'');
-        dump(json_encode($re));
+//        $data = [
+//            'openid_list'=>['oyhbt1PNT38bzuM5rvwF71ePtUFI'],
+//            'tagid' =>'35'
+//        ];
+//        $auth_info = \Wechat::refresh_authorization_info($admin_data['organization_id']);//刷新并获取授权令牌
+//        $re = \Wechat::add_fans_tag_label($auth_info['authorizer_access_token'],'');
+//        dump(json_encode($re));
 
         return view('Fansmanage/Wechatmenu/conditional_menu',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
    }
