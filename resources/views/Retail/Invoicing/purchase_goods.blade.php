71cb5e80df5ed186681210ad3d42ae0f1d4c6602
mail@54zm.com <mail@54zm.com>
2018-03-28 16:46:09 +0800

    '1'

--- resources/views/Retail/Invoicing/purchase_goods.blade.php
+++ resources/views/Retail/Invoicing/purchase_goods.blade.php
@@ -124,94 +124,7 @@
                             <div class="line line-border b-b pull-in"></div>
                             <div class="tab-pane">
                                 <div class="col-lg-7">
-                                    <section class="panel panel-default">
-                                        <header class="panel-heading font-bold">
-                                            选择商品
-                                        </header>
-                                        <table class="table table-striped table-bordered ">
-                                            <thead>
-                                            <tr>
-                                                <th>商品ID</th>
-                                                <th>商品标题</th>
-                                                <th>商品价格</th>
-                                                <th>操作</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            <tr id="1">
-                                                <td class="id">1</td>
-                                                <td class="name">性感女人香水</td>
-                                                <td class="price">88.00</td>
-                                                <td>
-                                                    <button onclick="goodsSelect(1);" class="btn btn-info btn-xs" type="button">
-                                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;选择
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr id="2">
-                                                <td class="id">2</td>
-                                                <td class="name">性感女人香水</td>
-                                                <td class="price">88.00</td>
-                                                <td>
-                                                    <button onclick="goodsSelect(2);" class="btn btn-info btn-xs" type="button">
-                                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;选择
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr id="3">
-                                                <td class="id">3</td>
-                                                <td class="name">性感女人香水</td>
-                                                <td class="price">88.00</td>
-                                                <td>
-                                                    <button onclick="goodsSelect(3);" class="btn btn-info btn-xs" type="button">
-                                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;选择
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr id="4">
-                                                <td class="id">4</td>
-                                                <td class="name">性感女人香水</td>
-                                                <td class="price">88.00</td>
-                                                <td>
-                                                    <button onclick="goodsSelect(4);" class="btn btn-info btn-xs" type="button">
-                                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;选择
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr id="5">
-                                                <td class="id">5</td>
-                                                <td class="name">性感女人香水</td>
-                                                <td class="price">88.00</td>
-                                                <td>
-                                                    <button onclick="goodsSelect(5);" class="btn btn-info btn-xs" type="button">
-                                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;选择
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr id="6">
-                                                <td class="id">6</td>
-                                                <td class="name">性感女人香水</td>
-                                                <td class="price">88.00</td>
-                                                <td>
-                                                    <button onclick="goodsSelect(6);" class="btn btn-info btn-xs" type="button">
-                                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;选择
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr id="7">
-                                                <td class="id">7</td>
-                                                <td class="name">性感女人香水</td>
-                                                <td class="price">88.00</td>
-                                                <td>
-                                                    <button onclick="goodsSelect(7);" class="btn btn-info btn-xs" type="button">
-                                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;选择
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
-                                        <div style="clear: both;"></div>
-                                    </section>
+                                    @include('Retail/Invoicing/goods_list')
                                 </div>
 
                                 <div class="col-lg-5">
@@ -255,6 +168,7 @@
                                 </div>
                                 <div style="clear: both;"></div>
                             </div>
+
                             <footer class="panel-footer">
                                 <div class="row">
 
