338fc834320cd5c2d80d8e1467f12f693f35718e
kbzz <463661663@qq.com>
2018-03-28 16:36:19 +0800

    1

--- app/Http/Controllers/Fansmanage/WechatmenuController.php
+++ app/Http/Controllers/Fansmanage/WechatmenuController.php
@@ -583,8 +583,9 @@ class WechatmenuController extends Controller{
         $parent_id = $request->get('parent_id');                //获取上级菜单ID
 
         $data =WechatConditionalMenu::getOne([['id',$menu_id]]);//获取菜单的信息
-        dd($data);
         $ziparent_tree = $data['parent_tree'].$data['id'].',';
+        dd($ziparent_tree);
+
         $re = WechatConditionalMenu::checkRowExists([['organization_id',$admin_data['organization_id'],['tag_id',$data['tag_id']],['parent_tree',$ziparent_tree]]]);
         if($re){
             return response()->json(['data' => '菜单下面还有别的子菜单，不能更改', 'status' => '0']);
