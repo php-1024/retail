a71864d8308a4d316fe63eacc8a7e5e8745efffd
mail@54zm.com <mail@54zm.com>
2018-03-28 19:05:34 +0800

    '1'

--- app/Http/Middleware/Retail/RetailCheckAjax.php
+++ app/Http/Middleware/Retail/RetailCheckAjax.php
@@ -534,6 +534,7 @@ class RetailCheckAjax
     //检测搜索商品的数据
     public function checkSearch($request)
     {
+        dd($request);
         if (empty($request->input('category_id')) || empty($request->input('goods_name'))) {
             return self::res(0, response()->json(['data' => '请选择商品分类，或者输入商品名称进行搜索!', 'status' => '0']));
         }
