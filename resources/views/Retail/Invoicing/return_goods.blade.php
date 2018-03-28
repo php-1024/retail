669d87492e744e5519de2052c2edbbc9eb1f5bdc
mail@54zm.com <mail@54zm.com>
2018-03-28 13:51:24 +0800

    '1'

--- resources/views/Retail/Invoicing/return_goods.blade.php
+++ resources/views/Retail/Invoicing/return_goods.blade.php
@@ -55,41 +55,6 @@
                                 <form class="form-horizontal" method="get">
 
 
-                                    <label class="col-sm-1 control-label">供应商</label>
-
-                                    <div class="col-sm-1">
-                                        <input class="input-sm form-control" size="16" type="text" value=""
-                                               placeholder="供应商id">
-                                    </div>
-
-
-                                    <div class="col-sm-2">
-                                        <input class="input-sm form-control" size="16" type="text" value=""
-                                               placeholder="公司名称或联系人姓名">
-                                    </div>
-
-                                    <div class="col-sm-2">
-                                        <input class="input-sm form-control" size="16" type="text" value=""
-                                               placeholder="联系人手机">
-                                    </div>
-
-
-                                    <div class="col-sm-1">
-
-                                        <button type="button" class="btn btn-s-md btn-info"><i class="fa fa-search"></i>&nbsp;&nbsp;搜索
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-
-
-                            <div style="clear:both"></div>
-                            <div class="line line-border b-b pull-in"></div>
-
-                            <div class="col-sm-12">
-                                <form class="form-horizontal" method="get">
-
-
                                     <label class="col-sm-1 control-label">商&nbsp; &nbsp; &nbsp;品</label>
 
 
