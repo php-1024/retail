7489191f5d155635a0925054cb8e8a70828b3cc3
ismzxw <mail@54zm.com>
2018-03-29 19:44:44 +0800

    '1'

--- app/Http/Middleware/Retail/RetailCheckAjax.php
+++ app/Http/Middleware/Retail/RetailCheckAjax.php
@@ -35,6 +35,7 @@ class RetailCheckAjax
             case "retail/ajax/user_list_edit":          //会员列表编辑显示页面
             case "retail/ajax/user_list_lock":          //会员列表冻结显示页面
             case "retail/ajax/user_list_wallet":        //会员列表粉丝钱包显示页面
+            case "retail/ajax/search_company":          //供应商搜索处理
                 $re = $this->checkLoginAndRule($request);
                 return self::format_response($re, $next);
                 break;
