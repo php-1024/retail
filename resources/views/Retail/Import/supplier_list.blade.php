e98f50037dac9a94f1bc12bc907d56f6b261d2a1
mail@54zm.com <mail@54zm.com>
2018-03-29 11:45:16 +0800

    '1'

--- resources/views/Retail/Import/supplier_list.blade.php
+++ resources/views/Retail/Import/supplier_list.blade.php
@@ -29,6 +29,7 @@
                 <section class="vbox">
                     <section class="scrollable padder">
                         <div class="m-b-md">
+                            <h3 class="m-b-none">供应商列表</h3>
                             <div class="col-sm-12">
                                 <button type="button" class="btn btn-s-md btn-success" onclick="window.location='supplier_list'">供应商列表</button>
                                 <button type="button" class="btn btn-s-md btn" onclick="window.location='supplier_add'">&nbsp;&nbsp;添加供应商</button>
