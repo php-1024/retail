dfab744eb6fdce38da834694930e8951d127717d
ismzxw <mail@54zm.com>
2018-03-29 22:42:46 +0800

    '1'

--- app/Http/Middleware/Retail/RetailCheckAjax.php
+++ app/Http/Middleware/Retail/RetailCheckAjax.php
@@ -644,7 +644,7 @@ class RetailCheckAjax
     //检测供应商进货开单的数据
     public function checkPurchaseGoods($request)
     {
-        dd($request);
+        dd($request->orders);
         if (empty($request->orders['company'])) {
             return self::res(0, response()->json(['data' => '请选择供应商!', 'status' => '0']));
         }
