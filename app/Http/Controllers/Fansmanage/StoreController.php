7d0cfd4257ac3982aa2dfedbd8a334e3872a2838
kbzz <463661663@qq.com>
2018-03-29 18:11:00 +0800

    1

--- app/Http/Controllers/Fansmanage/StoreController.php
+++ app/Http/Controllers/Fansmanage/StoreController.php
@@ -120,6 +120,7 @@ class StoreController extends Controller{
             }
             DB::commit();//提交事务
         }catch (\Exception $e) {
+            dd($e);
             DB::rollBack();//事件回滚
             return response()->json(['data' => '创建店铺失败，请稍后再试！', 'status' => '0']);
         }
