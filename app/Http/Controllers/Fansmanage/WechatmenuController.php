e99466470c64f38618824ae76d91bb81a9ec756a
kbzz <463661663@qq.com>
2018-03-29 13:49:55 +0800

    1

--- app/Http/Controllers/Fansmanage/WechatmenuController.php
+++ app/Http/Controllers/Fansmanage/WechatmenuController.php
@@ -325,13 +325,13 @@ class WechatmenuController extends Controller{
 
 
         $data = [
-            'openid_list'=>['oyhbt1PNT38bzuM5rvwF71ePtUFI'],
+            'openid_list'=>['oyhbt1PNT38bzuM5rvwF71ePtUFI','oyhbt1C__b9gvm_wg9bf5aFika48'],
             'tagid' =>'117'
         ];
         $auth_info = \Wechat::refresh_authorization_info($admin_data['organization_id']);//刷新并获取授权令牌
-////        \Wechat::get_fans_info($auth_info['authorizer_access_token'],'oyhbt1C__b9gvm_wg9bf5aFika48');
-        $re = \Wechat::add_fans_tag_label($auth_info['authorizer_access_token'],$data);
-        dump($re);
+        \Wechat::get_fans_info($auth_info['authorizer_access_token'],'oyhbt1PNT38bzuM5rvwF71ePtUFI');
+//        $re = \Wechat::add_fans_tag_label($auth_info['authorizer_access_token'],$data);
+//        dump($re);
         return view('Fansmanage/Wechatmenu/conditional_menu',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
    }
 
