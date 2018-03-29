650cea0a8ac664917c76a5756ae0a7e5ce45f311
mail@54zm.com <mail@54zm.com>
2018-03-29 11:35:05 +0800

    '1'

--- app/Http/Controllers/Retail/ImportController.php
+++ app/Http/Controllers/Retail/ImportController.php
@@ -34,6 +34,20 @@ class ImportController extends Controller
         $category = RetailCategory::getList($where, '0', 'displayorder', 'DESC');   //栏目
         return  view('Retail/Import/supplier_add',['category'=>$category,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
+
+    //零售进销存管理--供应商列表
+    public function supplier_list(Request $request)
+    {
+        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
+        $menu_data = $request->get('menu_data');            //中间件产生的菜单数据参数
+        $son_menu_data = $request->get('son_menu_data');    //中间件产生的子菜单数据参数
+        $route_name = $request->path();                         //获取当前的页面路由
+        $where = [
+            'retail_id' => $admin_data['organization_id'],
+        ];
+        $category = RetailCategory::getList($where, '0', 'displayorder', 'DESC');   //栏目
+        return  view('Retail/Import/supplier_list',['category'=>$category,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+    }
 }
 
 ?>
