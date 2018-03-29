96d8f8f3e47d3957d59eb6841505f29e254d08a2
kbzz <463661663@qq.com>
2018-03-29 17:56:01 +0800

    1

--- app/Http/Controllers/Fansmanage/StoreController.php
+++ app/Http/Controllers/Fansmanage/StoreController.php
@@ -20,6 +20,9 @@ class StoreController extends Controller{
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
+        $randStr = str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890');
+        $rand = substr($randStr,0,6);
+        dump($rand);
         return view('Fansmanage/Store/store_create',['admin_data'=>$admin_data,'route_name'=>$route_name,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data]);
     }
 
