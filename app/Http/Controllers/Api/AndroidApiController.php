a94b89fa557f9aebbf9b88340352ec5ee96373b3
kbzz <463661663@qq.com>
2018-03-30 20:36:39 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -18,9 +18,7 @@ class AndroidApiController extends Controller{
         $key = config("app.retail_encrypt_key");//获取加密盐
         $encrypted = md5($password);//加密密码第一重
         $encryptPwd = md5("lingyikeji".$encrypted.$key);//加密密码第二重
-        dd($encryptPwd);
-//        f964c24293ce996ca3c1532714340262
-        if($encryptPwd!=$data['passwrd']){
+        if($encryptPwd != $data['password']){
             return response()->json(['data' => '密码错误', 'status' => '0']);
         }
         return response()->json(['status' => '1','msg'=>'登陆成功','account_id'=>$data['id'],'account'=>$data['account'],'organization_id'=>$data['organization_id'],'uuid'=>$data['uuid']]);
