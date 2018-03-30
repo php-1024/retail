a6ed867cf46c59538a5875ae57acd7633d5de0e5
kbzz <463661663@qq.com>
2018-03-30 11:16:34 +0800

    1

--- app/Http/Controllers/Fansmanage/UserController.php
+++ app/Http/Controllers/Fansmanage/UserController.php
@@ -260,6 +260,7 @@ class UserController extends Controller{
                     //减少原粉丝标签的人数
                     $label_number = Label::getPluck([['id',$oneData->label_id]],'label_number')->first();//获取原粉丝标签的人数
                     $number = $label_number-1;
+                    dd($number);
                     Label::editLabel([['id',$oneData->label_id]],['label_number'=>$number]);//修改粉丝标签的人数
                 }
                  if($label_id != 0){ //选择无标签的时候 不执行
