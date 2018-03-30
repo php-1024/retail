7f8f5e1c08c0bcda18201680490128577f560ca4
kbzz <463661663@qq.com>
2018-03-30 11:17:03 +0800

    1

--- app/Http/Controllers/Fansmanage/UserController.php
+++ app/Http/Controllers/Fansmanage/UserController.php
@@ -259,8 +259,8 @@ class UserController extends Controller{
                 if($oneData->label_id != 0){ //当粉丝标签关联表里标签id为0时 不执行
                     //减少原粉丝标签的人数
                     $label_number = Label::getPluck([['id',$oneData->label_id]],'label_number')->first();//获取原粉丝标签的人数
+                    dd($label_number);
                     $number = $label_number-1;
-                    dd($number);
                     Label::editLabel([['id',$oneData->label_id]],['label_number'=>$number]);//修改粉丝标签的人数
                 }
                  if($label_id != 0){ //选择无标签的时候 不执行
