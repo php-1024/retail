fde8911170cc48256fc66222dd585e62da48a54e
kbzz <463661663@qq.com>
2018-03-31 14:53:46 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -36,9 +36,15 @@ class AndroidApiController extends Controller{
 
         $organization_id = $request->organization_id;//店铺id
         $categorylist = RetailCategory::getList([['fansmanage_id',$organization_id]],'0','displayorder','asc');
-
         foreach($categorylist as $key=>$value){
-
+            switch ($key){
+                case "created_at":
+                case "updated_at":
+                case "deleted_at":
+                case "retail_id":
+                    unset($categorylist['$key']);
+                    break;
+            }
         }
         $data = ['status' => '1', 'msg' => '获取分类成功', 'data' => ['categorylist' => $categorylist]];
         return response()->json($data);
