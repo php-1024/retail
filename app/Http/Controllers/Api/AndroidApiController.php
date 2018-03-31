e99d104ff576a28eb36e05d8fc2457b78c8c6706
kbzz <463661663@qq.com>
2018-03-31 19:33:53 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -84,7 +84,7 @@ class AndroidApiController extends Controller{
         $order_price = 0;
         foreach($goodsdata as $key=>$value){
             foreach($value as $k=>$v){
-                $order_price += $v['price'];
+                echo $v['price'];exit;
             }
         }
         echo $order_price;exit;
