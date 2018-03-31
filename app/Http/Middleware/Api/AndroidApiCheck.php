5460afa4154d3d497e675e3cb72e890d6fbf7b2d
kbzz <463661663@qq.com>
2018-03-31 12:41:18 +0800

    1

--- app/Http/Middleware/Api/AndroidApiCheck.php
+++ app/Http/Middleware/Api/AndroidApiCheck.php
@@ -33,6 +33,7 @@ class AndroidApiCheck{
     public function checkTokenAndGoodsListData($request){
         $re = $this->checkToken($request);//判断是否登录
         if($re['status']=='0'){//检测是否登录
+            echo 1;exit;
             return $re['response'];
         }else{
             $re2 = $this->checkGoodsListData($re['response']);//检测是否具有权限
@@ -79,7 +80,6 @@ class AndroidApiCheck{
             return self::res(0, response()->json(['msg' => '当前时间戳不能为空', 'status' => '0', 'data' => '']));
         }
         if (empty($request->input('token'))) {
-            echo 1;exit;
             return self::res(0, response()->json(['msg' => 'token值不能为空', 'status' => '0', 'data' => '']));
         }
         $account_id = $request->account_id;//用户账号id
