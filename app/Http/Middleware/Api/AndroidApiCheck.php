1d885626d9bc601f73d236e81eff0e1cdf77fa96
kbzz <463661663@qq.com>
2018-03-31 15:34:12 +0800

    1

--- app/Http/Middleware/Api/AndroidApiCheck.php
+++ app/Http/Middleware/Api/AndroidApiCheck.php
@@ -83,7 +83,6 @@ class AndroidApiCheck{
         if (empty($request->input('token'))) {
             return self::res(0, response()->json(['msg' => 'token值不能为空', 'status' => '0', 'data' => '']));
         }
-        echo(microtime());exit;
 //        if(microtime()-$request->input('timestamp')>120000){
 //            return self::res(0, response()->json(['msg' => '访问超时', 'status' => '0', 'data' => '']));
 //        }
@@ -104,8 +103,8 @@ class AndroidApiCheck{
         }
 
         $store_token = base64_encode($store_token.$data['uuid']).'lingyi2018';//第一次加密
-        echo $store_token;exit;
         $store_token = md5($store_token);//第二次加密
+        echo $store_token;exit;
 
 //        if($store_token !=$token){
 //            return self::res(0, response()->json(['msg' => 'token值不正确,无权访问', 'status' => '0', 'data' => '']));
