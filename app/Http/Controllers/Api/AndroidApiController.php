b7ba76e263e45ebd4d313b1aaa149fd971bec31e
kbzz <463661663@qq.com>
2018-03-31 15:41:54 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -35,7 +35,7 @@ class AndroidApiController extends Controller{
     public function goodscategory(Request $request){
 
         $organization_id = $request->organization_id;//店铺id
-        $categorylist = RetailCategory::where([['fansmanage_id',$organization_id]])->orderBy('displayorder','asc')->orderBy('id','desc')->select('id,displayorder,category_name')->get();
+        $categorylist = RetailCategory::where([['fansmanage_id',$organization_id]])->orderBy('displayorder','asc')->orderBy('id','desc')->lists('id,displayorder,category_name')->get();
 //
 //        $model = new RetailCategory();
 //        if(!empty($limit)){
