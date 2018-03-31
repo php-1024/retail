23e4d46ea42fb962053d13c3c8c2a9561cccbe40
kbzz <463661663@qq.com>
2018-03-31 18:00:37 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -71,6 +71,10 @@ class AndroidApiController extends Controller{
         $user_id = $request->user_id;//用户id 散客为0
         $account_id = $request->account_id;//操作员id
         $goodsdata = json_encode($request->goodsdata);//商品数组
+        $re = RetailOrder::where([['fansmanage_id',$organization_id]])->count();
+        echo $re;
+//        if()
+        $account = $account + 1;
         $shuzi = '100000';
         $ordersn ='LS'.date("Ymd",time()).'_'.$organization_id.'_'.$shuzi;
         echo $ordersn;
