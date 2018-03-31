42b3cb81c74ee4275649f556c6358d6f7b90b3fe
kbzz <463661663@qq.com>
2018-03-31 19:03:59 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -61,7 +61,7 @@ class AndroidApiController extends Controller{
         $goodslist = RetailGoods::getList($where,'0','displayorder','asc',['id','name','category_id','details','price','stock']);
         foreach($goodslist as $key=>$value){
             $goodslist[$key]['category_name']=RetailCategory::getPluck([['id',$value['category_id']]],'name')->first();
-            $goodslist[$key]['thumb']=RetailGoodsThumb::where([['goods_id',$value['id']]])->get();
+            $goodslist[$key]['thumb']=RetailGoodsThumb::where([['goods_id',$value['id']]])->select('thumb')->get();
         }
         $data = ['status' => '1', 'msg' => '获取分类成功', 'data' => ['goodslist' => $goodslist]];
         return response()->json($data);
