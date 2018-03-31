837c9de38b12c7d736f277f2ee6e505b5cd589a8
mail@54zm.com <mail@54zm.com>
2018-03-31 10:21:48 +0800

    '1'

--- app/Http/Controllers/Retail/BillingController.php
+++ app/Http/Controllers/Retail/BillingController.php
@@ -207,17 +207,14 @@ class BillingController extends Controller
         $menu_data = $request->get('menu_data');            //中间件产生的菜单数据参数
         $son_menu_data = $request->get('son_menu_data');    //中间件产生的子菜单数据参数
         $route_name = $request->path();                         //获取当前的页面路由
-        $ordersn = $request->get('ordersn');                //订单编号
-        $fansmanage_id = Organization::getPluck(['id'=>$admin_data['organization_id']],'parent_id')->first();         //获取粉丝管理平台的组织id
-        $search_data = [
-            'ordersn' => $ordersn,
-        ];
+        $company_name = $request->get('company_name');         //获取供应商名称
+        $fansmanage_id = Organization::getPluck(['id'=>$admin_data['organization_id']],'parent_id');    //获取粉丝管理平台的组织id
         $where = [
-            'retail_id' => $admin_data['organization_id'],
             'fansmanage_id' => $fansmanage_id,
+            'retail_id' => $admin_data['organization_id'],
         ];
-        $list = RetailLossOrder::getPaginage($where,$search_data,'10','created_at','DESC'); //订单信息
-        return view('Retail/Billing/stock_list',['ordersn'=>$ordersn,'list'=>$list,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
+        $supplier = RetailSupplier::getPaginage($where,$company_name,'0', 'displayorder', 'DESC');   //供应商信息
+        return  view('Retail/Billing/stock_list',['supplier'=>$supplier,'company_name'=>$company_name,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
 
