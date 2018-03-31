d4a1c383c1afded7092fec4b440ec43b97158761
kbzz <463661663@qq.com>
2018-03-31 14:06:38 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -5,6 +5,7 @@
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Account;
+use App\Models\RetailCategory;
 use Illuminate\Http\Request;
 use Session;
 class AndroidApiController extends Controller{
@@ -34,16 +35,9 @@ class AndroidApiController extends Controller{
     public function goodscategory(Request $request){
 
         $organization_id = $request->organization_id;//店铺id
-        $account_id = $request->account_id;//用户账号id
-        echo 1;exit;
-//        $key = config("app.retail_encrypt_key");//获取加密盐
-//        $encrypted = md5($password);//加密密码第一重
-//        $encryptPwd = md5("lingyikeji".$encrypted.$key);//加密密码第二重
-//        if($encryptPwd != $data['password']){
-//            return response()->json(['msg' => '密码不正确', 'status' => '0']);
-//        }
-//        $data = ['status' => '1', 'msg' => '登陆成功', 'data' => ['account_id' => $data['id'], 'account' => $data['account'], 'organization_id' => $data['organization_id'], 'uuid' => $data['uuid']]];
-//        return response()->json($data);
+        $categorylist = RetailCategory::getList([['fansmanage_id',$organization_id]],'0','displayorder','asc');
+        $data = ['status' => '1', 'msg' => '获取分类成功', 'data' => ['categorylist' => $categorylist]];
+        return response()->json($data);
     }
 
 }
