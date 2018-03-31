60823d5a77ebd554b96e874ea2cb34bb1186d96a
kbzz <463661663@qq.com>
2018-03-31 15:07:34 +0800

    1

--- app/Http/Middleware/Api/AndroidApiCheck.php
+++ app/Http/Middleware/Api/AndroidApiCheck.php
@@ -101,8 +101,8 @@ class AndroidApiCheck{
         foreach($sort as $key=>$value){//拼接token
             $store_token .= $value;
         }
+        echo $store_token.$data['uuid'];exit;
         $store_token = base64_encode($store_token.$data['uuid']).'lingyi2018';//第一次加密
-        echo $store_token;exit;
         $store_token = md5($store_token);//第二次加密
         echo $store_token;exit;
 //        if($store_token !=$token){
