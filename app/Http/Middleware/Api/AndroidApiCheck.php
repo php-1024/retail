e48fe182b8ffaacc8502d769a5b48563e6cc0095
kbzz <463661663@qq.com>
2018-03-31 11:25:24 +0800

    1

--- app/Http/Middleware/Api/AndroidApiCheck.php
+++ app/Http/Middleware/Api/AndroidApiCheck.php
@@ -32,7 +32,7 @@ class AndroidApiCheck{
      */
     public function checkTokenAndGoodsListData($request){
         $re = $this->checkToken($request);//判断是否登录
-        dd(1);
+        echo 1;exit;
         if($re['status']=='0'){//检测是否登录
             return $re;
         }else{
