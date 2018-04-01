6981b3067e4b92b42f271856811e9f959408371d
kbzz <463661663@qq.com>
2018-04-01 11:27:20 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -94,7 +94,6 @@ class AndroidApiController extends Controller{
         $fansmanage_id = Organization::getPluck([['id',$organization_id]],'parent_id')->first();
         $num = RetailOrder::where([['retail_id',$organization_id],['ordersn','LIKE','%'.date("Ymd",time()).'%']])->count();//查询订单今天的数量
         $num += 1;
-        echo $num;exit;
         $sort = 100000 + $num;
         $ordersn ='LS'.date("Ymd",time()).'_'.$organization_id.'_'.$sort;
         $orderData = [
