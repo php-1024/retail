ac895f1634c4e6f06de3ba131169a8466a53fff6
kbzz <463661663@qq.com>
2018-04-02 16:29:51 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -278,12 +278,11 @@ class AndroidApiController extends Controller{
         $paytype = $request->paytype;//支付方式
         $power = RetailConfig::getPluck([['retail_id',$organization_id],['cfg_name','change_stock_role']],'cfg_value')->first();//查询是下单减库存/付款减库存
         $config = RetailConfig::getPluck([['retail_id',$organization_id],['cfg_name','allow_zero_stock']],'cfg_value')->first();//查询是否可零库存开单
-//        DB::beginTransaction();
-//        try{
+        DB::beginTransaction();
+        try{
             if($power == '1'){//说明付款减库存
                 $list = RetailOrderGoods::where([['order_id',$order_id]])->get();//查询订单快照里的商品信息
                 foreach($list as $key=>$value){
-                    print_r($value);
                     $goods = RetailGoods::getOne([['id',$value['goods_id']]]);//查询现在商品的信息
                     if($config != '1'){//如果不允许零库存开单
                         if($goods['stock'] - $value['num'] < 0){//库存小于0 打回
@@ -295,13 +294,12 @@ class AndroidApiController extends Controller{
                 }
             }
             RetailOrder::editRetailOrder([['id',$order_id]],['paytype'=>$paytype,'status'=>'1']);//修改订单状态
-//            DB::commit();//提交事务
-//        }catch (\Exception $e) {
-//            dd($e);
-//            DB::rollBack();//事件回滚
-//            return response()->json(['msg' => '现金付款失败', 'status' => '0', 'data' => '']);
-//        }
-//        return response()->json(['status' => '1', 'msg' => '现金付款成功', 'data' => ['order_id' => $order_id]]);
+            DB::commit();//提交事务
+        }catch (\Exception $e) {
+            DB::rollBack();//事件回滚
+            return response()->json(['msg' => '现金付款失败', 'status' => '0', 'data' => '']);
+        }
+        return response()->json(['status' => '1', 'msg' => '现金付款成功', 'data' => ['order_id' => $order_id]]);
     }
 
     /**
