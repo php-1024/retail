abcac2533c82df2dd8f2718d51af3524915dc554
mail@54zm.com <mail@54zm.com>
2018-04-02 14:36:47 +0800

    '1'

--- app/Http/Controllers/Retail/SupplierController.php
+++ app/Http/Controllers/Retail/SupplierController.php
@@ -101,7 +101,24 @@ class SupplierController extends Controller
     //供应商删除操作
     public function supplier_delete_check(Request $request)
     {
-        dd($request);
+        $admin_data = $request->get('admin_data');          //中间件产生的管理员数据参数
+        $route_name = $request->path();                         //获取当前的页面路由
+        $supplier_id = $request->get('supplier_id');
+        DB::beginTransaction();
+        try {
+            RetailSupplier::select_delete($supplier_id);
+            //添加操作日志
+            if ($admin_data['is_super'] == 1){//超级管理员删除零售店铺供应商的记录
+                OperationLog::addOperationLog('1','1','1',$route_name,'在零售管理系统删除了供应商！');//保存操作记录
+            }else{//零售店铺本人操作记录
+                OperationLog::addOperationLog('10',$admin_data['organization_id'],$admin_data['id'],$route_name, '删除了供应商！');//保存操作记录
+            }
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();//事件回滚
+            return response()->json(['data' => '删除供应商失败，请检查', 'status' => '0']);
+        }
+        return response()->json(['data' => '删除供应商成功', 'status' => '1']);
     }
 
     //供应商编辑操作
