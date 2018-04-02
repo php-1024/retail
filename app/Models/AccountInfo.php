40591e46f35939fa579ba93546f28e96b975e2e9
mail@54zm.com <mail@54zm.com>
2018-04-02 12:30:56 +0800

    '1'

--- app/Models/AccountInfo.php
+++ app/Models/AccountInfo.php
@@ -30,6 +30,11 @@ class AccountInfo extends Model
         return $this->belongsTo('App\Models\RetailLossOrder', 'id','operator_id');
     }
 
+    //和RetailCheckOrder表是一对一的关系
+    public function RetailCheckOrder(){
+        return $this->belongsTo('App\Models\RetailCheckOrder', 'id','operator_id');
+    }
+
     //修改账号
     public static function editAccountInfo($where,$param){
         $model = self::where($where)->first();
