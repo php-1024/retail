e4c0c1cf8c12c7376638f9e726c47463fe341a5f
zeo <234521890@qq.com>
2018-04-04 14:42:12 +0800

    by zeo

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -4,10 +4,32 @@
  */
 namespace App\Http\Controllers\Pay;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request;
 use Session;
 class SftController extends Controller{
     public function test(){
         echo "测试盛付通相关接口";
+        $url = 'http://mgw.shengpay.com/web-acquire-channel/pay/order.htm';
+
+        $data = [
+            'merchantNo' => '11548088',
+            'charset'=>'UTF-8',
+            'requestTime'=>date('YmdHis'),
+            'merchantOrderNo'=>'sft_test'.mt_rand('1000000','9999999'),
+            'amount'=>'0.01',
+            'expireTime'=>date('YmdHis'),
+            'notifyUrl'=>'',
+            'productName'=>'测试产品',
+            'currency'=>'CNY',
+            'userIp'=>$ip = Request::getClientIp(),
+            'openid'=>'',
+            'pageUrl'=>'',
+            'exts'=>'',
+        ];
+
+        dump($data);
+
     }
+
 }
 ?>
