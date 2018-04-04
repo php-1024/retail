a498d61ccd6b017222f390208dfd4a7dd5fab992
zeo <234521890@qq.com>
2018-04-04 10:29:27 +0800

    by zeo

---
+++ app/Http/Controllers/Pay/SftController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Android接口
+ */
+namespace App\Http\Controllers\Pay;
+use App\Http\Controllers\Controller;
+use Session;
+class SftpayController extends Controller{
+    public function test(){
+        echo "测试盛付通相关接口";
+    }
+}
+?>
