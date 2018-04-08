833c62bb437a5af3a6ed0365943d50c89f8ae001
xuho <781966658@qq.com>
2018-04-08 09:49:47 +0800

    1

--- app/Http/Controllers/Fansmanage/WechatmenuController.php
+++ app/Http/Controllers/Fansmanage/WechatmenuController.php
@@ -372,13 +372,13 @@ class WechatmenuController extends CommonController
                 }
             }
         }
+
         // 刷新并获取授权令牌
         $auth_info = \Wechat::refresh_authorization_info($organization_id);
-        dump($auth_info);
+
         // 创建微信菜单
         $re = \Wechat::create_menu($auth_info['authorizer_access_token'], $data);
         $re = json_decode($re, true);
-        dd($re);
 
         // 返回创建的数据结构
         if ($re['errmsg'] == 'ok') {
