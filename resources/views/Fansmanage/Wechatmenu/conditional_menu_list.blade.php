00c27ab3ace4057086a407719eadafe60822c68e
xuho <781966658@qq.com>
2018-04-08 15:26:48 +0800

    1

--- resources/views/Fansmanage/Wechatmenu/conditional_menu_list.blade.php
+++ resources/views/Fansmanage/Wechatmenu/conditional_menu_list.blade.php
@@ -1,9 +1,9 @@
 <label class="col-sm-2 control-label" for="input-id-1">上级菜单</label>
 <div class="col-sm-10">
     <select name="parent_id" class="form-control m-b" id="parent_id">
-        <option value ="0">无</option>
+        <option value="0">无</option>
         @foreach($list as $key=>$val)
-            <option value ="{{$val->id}}">{{$val->menu_name}}</option>
+            <option value="{{$val->id}}" @if($val->id == $parent_id) selected @endif>{{$val->menu_name}}</option>
         @endforeach
     </select>
 </div>
