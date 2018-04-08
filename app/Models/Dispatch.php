069dff2b4ffe1109cb6bbe459a6f5035a38ca01d
mail@54zm.com <mail@54zm.com>
2018-04-08 15:00:28 +0800

    '1'

--- app/Models/Dispatch.php
+++ app/Models/Dispatch.php
@@ -65,7 +65,6 @@ class Dispatch extends Model{
         }
         return $model->with('create_account')->with('Organization')->where($where)->orderBy($orderby,$sort)->paginate($paginate);
     }
-
     //查询出模型，再删除模型 一定要查询到才能删除
     public static function select_delete($id){
         $model = Self::find($id);
