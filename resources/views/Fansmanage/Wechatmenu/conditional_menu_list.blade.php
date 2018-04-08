3c2b889239837268eed0da1c91910974a4abc831
xuho <781966658@qq.com>
2018-04-08 15:26:04 +0800

    1

--- resources/views/Fansmanage/Wechatmenu/conditional_menu_list.blade.php
+++ resources/views/Fansmanage/Wechatmenu/conditional_menu_list.blade.php
@@ -1,6 +1,6 @@
 <label class="col-sm-2 control-label" for="input-id-1">上级菜单</label>
 <div class="col-sm-10">
-    <select name="parent_id" class="form-control m-b">
+    <select name="parent_id" class="form-control m-b" id="parent_id">
         <option value ="0">无</option>
         @foreach($list as $key=>$val)
             <option value ="{{$val->id}}">{{$val->menu_name}}</option>
