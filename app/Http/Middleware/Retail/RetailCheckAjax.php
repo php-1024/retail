4d57c26a90c4d28aa2fd3a5c83625c50e3c13b2c
QA\Administrator <810106148@qq.com>
2018-04-08 17:57:04 +0800

    '1'

--- app/Http/Middleware/Retail/RetailCheckAjax.php
+++ app/Http/Middleware/Retail/RetailCheckAjax.php
@@ -73,6 +73,7 @@ class RetailCheckAjax
             case "retail/ajax/check_list_confirm_check":      //审核订单安全密码确认
             case "retail/ajax/supplier_delete_check":         //进销存管理--删除供应商确认
             case "retail/ajax/dispatch_add_check":            //运费模板--添加运费模板安全密码检测
+            case "retail/ajax/goods_thumb_delete_check":     //商品图片删除--检测登录安全密码和权限
             $re = $this->checkLoginAndRuleAndSafe($request);
                 return self::format_response($re, $next);
                 break;
