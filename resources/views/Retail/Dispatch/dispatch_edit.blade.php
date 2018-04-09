b8042919d979be86ec4e79df261e102918665a31
mail@54zm.com <mail@54zm.com>
2018-04-09 11:29:59 +0800

    '1'

--- resources/views/Retail/Dispatch/dispatch_edit.blade.php
+++ resources/views/Retail/Dispatch/dispatch_edit.blade.php
@@ -201,7 +201,7 @@
         });
         console.log(province);
         province = province.substring(0, province.length-1);
-        var data = '_token='+_token+province;
+        var data = '_token='+_token+'&'+province;
 //        console.log(data);
         $.post(url, data, function (json) {
             if (json.status == -1) {
