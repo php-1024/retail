da15f05cf1b3b75bc7f3d9b4b5e94af59481ee80
mail@54zm.com <mail@54zm.com>
2018-04-09 20:53:38 +0800

    '1'

--- app/Http/Controllers/Retail/DispatchController.php
+++ app/Http/Controllers/Retail/DispatchController.php
@@ -96,8 +96,9 @@ class DispatchController extends Controller
             dump($provinces);
             foreach ($provinces as $kk=>$vv){
                 $province_name[] = Province::getOne(['id'=>$vv])->first()->toArray();
+                $province_names[$kk] = Province::getOne(['id'=>$vv])->first()->toArray();
             }
-            $val->province_name = $province_name;       //将查询出来的省份存进原有模型
+            $val->province_name = $province_names;       //将查询出来的省份存进原有模型
         }
         $province = Province::getList([],0,'id','ASC')->toArray();  //  查询出所有省份
         //找出已选的省份并删除
