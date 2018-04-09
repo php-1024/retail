2b30be07cd995888c824f2d340d0ac4e37f46fec
mail@54zm.com <mail@54zm.com>
2018-04-09 19:56:56 +0800

    '1'

--- app/Http/Controllers/Retail/DispatchController.php
+++ app/Http/Controllers/Retail/DispatchController.php
@@ -146,7 +146,8 @@ class DispatchController extends Controller
         DB::beginTransaction();
         try {
             foreach ($dispatch_data as $key=>$val){
-                DispatchProvince::editDispatchProvince(['id'=>$key,'dispatch_id'=>$dispatch_id],['first_weight'=>$val['first_weight'],'additional_weight'=>$val['additional_weight'],'freight'=>$val['freight'],'renewal'=>$val['renewal']]);
+               $model = DispatchProvince::editDispatchProvince(['id'=>$key,'dispatch_id'=>$dispatch_id],['first_weight'=>$val['first_weight'],'additional_weight'=>$val['additional_weight'],'freight'=>$val['freight'],'renewal'=>$val['renewal']]);
+               dd($model);
             }
             //添加操作日志
             if ($admin_data['is_super'] == 1){//超级管理员的记录
