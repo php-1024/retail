d7c2bfa1c5d12d433d0144105ab6124701d7dd12
mail@54zm.com <mail@54zm.com>
2018-04-09 20:20:44 +0800

    '1'

--- resources/views/Retail/Dispatch/dispatch_edit.blade.php
+++ resources/views/Retail/Dispatch/dispatch_edit.blade.php
@@ -97,7 +97,7 @@
                                                         <select name="province[]" id="multiselect_to" class="form-control" size="15" multiple="multiple"></select>
                                                     </td>
                                                     <td>
-                                                        <button onclick="province_add_check();" class="btn btn-info btn-xs" type="button"><i class="fa fa-plus"></i>添加选择</button>
+                                                        <button onclick="province_add_check()" class="btn btn-info btn-xs" type="button"><i class="fa fa-plus"></i>添加选择</button>
                                                     </td>
                                                 </tr>
                                                 </tbody>
