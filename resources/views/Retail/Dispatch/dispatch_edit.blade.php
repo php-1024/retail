fa073a3eadc2d0056e6e05fa8305d0fa8f6a6f47
mail@54zm.com <mail@54zm.com>
2018-04-09 20:28:56 +0800

    '1'

--- resources/views/Retail/Dispatch/dispatch_edit.blade.php
+++ resources/views/Retail/Dispatch/dispatch_edit.blade.php
@@ -211,9 +211,9 @@
                     text: json.data,
                     confirmButtonColor: "#DD6B55",
                     confirmButtonText: "确定"
-                },function(){
+                }),function(){
                     window.location.reload();
-                });
+                };
             }else{
                 swal({
                     title: "提示信息",
