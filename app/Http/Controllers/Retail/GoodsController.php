c772919c8c638edc13f70c7fa6e371897eda7fbf
QA\Administrator <810106148@qq.com>
2018-04-09 14:33:26 +0800

    '1'

--- app/Http/Controllers/Retail/GoodsController.php
+++ app/Http/Controllers/Retail/GoodsController.php
@@ -195,9 +195,14 @@ class GoodsController extends Controller
             $entension = $file->getClientOriginalExtension();                           //获取上传文件后缀名
 
 
+
             $new_name = date('Ymdhis') . mt_rand(100, 999) . '.' . $entension;  //重命名
             $file->move(base_path() . '/uploads/catering/', $new_name);         //$path上传后的文件路径
             $file_path =  'uploads/catering/'.$new_name;
+        }
+        else{
+
+            return response()->json(['data' => '上传商品图片无效，请检查', 'status' => '0']);
         }
         $goods_thumb = ['goods_id' => $goods_id,'thumb' => $file_path];         //商品图片信息
         DB::beginTransaction();
