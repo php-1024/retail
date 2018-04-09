8a11d87bbfeda68e01c2685b4d8012914b1c1b4e
kbzz <463661663@qq.com>
2018-04-09 11:01:59 +0800

    1

--- resources/views/Zerone/Agent/agent_examine.blade.php
+++ resources/views/Zerone/Agent/agent_examine.blade.php
@@ -1,8 +1,8 @@
 <link href="{{asset('public/Zerone/library/iCheck')}}/css/custom.css" rel="stylesheet">
 <form method="post" role="form" id="currentForm" action="{{ url('zerone/ajax/agent_examine_check') }}">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
-    <input type="hidden" name="id"  value="{{$info->id}}">
-    <input type="hidden" name="status"  value="{{$status}}">
+    <input type="hidden" name="id" value="{{$info->id}}">
+    <input type="hidden" name="status" value="{{$status}}">
     <div class="modal-dialog modal-lg">
         <div class="modal-content animated fadeIn">
             <div class="modal-header">
@@ -18,14 +18,15 @@
             <div class="modal-body">
                 <div class="form-group">
                     <label class="col-sm-2 control-label">安全密码</label>
-                    <div class="col-sm-10"><input type="password" class="form-control" value="" name="safe_password"></div>
+                    <div class="col-sm-10"><input type="password" class="form-control" value="" name="safe_password">
+                    </div>
                 </div>
                 <div style="clear:both"></div>
             </div>
 
             <div class="modal-footer">
                 <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
-                <button type="button" class="btn btn-primary saveBtn" onclick="postForm()" >保存</button>
+                <button type="button" class="btn btn-primary saveBtn" onclick="postForm()">保存</button>
             </div>
         </div>
     </div>
@@ -41,16 +42,16 @@
         $.post(url, data, function (json) {
             if (json.status == -1) {
                 window.location.reload();
-            } else if(json.status == 1) {
+            } else if (json.status == 1) {
                 swal({
                     title: "提示信息",
                     text: json.data,
                     confirmButtonColor: "#DD6B55",
                     confirmButtonText: "确定",
-                },function(){
+                }, function () {
                     window.location.reload();
                 });
-            }else{
+            } else {
                 swal({
                     title: "提示信息",
                     text: json.data,
