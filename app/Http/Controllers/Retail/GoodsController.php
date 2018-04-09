34a45d00fb689a75cd8e44d04dc06b613c229b3e
QA\Administrator <810106148@qq.com>
2018-04-09 10:14:57 +0800

    '1'

--- app/Http/Controllers/Retail/GoodsController.php
+++ app/Http/Controllers/Retail/GoodsController.php
@@ -145,8 +145,9 @@ class GoodsController extends Controller
     {
         $admin_data = $request->get('admin_data');           //中间件产生的管理员数据参数
         $route_name = $request->path();                          //获取当前的页面路由
-        $goods_thumb_id = $request->get('goods_thumb_id');              //获取分类栏目ID
-      //  $goods_thumb = RetailGoodsThumb::getPluck(['goods_thumb_id'=>$goods_thumb_id],'thumb')->first();
+        $goods_thumb_id = $request->get('goods_thumb_id');              //获取图片ID
+
+        $goods_thumb = RetailGoodsThumb::getPluck(['goods_thumb_id'=>$goods_thumb_id],'thumb')->first();
        // $goods_thumb_filepath=$request->get('filepath');
         //  $id = RetailStock::getPluck(['goods_thumb_id'=>$goods_thumb_id],'id')->first();
         DB::beginTransaction();
@@ -166,7 +167,7 @@ class GoodsController extends Controller
             DB::rollBack();//事件回滚
             return response()->json(['data' => '删除商品图片失败，请检查', 'status' => '0']);
         }
-        return response()->json(['data' => '删除商品图片成功', 'status' => '1']);
+        return response()->json(['data' => '删除商品图片成功'.$goods_thumb, 'status' => '1']);
     }
     //图片异步加载部分
     public function goods_thumb(Request $request)
