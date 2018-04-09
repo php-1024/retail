c6a80207196b1ba1b4f8eb8b5c5b9c321bce22cf
mail@54zm.com <mail@54zm.com>
2018-04-09 19:09:01 +0800

    'iszmxw'

--- app/Http/Controllers/Retail/DispatchController.php
+++ app/Http/Controllers/Retail/DispatchController.php
@@ -151,7 +151,7 @@ class DispatchController extends Controller
 //
 //        };
 //        DispatchProvince::editDispatchProvince(['id'=>$dispatch_id],$dispatch_data);
-        dd($request);
+        dd($request->dispatch_data*);
     }
 
 }
