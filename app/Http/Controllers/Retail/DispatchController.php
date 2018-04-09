a0f95b6ef3f7faeebbf7f0053e4f70d5e6f72279
mail@54zm.com <mail@54zm.com>
2018-04-09 16:55:20 +0800

    '1'

--- app/Http/Controllers/Retail/DispatchController.php
+++ app/Http/Controllers/Retail/DispatchController.php
@@ -99,7 +99,7 @@ class DispatchController extends Controller
         }
         $province = Province::getList([],0,'id','ASC');
 //        dump($province_name);
-        $new_array = array_diff($province_name,$province);
+        $new_array = array_diff($province,$province_name);
 //        dump($province->toArray());
         dump($new_array);
         return view('Retail/Dispatch/dispatch_edit',['province'=>$province,'dispatch'=>$dispatch,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
