db1b369176b6698e5a94b1a729e523e2af0b1d42
mail@54zm.com <mail@54zm.com>
2018-04-10 18:15:01 +0800

    '1'

--- resources/views/Retail/Billing/stock_list.blade.php
+++ resources/views/Retail/Billing/stock_list.blade.php
@@ -87,9 +87,7 @@
                                 <div class="row">
                                     <div class="col-sm-12 text-right text-center-xs">
                                         @if(!is_array($stock_list))
-
                                         {!! $stock_list->links() !!}
-
                                         @endif
                                     </div>
                                 </div>
