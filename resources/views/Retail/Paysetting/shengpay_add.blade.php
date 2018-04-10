b48b7cc755ad04405d9f8beb74e4e1fde485ff8a
kbzz <463661663@qq.com>
2018-04-10 15:43:04 +0800

    1

--- resources/views/Retail/Paysetting/shengpay_add.blade.php
+++ resources/views/Retail/Paysetting/shengpay_add.blade.php
@@ -39,16 +39,14 @@
                                 盛付通终端机器号添加
                             </header>
                             <div class="row wrapper">
-
                                 <div class="well">
                                     <h3>温馨提示</h3>
                                     <p class="text-danger">1.你必须向微信公众平台提交企业信息以及银行账户资料，审核通过并签约后才能使用微信支付功能</p>
                                 </div>
-
                             </div>
                             <div class="table-responsive">
-                                <form class="form-horizontal" method="get">
-
+                                <form class="form-horizontal" method="post" role="form" id="currentForm" action="{{ url('retail/ajax/shengpay_add_check') }}">
+                                    <input type="hidden" name="_token" value="{{csrf_token()}}">
                                     <div class="form-group">
                                         <label class="col-sm-2 control-label" for="input-id-1">终端号</label>
                                         <div class="col-sm-8">
@@ -61,7 +59,7 @@
                                     <div class="form-group">
                                         <div class="col-sm-12 col-sm-offset-6">
 
-                                            <button type="button" class="btn btn-success" id="addBtn">确定添加</button>
+                                            <button type="button" class="btn btn-success"  onclick="postForm()">确定添加</button>
                                         </div>
                                     </div>
                                     <div class="line line-dashed b-b line-lg pull-in"></div>
@@ -69,7 +67,6 @@
                                 </form>
                             </div>
                         </section>
-
                     </section>
                 </section>
             </section>
@@ -102,6 +99,43 @@
             $(this).addClass('btn-success').removeClass('btn-info');
         });
     });
+
+
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    //type: "warning"
+                });
+            }
+        });
+    }
+
+
+
+
+
+
+
 </script>
 </body>
 </html>
