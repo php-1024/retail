24e076b6fcfdbf9d68715ac5a446e2148e258f28
kbzz <463661663@qq.com>
2018-04-10 18:46:10 +0800

    1

--- app/Http/Controllers/Retail/PaysettingController.php
+++ app/Http/Controllers/Retail/PaysettingController.php
@@ -31,6 +31,50 @@ class PaysettingController extends Controller
         $route_name = $request->path();
         return view('Retail/Paysetting/payconfig', ['admin_data' => $admin_data, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data, 'route_name' => $route_name]);
     }
+
+    /**
+     * 收款信息设置功能提交
+     */
+    public function payconfig_check(Request $request)
+    {
+        // 中间件产生的管理员数据参数
+        $admin_data = $request->get('admin_data');
+        // 店铺id
+        $retail_id = $admin_data['organization_id'];
+        // 获取当前的页面路由
+        $route_name = $request->path();
+        // 终端号
+        $terminal_num = $request->terminal_num;
+        // 查询终端号是否存在
+        if (RetailShengpayTerminal::checkRowExists([['terminal_num', $terminal_num]])) {
+            return response()->json(['data' => '该终端号已绑定！', 'status' => '0']);
+        }
+        DB::beginTransaction();
+        try {
+            // 数据处理
+            $data = [
+                // 店铺id
+                'retail_id' => $retail_id,
+                // 终端号
+                'terminal_num' => $terminal_num,
+            ];
+            // 添加终端号
+            RetailShengpayTerminal::addShengpayTerminal($data);
+            // 如果不是超级管理员
+            if ($admin_data['is_super'] != 1) {
+                // 保存操作记录
+                OperationLog::addOperationLog('10', $admin_data['organization_id'], $admin_data['id'], $route_name, '添加了终端号：' . $terminal_num);
+            }
+            // 事件提交
+            DB::commit();
+        } catch (\Exception $e) {
+            // 事件回滚
+            DB::rollBack();
+            return response()->json(['data' => '添加失败！', 'status' => '0']);
+        }
+        return response()->json(['data' => '添加成功！', 'status' => '1']);
+    }
+
     /**
      * 添加终端机器号信息
      */
