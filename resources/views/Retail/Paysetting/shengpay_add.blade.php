1f29f2e7ce23522d462b635acead165bbcaccb20
kbzz <463661663@qq.com>
2018-04-10 15:39:33 +0800

    1

--- resources/views/Retail/Paysetting/shengpay_add.blade.php
+++ resources/views/Retail/Paysetting/shengpay_add.blade.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="utf-8"/>
     <title>零壹云管理平台 | 零售版店铺管理系统</title>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/library/jPlayer/jplayer.flat.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/bootstrap.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/animate.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/font-awesome.min.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/simple-line-icons.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/font.css" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('public/Branch')}}/css/app.css" type="text/css"/>
-    <link href="{{asset('public/Branch')}}/library/sweetalert/sweetalert.css" rel="stylesheet"/>
-    <link href="{{asset('public/Branch')}}/library/wizard/css/custom.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{asset('public/Retail')}}/library/jPlayer/jplayer.flat.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Retail')}}/css/bootstrap.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Retail')}}/css/animate.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Retail')}}/css/font-awesome.min.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Retail')}}/css/simple-line-icons.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Retail')}}/css/font.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Retail')}}/css/app.css" type="text/css"/>
+    <link href="{{asset('public/Retail')}}/library/sweetalert/sweetalert.css" rel="stylesheet"/>
+    <link href="{{asset('public/Retail')}}/library/wizard/css/custom.css" rel="stylesheet"/>
     <!--[if lt IE 9]>
-    <script src="{{asset('public/Branch')}}/library/ie/html5shiv.js"></script>
-    <script src="{{asset('public/Branch')}}/library/ie/respond.min.js"></script>
-    <script src="{{asset('public/Branch')}}/library/ie/excanvas.js"></script>
+    <script src="{{asset('public/Retail')}}/library/ie/html5shiv.js"></script>
+    <script src="{{asset('public/Retail')}}/library/ie/respond.min.js"></script>
+    <script src="{{asset('public/Retail')}}/library/ie/excanvas.js"></script>
     <![endif]-->
 </head>
 <body class="">
@@ -32,11 +32,11 @@
                 <section class="vbox">
                     <section class="scrollable padder">
                         <div class="m-b-md">
-                            <h3 class="m-b-none">微信支付设置</h3>
+                            <h3 class="m-b-none">盛付通终端机器号添加</h3>
                         </div>
                         <section class="panel panel-default">
                             <header class="panel-heading">
-                                微信支付设置
+                                盛付通终端机器号添加
                             </header>
                             <div class="row wrapper">
 
@@ -50,35 +50,18 @@
                                 <form class="form-horizontal" method="get">
 
                                     <div class="form-group">
-                                        <label class="col-sm-2 control-label" for="input-id-1">商户号</label>
+                                        <label class="col-sm-2 control-label" for="input-id-1">终端号</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" value="">
-                                        </div>
-                                    </div>
-
-
-                                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label" for="input-id-1">PaySignKey</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" value="">
+                                            <input type="text" class="form-control" value="" name="terminal_num">
                                         </div>
                                     </div>
 
                                     <div class="line line-dashed b-b line-lg pull-in"></div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label" for="input-id-1">秘钥</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" value="">
-                                        </div>
-                                    </div>
 
-                                    <div class="line line-dashed b-b line-lg pull-in"></div>
-                                    
                                     <div class="form-group">
                                         <div class="col-sm-12 col-sm-offset-6">
 
-                                            <button type="button" class="btn btn-success" id="addBtn">保存资料</button>
+                                            <button type="button" class="btn btn-success" id="addBtn">确定添加</button>
                                         </div>
                                     </div>
                                     <div class="line line-dashed b-b line-lg pull-in"></div>
@@ -93,99 +76,20 @@
         </section>
     </section>
 </section>
-
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <form class="form-horizontal tasi-form" method="get">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">店铺信息编辑</h4>
-                </div>
-                <div class="modal-body">
-                    <form class="form-horizontal" method="get">
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">店铺名称</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="刘记鸡煲王【龙岗店】" placeholder="店铺名称" class="form-control">
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">负责人</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="张老三" placeholder="负责人" class="form-control">
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">手机号码</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="13123456789" placeholder="手机号码" class="form-control">
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">店铺LOGO</label>
-                            <div class="col-sm-10">
-                                <input type="file" class="filestyle" style="display: none;" data-icon="false"
-                                       data-classButton="btn btn-default"
-                                       data-classInput="form-control inline v-middle input-s">
-                            </div>
-                        </div>
-
-                        <div style="clear:both;"></div>
-
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">店铺地址</label>
-                            <div class="col-sm-10">
-                                <input type="text" value="广东省深圳市龙岗区万汇大厦1606" placeholder="店铺地址" class="form-control">
-                            </div>
-                        </div>
-
-                        <div style="clear:both;"></div>
-                        <div class="line line-dashed b-b line-lg pull-in"></div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 text-right">安全密码</label>
-                            <div class="col-sm-10">
-                                <input type="password" value="" placeholder="安全密码" class="form-control">
-                            </div>
-                        </div>
-                        <div style="clear:both;"></div>
-
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button data-dismiss="modal" class="btn btn-default" type="button">取消</button>
-                    <button class="btn btn-success" type="button" id="save_btn">确定</button>
-                </div>
-            </div>
-        </div>
-    </form>
-</div>
 <!-- App -->
-<script src="{{asset('public/Branch')}}/js/jquery.min.js"></script>
+<script src="{{asset('public/Retail')}}/js/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="{{asset('public/Branch')}}/js/bootstrap.js"></script>
+<script src="{{asset('public/Retail')}}/js/bootstrap.js"></script>
 <!-- App -->
-<script src="{{asset('public/Branch')}}/js/app.js"></script>
-<script src="{{asset('public/Branch')}}/library/slimscroll/jquery.slimscroll.min.js"></script>
-<script src="{{asset('public/Branch')}}/js/app.plugin.js"></script>
-<script src="{{asset('public/Branch')}}/library/file-input/bootstrap-filestyle.min.js"></script>
-<script type="text/javascript" src="{{asset('public/Branch')}}/library/jPlayer/jquery.jplayer.min.js"></script>
-<script type="text/javascript" src="{{asset('public/Branch')}}/library/jPlayer/add-on/jplayer.playlist.min.js"></script>
-<script type="text/javascript" src="{{asset('public/Branch')}}/library/sweetalert/sweetalert.min.js"></script>
+<script src="{{asset('public/Retail')}}/js/app.js"></script>
+<script src="{{asset('public/Retail')}}/library/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Retail')}}/js/app.plugin.js"></script>
+<script src="{{asset('public/Retail')}}/library/file-input/bootstrap-filestyle.min.js"></script>
+<script type="text/javascript" src="{{asset('public/Retail')}}/library/jPlayer/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="{{asset('public/Retail')}}/library/jPlayer/add-on/jplayer.playlist.min.js"></script>
+<script type="text/javascript" src="{{asset('public/Retail')}}/library/sweetalert/sweetalert.min.js"></script>
 <script type="text/javascript"
-        src="{{asset('public/Branch')}}/library/wizard/js/jquery.bootstrap.wizard.min.js"></script>
+        src="{{asset('public/Retail')}}/library/wizard/js/jquery.bootstrap.wizard.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function () {
         $('#rootwizard').bootstrapWizard({'tabClass': 'bwizard-steps'});
