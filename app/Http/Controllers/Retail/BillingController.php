68c90c8c1616ee9b8d8dddd79800c411c3a04d8b
mail@54zm.com <mail@54zm.com>
2018-04-11 18:21:59 +0800

    '1'

--- app/Http/Controllers/Retail/BillingController.php
+++ app/Http/Controllers/Retail/BillingController.php
@@ -267,16 +267,16 @@ class BillingController extends Controller
         $goods_name = $request->get('goods_name');         //获取供应商名称
         $stock_list = [];
         if ($goods_name != null){
-            $where = [['retail_id',$admin_data['organization_id']],['name','like','%'.$goods_name.'%']];
+            $where = [['retail_id',$admin_data['organization_id']],['name','like','%'.$goods_name.'%']];.
+            $goods = RetailGoods::getPaginage($where,'','10','created_at','DESC');
+            foreach ($goods as $key=>$val){
+                $stock_list[] = RetailStock::getOne(['goods_id'=>$val->id])->first();
+            }
         }else{
             $where = [['retail_id',$admin_data['organization_id']]];
             $stock_list = RetailStock::getPaginage($where,'10','created_at','ASC'); //查询商品信息
         }
 //        $goods = RetailGoods::getList($where,0,'created_at','DESC');
-        $goods = RetailGoods::getPaginage($where,'','10','created_at','DESC');
-        foreach ($goods as $key=>$val){
-            $stock_list[] = RetailStock::getOne(['goods_id'=>$val->id])->first();
-        }
         dump($stock_list);
         return  view('Retail/Billing/stock_list',['stock_list'=>$stock_list,'goods_name'=>$goods_name,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
