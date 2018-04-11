0f10e6393bbe87e321d9f49e064b8ed78a71de61
kbzz <463661663@qq.com>
2018-04-11 11:49:55 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -14,6 +14,8 @@ use App\Models\RetailGoods;
 use App\Models\RetailGoodsThumb;
 use App\Models\RetailOrder;
 use App\Models\RetailOrderGoods;
+use App\Models\RetailShengpay;
+use App\Models\RetailShengpayTerminal;
 use App\Models\RetailStockLog;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -27,19 +29,42 @@ class AndroidApiController extends Controller
      */
     public function login(Request $request)
     {
-        $account = $request->account;//登入账号
-        $password = $request->password;//登入密码
-        $data = Account::where([['account', $account]])->orWhere([['mobile', $account]])->first();//根据账号进行查询
+        // 登入账号
+        $account = $request->account;
+        // 登入密码
+        $password = $request->password;
+        // 根据账号进行查询
+        $data = Account::where([['account', $account]])->orWhere([['mobile', $account]])->first();
         if (empty($data)) {
             return response()->json(['msg' => '用户不存在', 'status' => '0', 'data' => '']);
         }
-        $key = config("app.retail_encrypt_key");//获取加密盐
-        $encrypted = md5($password);//加密密码第一重
-        $encryptPwd = md5("lingyikeji" . $encrypted . $key);//加密密码第二重
+        // 获取加密盐
+        $key = config("app.retail_encrypt_key");
+        // 加密密码第一重
+        $encrypted = md5($password);
+        // 加密密码第二重
+        $encryptPwd = md5("lingyikeji" . $encrypted . $key);
         if ($encryptPwd != $data['password']) {
             return response()->json(['msg' => '密码不正确', 'status' => '0', 'data' => '']);
         }
-        $data = ['status' => '1', 'msg' => '登陆成功', 'data' => ['account_id' => $data['id'], 'account' => $data['account'], 'organization_id' => $data['organization_id'], 'uuid' => $data['uuid']]];
+        // 查询pos商户号
+        $shengpay = RetailShengpay::getOne([['retail_id',$data['organization_id']]]);
+        if(!empty($shengpay)){
+            return response()->json(['msg' => 'pos商户号不存在', 'status' => '0', 'data' => '']);
+        }
+        if($shengpay->status != '1'){
+            return response()->json(['msg' => 'pos商户号没通过审核', 'status' => '0', 'data' => '']);
+        }
+        // 查询pos机终端号
+        $terminal = RetailShengpayTerminal::getOne([['retail_id',$data['organization_id']],'terminal_num',$terminal_num]);
+        if(!empty($terminal)){
+            return response()->json(['msg' => 'pos机终端号不存在', 'status' => '0', 'data' => '']);
+        }
+        if($terminal->status != '1'){
+            return response()->json(['msg' => 'pos机终端号没通过审核', 'status' => '0', 'data' => '']);
+        }
+        $organization_name = Organization::getPluck([['id',$data['organization_id']]],'organization_name')->first();
+        $data = ['status' => '1', 'msg' => '登陆成功', 'data' => ['account_id' => $data['id'], 'account' => $data['account'], 'organization_id' => $data['organization_id'], 'uuid' => $data['uuid'],'sft_num'=>$shengpay['sft_num'],'organization_name'=>$organization_name]];
         return response()->json($data);
     }
 
