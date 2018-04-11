2ebf36cfe4c1ce55928507860d97d263d242c056
kbzz <463661663@qq.com>
2018-04-11 15:55:21 +0800

    1

--- app/Http/Controllers/Zerone/PaysettingController.php
+++ app/Http/Controllers/Zerone/PaysettingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Zerone;
 
 use App\Http\Controllers\Controller;
+use App\Models\RetailShengpay;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Session;
@@ -22,8 +23,9 @@ class PaysettingController extends Controller
         $son_menu_data = $request->get('son_menu_data');
         // 获取当前的页面路由
         $route_name = $request->path();
-
-        return view('Zerone/Paysetting/payconfig_apply', [ 'admin_data' => $admin_data, 'route_name' => $route_name, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data]);
+        // 查询收款信息列表
+        $list = RetailShengpay::getPaginage([], 15, 'id');
+        return view('Zerone/Paysetting/payconfig_apply', ['list' => $list, 'admin_data' => $admin_data, 'route_name' => $route_name, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data]);
     }
 
 
