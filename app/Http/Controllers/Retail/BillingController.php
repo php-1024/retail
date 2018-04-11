09a32f21e75593052137e384d5da8fb869e24896
mail@54zm.com <mail@54zm.com>
2018-04-11 18:07:00 +0800

    '1'

--- app/Http/Controllers/Retail/BillingController.php
+++ app/Http/Controllers/Retail/BillingController.php
@@ -275,6 +275,7 @@ class BillingController extends Controller
         foreach ($goods as $key=>$val){
             $stock_list[] = RetailStock::getOne(['goods_id'=>$val->id])->first();
         }
+        dump($stock_list);
         return  view('Retail/Billing/stock_list',['stock_list'=>$stock_list,'goods_name'=>$goods_name,'admin_data'=>$admin_data,'menu_data'=>$menu_data,'son_menu_data'=>$son_menu_data,'route_name'=>$route_name]);
     }
 
