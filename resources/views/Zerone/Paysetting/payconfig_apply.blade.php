cd3d7a01c296ca49be160a2308828282b536e09d
kbzz <463661663@qq.com>
2018-04-11 16:13:18 +0800

    1

--- resources/views/Zerone/Paysetting/payconfig_apply.blade.php
+++ resources/views/Zerone/Paysetting/payconfig_apply.blade.php
@@ -51,24 +51,6 @@
                                 {{--<input type="text" id="amount" name="organization_name" value="{{ $search_data['organization_name'] }}" placeholder="请输入商户名称" class="form-control">--}}
                             </div>
                         </div>
-                        {{--<div class="col-sm-3">--}}
-                        {{--<div class="form-group">--}}
-                        {{--<label class="control-label" for="amount">手机号码</label>--}}
-                        {{--<input type="text" id="amount" name="amount" value="" placeholder="手机号码" class="form-control">--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-sm-3">--}}
-                        {{--<div class="form-group">--}}
-                        {{--<label class="control-label" for="amount">所在战区</label>--}}
-                        {{--<select class="form-control m-b" name="account">--}}
-                        {{--<option>东部战区</option>--}}
-                        {{--<option>西部战区</option>--}}
-                        {{--<option>南部战区</option>--}}
-                        {{--<option>北部战区</option>--}}
-                        {{--<option>中部战区</option>--}}
-                        {{--</select>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
                         <div class="col-sm-3">
                             <div class="form-group">
                                 <label class="control-label" for="amount"> &nbsp;</label>
