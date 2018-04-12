606c77394c8067ae3c135ca78442758f0d41f368
zeo <234521890@qq.com>
2018-04-12 14:31:13 +0800

    by zeo

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -74,13 +74,18 @@ class SftController extends Controller
         $param_body["pageUrl"] = 'http://o2o.01nnt.com/pay/sft/test2';
 //        $param_body["exts"] = '11548088';
 
-        /*
-        $param_body["exts"] = [
-            'requestFrom'=>'WAP',
-            'wap_name'=>'测试盛付通支付',
-            'wap_url'=>'http://o2o.01nnt.com',
-        ];
-        */
+
+        $param_body["exts"] = array(
+            "requestFrom"=>"WAP",//ANDROID_APP, IOS_APP, WAP
+            "app_name"=>"",// APP应用名称
+            "bundle_id"=>"",//IOS 应用唯一标识
+            "package_name"=>"",//Android 应用在一台设备上的唯一标识，在manifest文件里声明  ,示例值：com.tecet.tmgp.game
+            "wap_url"=>'45.77.198.100',//授权域名(报备时填写的域名地址)
+            "wap_name"=>"测试WAP",//WAP应用名称,网页标题
+            "note"=>"45.77.198.100",//为商户自定义的跟本次交易有关的参数
+            "attach"=>"" //可以为空，或者为任何自己想要卡网关回传的校验类型的数据。
+        );
+
         $param_body_json = json_encode($param_body, JSON_UNESCAPED_UNICODE);
 
         $origin_key = "support4test";
