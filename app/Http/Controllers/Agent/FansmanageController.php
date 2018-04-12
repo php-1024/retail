b6acb73ae8b759fbdc9520bdfdc01867c3e8b38a
zouxq <zouxq2016@163.com>
2018-04-12 15:44:30 +0800

    1

--- app/Http/Controllers/Agent/FansmanageController.php
+++ app/Http/Controllers/Agent/FansmanageController.php
@@ -44,17 +44,8 @@ class FansmanageController extends Controller
         $menu_data = $request->get('menu_data');//中间件产生的管理员数据参数
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
-        $fansmanage_name = $request->input('fansmanage_name');
-        $fansmanage_owner_mobile = $request->input('fansmanage_owner_mobile');
-        $where = [['parent_id', $admin_data['organization_id']]];
-        if (!empty($fansmanage_name)) {
-            $where[] = ['organization_name', 'like', '%' . $fansmanage_name . '%'];
-        }
-        if (!empty($fansmanage_owner_mobile)) {
-
-            $where[] = ['fansmanage_owner_mobile', $fansmanage_owner_mobile];
-        }
-        $list = Organization::getPaginagefansmanage([$where, ['program_id', 3]], 10, 'id');
+        $organization = $admin_data['organization_id'];
+        $list = Organization::getPaginagefansmanage([['parent_id', $organization], ['program_id', 3]], 10, 'id');
         return view('Agent/Fansmanage/fansmanage_list', ['list' => $list, 'admin_data' => $admin_data, 'route_name' => $route_name, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data]);
     }
 
