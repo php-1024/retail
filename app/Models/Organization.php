1e7caceade8f564f769fba8d7f730a9e9e621ee0
kbzz <463661663@qq.com>
2018-04-12 11:00:59 +0800

    1

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -202,7 +202,7 @@ class Organization extends Model
     //获取单行数据的其中一列
     public static function getPluck($where, $pluck)
     {
-        return self::where($where)->value($pluck);
+        return self::where($where)->pluck($pluck);
     }
 
     //获取分页数据-服务商
