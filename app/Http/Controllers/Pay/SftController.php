b83ba799b047586f04d9d834cac007f8831e9798
zeo <234521890@qq.com>
2018-04-12 14:48:00 +0800

    by zeo

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -74,7 +74,7 @@ class SftController extends Controller
         $param_body["pageUrl"] = 'http://o2o.01nnt.com/pay/sft/test2';
 //        $param_body["exts"] = '11548088';
 
-        /*
+
         $param_body["exts"] = array(
             "requestFrom"=>"WAP",//ANDROID_APP, IOS_APP, WAP
             "app_name"=>"",// APP应用名称
@@ -85,7 +85,7 @@ class SftController extends Controller
             "note"=>"http://www.17kx.com",//为商户自定义的跟本次交易有关的参数
             "attach"=>"" //可以为空，或者为任何自己想要卡网关回传的校验类型的数据。
         );
-        */
+
 
         $param_body_json = json_encode($param_body, JSON_UNESCAPED_UNICODE);
 
