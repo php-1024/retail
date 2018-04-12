320aa0cb6aa9499d23cc41d76180c2590acb9533
xuho <781966658@qq.com>
2018-04-12 15:27:25 +0800

    1

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -78,16 +78,16 @@ class SftController extends Controller
         $param_body["pageUrl"] = 'http://o2o.01nnt.com/pay/sft/test2';
 //        $param_body["exts"] = '11548088';
 
-//        $param_body["exts"] = array(
-//            "requestFrom"=>"WAP",//ANDROID_APP, IOS_APP, WAP
-//            "app_name"=>"",// APP应用名称
-//            "bundle_id"=>"",//IOS 应用唯一标识
-//            "package_name"=>"",//Android 应用在一台设备上的唯一标识，在manifest文件里声明  ,示例值：com.tecet.tmgp.game
-//            "wap_url"=>'http://www.17kx.com',//授权域名(报备时填写的域名地址)
-//            "wap_name"=>"测试WAP",//WAP应用名称,网页标题
-//            "note"=>"http://www.17kx.com",//为商户自定义的跟本次交易有关的参数
-//            "attach"=>"" //可以为空，或者为任何自己想要卡网关回传的校验类型的数据。
-//        );
+        $param_body["exts"] = array(
+            "requestFrom"=>"WAP",//ANDROID_APP, IOS_APP, WAP
+            "app_name"=>"",// APP应用名称
+            "bundle_id"=>"",//IOS 应用唯一标识
+            "package_name"=>"",//Android 应用在一台设备上的唯一标识，在manifest文件里声明  ,示例值：com.tecet.tmgp.game
+            "wap_url"=>'http://www.17kx.com',//授权域名(报备时填写的域名地址)
+            "wap_name"=>"测试WAP",//WAP应用名称,网页标题
+            "note"=>"http://www.17kx.com",//为商户自定义的跟本次交易有关的参数
+            "attach"=>"" //可以为空，或者为任何自己想要卡网关回传的校验类型的数据。
+        );
 
         $param_body_json = json_encode($param_body, JSON_UNESCAPED_UNICODE);
 
