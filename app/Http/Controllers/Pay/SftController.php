97dbec83dc83eb098ce2e41051ed8e68ad3f2546
zeo <234521890@qq.com>
2018-04-12 14:06:21 +0800

    by zeo

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -77,7 +77,7 @@ class SftController extends Controller
         $param_body["exts"] = [
             'requestFrom'=>'requestFrom',
             'wap_name'=>'测试盛付通支付',
-            'wap_url'=>'http://o2o.01nnt.com',
+            'wap_url'=>'o2o.01nnt.com',
         ];
         $param_body_json = json_encode($param_body, JSON_UNESCAPED_UNICODE);;
         $origin_key = "support4test";
