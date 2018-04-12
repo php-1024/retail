596b40b6e01ea71af3e1cd1cc927111815408773
zeo <234521890@qq.com>
2018-04-12 14:26:53 +0800

    by zeo

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -60,7 +60,7 @@ class SftController extends Controller
 
         // 业务参数
         // 订单号
-        $param_body["merchantOrderNo"] = "LS20180408_5_1000105";
+        $param_body["merchantOrderNo"] = "LS20180408_5_1000".mt_rand(100,999);
         // 交易金额
         $param_body["amount"] = "0.01";
         $param_body["expireTime"] = date('YmdHis', strtotime("+2 hours"));
