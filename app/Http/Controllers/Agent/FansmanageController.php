9c2c6675c7c3a49db529c96766e96fa993b737e6
zouxq <zouxq2016@163.com>
2018-04-12 15:49:42 +0800

    1

--- app/Http/Controllers/Agent/FansmanageController.php
+++ app/Http/Controllers/Agent/FansmanageController.php
@@ -45,12 +45,9 @@ class FansmanageController extends Controller
         $son_menu_data = $request->get('son_menu_data');//中间件产生的管理员数据参数
         $route_name = $request->path();//获取当前的页面路由
         $organization = $admin_data['organization_id'];
-
         $list = Organization::getPaginagefansmanage([['parent_id', $organization], ['program_id', 3]], 10, 'id');
         return view('Agent/Fansmanage/fansmanage_list', ['list' => $list, 'admin_data' => $admin_data, 'route_name' => $route_name, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data]);
     }
-
-
 
     //店铺结构
     public function fansmanage_structure(Request $request)
