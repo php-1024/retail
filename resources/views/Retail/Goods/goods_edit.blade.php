69ee5e71f974db8d448320acda2fd66343ab5e3d
mail@54zm.com <mail@54zm.com>
2018-04-12 15:05:00 +0800

    '1'

--- resources/views/Retail/Goods/goods_edit.blade.php
+++ resources/views/Retail/Goods/goods_edit.blade.php
@@ -214,7 +214,7 @@
     });
     //弹出大图
     function bigthumb() {
-        var imgurl = $(this).attr();
+        var imgurl = $(this).src;
         console.log(imgurl);
         $('#myModal').modal();
     }
