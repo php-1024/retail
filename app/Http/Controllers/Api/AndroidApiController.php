bea12936ee099bb6bd67bf9dcc71660310471bf6
kbzz <463661663@qq.com>
2018-04-13 19:37:18 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -506,6 +506,7 @@ class AndroidApiController extends Controller
                     RetailStockLog::addStockLog($stock_data);//商品操作记录
 
                     $stock_data = RetailStock::getOne([['retail_id', $data['retail_id']], ['goods_id', $value['goods_id']]]);
+                    dd($stock_data);
                     $retail_stock = $stock_data['stock'] + $value['total'];
                     RetailStock::editStock([['id', $stock_data['id']]], ['stock' => $retail_stock]);
                 }
