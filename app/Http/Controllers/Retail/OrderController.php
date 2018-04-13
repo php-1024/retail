7ebaa07bbafee3a27a46b61b1514841444745163
mail@54zm.com <mail@54zm.com>
2018-04-13 18:41:16 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -105,15 +105,14 @@ class OrderController extends Controller
             $return_goods = [];
             foreach ($orders as $key=>$val){
                 foreach ($val->RetailOrderGoods as $kk=>$vv){
-                    dd($vv->goods_id);
-//                    $return_goods[] = [
-//                        'goods_id' => $vv->goods_id,
-//                        'total' => $vv->total
-//                    ];
+                    $return_goods = [
+                        'goods_id' => $vv->goods_id,
+                        'total' => $vv->total
+                    ];
                 }
             }
         }
-//        dd($return_goods);
+        dd($return_goods);
 
 
         DB::beginTransaction();
