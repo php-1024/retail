ed6d2d95786af1eee75e04451e67da35a9fa322f
mail@54zm.com <mail@54zm.com>
2018-04-13 18:52:47 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Retail;
 
 use App\Http\Controllers\Controller;
 use App\Models\Account;
+use App\Models\RetailGoods;
 use App\Models\RetailOrder;
 use App\Models\OperationLog;
 use App\Models\RetailStock;
