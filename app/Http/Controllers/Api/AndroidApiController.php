1be98f3ddd4e4ac9782695ee8b0afa5e7eea3817
kbzz <463661663@qq.com>
2018-04-13 10:58:07 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -231,11 +231,12 @@ class AndroidApiController extends Controller
         $organization_id = $request->organization_id;
         // 订单状态
         $status = $request->status;
+        if ($status === 0){
+            echo 1;exit;
+        }
         $where[] = ['retail_id', $organization_id];
         if ($status) {
-            if ($status === 0){
-                echo 1;exit;
-            }
+
             $where[] = ['status', $status];
         }
 
