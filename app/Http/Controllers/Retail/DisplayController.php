82320598a8dc6f09b779b67cc8b7168d3fdc7273
mail@54zm.com <mail@54zm.com>
2018-04-13 14:33:54 +0800

    '1'

--- app/Http/Controllers/Retail/DisplayController.php
+++ app/Http/Controllers/Retail/DisplayController.php
@@ -46,7 +46,7 @@ class DisplayController extends Controller
             $program = Program::getOne([['id',$organization->program_id]]);
             $organization->program_name = $program;
             $fans = FansmanageUser::getCount(['fansmanage_id'=>$admin_data['organization_id']]);//查询当前店铺粉丝数量
-            $order = RetailOrder::getList(['retail_id'=>$admin_data['organization_id'],'status'=>'3'],'0','id','DESC');
+            $order = RetailOrder::getList(['retail_id'=>$admin_data['organization_id'],'status'=>'1'],'0','id','DESC');
             $order_spot = RetailOrder::getList(['retail_id'=>$admin_data['organization_id']],'0','id','DESC')->count();
             $goods = RetailGoods::getList(['retail_id'=>$admin_data['organization_id']],'0','id','DESC')->count();
             $operating_receipt = 0.00;//营业收入
