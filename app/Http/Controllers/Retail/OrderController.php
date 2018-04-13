b77e3f964ec91d62625e14c38a8224e97e68b34b
mail@54zm.com <mail@54zm.com>
2018-04-13 21:39:31 +0800

    '1'

--- app/Http/Controllers/Retail/OrderController.php
+++ app/Http/Controllers/Retail/OrderController.php
@@ -108,11 +108,11 @@ class OrderController extends Controller
         try {
             if ($status == '-1' && $order->status == '0'){//待付款时取消订单    1、判断是否下单减库存
                 if ($power != '1') {//说明下单减库存，此时库存已经减去，需要还原
-                    $this->return_stock($order,6);
+                    $this->return_stock($order,'6');
                 }
             }else{
                 if ($power == '1') {//说明付款减库存，此时库存已经减去，需要还原
-                    $this->return_stock($order,6);
+                    $this->return_stock($order,'6');
                 }
             }
             RetailOrder::editRetailOrder(['id'=>$order_id],['status'=>$status]);
@@ -147,7 +147,7 @@ class OrderController extends Controller
         }
         if ($status == '1' && $order->status == '0'){//手动确认付款    1、判断是否付款减库存
             if ($power == '1') {//说明付款减库存，此时库存已经减去，需要还原
-                $this->return_stock($order,7);
+                $this->return_stock($order,'7');
             }
         }
 
@@ -172,9 +172,9 @@ class OrderController extends Controller
     {
         foreach ($order->RetailOrderGoods as $key=>$val){
             $old_stock = RetailGoods::getPluck(['id'=>$val->goods_id],'stock')->first(); //查询原来商品的库存
-            if ($type == 6){//销售出库
+            if ($type == '6'){//销售出库
                 $new_stock = $old_stock-$val->total;         //确认付款后处理的新库存
-            }elseif($type == 7){//销退入库
+            }elseif($type == '7'){//销退入库
                 $new_stock = $old_stock+$val->total;         //退货后处理的新库存
             }
             //1、更新商品信息中的库存
