8066dc5fc4468ae8d003cf8f9822e1ff6a714ad4
kbzz <463661663@qq.com>
2018-04-13 11:21:20 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -91,7 +91,6 @@ class AndroidApiController extends Controller
               unset($categorylist[$key]);
             };
         }
-        exit;
         return response()->json(['status' => '1', 'msg' => '获取分类成功', 'data' => ['categorylist' => $categorylist]]);
     }
 
