3d224d805f2de47abd5aee6a37b4da00c3c63c1b
kbzz <463661663@qq.com>
2018-04-13 10:53:32 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -231,10 +231,11 @@ class AndroidApiController extends Controller
         $organization_id = $request->organization_id;
         // 订单状态
         $status = $request->status;
-        echo $status;exit;
         $where[] = ['retail_id', $organization_id];
         if ($status) {
             $where[] = ['status', $status];
+        }elseif ($status == '0'){
+            echo 1;exit;
         }
 
         $orderlist = RetailOrder::getList($where, '0', 'id', '', ['id', 'ordersn', 'order_price', 'status', 'created_at']);
