0731f88b798edfad184f170b48ec787d9aead41e
kbzz <463661663@qq.com>
2018-04-17 17:11:41 +0800

    1

--- app/Http/Controllers/Zerone/FansmanageController.php
+++ app/Http/Controllers/Zerone/FansmanageController.php
@@ -382,7 +382,12 @@ class FansmanageController extends Controller
         }
         // 商户列表查询
 //        $list = Organization::getPaginageFansmanage1($where, $mobile, '10', 'id');
-        $list = Organization::where($where)->leftjoin('account','organization.id','=','account.organization_id')->orderBy('organization.id', 'DESC')->paginate('10');
+        $list = Organization::where($where)->leftjoin('account',function ($join) use ($mobile){
+            $join->on('organization.id','=','account.organization_id')->where('deepth','=','1');
+            if($mobile){
+                $join->where('account.mobile','=',$mobile);
+            }
+        })->orderBy('organization.id', 'DESC')->paginate('10');
 
         dump($list);
         // 循环数据
