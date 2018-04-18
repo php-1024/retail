0fb97bd2e04efbdb76fd219037ef9a16733e4eb3
mail@54zm.com <mail@54zm.com>
2018-04-18 15:14:35 +0800

    '1'

--- app/Http/Controllers/Zerone/LoginController.php
+++ app/Http/Controllers/Zerone/LoginController.php
@@ -22,6 +22,7 @@ class LoginController extends Controller
      */
     public function display()
     {
+        dump(Request::getClientIp());
         return view('Zerone/Login/display');
     }
 
