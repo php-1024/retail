c101466d5cab7135ddde5a96cfb1d4dc11517319
kbzz <463661663@qq.com>
2018-04-18 10:45:25 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -91,10 +91,11 @@ class AndroidApiController extends Controller
             if (!RetailGoods::checkRowExists([['category_id', $value['id']]])) {
                 unset($categorylist[$key]);
             };
+            print_r($categorylist->toArray());exit;
+
             array_values($categorylist[$key]);
         }
 
-        print_r($categorylist->toArray());exit;
         return response()->json(['status' => '1', 'msg' => '获取分类成功', 'data' => ['categorylist' => $categorylist]]);
     }
 
