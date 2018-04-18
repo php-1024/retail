deb548a5f3088a00857627f71cf74b8f886704ff
kbzz <463661663@qq.com>
2018-04-18 10:20:27 +0800

    1

--- app/Http/Controllers/Api/AndroidApiController.php
+++ app/Http/Controllers/Api/AndroidApiController.php
@@ -83,7 +83,6 @@ class AndroidApiController extends Controller
     {
         $organization_id = $request->organization_id;//店铺id
         $categorylist = RetailCategory::getList([['retail_id', $organization_id]], '0', 'displayorder', 'asc', ['id', 'name', 'displayorder']);
-        print_r($categorylist);exit;
         if (empty($categorylist->toArray())) {
             return response()->json(['status' => '0', 'msg' => '没有分类', 'data' => '']);
         }
