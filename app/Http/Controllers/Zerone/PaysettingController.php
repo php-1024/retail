044cc1f14de57d1e1cd17869c00db838c3da0c87
kbzz <463661663@qq.com>
2018-04-18 15:10:17 +0800

    1

--- app/Http/Controllers/Zerone/PaysettingController.php
+++ app/Http/Controllers/Zerone/PaysettingController.php
@@ -34,7 +34,7 @@ class PaysettingController extends Controller
         }
         $search_data = ['organization_name' => $organization_name];
 
-        $data = Organization::where($where)->rightjoin('retail_shengpay','organization.id','=','retail_shengpay.retail_id')->orderBy('retail_shengpay.id', 'DESC')->paginate('15');
+        $data = Organization::from('organization as o')->where($where)->rightjoin('retail_shengpay as r','o.id','=','r.retail_id')->orderBy('r.id', 'DESC')->paginate('15');
         dump($data);
         // 查询收款信息列表
         $list = RetailShengpay::getPaginage([], 15, 'id');
