d4814137afe77c540e06a6ebb537803d335d597e
mail@54zm.com <mail@54zm.com>
2018-04-19 09:27:11 +0800

    'lingyixinkeji'

--- config/database.php
+++ config/database.php
@@ -164,6 +164,13 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => 10,
         ],
+        //定义一个自己的缓存的服务器，简版店铺平台使用
+        'simple' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 12,
+        ],
         //定义一个公司测试集群redis服务片区，测试使用
         'clusters' => [
             'lingyikeji' => [
