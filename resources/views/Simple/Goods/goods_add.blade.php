181dbdae394446dfe838e49647d365a6ef45acc6
mail@54zm.com <mail@54zm.com>
2018-04-19 13:51:15 +0800

    'lingyixinkeji'

---
+++ resources/views/Simple/Goods/goods_add.blade.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en" class="app">
+<head>
+    <meta charset="utf-8" />
+    <title>添加商品 | 零壹云管理平台 | 零售版店铺管理系统</title>
+    <link rel="stylesheet" href="{{asset('public/Simple/library/jPlayer')}}/jplayer.flat.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/app.css" type="text/css" />
+    <link href="{{asset('public/Simple/library/sweetalert')}}/sweetalert.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('public/Simple/library/trumbowyg')}}/design/css/trumbowyg.css">
+    <!--[if lt IE 9]>
+    <script src="{{asset('public/Simple/library/ie')}}/html5shiv.js"></script>
+    <script src="{{asset('public/Simple/library/ie')}}/respond.min.js"></script>
+    <script src="{{asset('public/Simple/library/ie')}}/excanvas.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<section class="vbox">
+    @include('Simple/Public/Header')
+    <section>
+        <section class="hbox stretch">
+            <!-- .aside -->
+            @include('Simple/Public/Nav')
+            <!-- /.aside -->
+            <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">添加商品</h3>
+                        </div>
+                        <section class="panel panel-default">
+                            <header class="panel-heading text-right bg-light">
+                                <ul class="nav nav-tabs pull-left">
+                                    <li class="active"><a href="#baseinfo" data-toggle="tab"><i class="fa fa-file-text-o text-muted"></i>&nbsp;&nbsp;基础信息</a></li>
+                                </ul>
+                                <span class="hidden-sm">&nbsp;</span>
+                            </header>
+                            <div class="panel-body">
+
+                                <div class="tab-content">
+                                    <div class="tab-pane fade in active" id="baseinfo">
+                                        <form method="post" class="form-horizontal"  role="form" id="currentForm" action="{{ url('simple/ajax/goods_add_check') }}">
+                                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label" for="input-id-1">商品分类</label>
+                                                <div class="col-sm-8">
+                                                    <select name="category_id" class="form-control m-b">
+                                                        <option value ="0">请选择</option>
+                                                        @foreach($category as $key=>$val)
+                                                        <option value ="{{$val->id}}">{{$val->name}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label" for="input-id-1">商品名称</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" class="form-control" name="name">
+                                                </div>
+                                            </div>
+
+                                            <div class="line line-dashed b-b line-lg pull-in"></div>
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label" for="input-id-1">价格</label>
+                                                <div class="col-sm-8">
+                                                    <input type="number" class="form-control" name="price">
+                                                </div>
+                                            </div>
+
+                                            <div class="line line-dashed b-b line-lg pull-in"></div>
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label" for="input-id-1">库存</label>
+                                                <div class="col-sm-8">
+                                                    <input type="number" class="form-control" name="stock">
+                                                </div>
+                                            </div>
+
+                                            <div class="line line-dashed b-b line-lg pull-in"></div>
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label" for="input-id-1">商品条码</label>
+                                                <div class="col-sm-8">
+                                                    <input type="number" class="form-control" name="barcode">
+                                                </div>
+                                            </div>
+
+                                            <div class="line line-dashed b-b line-lg pull-in"></div>
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label" for="input-id-1">排序</label>
+                                                <div class="col-sm-8">
+                                                    <input type="number" class="form-control" name="displayorder">
+                                                </div>
+                                            </div>
+
+                                            <div class="line line-dashed b-b line-lg pull-in"></div>
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label" for="input-id-1">商品详情</label>
+                                                <div class="col-sm-8">
+                                                    <textarea id="form-content" name="details" class="editor" cols="30" rows="10"> </textarea>
+                                                </div>
+                                            </div>
+
+                                            <div class="line line-dashed b-b line-lg pull-in"></div>
+                                            <div class="form-group">
+                                                <div class="col-sm-12 col-sm-offset-6">
+                                                    <button type="button" class="btn btn-success" onclick="return postForm();">保存信息</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </section>
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+</section>
+
+<script src="{{asset('public/Simple')}}/js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('public/Simple')}}/js/bootstrap.js"></script>
+<!-- App -->
+<script src="{{asset('public/Simple')}}/js/app.js"></script>
+<script src="{{asset('public/Simple/library')}}/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Simple')}}/js/app.plugin.js"></script>
+<script src="{{asset('public/Simple/library')}}/file-input/bootstrap-filestyle.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/jPlayer/jquery.jplayer.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/jPlayer/add-on/jplayer.playlist.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/sweetalert/sweetalert.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/trumbowyg/trumbowyg.js"></script>
+<script src="{{asset('public/Simple/library')}}/trumbowyg/plugins/upload/trumbowyg.upload.js"></script>
+<script src="{{asset('public/Simple/library')}}/trumbowyg/plugins/base64/trumbowyg.base64.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#form-content').trumbowyg({
+            lang: 'fr',
+            closable: false,
+            mobile: true,
+            fixedBtnPane: true,
+            fixedFullWidth: true,
+            semantic: true,
+            resetCss: true,
+            autoAjustHeight: true,
+            autogrow: true
+        });
+    });
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.href = "{{asset("simple/goods/goods_edit?goods_id=")}}"+json.goods_id;
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                });
+            }
+        });
+    }
+</script>
+</body>
+</html>
---
+++ resources/views/Simple/Goods/goods_list.blade.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en" class="app">
+<head>
+    <meta charset="utf-8" />
+    <title>商品列表 | 零壹云管理平台 | 零售版店铺管理系统</title>
+    <link rel="stylesheet" href="{{asset('public/Simple/library/jPlayer')}}/jplayer.flat.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/app.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple/library/sweetalert')}}/sweetalert.css">
+    <!--[if lt IE 9]>
+    <script src="{{asset('public/Company/library/ie')}}/html5shiv.js"></script>
+    <script src="{{asset('public/Company/library/ie')}}/respond.min.js"></script>
+    <script src="{{asset('public/Company/library/ie')}}/excanvas.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<section class="vbox">
+    @include('Simple/Public/Header')
+    <section>
+        <section class="hbox stretch">
+            <!-- .aside -->
+            @include('Simple/Public/Nav')
+            <!-- /.aside -->
+            <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="m-b-md">
+                            <h3 class="m-b-none">商品查询</h3>
+                        </div>
+                        <section class="panel panel-default">
+                            <header class="panel-heading">
+                                商品列表
+                            </header>
+                            <div class="row wrapper">
+                                <form class="form-horizontal" method="get">
+                                    <input type="hidden" id="goods_delete_comfirm_url" value="{{ url('simple/ajax/goods_delete') }}">
+                                    <input type="hidden" id="goods_status_comfirm_url" value="{{ url('simple/ajax/goods_status') }}">
+                                    <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
+                                    <div class="col-sm-2">
+                                        <select name="category_id" class="form-control m-b">
+                                            <option value="0">所有分类</option>
+                                            @foreach($category as $key=>$val)
+                                                <option value="{{$val->id}}" @if($val->id == $search_data['category_id']) selected @endif >{{$val->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <label class="col-sm-1 control-label">商品标题</label>
+
+                                    <div class="col-sm-2">
+                                        <input class="input-sm form-control" size="16" type="text" name="goods_name" value="{{$search_data['goods_name']}}">
+                                    </div>
+
+                                    <div class="col-sm-3">
+                                        <button type="submit" class="btn btn-s-md btn-info"><i class="fa fa-search"></i>&nbsp;&nbsp;搜索</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table table-striped b-t b-light">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>商品标题</th>
+                                        <th>商品价格</th>
+                                        <th>商品分类</th>
+                                        <th>库存</th>
+                                        <th>排序</th>
+                                        <th>商品状态（点击可切换状态）</th>
+                                        <th>添加时间</th>
+                                        <th>操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($goods as $key=>$val)
+                                    <tr>
+                                        <td>{{$val->id}}</td>
+                                        <td>{{$val->name}}</td>
+                                        <td>
+                                            {{$val->price}}
+                                        </td>
+                                        <td>
+                                            <label class="label label-info">@if(!empty($val->category->name)){{$val->category->name}}@else其他@endif</label>
+                                        </td>
+                                        <td>
+                                            {{$val->stock}}
+                                        </td>
+                                        <td>
+                                            {{$val->displayorder}}
+                                        </td>
+                                        <td>
+                                            @if($val->status == 1)
+                                                <label class="label label-success" onclick="getlockForm('{{$val->id}}','{{$val->status}}')">销售中</label>
+                                            @elseif($val->status == 0)
+                                                <label class="label label-danger" onclick="getlockForm('{{$val->id}}','{{$val->status}}')">已下架</label>
+                                            @endif
+                                        </td>
+                                        <td>{{$val->created_at}}</td>
+                                        <td>
+                                            <button class="btn btn-info btn-xs" id="editBtn" onclick="location.href='{{url('simple/goods/goods_edit')}}?goods_id={{$val->id}}'"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>
+                                            <button class="btn btn-danger btn-xs" id="deleteBtn" onclick="getDeleteForm({{ $val->id }})"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <footer class="panel-footer">
+                                <div class="row">
+
+                                    <div class="col-sm-12 text-right text-center-xs">
+                                        {!! $goods->appends($search_data)->links() !!}
+                                    </div>
+                                </div>
+                            </footer>
+                        </section>
+
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+</section>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"></div>
+
+<script src="{{asset('public/Simple')}}/js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('public/Simple')}}/js/bootstrap.js"></script>
+<!-- App -->
+<script src="{{asset('public/Simple')}}/js/app.js"></script>
+<script src="{{asset('public/Simple')}}/js/app.plugin.js"></script>
+<script src="{{asset('public/Simple/library')}}/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/file-input/bootstrap-filestyle.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/jPlayer/jquery.jplayer.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/jPlayer/add-on/jplayer.playlist.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/sweetalert/sweetalert.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#deleteBtn').click(function(){
+            $('#myModal').modal();
+        });
+        $('#save_btn').click(function(){
+            swal({
+                title: "温馨提示",
+                text: "操作成功",
+                type: "success"
+            });
+        });
+    });
+
+    //上下架商品
+    function getlockForm(id,status){
+        var url = $('#goods_status_comfirm_url').val();
+        var token = $('#_token').val();
+        var data = {'_token':token,'id':id,'status':status};
+        $.post(url,data,function(response){
+            if(response.status=='-1'){
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                },function(){
+                    window.location.reload();
+                });
+                return;
+            }else{
+                $('#myModal').html(response);
+                $('#myModal').modal();
+            }
+        });
+    }
+
+    //删除商品信息
+    function getDeleteForm(id){
+        var url = $('#goods_delete_comfirm_url').val();
+        var token = $('#_token').val();
+        var data = {'_token':token,'id':id};
+        $.post(url,data,function(response){
+            if(response.status=='-1'){
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+                return;
+            }else{
+                $('#myModal').html(response);
+                $('#myModal').modal();
+            }
+        });
+    }
+</script>
+</body>
+</html>
