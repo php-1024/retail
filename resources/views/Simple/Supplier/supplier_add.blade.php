5426bedb4d99f59697f653d8df41f7a5105e0c9b
mail@54zm.com <mail@54zm.com>
2018-04-19 16:19:55 +0800

    '1'

---
+++ resources/views/Simple/Supplier/supplier_add.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en" class="app">
+<head>
+    <meta charset="utf-8" />
+    <title>新增供应商 | 零壹云管理平台 | 零售版店铺管理系统</title>
+    <link rel="stylesheet" href="{{asset('public/Simple/library/jPlayer')}}/jplayer.flat.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/app.css" type="text/css" />
+    <link rel="stylesheet" href="{{asset('public/Simple/library/sweetalert')}}/sweetalert.css">
+    <!--[if lt IE 9]>
+    <script src="{{asset('public/Simple/library/ie')}}/html5shiv.js"></script>
+    <script src="{{asset('public/Simple/library/ie')}}/respond.min.js"></script>
+    <script src="{{asset('public/Simple/library/ie')}}/excanvas.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<section class="vbox">
+    @include('Simple/Public/Header')
+    <section>
+        <section class="hbox stretch">
+            <!-- .aside -->
+        @include('Simple/Public/Nav')
+        <!-- /.aside -->
+            <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="row wrapper">
+                            <div class="col-sm-12">
+                                <button type="button" class="btn btn-s-md btn" onclick="window.location='supplier_list'">供应商列表</button>
+                                <button type="button" class="btn btn-s-md btn-success" onclick="window.location='supplier_add'">&nbsp;&nbsp;添加供应商</button>
+                            </div>
+                        </div>
+                        <section class="panel panel-default">
+
+                            <header class="panel-heading font-bold">
+                                添加商品供应商
+                            </header>
+                            <div class="panel-body">
+                                <form method="post" class="form-horizontal"  role="form" id="currentForm" action="{{ url('simple/ajax/supplier_add_check') }}">
+                                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">公司名称</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" name="company_name" value="">
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">联系人姓名</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" name="contactname">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">联系人电话</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" name="contactmobile">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">排序</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" name="displayorder" value="0">
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label" for="input-id-1">安全密码</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" name="safe_password" value="">
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                                    <div class="form-group">
+                                        <div class="col-sm-12 col-sm-offset-6">
+                                            <button type="button" class="btn btn-success" onclick="return postForm();">保存信息</button>
+                                        </div>
+                                    </div>
+                                    <div class="line line-dashed b-b line-lg pull-in"></div>
+                                </form>
+                            </div>
+                        </section>
+
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+</section>
+<!-- App -->
+<script src="{{asset('public/Simple')}}/js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('public/Simple')}}/js/bootstrap.js"></script>
+<!-- App -->
+<script src="{{asset('public/Simple')}}/js/app.js"></script>
+<script src="{{asset('public/Simple/library/slimscroll')}}/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Simple')}}/js/app.plugin.js"></script>
+<script src="{{asset('public/Simple/library/file-input')}}/bootstrap-filestyle.min.js"></script>
+<script src="{{asset('public/Simple/library/jPlayer')}}/jquery.jplayer.min.js"></script>
+<script src="{{asset('public/Simple/library/jPlayer')}}/add-on/jplayer.playlist.min.js"></script>
+<script src="{{asset('public/Simple/library/sweetalert')}}/sweetalert.min.js"></script>
+<script>
+    //提交表单
+    function postForm() {
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if (json.status == -1) {
+                window.location.reload();
+            } else if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                },function(){
+                    window.location.href='supplier_list';
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                });
+            }
+        });
+    }
+</script>
+</body>
+</html>
---
+++ resources/views/Simple/Supplier/supplier_list.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en" class="app">
+<head>
+    <meta charset="utf-8"/>
+    <title>供应商列表 | 零壹云管理平台 | 零售版店铺管理系统</title>
+    <link rel="stylesheet" href="{{asset('public/Simple/library/jPlayer')}}/jplayer.flat.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/bootstrap.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/animate.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font-awesome.min.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/simple-line-icons.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/font.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Simple')}}/css/app.css" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('public/Simple/library/sweetalert')}}/sweetalert.css">
+    <!--[if lt IE 9]>
+    <script src="{{asset('public/Simple/library/ie')}}/html5shiv.js"></script>
+    <script src="{{asset('public/Simple/library/ie')}}/respond.min.js"></script>
+    <script src="{{asset('public/Simple/library/ie')}}/excanvas.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<section class="vbox">
+    @include('Simple/Public/Header')
+    <section>
+        <section class="hbox stretch">
+            <!-- .aside -->
+        @include('Simple/Public/Nav')
+        <!-- /.aside -->
+            <section id="content">
+                <section class="vbox">
+                    <section class="scrollable padder">
+                        <div class="row wrapper">
+                            <div class="col-sm-12">
+                                <button type="button" class="btn btn-s-md btn-success" onclick="window.location='supplier_list'">供应商列表</button>
+                                <button type="button" class="btn btn-s-md btn" onclick="window.location='supplier_add'">&nbsp;&nbsp;添加供应商</button>
+                            </div>
+                        </div>
+                        <section class="panel panel-default">
+                            <header class="panel-heading">
+                                供应商列表
+                            </header>
+                            <div class="row wrapper">
+                                <form class="form-horizontal" method="get" id="searchForm" action="">
+                                    <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
+                                    <input type="hidden" id="supplier_edit_url" value="{{ url('simple/ajax/supplier_edit') }}">
+                                    <input type="hidden" id="supplier_delete_comfirm_url" value="{{ url('simple/ajax/supplier_delete') }}">
+                                    <label class="col-sm-1 control-label">供应商名称</label>
+                                    <div class="col-sm-2">
+                                        <input class="input-sm form-control" size="16" type="text" name="company_name" value="{{$search_data['company_name']}}">
+                                    </div>
+
+                                    <div class="col-sm-3">
+                                        <button type="submit" class="btn btn-s-md btn-info"><i class="fa fa-search"></i>&nbsp;&nbsp;搜索
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table table-striped b-t b-light">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>公司名称</th>
+                                        <th>联系人姓名</th>
+                                        <th>联系人电话</th>
+                                        <th>操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($supplier as $key=>$val)
+                                        <tr>
+                                            <td>{{ $val->id }}</td>
+                                            <td>{{ $val->company_name }}</td>
+                                            <td>{{ $val->contactname }}</td>
+                                            <td>{{ $val->contactmobile }}</td>
+                                            <td>
+                                                <button class="btn btn-info btn-xs" onclick="getEditForm({{ $val->id }})"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑
+                                                </button>
+                                                <button class="btn btn-danger btn-xs" onclick="getDeleteForm({{ $val->id }})"><i class="fa fa-times"></i>&nbsp;&nbsp;删除
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <footer class="panel-footer">
+                                <div class="row">
+                                    <div class="col-sm-12 text-right text-center-xs">
+                                        {!! $supplier->appends($search_data)->links() !!}
+                                    </div>
+                                </div>
+                            </footer>
+                        </section>
+
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+</section>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"></div>
+
+<script src="{{asset('public/Simple')}}/js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('public/Simple')}}/js/bootstrap.js"></script>
+<!-- App -->
+<script src="{{asset('public/Simple')}}/js/app.js"></script>
+<script src="{{asset('public/Simple/library')}}/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('public/Simple')}}/js/app.plugin.js"></script>
+<script src="{{asset('public/Simple/library')}}/file-input/bootstrap-filestyle.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/jPlayer/jquery.jplayer.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/jPlayer/add-on/jplayer.playlist.min.js"></script>
+<script src="{{asset('public/Simple/library')}}/sweetalert/sweetalert.min.js"></script>
+<script>
+
+    //编辑分类信息
+    function getEditForm(id) {
+        var url = $('#supplier_edit_url').val();
+        var token = $('#_token').val();
+        var data = {'id': id, '_token': token};
+        $.post(url, data, function (response) {
+            if (response.status == '-1') {
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                }, function () {
+                    window.location.reload();
+                });
+                return;
+            } else {
+                $('#myModal').html(response);
+                $('#myModal').modal();
+            }
+        });
+    }
+
+
+    //删除分类信息
+    function getDeleteForm(id){
+        var url = $('#supplier_delete_comfirm_url').val();
+        var token = $('#_token').val();
+        var data = {'_token':token,'id':id};
+        $.post(url,data,function(response){
+            if(response.status=='-1'){
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+                return;
+            }else{
+                $('#myModal').html(response);
+                $('#myModal').modal();
+            }
+        });
+    }
+</script>
+</body>
+</html>
