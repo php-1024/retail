c0af59f4ce7669e83581f8e02380147000c8b31e
kbzz <463661663@qq.com>
2018-04-20 16:45:32 +0800

    1

--- app/Http/Controllers/Api/AndroidSimpleApiController.php
+++ app/Http/Controllers/Api/AndroidSimpleApiController.php
@@ -492,6 +492,7 @@ class AndroidSimpleApiController extends Controller
             }
             DB::commit();//提交事务
         } catch (\Exception $e) {
+            dd($e);
             DB::rollBack();//事件回滚
             return response()->json(['msg' => '提交失败', 'status' => '0', 'data' => '']);
         }
