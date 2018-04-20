849101f251e7f8d75248f5b339eaaacd14cb6f53
kbzz <463661663@qq.com>
2018-04-20 16:23:47 +0800

    1

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -32,5 +32,21 @@ class VerifyCsrfToken extends Middleware
         'api/androidapi/stock_cfg',//查询店铺设置
         /****Android接口****/
 
+        /****Android接口****/
+        'api/androidSimpleApi/login',//登入
+        'api/androidSimpleApi/simple_login',//登入
+        'api/androidSimpleApi/goodscategory',//商品分类
+        'api/androidSimpleApi/goodslist',//商品列表
+        'api/androidSimpleApi/order_check',//订单提交接口
+        'api/androidSimpleApi/cancel_order',//取消订单接口
+        'api/androidSimpleApi/order_list',//订单列表接口
+        'api/androidSimpleApi/order_detail',//订单详情接口
+        'api/androidSimpleApi/cash_payment',//现金支付接口
+        'api/androidSimpleApi/other_payment',//其他支付接口
+        'api/androidSimpleApi/allow_zero_stock',//开启/关闭零库存开单接口
+        'api/androidSimpleApi/change_stock_role',//下单减库存/付款减库存接口
+        'api/androidSimpleApi/stock_cfg',//查询店铺设置
+        /****Android接口****/
+
     ];
 }
