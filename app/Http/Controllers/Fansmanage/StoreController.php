b85924d0bc6f4047427a4da9f9ba6bcadcd5f84a
xuho <781966658@qq.com>
2018-04-21 15:50:54 +0800

    x

--- app/Http/Controllers/Fansmanage/StoreController.php
+++ app/Http/Controllers/Fansmanage/StoreController.php
@@ -120,7 +120,6 @@ class StoreController extends Controller
         }else{
             $key = config("app.simple_encrypt_key");
         }
-
         // 密码加密
         $encrypted = md5($password);//加密密码第一重
         $encryptPwd = md5("lingyikeji" . $encrypted . $key);//加密密码第二重
