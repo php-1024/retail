71aa333174f368be69bf2a2b10ef6d8a3590377d
xuho <781966658@qq.com>
2018-04-21 12:12:37 +0800

    x

--- app/Http/Controllers/Fansmanage/StoreController.php
+++ app/Http/Controllers/Fansmanage/StoreController.php
@@ -40,6 +40,8 @@ class StoreController extends Controller
         $route_name = $request->path();
         // 需要渲染模式里面的数据
         $res = Organization::getProgramAlone(["id" => $admin_data["organization_id"]]);
+        dump($res);
+        exit;
         // 渲染页面
         return view('Fansmanage/Store/store_create', ['info' => $res, 'admin_data' => $admin_data, 'route_name' => $route_name, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data]);
     }
