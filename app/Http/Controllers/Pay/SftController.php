669ef42c978911fd2668feaf64354fdf06911e1e
xuho <781966658@qq.com>
2018-04-21 17:40:15 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -398,6 +398,7 @@ class SftController extends Controller
         // 静默授权：通过授权使用的code,获取到用户openid
         $res_access_arr = \Wechat::get_web_access_token($code, $appid, $appsecret);
 
+        dd($res_access_arr);
         // 如果不存在授权所特有的access_token,则重新获取code,并且验证
         if (!empty($res_access_arr['access_token'])) {
             $openid = $res_access_arr['openid'];
