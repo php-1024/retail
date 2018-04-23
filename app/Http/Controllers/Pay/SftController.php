ad958118d63a7dbab32b878fce40e93e33059d93
xuho <781966658@qq.com>
2018-04-23 15:35:37 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -297,7 +297,6 @@ class SftController extends Controller
             return;
         }
 
-
         // 判断 session 中是否存在店铺id
         if (empty(session("zerone_auth_info.shop_user_id"))) {
             $this->getAuthorizeShopInfo($url);
