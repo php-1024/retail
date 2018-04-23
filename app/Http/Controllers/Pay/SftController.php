7f3461df9f94674e5aee2167b491614427560891
xuho <781966658@qq.com>
2018-04-23 16:05:15 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -291,7 +291,6 @@ class SftController extends Controller
         if ($authorization_info === false) {
             return "微信公众号没有授权到第三方";
         }
-        dump(session("zerone_auth_info"));
 
         // 判断是否存在 零壹服务用户id
         if (empty(session("zerone_auth_info")["zerone_user_id"])) {
