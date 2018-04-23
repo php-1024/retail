49791524a6e9263af208fd0df94e0fc56102b1d4
mail@54zm.com <mail@54zm.com>
2018-04-23 14:04:38 +0800

    '0423'

--- app/Http/Controllers/Api/AndroidSimpleApiController.php
+++ app/Http/Controllers/Api/AndroidSimpleApiController.php
@@ -333,17 +333,16 @@ class AndroidSimpleApiController extends Controller
         DB::beginTransaction();
         try {
             if ($power == '1') {//说明付款减库存
-                if ($stock_status != 1) {//说明该订单的库存还未减去，这里的判断是为了防止用户频繁切换下单减库存，付款减库存设置的检测
+//                if ($stock_status != 1) {//说明该订单的库存还未减去，这里的判断是为了防止用户频繁切换下单减库存，付款减库存设置的检测
                     $re = $this->reduce_stock($order_id, '1');//减库存
                     if ($re != 'ok') {
                         return response()->json(['msg' => '提交订单失败', 'status' => '0', 'data' => '']);
-                    }
+//                    }
                 }
             }
             SimpleOrder::editSimpleOrder([['id', $order_id]], ['paytype' => $paytype, 'status' => '1']);//修改订单状态
             DB::commit();//提交事务
         } catch (\Exception $e) {
-            dd($e);
             DB::rollBack();//事件回滚
             return response()->json(['msg' => '现金付款失败', 'status' => '0', 'data' => '']);
         }
