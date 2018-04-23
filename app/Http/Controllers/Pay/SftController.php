0c71ea0a44f02cd742318ec38fe3e5723187d2dc
xuho <781966658@qq.com>
2018-04-23 14:59:04 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -336,7 +336,6 @@ class SftController extends Controller
     {
         $code = request()->input('code');
         $appid = $this->wechat_info["authorizer_appid"];
-
         if (empty($code)) {
             \Wechat::get_open_web_auth_url($appid, $url,2);
             exit;
