a5152fcb6c8a5c8a7e2c43d9de0204f39cccde2f
xuho <781966658@qq.com>
2018-04-23 15:20:06 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -298,6 +298,8 @@ class SftController extends Controller
             return "微信公众号没有授权到第三方";
         }
 
+
+        dd(session(["zerone_auth_info.zerone_user_id"]));
         // 判断是否存在 零壹服务用户id
         if (empty(session("zerone_auth_info.zerone_user_id"))) {
             $this->getAuthorizeZeroneInfo($url);
@@ -435,7 +437,6 @@ class SftController extends Controller
             return;
         }
 
-        dd(session(["zerone_auth_info"])["zerone_user_id"]);
         DB::beginTransaction();
         try {
             // 店铺公众号的信息
