a1a0311879dd0746f0f53e23a2072777b5466a56
mail@54zm.com <mail@54zm.com>
2018-04-23 17:51:50 +0800

    '0423'

--- resources/views/Fansmanage/Account/safe_password.blade.php
+++ resources/views/Fansmanage/Account/safe_password.blade.php
@@ -169,7 +169,14 @@
         var data = target.serialize();
         $.post(url, data, function (json) {
             if (json.status == -1) {
-                window.location.reload();
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                },function(){
+                    window.location.reload();
+                });
             } else if(json.status == 1) {
                 swal({
                     title: "提示信息",
