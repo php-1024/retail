433214764b1985aed76b3f6ffe762fb971046697
xuho <781966658@qq.com>
2018-04-24 09:36:32 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -274,7 +274,6 @@ class SftController extends Controller
     public function test11()
     {
         dump(session("zerone_auth_info"));
-        dump(1111);
         request()->attributes->add(['organization_id' => 5]); //添加参数
         $res = $this->authorizeInfo();
 
