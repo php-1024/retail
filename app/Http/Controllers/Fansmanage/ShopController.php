1e3d8af6971f63dbdaddf248b5647b4ef27a2641
mail@54zm.com <mail@54zm.com>
2018-04-24 16:54:01 +0800

    'simple'

--- app/Http/Controllers/Fansmanage/ShopController.php
+++ app/Http/Controllers/Fansmanage/ShopController.php
@@ -12,6 +12,8 @@ use App\Models\Account;
 use App\Models\LoginLog;
 use App\Models\OperationLog;
 use App\Models\Organization;
+use App\Models\OrganizationAssets;
+use App\Models\Program;
 use App\Services\ZeroneRedis\ZeroneRedis;
 use Illuminate\Http\Request;
 use Session;
@@ -62,8 +64,11 @@ class ShopController extends Controller
             $acc_num = Account::where([['organization_id', $organization_id]])->count();
             // 查询服务商附属商务个数
             $org_num = Organization::where([['parent_id', $organization_id]])->count();
+            //查询当前商户用的哪套程序
+            $program_id = OrganizationAssets::getPluck(["organization_id" => $admin_data["organization_id"]],'program_id')->first();
+            $program = Program::getOne(['id'=>$program_id]);//获取当前粉丝管理系统能使用的资产程序
             // 渲染页面
-            return view('Fansmanage/Shop/index', ['login_log_list' => $login_log_list, 'operation_log_list' => $operation_log_list, 'acc_num' => $acc_num, 'org_num' => $org_num, 'admin_data' => $admin_data, 'route_name' => $route_name, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data]);
+            return view('Fansmanage/Shop/index', ['login_log_list' => $login_log_list, 'program'=>$program, 'operation_log_list' => $operation_log_list, 'acc_num' => $acc_num, 'org_num' => $org_num, 'admin_data' => $admin_data, 'route_name' => $route_name, 'menu_data' => $menu_data, 'son_menu_data' => $son_menu_data]);
         }
     }
 
