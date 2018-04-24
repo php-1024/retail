18c761fd1f5ea33caf0951bc8c90e30d16cb403b
xuho <781966658@qq.com>
2018-04-24 14:38:42 +0800

    x

--- app/Http/Middleware/User/UserCheck.php
+++ app/Http/Middleware/User/UserCheck.php
@@ -148,6 +148,7 @@ class UserCheck
 
     public function setAuthorizeShopInfo($appid, $code, $re_url = "")
     {
+        dump(111);
         // 静默授权：通过授权使用的code,获取到用户openid
         $res_access_arr = \Wechat::get_open_web_access_token($appid, $code);
 
