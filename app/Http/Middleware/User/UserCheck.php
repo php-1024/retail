d3a122835da1da447ee7e5b68d3a1004d59c4969
xuho <781966658@qq.com>
2018-04-24 14:49:06 +0800

    x

--- app/Http/Middleware/User/UserCheck.php
+++ app/Http/Middleware/User/UserCheck.php
@@ -46,6 +46,7 @@ class UserCheck
 
         \Session::flash("zerone_auth_info");
 
+        exit;
         // 判断是否存在 零壹服务用户id
         if (empty(session("zerone_auth_info.zerone_user_id"))) {
             $this->getAuthorizeZeroneInfo($url);
