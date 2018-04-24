afc48f2b3141fda3c83aee2fe001d82d959d083c
xuho <781966658@qq.com>
2018-04-24 16:06:52 +0800

    x

--- app/Http/Middleware/User/UserCheck.php
+++ app/Http/Middleware/User/UserCheck.php
@@ -45,11 +45,10 @@ class UserCheck
             return "微信公众号没有授权到第三方";
         }
 
-//        // 判断是否存在 零壹服务用户id
-//        if (empty(session("zerone_auth_info.zerone_user_id"))) {
-//            $res = $this->getAuthorizeZeroneInfo($url);
-////            Header("Location:".request()->url());
-//        }
+        // 判断是否存在 零壹服务用户id
+        if (empty(session("zerone_auth_info.zerone_user_id"))) {
+            $this->getAuthorizeZeroneInfo($url);
+        }
 
         // 判断 session 中是否存在店铺id
         if (empty(session("zerone_auth_info.shop_user_id"))) {
@@ -89,6 +88,7 @@ class UserCheck
             \Wechat::get_open_web_auth_url($appid, $url);
         } else {
             $this->setAuthorizeShopInfo($appid, $code);
+            dump(session("zerone_auth_info"));
         }
     }
 
