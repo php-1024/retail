b5062cd98e9f33f0528a1130e8c25b5f11687710
xuho <781966658@qq.com>
2018-04-24 16:22:16 +0800

    x

--- app/Http/Middleware/User/UserCheck.php
+++ app/Http/Middleware/User/UserCheck.php
@@ -52,10 +52,10 @@ class UserCheck
         }
 
         // 判断 session 中是否存在店铺id
-        if (empty(session("zerone_auth_info.shop_user_id"))) {
-            $this->getAuthorizeShopInfo($url);
-            \Session::save();
-        }
+//        if (empty(session("zerone_auth_info.shop_user_id"))) {
+//            $this->getAuthorizeShopInfo($url);
+//            \Session::save();
+//        }
 
         // 添加参数
         request()->attributes->add(['zerone_auth_info' => session("zerone_auth_info")]);
