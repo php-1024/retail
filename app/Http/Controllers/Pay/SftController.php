238ac90c378b140e9bb08674241b5660bf2a2c81
xuho <781966658@qq.com>
2018-04-24 17:08:59 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -360,6 +360,7 @@ class SftController extends Controller
 
     public function test13()
     {
+
         $this->getShopBaseInfo();
         $code = request()->input('code');
         $appid = $this->wechat_info["authorizer_appid"];
