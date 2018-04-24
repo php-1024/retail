5b958b22366fc1a6cb5f32ea52c05ba720ff3da8
xuho <781966658@qq.com>
2018-04-24 14:37:51 +0800

    x

--- app/Http/Middleware/User/UserCheck.php
+++ app/Http/Middleware/User/UserCheck.php
@@ -44,10 +44,10 @@ class UserCheck
             return "微信公众号没有授权到第三方";
         }
 
-        // 判断是否存在 零壹服务用户id
-        if (empty(session("zerone_auth_info.zerone_user_id"))) {
-            $this->getAuthorizeZeroneInfo($url);
-        }
+//        // 判断是否存在 零壹服务用户id
+//        if (empty(session("zerone_auth_info.zerone_user_id"))) {
+//            $this->getAuthorizeZeroneInfo($url);
+//        }
 
         // 判断 session 中是否存在店铺id
         if (empty(session("zerone_auth_info.shop_user_id"))) {
