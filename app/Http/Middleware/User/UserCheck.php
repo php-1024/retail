68cff74a06a96b575196e999972957ad0fdb37f8
xuho <781966658@qq.com>
2018-04-24 10:17:08 +0800

    x

--- app/Http/Middleware/User/UserCheck.php
+++ app/Http/Middleware/User/UserCheck.php
@@ -103,6 +103,7 @@ class UserCheck
     {
         // 获取组织id
         $organization_id = request()->get("organization_id");
+        dump($organization_id);
         // 获取公众号的基本信息
         $res = WechatAuthorization::getAuthInfo(["organization_id" => $organization_id], ["authorizer_appid", "authorizer_access_token"]);
 
