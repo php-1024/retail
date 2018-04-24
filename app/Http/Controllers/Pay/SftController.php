686f8283a87b8c2ad8cf5606bea59a2ee878da9b
xuho <781966658@qq.com>
2018-04-24 15:50:15 +0800

    x

--- app/Http/Controllers/Pay/SftController.php
+++ app/Http/Controllers/Pay/SftController.php
@@ -273,7 +273,11 @@ class SftController extends Controller
 
     public function test11()
     {
-        return session("zerone_auth_info");
+        if(empty(session("zerone_auth_info.zerone_user_id")) || empty(session("zerone_auth_info.shop_user_id"))) {
+          return redirect(request()->url());
+        }else{
+            return session("zerone_auth_info");
+        }
 //        request()->attributes->add(['organization_id' => 5]); //添加参数
 //        $res = $this->authorizeInfo();
 //        \Session::flash("zerone_auth_info");
