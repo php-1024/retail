cf14d20e92f197883c8cb3a37904ab850d1f8f68
mail@54zm.com <mail@54zm.com>
2018-04-24 14:13:48 +0800

    'simple'

--- app/Http/Controllers/Zerone/DashboardController.php
+++ app/Http/Controllers/Zerone/DashboardController.php
@@ -76,6 +76,7 @@ class DashboardController extends Controller
         foreach ($zone_info->province as $key => $val) {
             $selected_province[] = $val->id;
         }
+        dump($selected_province);
         return view('Zerone/Dashboard/warzone_edit', ['zone_info' => $zone_info, 'province' => $province]);
     }
 
