69c21280dab28cc0cce64b7caa17c47a7896aeca
mail@54zm.com <mail@54zm.com>
2018-04-25 16:56:27 +0800

    '111'

--- app/Models/SimpleGoods.php
+++ app/Models/SimpleGoods.php
@@ -52,7 +52,7 @@ class SimpleGoods extends Model{
 
     //查询数据是否存在（仅仅查询ID增加数据查询速度）
     public static function checkRowExists($where){
-        $row = self::getPluck($where,'id');
+        $row = self::where($where)->get();
         if(empty($row)){
             return false;
         }else{
