d929edca6a96fc3f06213e9183e96f598c4e4e75
xuho <781966658@qq.com>
2018-04-25 15:28:11 +0800

    x

--- resources/views/Fansmanage/Shop/index.blade.php
+++ resources/views/Fansmanage/Shop/index.blade.php
@@ -58,7 +58,11 @@
                                         <div class="form-group clearfix text-center m-t">
                                             <div class="inline">
                                                 <div class="thumb-lg">
-                                                    <img src="{{asset($company_info->logo)}}" class="img-circle" alt="...">
+                                                    @if(!empty($company_info->logo))
+                                                        <img src="{{asset('public/Fansmanage')}}/img/a5.png" class="img-circle" alt="...">
+                                                    @else
+                                                        <img src="{{asset($company_info->logo)}}" class="img-circle" alt="...">
+                                                    @endif
                                                 </div>
                                             </div>
                                         </div>
