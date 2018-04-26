75913dcd96086259e2d7253796a1614b6f8ae71c
xho <781966658.com>
2018-04-26 17:24:24 +0800

    x

--- app/Http/Middleware/User/UserCheck.php
+++ app/Http/Middleware/User/UserCheck.php
@@ -55,14 +55,16 @@ class UserCheck
             return "微信公众号没有授权到第三方";
         }
 
-        var_dump(session("zerone_auth_info"));
-        exit;
 
         // 判断是否存在 零壹服务用户id
         if (empty(session("zerone_auth_info.zerone_user_id"))) {
             XhoLog::create(["name"=>"跳转1","content"=>"zerone_user_id"]);
             Header("Location:http://develop.01nnt.com/pay/sft/test12");
         }
+
+
+        var_dump(session("zerone_auth_info"));
+        exit;
 
         // 判断 session 中是否存在店铺id
         if (empty(session("zerone_auth_info.shop_user_id"))) {
