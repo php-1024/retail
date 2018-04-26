8d2300df627d8d535c7ea06cd2f746911558978e
mail@54zm.com <mail@54zm.com>
2018-04-26 17:13:38 +0800

    'lingyi'

--- app/Models/Statistics.php
+++ app/Models/Statistics.php
@@ -21,7 +21,9 @@ class Statistics extends Model
     //修改账号
     public static function editStatistics($where,$param){
         $model = self::where($where)->first();
-        $model->item_value = $param['item_value'];
+        foreach($param as $key=>$val){
+            $model->$key=$val;
+        }
         $model->save();
     }
 }
