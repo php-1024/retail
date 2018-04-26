0fe12b50c7cffc1fd5b7fdeca039e92469e192e7
xho <781966658.com>
2018-04-26 15:12:17 +0800

    x

--- app/Http/Middleware/Api/WechatApiCheck.php
+++ app/Http/Middleware/Api/WechatApiCheck.php
@@ -150,10 +150,9 @@ class WechatApiCheck
 
         // 判断是否存在 零壹服务用户id
         if (empty(session("zerone_auth_info.zerone_user_id"))) {
-            dump(session("zerone_auth_info.zerone_user_id"));
             Header("Location:".request()->root() . "/api/authApi/zerone_auth");
         }
-
+        dump(session("zerone_auth_info.zerone_user_id"));
 
         exit;
         // 判断 session 中是否存在店铺id
