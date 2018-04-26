82786fa7cb12a30236d04fc7fc8c0528539002f5
xho <781966658.com>
2018-04-26 15:35:05 +0800

    x

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -132,6 +132,8 @@ class AuthApiController extends Controller
         // 静默授权：通过授权使用的code,获取到用户openid
         $res_access_arr = \Wechat::get_open_web_access_token($appid, $code);
 
+        var_dump($res_access_arr);
+        exit;
         // 如果不存在授权所特有的access_token,则重新获取code,并且验证
         if (!empty($res_access_arr['access_token'])) {
             $openid = $res_access_arr['openid'];
