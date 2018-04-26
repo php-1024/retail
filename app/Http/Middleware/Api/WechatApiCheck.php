f8ee2b7e171ed546c1217aaefba6998fa5756c7c
kbzz <463661663@qq.com>
2018-04-26 18:00:06 +0800

    1

--- app/Http/Middleware/Api/WechatApiCheck.php
+++ app/Http/Middleware/Api/WechatApiCheck.php
@@ -25,6 +25,7 @@ class WechatApiCheck
                 break;
             case "api/wechatApi/category"://检测店铺分类提交数据
             case "api/wechatApi/goods_list"://检测店铺分类提交数据
+            case "api/wechatApi/shopping_cart_list"://检测店铺购物车列表提交数据
                 $re = $this->checkTokenAndCategory($request);
                 return self::format_response($re, $next);
                 break;
