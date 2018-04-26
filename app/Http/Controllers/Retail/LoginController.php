71b3bf52159fb1595e5e1a5586fbb2776495f9c4
mail@54zm.com <mail@54zm.com>
2018-04-26 17:56:37 +0800

    'lingyi'

--- app/Http/Controllers/Retail/LoginController.php
+++ app/Http/Controllers/Retail/LoginController.php
@@ -20,6 +20,7 @@ class LoginController extends Controller
     //登录处理
     public function display()
     {
+        dump(Session::get('retail_system_captcha'));
         return view('Retail/Login/display');
     }
 
