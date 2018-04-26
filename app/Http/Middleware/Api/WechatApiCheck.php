fb24799fa27c3aaf9f3e643d5b3af61c1cedc8da
xho <781966658.com>
2018-04-26 18:34:49 +0800

    x

--- app/Http/Middleware/Api/WechatApiCheck.php
+++ app/Http/Middleware/Api/WechatApiCheck.php
@@ -44,7 +44,7 @@ class WechatApiCheck
 //            case "api/authApi/shop_auth" :
                 // 授权完毕中转站
             case "api/authApi/change_trains" :
-                request()->offsetSet('organization_id', 2);
+                request()->offsetSet('organization_id', 3);
                 $this->checkToken($request);
 
         }
