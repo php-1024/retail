18accc8231a8c0b1cb6e492d2206546546c3d4c6
781966658@qq.com <781966658@qq.com>
2018-04-27 19:48:52 +0800

    x

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -207,7 +207,6 @@ class AuthApiController extends Controller
     public function changeTrains()
     {
         $url = session("zerone_auth_info.initial_url_address");
-        var_dump(session("zerone_auth_info"));exit;
         Header("Location:{$url}");
     }
 
