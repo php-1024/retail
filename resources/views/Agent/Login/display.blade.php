6724da4c0bd5f2b872495c03da53589db4ca550a
kbzz <463661663@qq.com>
2018-04-27 09:58:22 +0800

    1

--- resources/views/Agent/Login/display.blade.php
+++ resources/views/Agent/Login/display.blade.php
@@ -67,13 +67,18 @@
             if(json.status==1){
                 window.location.reload();
             }else{
-                swal({
-                    title: "提示信息",
-                    text: json.data,
-                    confirmButtonColor:"#DD6B55",
-                    confirmButtonText: "确定",
-                    //type: "warning"
-                });
+
+                console.log(json);
+
+
+
+//                swal({
+//                    title: "提示信息",
+//                    text: json.data,
+//                    confirmButtonColor:"#DD6B55",
+//                    confirmButtonText: "确定",
+//                    //type: "warning"
+//                });
                 re_captcha();
             }
         });
