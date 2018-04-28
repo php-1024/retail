845a3cceb58908af4a4bb1c5da033a4b1d895322
suolong520 <1172468462@qq.com>
2018-04-28 17:02:44 +0800

    0

---
+++ public/Wechat/new-retail/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+       <div class="page">
+         <router-view/>
+       </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.page{
+  overflow: auto;
+}
+</style>
