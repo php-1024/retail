ee160cc9c18f8f3026e8c046bd6ee1385844b061
mail@54zm.com <mail@54zm.com>
2018-04-28 16:36:40 +0800

    'lingyi'

--- app/Http/Controllers/Simple/LoginController.php
+++ app/Http/Controllers/Simple/LoginController.php
@@ -56,7 +56,7 @@ class LoginController extends Controller
         } else {
             $key = config("app.simple_encrypt_key");//获取加密盐（分店专用）
         }
-        if (!empty($account_info->uuid)){
+        if ($account_info->deepth != 1){
             return response()->json(['data' => '您的账号是操作员身份，操作员不能登录后台，谢谢您的配合！', 'status' => '0']);
         }
         $encrypted = md5($password);//加密密码第一重
@@ -68,7 +68,6 @@ class LoginController extends Controller
             if (!empty($account_info)) {
                 if ($encryptPwd != $account_info->password) {//查询密码是否对的上
                     ErrorLog::addErrorTimes($ip, 12);
-                    dd($encryptPwd);
                     return response()->json(['data' => '登录账号、手机号或密码输入错误', 'status' => '0']);
                 } elseif ($account_info->status <> '1') {//查询账号状态
                     ErrorLog::addErrorTimes($ip, 12);
