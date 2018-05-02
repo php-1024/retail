88055d727511248e5f1ea93eda56444ea4059f30
kbzz <463661663@qq.com>
2018-05-02 16:36:27 +0800

    1

--- app/Http/Middleware/Api/WechatCheck.php
+++ app/Http/Middleware/Api/WechatCheck.php
@@ -29,6 +29,7 @@ class WechatCheck
     {
         // 获取组织id
         $organization_id = request()->get("organization_id");
+        echo $organization_id;exit;
         // 判断公众号是否授权给零壹第三方公众号平台
         $res = $this->getShopBaseInfo($organization_id);
         if ($res === false) {
