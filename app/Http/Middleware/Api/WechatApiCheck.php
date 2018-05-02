d81fbc864ac5b36e0aa7d9dd72f7c808e2bc6256
kbzz <463661663@qq.com>
2018-05-02 11:38:57 +0800

    1

--- app/Http/Middleware/Api/WechatApiCheck.php
+++ app/Http/Middleware/Api/WechatApiCheck.php
@@ -184,56 +184,56 @@ class WechatApiCheck
      */
     public function checkToken($request)
     {
-//        // 获取组织id
-//        $organization_id = request()->get("organization_id");
-//        // 判断公众号是否授权给零壹第三方公众号平台
-//        $res = $this->getShopBaseInfo($organization_id);
-//        if ($res === false) {
-//            exit("微信公众号没有授权到第三方");
-//        }
-//
-//        // 判断组织id 是否 跟之前一致
-//        if (!empty(session("zerone_auth_info.organization_id")) && session("zerone_auth_info.organization_id") != $organization_id) {
-//            \Session::put("zerone_auth_info", "");
-//        }
-//
-//        session(["zerone_auth_info.organization_id" => $organization_id]);
-//
-//        // 跳转自己的地址
-//        $self_path = ["api/authApi/zerone_auth", "api/authApi/shop_auth", "api/authApi/change_trains"];
-//        // 初次访问的地址
-//        $url = request()->fullUrl();
-//        if (!in_array(request()->path(), $self_path)) {
-//            session(["zerone_auth_info.initial_url_address" => $url]);
-//            \Session::save();
-//        }
-//
-//        // 判断是否存在 地址
-//        if (empty(session("zerone_auth_info.initial_url_address"))) {
-//            Header("Location:" . $url);
-//            return ;
-//        }
-//
-//        // 刷新并获取授权令牌
-//        $authorization_info = \Wechat::refresh_authorization_info($organization_id);
-//        if ($authorization_info === false) {
-//            exit("微信公众号没有授权到第三方");
-//        }
-//        // 判断是否存在 零壹服务用户id
-//        if (empty(session("zerone_auth_info.zerone_user_id"))) {
-//            Header("Location:" . request()->root() . "/api/authApi/zerone_auth?initial_url_address=$url");
-//            return;
-//        }
-//
-//
-//        // 判断 session 中是否存在店铺id
-//        if (empty(session("zerone_auth_info.shop_user_id"))) {
-//            Header("Location:" . request()->root() . "/api/authApi/shop_auth?organization_id={$organization_id}");
-//            return;
-//        }
-//
-//        // 添加参数
-//        request()->attributes->add(['zerone_auth_info' => session("zerone_auth_info")]);
+        // 获取组织id
+        $organization_id = request()->get("organization_id");
+        // 判断公众号是否授权给零壹第三方公众号平台
+        $res = $this->getShopBaseInfo($organization_id);
+        if ($res === false) {
+            exit("微信公众号没有授权到第三方");
+        }
+
+        // 判断组织id 是否 跟之前一致
+        if (!empty(session("zerone_auth_info.organization_id")) && session("zerone_auth_info.organization_id") != $organization_id) {
+            \Session::put("zerone_auth_info", "");
+        }
+
+        session(["zerone_auth_info.organization_id" => $organization_id]);
+
+        // 跳转自己的地址
+        $self_path = ["api/authApi/zerone_auth", "api/authApi/shop_auth", "api/authApi/change_trains"];
+        // 初次访问的地址
+        $url = request()->fullUrl();
+        if (!in_array(request()->path(), $self_path)) {
+            session(["zerone_auth_info.initial_url_address" => $url]);
+            \Session::save();
+        }
+
+        // 判断是否存在 地址
+        if (empty(session("zerone_auth_info.initial_url_address"))) {
+            Header("Location:" . $url);
+            return ;
+        }
+
+        // 刷新并获取授权令牌
+        $authorization_info = \Wechat::refresh_authorization_info($organization_id);
+        if ($authorization_info === false) {
+            exit("微信公众号没有授权到第三方");
+        }
+        // 判断是否存在 零壹服务用户id
+        if (empty(session("zerone_auth_info.zerone_user_id"))) {
+            Header("Location:" . request()->root() . "/api/authApi/zerone_auth?initial_url_address=$url");
+            return;
+        }
+
+
+        // 判断 session 中是否存在店铺id
+        if (empty(session("zerone_auth_info.shop_user_id"))) {
+            Header("Location:" . request()->root() . "/api/authApi/shop_auth?organization_id={$organization_id}");
+            return;
+        }
+
+        // 添加参数
+        request()->attributes->add(['zerone_auth_info' => session("zerone_auth_info")]);
         return self::res(1, $request);
     }
 
