2d5ba8f2bb08f3f5687c2e39a3b1c6a7f3e63a1d
kbzz <463661663@qq.com>
2018-05-02 15:10:12 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -986,14 +986,14 @@ Route::group(['prefix' => 'api'], function () {
 
     Route::group(['prefix' => 'authApi'], function () {
         // 零壹服务授权
-        Route::any('zerone_auth', 'Api\AuthApiController@getZeroneAuth')->middleware('WechatApiCheck');
+        Route::any('zerone_auth', 'Api\AuthApiController@getZeroneAuth')->middleware('WechatCheck');
         // 商户公众号授权
-        Route::any('shop_auth', 'Api\AuthApiController@getShopAuth')->middleware('WechatApiCheck');
+        Route::any('shop_auth', 'Api\AuthApiController@getShopAuth')->middleware('WechatCheck');
         // 授权完毕中转站
-        Route::any('change_trains', 'Api\AuthApiController@changeTrains')->middleware('WechatApiCheck');
+        Route::any('change_trains', 'Api\AuthApiController@changeTrains')->middleware('WechatCheck');
         // 测试
-        Route::any('test11', 'Api\AuthApiController@test11')->middleware('WechatApiCheck');
-        Route::any('test12', 'Api\AuthApiController@test12')->middleware('WechatApiCheck');
+        Route::any('test11', 'Api\AuthApiController@test11')->middleware('WechatCheck');
+        Route::any('test12', 'Api\AuthApiController@test12')->middleware('WechatCheck');
 
     });
 
