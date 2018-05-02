acd3f9b3326cb0cc0c3f62d3726ad2ca12416964
kbzz <463661663@qq.com>
2018-05-02 16:43:21 +0800

    1

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -17,11 +17,9 @@ class WechatController extends Controller
      */
     public function display(Request $request)
     {
-       echo session("zerone_auth_info.organization_id");exit;
-        session("organization_id",request()->get("zerone_auth_info.organization_id"));
-        session("zerone_user_id",request()->get("zerone_auth_info.zerone_user_id"));
-        session("fansmanage_user_id",request()->get("zerone_auth_info.shop_user_id"));
-        echo  $aa = Session::get('fansmanage_user_id');
+       echo session("zerone_auth_info.organization_id");
+       echo session("zerone_auth_info.zerone_user_id");
+       echo session("zerone_auth_info.shop_user_id");
         // 渲染页面
         return view('Simple/Wechat/display');
 
