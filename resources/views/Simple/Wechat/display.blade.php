b0764b5cae96412b3063f52e87c1a7ca9fdcde70
suolong520 <1172468462@qq.com>
2018-05-03 17:46:12 +0800

    0

--- resources/views/Simple/Wechat/display.blade.php
+++ resources/views/Simple/Wechat/display.blade.php
@@ -193,11 +193,12 @@
      wx.ready(function(){
          // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
          wx.checkJsApi({
-         jsApiList: ['chooseImage'] // 需要检测的JS接口列表，所有JS接口列表见附录2,
-         success: function(res) {
-         	alert(res);
-             // 以键值对的形式返回，可用的api值true，不可用为false
-             // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+	         jsApiList: ['chooseImage'] // 需要检测的JS接口列表，所有JS接口列表见附录2,
+	         success: function(res) {
+	         	alert(res);
+	             // 以键值对的形式返回，可用的api值true，不可用为false
+	             // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+	         });
          });
      });
      wx.error(function(res){
