b88c2ac55fc797d9b48ed6a4f8fd862449c2789c
suolong520 <1172468462@qq.com>
2018-05-03 17:49:44 +0800

    0

--- resources/views/Simple/Wechat/display.blade.php
+++ resources/views/Simple/Wechat/display.blade.php
@@ -192,14 +192,18 @@
       });
      wx.ready(function(){
          // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-         wx.checkJsApi({
-	         jsApiList: ['chooseImage'] // 需要检测的JS接口列表，所有JS接口列表见附录2,
-	         success: function(res) {
-	         	alert(res);
-	             // 以键值对的形式返回，可用的api值true，不可用为false
-	             // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-	         });
-         });
+         // 1 判断当前版本是否支持指定 JS 接口，支持批量判断
+           document.querySelector('#checkJsApi').onclick = function () {
+             wx.checkJsApi({
+               jsApiList: [
+                 'getNetworkType',
+                 'previewImage'
+               ],
+               success: function (res) {
+                 alert(JSON.stringify(res));
+               }
+             });
+           };
      });
      wx.error(function(res){
          // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
