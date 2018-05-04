9b77d78175e749afd361221ed20acc1e8554ef66
suolong520 <1172468462@qq.com>
2018-05-04 15:13:21 +0800

    0

--- public/Wechat/js/selectshop.js
+++ public/Wechat/js/selectshop.js
@@ -8,7 +8,40 @@ $(function(){
 	$("#month").html(dateri+"<span>"+month+"月</span>");
 });
 function searchshop(){
-	alert();
+     wx.getLocation({
+      success: function (res) {
+      	var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+	       	var longitude = res.longitude ; // 经度，浮点数，范围为180 ~ -180。
+	        var speed = res.speed; // 速度，以米/每秒计
+	        var accuracy = res.accuracy; // 位置精度
+	        var organization_id=$("#organization_id").val();
+	        var _token=$("#_token").val();
+	        var keyword=$("#keyword").val();
+	        var url = "http://develop.01nnt.com/api/wechatApi/store_list";
+         $.showPreloader('加载中');
+         //获取店铺
+	        $.post(
+	        	url,
+	            {'organization_id': organization_id,'_token':_token,'keyword':keyword,'lat':latitude,'lng':longitude},
+	        	function(json){
+	        		if (json.status == 1) {
+	        			var str="";
+	        			for (var i = json.data.storelist.length - 1; i >= 0; i--) {
+    					str += shoplist(json.data.storelist[i].name,json.data.storelist[i].address,json.data.storelist[i].logo,json.data.storelist[i].distance);
+	        			}
+	        			var $shoplist = $("#shoplist");
+	        			var $shopnumber = $("#shopnumber");
+	        			$shoplist.empty();
+	        			$shoplist.append(str);
+	        			$shopnumber.text("("+json.data.storelist.length+"家)");
+    				$.hidePreloader();
+	        		}
+        	})
+      },
+      cancel: function (res) {
+        alert('用户拒绝授权获取地理位置');
+      }
+    });
 }
 
 function shoplist(name,address,logo,distance) {
