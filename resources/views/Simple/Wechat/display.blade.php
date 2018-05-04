3187d673314ae09ace963541df1fbb036af9bee7
suolong520 <1172468462@qq.com>
2018-05-04 11:31:40 +0800

    0

--- resources/views/Simple/Wechat/display.blade.php
+++ resources/views/Simple/Wechat/display.blade.php
@@ -46,19 +46,7 @@
 			</div>
 			<div class="shoplist_box">
 				<ul id="shoplist">
-					<li>
-						<a href="javascript:;">
-							<div class="shop_img">
-								<img src="images/emotion@2x.png">
-							</div>
-							<div class="shop_right">
-								<section class="shop_name"><h3><span>半夏花年（南山店）</span></h3></section>
-								<section class="shop_youhui"><p>满120减10</p></section>
-								<section class="shop_juli"><p>小吃快餐<span><em><</em>&nbsp;920m</span></p></section>
-								<section class="shop_juli"><p>地址:广东省深圳市龙岗区龙城广场万汇大厦1606</p></section>
-							</div>
-						</a>
-					</li>
+					
 				</ul>
 			</div>
 	    </div>
