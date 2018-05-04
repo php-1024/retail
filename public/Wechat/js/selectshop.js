0c7ecd919d2c756af96bae25d2642f948a4ce894
suolong520 <1172468462@qq.com>
2018-05-04 11:44:47 +0800

    0

--- public/Wechat/js/selectshop.js
+++ public/Wechat/js/selectshop.js
@@ -20,7 +20,7 @@ $(function(){
 	var month = myDate.getMonth();
 	var dateri = myDate.getDate();
 	var arr = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'];
-	arr[parseInt(month+1,10)-1]
-	console.log("month"+parseInt(month+1));
+	var shijian = arr[parseInt(month+1,10)-1]
+	console.log("month"+shijian);
 	console.log(dateri);
 });
