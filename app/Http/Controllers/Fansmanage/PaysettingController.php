1c59f60a6d1bae49b9b85613b76444fa3f02470b
xho <781966658.com>
2018-05-05 17:11:24 +0800

    x

--- app/Http/Controllers/Fansmanage/PaysettingController.php
+++ app/Http/Controllers/Fansmanage/PaysettingController.php
@@ -112,7 +112,8 @@ class PaysettingController extends Controller
         $validate = \Validator::make($data, $rule, $message);
 
         if (!$validate->passes()) {
-            dd($validate->errors());
+            $res = $validate->errors();
+dd($res);
         }
 
         dd(123);
