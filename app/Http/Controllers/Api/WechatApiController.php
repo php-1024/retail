d5bb813bbdb093a18c6ac60a63c6faf969384d36
kbzz <463661663@qq.com>
2018-05-05 11:29:50 +0800

    1

--- app/Http/Controllers/Api/WechatApiController.php
+++ app/Http/Controllers/Api/WechatApiController.php
@@ -441,9 +441,8 @@ class WechatApiController extends Controller
         $dispatch_info = '';
         if ($dispatch->toArray()) {
             foreach ($dispatch->toArray() as $key => $value) {
-                $dispatch_info = DispatchProvince::getList([['dispatch_id',$value['id']]],'','id','DESC',['dispatch_id','province_id','first_weight','additional_weight','freight','renewal']);
-                print_r($dispatch_info->toArray());
-            }exit;
+                $dispatch_info[$key] = DispatchProvince::getList([['dispatch_id',$value['id']]],'','id','DESC',['dispatch_id','province_id','first_weight','additional_weight','freight','renewal'])->toArray();
+            }
 
         }
         $data = ['status' => '1', 'msg' => '查询成功', 'data' => ['address_info' => $address_info, 'dispatch_info' => $dispatch_info]];
