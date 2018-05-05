a593a5f57df2dd1df3b051a59b589662bdc9fb7f
xho <781966658.com>
2018-05-05 17:32:57 +0800

    x

--- app/Models/WechatPay.php
+++ app/Models/WechatPay.php
@@ -23,6 +23,7 @@ class WechatPay extends Model
     public static function getInfo($where = [], $field = [])
     {
         $res = self::select($field)->where($where)->first();
+        var_dump($res);
         if (!empty($res)) {
             return $res->toArray();
         } else {
