8879140ed0b1248de455bb0d8987b558e71be0d4
xho <781966658.com>
2018-05-05 17:27:54 +0800

    x

--- app/Http/Controllers/Fansmanage/PaysettingController.php
+++ app/Http/Controllers/Fansmanage/PaysettingController.php
@@ -92,34 +92,7 @@ class PaysettingController extends Controller
         $data["organization_id"] = $organization_id = request()->get('organization_id');
 
 
-        $rule = [
-            'appid' => 'required',
-            'appsecret' => 'required',
-            'mchid' => 'required',
-            'api_key' => 'required',
-            'apiclient_cert_pem' => 'required',
-            'apiclient_key_pem' => 'required',
-        ];
-        $message = [
-            "appid.required" => "appid 必须填写",
-            "appsecret.required" => "appsecret 必须填写",
-            "mchid.required" => "mchid 必须填写",
-            "api_key.required" => "api_key 必须填写",
-            "apiclient_cert_pem.required" => "apiclient_cert_pem 必须填写",
-            "apiclient_key_pem.required" => "apiclient_key_pem 必须填写",
-        ];
 
-        $validate = \Validator::make($data, $rule, $message);
-
-        if (!$validate->passes()) {
-            $error_msg = $validate->errors();
-            $res = json_encode($error_msg, JSON_UNESCAPED_UNICODE);
-            $res = json_decode($res, true);
-            foreach ($res as $val) {
-                $error_msg = $val[0];
-            }
-            return response()->json(['data' => $error_msg, 'status' => '0']);
-        }
 
         // 事务处理
         DB::beginTransaction();
@@ -148,5 +121,39 @@ class PaysettingController extends Controller
         return response()->json(['data' => '编辑微信支付信息成功', 'status' => '1']);
     }
 
+
+    public function validateMsg($data)
+    {
+        $rule = [
+            'organization_id' => 'required',
+            'appid' => 'required',
+            'appsecret' => 'required',
+            'mchid' => 'required',
+            'api_key' => 'required',
+            'apiclient_cert_pem' => 'required',
+            'apiclient_key_pem' => 'required',
+        ];
+        $message = [
+            "organization_id.required" => "organization_id 必须填写",
+            "appid.required" => "appid 必须填写",
+            "appsecret.required" => "appsecret 必须填写",
+            "mchid.required" => "mchid 必须填写",
+            "api_key.required" => "api_key 必须填写",
+            "apiclient_cert_pem.required" => "apiclient_cert_pem 必须填写",
+            "apiclient_key_pem.required" => "apiclient_key_pem 必须填写",
+        ];
+
+        $validate = \Validator::make($data, $rule, $message);
+
+        if (!$validate->passes()) {
+            $error_msg = $validate->errors();
+            $res = json_encode($error_msg, JSON_UNESCAPED_UNICODE);
+            $res = json_decode($res, true);
+            foreach ($res as $val) {
+                $error_msg = $val[0];
+            }
+            return response()->json(['data' => $error_msg, 'status' => '0']);
+        }
+    }
 }
 
