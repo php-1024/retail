e5d78b98622affdf8d92542ceaca21be5606cccc
suolong520 <1172468462@qq.com>
2018-05-05 17:08:28 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -72,7 +72,7 @@ $(function(){
     		if (json.status == 1) {
                 for (var i = 0; i < json.data.goodslist.length; i++) {
                     str += goods_list_box(json.data.goodslist[i].name,json.data.goodslist[i].details,
-                    json.data.goodslist[i].stock,json.data.goodslist[i].price,json.data.goodslist[i].thumb[0].thumb);
+                    json.data.goodslist[i].stock,json.data.goodslist[i].price,json.data.goodslist[i].thumb[0]);
                 }
                 var goodslist = $("#goodslist");
                 goodslist.empty();
