1d1c3e78a14914ee96c2b0028f048a65d3582b1b
suolong520 <1172468462@qq.com>
2018-05-07 10:06:32 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -1,14 +1,15 @@
-$(function(){
+jQuery.noConflict();
+jQuery(function(){
     var total_price = 0;//购物车总价格
 
-    var fansmanage_id=$("#fansmanage_id").val();//联盟主组织ID
-    var _token=$("#_token").val();
-    var store_id=$("#store_id").val();//店铺ID
+    var fansmanage_id=jQuery("#fansmanage_id").val();//联盟主组织ID
+    var _token=jQuery("#_token").val();
+    var store_id=jQuery("#store_id").val();//店铺ID
 	//获取goods分类列表
     var class_url = "http://develop.01nnt.com/api/wechatApi/category";
     $.showPreloader();
 
-    $.post(
+    jQuery.post(
     	class_url,
         {'fansmanage_id': fansmanage_id,'_token':_token,'store_id':store_id},
     	function(json){
@@ -22,21 +23,21 @@ $(function(){
     				str +="<li><a href='javascript:;'>"+json.data.categorylist[i].name+"</a></li>";
     			}
     			//赋值分类列表
-    			var $goods_cs_lt = $("#goods_cs_lt");
-    			$goods_cs_lt.empty();
-    			$goods_cs_lt.append(str);
+    			var jQuerygoods_cs_lt = jQuery("#goods_cs_lt");
+    			jQuerygoods_cs_lt.empty();
+    			jQuerygoods_cs_lt.append(str);
     			//赋值弹出框的分类列表
-    			var $goods_cs_lt_alert = $("#goods_cs_lt_alert");
-    			$goods_cs_lt_alert.empty();
-    			$goods_cs_lt_alert.append(str);
+    			var jQuerygoods_cs_lt_alert = jQuery("#goods_cs_lt_alert");
+    			jQuerygoods_cs_lt_alert.empty();
+    			jQuerygoods_cs_lt_alert.append(str);
     		}
 		}
 	);
 	//获取购物车商品
 	var cart_list_url = "http://develop.01nnt.com/api/wechatApi/shopping_cart_list";
-    var shop_user_id=$("#shop_user_id").val();//用户店铺ID
-    var zerone_user_id=$("#zerone_user_id").val();//用户零壹ID
-    $.post(
+    var shop_user_id=jQuery("#shop_user_id").val();//用户店铺ID
+    var zerone_user_id=jQuery("#zerone_user_id").val();//用户零壹ID
+    jQuery.post(
     	cart_list_url,
         {'fansmanage_id': fansmanage_id,'_token':_token,'store_id':store_id,'user_id':shop_user_id,'zerone_user_id':zerone_user_id},
     	function(json){
@@ -49,21 +50,21 @@ $(function(){
                     total_price += parseFloat(json.data.goods_list[i].goods_price);
                 }
                 //购物车总价格
-                $("#cart_price").html("金额总计<em>&yen;"+total_price+"</em>");
+                jQuery("#cart_price").html("金额总计<em>&yen;"+total_price+"</em>");
                 //购物车总数
                 var total = json.data.total;
-                $("#total").text(total);
-                $("#goods_total").text(total);
+                jQuery("#total").text(total);
+                jQuery("#goods_total").text(total);
 
-                var $cart_list = $("#cart_list");
-                $cart_list.empty();
-                $cart_list.append(str);
+                var jQuerycart_list = jQuery("#cart_list");
+                jQuerycart_list.empty();
+                jQuerycart_list.append(str);
     		}
 		}
 	);
     //获取商品列表
     var goodslist_url = "http://develop.01nnt.com/api/wechatApi/goods_list";
-    $.post(
+    jQuery.post(
     	goodslist_url,
         {'fansmanage_id': fansmanage_id,'_token':_token,'store_id':store_id},
     	function(json){
@@ -76,7 +77,7 @@ $(function(){
                     json.data.goodslist[i].stock,json.data.goodslist[i].price,json.data.goodslist[i].thumb[0].thumb);
 
                 }
-                var goodslist = $("#goodslist");
+                var goodslist = jQuery("#goodslist");
                 goodslist.empty();
                 goodslist.append(str);
     		}
@@ -137,12 +138,12 @@ function goods_list_box(name,details,stock,price,thumb) {
     return str;
 }
 //隐藏alert
-$("#alert").click(function(e){
+jQuery("#alert").click(function(e){
     //stopPropagation(e);
-    if(!$(e.target).is(".popup_alert_hook *") && !$(e.target).is(".popup_alert_hook")){
-        $(".popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
+    if(!jQuery(e.target).is(".popup_alert_hook *") && !jQuery(e.target).is(".popup_alert_hook")){
+        jQuery(".popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
            setTimeout(function(){
-              $(".popup_alert").css({display: 'none'});
+              jQuery(".popup_alert").css({display: 'none'});
          },250);
     }
 });
@@ -157,40 +158,40 @@ function stopPropagation(e) {
     }
 }
 function show(obj){
-     $("#"+obj).css({display: 'flex'});
-    $("#"+obj+" .popup_alert_hook").addClass('fadeInUp');
+     jQuery("#"+obj).css({display: 'flex'});
+    jQuery("#"+obj+" .popup_alert_hook").addClass('fadeInUp');
 }
 function hide(obj) {
-    $("#"+obj).css({display: 'none'});
-    $("#"+obj+" .popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
+    jQuery("#"+obj).css({display: 'none'});
+    jQuery("#"+obj+" .popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
 }
 function showcart(obj,em){
-    $(em).hide();
-    $("#"+obj).css({display: 'flex'});
-    $("#"+obj+" .popup_alert_hook").addClass('fadeInUp');
+    jQuery(em).hide();
+    jQuery("#"+obj).css({display: 'flex'});
+    jQuery("#"+obj+" .popup_alert_hook").addClass('fadeInUp');
 }
 function goodsclass(obj){
-    $("#"+obj).css({display: 'flex'});
-    $("#"+obj+" .popup_alert_hook").removeClass('fadeOutUp').addClass('fadeInDown');
+    jQuery("#"+obj).css({display: 'flex'});
+    jQuery("#"+obj+" .popup_alert_hook").removeClass('fadeOutUp').addClass('fadeInDown');
 }
 function hidegoodsclass(obj){
-    $("#"+obj+" .popup_alert_hook").removeClass('fadeInDown').addClass('fadeOutUp');
+    jQuery("#"+obj+" .popup_alert_hook").removeClass('fadeInDown').addClass('fadeOutUp');
     setTimeout(function(){
-      $("#"+obj).css({display: 'none'});
+      jQuery("#"+obj).css({display: 'none'});
      },250);
 }
 function hidecart(obj,em) {
-    $(em).hidden();
-    $("#"+obj).css({display: 'none'});
-    $("#"+obj+" .popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
+    jQuery(em).hidden();
+    jQuery("#"+obj).css({display: 'none'});
+    jQuery("#"+obj+" .popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
 }
-$("#cart").click(function(e){
+jQuery("#cart").click(function(e){
     //stopPropagation(e);
-    if(!$(e.target).is(".popup_alert_hook *") && !$(e.target).is(".popup_alert_hook")){
-        $(".popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
+    if(!jQuery(e.target).is(".popup_alert_hook *") && !jQuery(e.target).is(".popup_alert_hook")){
+        jQuery(".popup_alert_hook").removeClass('fadeInUp').addClass("fadeOutDown");
            setTimeout(function(){
-              $(".popup_alert").css({display: 'none'});
+              jQuery(".popup_alert").css({display: 'none'});
          },250);
-        $("#cart_btn_b").show();
+        jQuery("#cart_btn_b").show();
     }
 })
