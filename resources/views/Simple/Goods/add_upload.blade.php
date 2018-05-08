633fd4f7de1e77ff97fa7d8517e14bfd41f8913a
mail@54zm.com <mail@54zm.com>
2018-05-08 16:13:31 +0800

    '0'

---
+++ resources/views/Simple/Goods/add_upload.blade.php
@@ -0,0 +1,48 @@
+<div class="table-responsive">
+    @foreach($thumb as $key=>$val)
+        $val->thumb
+    @endforeach
+</div>
+
+
+<script>
+    function editDisplayorder(id,obj){
+        var url = $('#thumb_edit_displayorder_url').val();
+        var token = $('#_token').val();
+        var displayorder = $(obj).val();
+
+        if(id==''){
+            swal({
+                title: "提示信息",
+                text: '数据传输错误',
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+            },function(){
+                window.location.reload();
+            });
+            return;
+        }
+
+        var data = {'id':id,'displayorder':displayorder,'_token':token};
+        $.post(url,data,function(response){
+            if(response.status!='1'){
+                swal({
+                    title: "提示信息",
+                    text: response.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    //window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "温馨提示",
+                    text: response.data,
+                    type: "success"
+                });
+            }
+
+        });
+    }
+</script>
+
