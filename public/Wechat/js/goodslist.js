77f5987f13c8f2def3af9cc7dfbdba87ffcaeee2
suolong520 <1172468462@qq.com>
2018-05-08 16:49:02 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -12,8 +12,8 @@ $(function(){
                 console.log(json);
     			var str = "<li class='action' onclick='category_list();'  data-id='0'><a href='javascript:;'>全部</a></li>";
     			for (var i = json.data.categorylist.length - 1; i >= 0; i--) {
-    				str +="<li onclick='category_list();' data-id="+json.data.categorylist[i].id+"
-                            ><a href='javascript:;' external>"+json.data.categorylist[i].name+"</a></li>";
+    				str +="<li onclick='category_list();' data-id='"+json.data.categorylist[i].id+"'"+
+                            "><a href='javascript:;' external>"+json.data.categorylist[i].name+"</a></li>";
     			}
     			//赋值分类列表
     			var $goods_cs_lt = $("#goods_cs_lt");
