1c4295ae688515820d6b776c6b52a13f4b41acdf
suolong520 <1172468462@qq.com>
2018-05-08 17:16:20 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -32,6 +32,7 @@ $(function(){
 	var cart_list_url = "http://develop.01nnt.com/api/wechatApi/shopping_cart_list";
     var shop_user_id=$("#shop_user_id").val();//用户店铺ID
     var zerone_user_id=$("#zerone_user_id").val();//用户零壹ID
+    var category_id = 0;
     $.post(
     	cart_list_url,
         {'fansmanage_id': fansmanage_id,'_token':_token,'store_id':store_id,'user_id':shop_user_id,'zerone_user_id':zerone_user_id},
@@ -65,7 +66,8 @@ $(function(){
             var goodslist_url = "http://develop.01nnt.com/api/wechatApi/goods_list";
             $.post(
             	goodslist_url,
-                {'fansmanage_id': fansmanage_id,'_token':_token,'store_id':store_id},
+                {'fansmanage_id': fansmanage_id,'_token':_token,'store_id':store_id,
+                'category_id':category_id},
             	function(json){
                     var str = "";
                     console.log(json);
