5c14d5dd4d28132fa27e706f16b8878c3ca109fd
mail@54zm.com <mail@54zm.com>
2018-05-08 16:21:40 +0800

    '0'

--- resources/views/Simple/Goods/add_upload.blade.php
+++ resources/views/Simple/Goods/add_upload.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="form-group">
                     @foreach($thumb as $key=>$val)
-                        <div class="col-sm-2">
+                        <div class="col-sm-3">
                         <img src="http://develop.01nnt.com/uploads/simple/20180425104330148.jpg" style="width: 100px; height: 100px;" onclick="bigthumb(this.src)">
                         </div>
                     @endforeach
