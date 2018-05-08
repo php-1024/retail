2c4e9dfd9f3d8df7088759c48b7874f8c1596327
mail@54zm.com <mail@54zm.com>
2018-05-08 15:35:34 +0800

    '0'

--- resources/views/Simple/Goods/goods_add.blade.php
+++ resources/views/Simple/Goods/goods_add.blade.php
@@ -74,6 +74,7 @@
                                                 </div>
                                             </div>
 
+
                                             <div class="line line-dashed b-b line-lg pull-in"></div>
                                             <div class="form-group">
                                                 <label class="col-sm-2 control-label" for="input-id-1">价格</label>
