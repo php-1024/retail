7149d1d20d3f311803b5be03845971a68e642466
xho <781966658.com>
2018-05-09 09:57:11 +0800

    x

--- app/Http/Controllers/Pay/WxController.php
+++ app/Http/Controllers/Pay/WxController.php
@@ -87,7 +87,7 @@ class WxController extends Controller
 //        $data["out_trade_no"] = '';
 
 //        $data["out_refund_no"] = md5(time());
-        $data["out_refund_no"] = "1111111";
+//        $data["out_refund_no"] = "1111111";
         $data["total_fee"] = 10;
         $data["refund_fee"] = 2;
         $data["refund_desc"] = "12312321";
